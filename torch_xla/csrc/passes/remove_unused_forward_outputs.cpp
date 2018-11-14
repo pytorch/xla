0fe6b61c785bfc14284197240f03d7f541b16704
Alex Suhan <asuhan@users.noreply.github.com>
2018-11-14 15:19:57 -0800

    Fix RemoveUnusedForwardOutputs (#3)
    Also undo autodiff hacks compensating for the fixed bug and allow
    modules to run forward only to salvage TestStack, which relied on said
    hacks and doesn't occur yet in the models we run.

--- torch_xla/csrc/passes/remove_unused_forward_outputs.cpp
+++ torch_xla/csrc/passes/remove_unused_forward_outputs.cpp
@@ -9,20 +9,24 @@ namespace {
 
 // Remove an unused input from the backward graph from both the outputs and
 // captured outputs sections of its input.
-void RemoveInputFromBackwardGraph(Gradient& gradient, const size_t output_idx,
+void RemoveInputFromBackwardGraph(Gradient& gradient,
+                                  const at::optional<size_t> output_idx_maybe,
                                   const size_t captured_output_idx) {
   const auto backward_inputs = gradient.df->inputs();
-  const Value* grad_output = backward_inputs[output_idx];
+  const Value* grad_output =
+      output_idx_maybe ? backward_inputs[*output_idx_maybe] : nullptr;
   const Value* captured_output = backward_inputs[captured_output_idx];
   // Remove grad_output and captured_output from the inputs of the backward
   // graph.
   for (auto it = gradient.df->nodes().begin(), end = gradient.df->nodes().end();
        it != end; ++it) {
     const auto node_inputs = it->inputs();
-    const auto grad_output_it =
-        std::find(node_inputs.begin(), node_inputs.end(), grad_output);
-    // Assert that grad_output doesn't have remaining uses.
-    JIT_ASSERT(grad_output_it == node_inputs.end());
+    if (grad_output) {
+      const auto grad_output_it =
+          std::find(node_inputs.begin(), node_inputs.end(), grad_output);
+      // Assert that grad_output doesn't have remaining uses.
+      JIT_ASSERT(grad_output_it == node_inputs.end());
+    }
     const auto captured_output_it =
         std::find(node_inputs.begin(), node_inputs.end(), captured_output);
     if (captured_output_it != node_inputs.end()) {
@@ -36,10 +40,27 @@ void RemoveInputFromBackwardGraph(Gradient& gradient, const size_t output_idx,
   // points inside the outputs section. We thus have captured_output_idx >
   // output_idx because outputs come before captured outputs. Remove the
   // captured_output_idx first to avoid invalidation of indices.
-  JIT_ASSERT(captured_output_idx > output_idx);
+  JIT_ASSERT(!output_idx_maybe || captured_output_idx > *output_idx_maybe);
   gradient.df->eraseInput(captured_output_idx);
-  gradient.df->eraseInput(output_idx);
+  if (output_idx_maybe) {
+    gradient.df->eraseInput(*output_idx_maybe);
+  }
 }
+
+namespace {
+
+// Counts the number of additional outputs after the "real" outputs used by
+// differentiation the same way autodiff does.
+size_t IntermediateRequireGradOutputsCount(const Gradient& gradient) {
+  const auto forward_outputs = gradient.f->outputs();
+  return std::count_if(forward_outputs.begin() + gradient.f_real_outputs,
+                       forward_outputs.end(),
+                       [](const Value* intermediate_output) {
+                         return intermediate_output->requires_grad();
+                       });
+}
+
+}  // namespace
 
 // Remove the unused output specified by node_output_idx from the given node,
 // with subsequent removal from the backward graph input as well.
@@ -54,17 +75,23 @@ void RemoveNodeOutputFromGradient(Node* node, const size_t node_output_idx,
   if (output_it == forward_outputs.end()) {
     return;
   }
-  size_t output_idx = output_it - forward_outputs.begin();
+  const size_t output_idx = output_it - forward_outputs.begin();
+  const auto output_idx_maybe = (*output_it)->requires_grad()
+                                    ? at::optional<size_t>(output_idx)
+                                    : at::nullopt;
+
+  // Find the captured_output_idx absolute index of the backward graph input to
+  // remove. First, position it at the beginning of the captured outputs, right
+  // after the outputs of the forward graph and the captureed inputs.
+  size_t captured_output_idx = gradient.f_real_outputs +
+                               IntermediateRequireGradOutputsCount(gradient) +
+                               gradient.df_input_captured_inputs.size();
+
   // Remove the given output from the graph outputs.
   gradient.f->eraseOutput(output_idx);
   // Remove the given output from the node outputs.
   node->eraseOutput(node_output_idx);
 
-  // Find the captured_output_idx absolute index of the backward graph input to
-  // remove. First, position it at the beginning of the captured outputs, right
-  // after the outputs of the forward graph and the captureed inputs.
-  size_t captured_output_idx =
-      forward_outputs.size() + gradient.df_input_captured_inputs.size();
   // Next, find the index and value in df_input_captured_outputs of the node to
   // remove. Use it to adjust captured_output_idx and update
   // df_input_captured_outputs.
@@ -92,8 +119,10 @@ void RemoveNodeOutputFromGradient(Node* node, const size_t node_output_idx,
   }
 
   // Finally, remove the node from all inputs of the backward graph.
-  RemoveInputFromBackwardGraph(/*gradient=*/gradient, /*output_idx=*/output_idx,
-                               /*captured_output_idx=*/captured_output_idx);
+  RemoveInputFromBackwardGraph(
+      /*gradient=*/gradient,
+      /*output_idx_maybe=*/output_idx_maybe,
+      /*captured_output_idx=*/captured_output_idx);
 }
 
 }  // namespace
