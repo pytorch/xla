d029633bbc6e8cb4706f366359ac07ef8733474d
Alex Şuhan <asuhan@google.com>
2018-11-20 14:08:46 -0800

    Make SGD optimizer work without upstream changes wip

--- torch_xla/__init__.py
+++ torch_xla/__init__.py
@@ -1,2 +1,14 @@
 import torch
 import _C
+
+torch_zeros_like_builtin = torch.zeros_like
+
+
+def _torch_xla_zeros_like(p):
+    import torch_xla
+    if isinstance(p, torch_xla._C.XLATensor):
+        return torch_xla._C.XLATensor(torch.zeros(p.size()), device=p.device())
+    return torch_zeros_like_builtin(p)
+
+
+torch.zeros_like = _torch_xla_zeros_like
