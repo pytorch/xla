3b9d3683e519841025ee5e715d3829ba3f1afd32
Davide Libenzi <dlibenzi@google.com>
2018-12-01 17:33:55 -0800

    Use common lock for conditions and use lock context manager to simplify code.

--- torch_xla_py/keyd_queue.py
+++ torch_xla_py/keyd_queue.py
@@ -4,96 +4,77 @@ import collections
 import threading
 
 
-def _notify_cv(cv):
-    cv.acquire()
-    cv.notify()
-    cv.release()
-
-
-class KeydQueue(object):
+class QueueBase(object):
     def __init__(self, maxsize=1024):
         self._maxsize = maxsize
+        self._lock = threading.Lock()
+        self._ready_cv = threading.Condition(self._lock)
+        self._space_available_cv = threading.Condition(self._lock)
+        self._close_read = False
+        self._close_write = False
+
+    def close(self):
+        with self._lock:
+            self._close_read = True
+            self._close_write = True
+            self._ready_cv.notify_all()
+            self._space_available_cv.notify_all()
+
+    def close_write(self):
+        with self._lock:
+            self._close_write = True
+            self._ready_cv.notify_all()
+
+
+class KeydQueue(QueueBase):
+    def __init__(self, maxsize=1024):
+        super(KeydQueue, self).__init__(maxsize=maxsize)
         self._items = dict()
         self._waited_keys = set()
-        self._ready_cv = threading.Condition()
-        self._space_available_cv = threading.Condition()
-        self._close_read = False
-        self._close_write = False
-
-    def close(self):
-        self._close_read = True
-        self._close_write = True
-        _notify_cv(self._ready_cv)
-        _notify_cv(self._space_available_cv)
-
-    def close_write(self):
-        self._close_write = True
-        _notify_cv(self._ready_cv)
 
     def put(self, key, item):
-        self._space_available_cv.acquire()
-        # Wait for space available, unless there is a waiter for the incoming
-        # key.
-        while (len(self._items) >= self._maxsize and
-               key not in self._waited_keys and not self._close_read):
-            self._space_available_cv.wait()
-        self._space_available_cv.release()
-        if not self._close_read:
-            self._ready_cv.acquire()
-            self._items[key] = item
-            if key in self._waited_keys:
-                self._ready_cv.notify()
-            self._ready_cv.release()
+        with self._lock:
+            # Wait for space available, unless there is a waiter for the incoming
+            # key.
+            while (len(self._items) >= self._maxsize and
+                   key not in self._waited_keys and not self._close_read):
+                self._space_available_cv.wait()
+            if not self._close_read:
+                self._items[key] = item
+                if key in self._waited_keys:
+                    self._ready_cv.notify_all()
 
     def get(self, key):
-        self._ready_cv.acquire()
-        while key not in self._items and not self._close_write:
-            self._waited_keys.add(key)
-            self._ready_cv.wait()
-            self._waited_keys.discard(key)
-        item = self._items.pop(key, None)
-        self._ready_cv.release()
-        if item is not None and not self._close_write:
-            _notify_cv(self._space_available_cv)
-        return item
+        with self._lock:
+            while key not in self._items and not self._close_write:
+                self._waited_keys.add(key)
+                self._space_available_cv.notify_all()
+                self._ready_cv.wait()
+                self._waited_keys.discard(key)
+            item = self._items.pop(key, None)
+            if item is not None and not self._close_write:
+                self._space_available_cv.notify()
+            return item
 
 
-class Queue(object):
+class Queue(QueueBase):
     def __init__(self, maxsize=1024):
-        self._maxsize = maxsize
+        super(Queue, self).__init__(maxsize=maxsize)
         self._items = collections.deque()
-        self._ready_cv = threading.Condition()
-        self._space_available_cv = threading.Condition()
-        self._close_read = False
-        self._close_write = False
-
-    def close(self):
-        self._close_read = True
-        self._close_write = True
-        _notify_cv(self._ready_cv)
-        _notify_cv(self._space_available_cv)
-
-    def close_write(self):
-        self._close_write = True
-        _notify_cv(self._ready_cv)
 
     def put(self, item):
-        self._space_available_cv.acquire()
-        while (len(self._items) >= self._maxsize and not self._close_read):
-            self._space_available_cv.wait()
-        self._space_available_cv.release()
-        if not self._close_read:
-            self._ready_cv.acquire()
-            self._items.append(item)
-            self._ready_cv.notify()
-            self._ready_cv.release()
+        with self._lock:
+            while (len(self._items) >= self._maxsize and not self._close_read):
+                self._space_available_cv.wait()
+            if not self._close_read:
+                self._items.append(item)
+                self._ready_cv.notify()
 
     def get(self):
-        self._ready_cv.acquire()
-        while not self._items and not self._close_write:
-            self._ready_cv.wait()
-        item = self._items.popleft() if self._items else None
-        self._ready_cv.release()
-        if item is not None and not self._close_write:
-            _notify_cv(self._space_available_cv)
-        return item
+        with self._lock:
+            while not self._items and not self._close_write:
+                self._ready_cv.wait()
+            item = self._items.popleft() if self._items else None
+            if item is not None and not self._close_write:
+                self._space_available_cv.notify()
+            return item
