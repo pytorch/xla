13341ab0845891a78e2685f88aaf0f45d9c5f422
Davide Libenzi <dlibenzi@google.com>
2018-12-11 09:56:59 -0800

    Fix build script.

--- build_torch_xla_libs.sh
+++ build_torch_xla_libs.sh
@@ -10,10 +10,14 @@ BASE_DIR="$PWD"
 echo $BASE_DIR
 THIRD_PARTY_DIR="$BASE_DIR/third_party"
 
-cp -r -f $THIRD_PARTY_DIR/xla_client $THIRD_PARTY_DIR/tensorflow/tensorflow/compiler/xla/
+if [ "$CMD" == "clean" ]; then
+  pushd $THIRD_PARTY_DIR/tensorflow
+  bazel clean
+  popd
+else
+  cp -r -f $THIRD_PARTY_DIR/xla_client $THIRD_PARTY_DIR/tensorflow/tensorflow/compiler/xla/
 
-pushd $THIRD_PARTY_DIR/tensorflow
-if [ "$CMD" == "install" ]; then
+  pushd $THIRD_PARTY_DIR/tensorflow
   git reset --hard
   git clean -f
   bazel build --define framework_shared_object=false -c opt --cxxopt="-Wno-c++11-narrowing" \
@@ -23,9 +27,4 @@ if [ "$CMD" == "install" ]; then
   mkdir -p torch_xla/lib
   chmod 0644 $THIRD_PARTY_DIR/tensorflow/bazel-bin/tensorflow/compiler/xla/xla_client/libxla_computation_client.so
   cp $THIRD_PARTY_DIR/tensorflow/bazel-bin/tensorflow/compiler/xla/xla_client/libxla_computation_client.so torch_xla/lib
-elif [ "$CMD" == "clean" ]; then
-  bazel clean
-  popd
-else
-  popd
 fi
