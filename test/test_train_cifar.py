5792663043494c444a530debd3e4f8d4fbddab72
Davide Libenzi <dlibenzi@google.com>
2018-12-10 17:22:22 -0800

    Reformat Python code using Google pyformat.

--- test/test_train_cifar.py
+++ test/test_train_cifar.py
@@ -1,7 +1,9 @@
 import test_utils
 
 FLAGS = test_utils.parse_common_options(
-    datadir='/tmp/cifar-data', batch_size=128, num_epochs=15,
+    datadir='/tmp/cifar-data',
+    batch_size=128,
+    num_epochs=15,
     target_accuracy=80.0)
 
 from common_utils import TestCase, run_tests
@@ -18,140 +20,145 @@ import unittest
 
 
 class BasicBlock(nn.Module):
-    expansion = 1
+  expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1,
-                               padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
+  def __init__(self, in_planes, planes, stride=1):
+    super(BasicBlock, self).__init__()
+    self.conv1 = nn.Conv2d(
+        in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+    self.bn1 = nn.BatchNorm2d(planes)
+    self.conv2 = nn.Conv2d(
+        planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+    self.bn2 = nn.BatchNorm2d(planes)
 
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion * planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1,
-                          stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion * planes)
-            )
+    self.shortcut = nn.Sequential()
+    if stride != 1 or in_planes != self.expansion * planes:
+      self.shortcut = nn.Sequential(
+          nn.Conv2d(
+              in_planes,
+              self.expansion * planes,
+              kernel_size=1,
+              stride=stride,
+              bias=False), nn.BatchNorm2d(self.expansion * planes))
 
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
+  def forward(self, x):
+    out = F.relu(self.bn1(self.conv1(x)))
+    out = self.bn2(self.conv2(out))
+    out += self.shortcut(x)
+    out = F.relu(out)
+    return out
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet, self).__init__()
-        self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512 * block.expansion, num_classes)
+  def __init__(self, block, num_blocks, num_classes=10):
+    super(ResNet, self).__init__()
+    self.in_planes = 64
 
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1] * (num_blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
+    self.conv1 = nn.Conv2d(
+        3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+    self.bn1 = nn.BatchNorm2d(64)
+    self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+    self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+    self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+    self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+    self.linear = nn.Linear(512 * block.expansion, num_classes)
 
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return F.log_softmax(out, dim=1)
+  def _make_layer(self, block, planes, num_blocks, stride):
+    strides = [stride] + [1] * (num_blocks - 1)
+    layers = []
+    for stride in strides:
+      layers.append(block(self.in_planes, planes, stride))
+      self.in_planes = planes * block.expansion
+    return nn.Sequential(*layers)
+
+  def forward(self, x):
+    out = F.relu(self.bn1(self.conv1(x)))
+    out = self.layer1(out)
+    out = self.layer2(out)
+    out = self.layer3(out)
+    out = self.layer4(out)
+    out = F.avg_pool2d(out, 4)
+    out = out.view(out.size(0), -1)
+    out = self.linear(out)
+    return F.log_softmax(out, dim=1)
 
 
 def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+  return ResNet(BasicBlock, [2, 2, 2, 2])
 
 
 def train_cifar():
-    print('==> Preparing data..')
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465),
-                             (0.2023, 0.1994, 0.2010)),
-    ])
+  print('==> Preparing data..')
+  transform_train = transforms.Compose([
+      transforms.RandomCrop(32, padding=4),
+      transforms.RandomHorizontalFlip(),
+      transforms.ToTensor(),
+      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+  ])
 
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465),
-                             (0.2023, 0.1994, 0.2010)),
-    ])
+  transform_test = transforms.Compose([
+      transforms.ToTensor(),
+      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+  ])
 
-    trainset = torchvision.datasets.CIFAR10(
-        root=FLAGS.datadir, train=True, download=True,
-        transform=transform_train)
-    train_loader = torch.utils.data.DataLoader(
-        trainset, batch_size=FLAGS.batch_size, shuffle=True,
-        num_workers=FLAGS.num_workers)
+  trainset = torchvision.datasets.CIFAR10(
+      root=FLAGS.datadir, train=True, download=True, transform=transform_train)
+  train_loader = torch.utils.data.DataLoader(
+      trainset,
+      batch_size=FLAGS.batch_size,
+      shuffle=True,
+      num_workers=FLAGS.num_workers)
 
-    testset = torchvision.datasets.CIFAR10(
-        root=FLAGS.datadir, train=False, download=True,
-        transform=transform_test)
-    test_loader = torch.utils.data.DataLoader(
-        testset, batch_size=FLAGS.batch_size, shuffle=False,
-        num_workers=FLAGS.num_workers)
+  testset = torchvision.datasets.CIFAR10(
+      root=FLAGS.datadir, train=False, download=True, transform=transform_test)
+  test_loader = torch.utils.data.DataLoader(
+      testset,
+      batch_size=FLAGS.batch_size,
+      shuffle=False,
+      num_workers=FLAGS.num_workers)
 
-    torch.manual_seed(42)
+  torch.manual_seed(42)
 
-    print('==> Building model..')
-    momentum = 0.9
-    lr = 0.1
-    log_interval = max(1, int(10 / FLAGS.num_cores))
+  print('==> Building model..')
+  momentum = 0.9
+  lr = 0.1
+  log_interval = max(1, int(10 / FLAGS.num_cores))
 
-    model = ResNet18()
+  model = ResNet18()
 
-    devices = [':{}'.format(n) for n in range(0, FLAGS.num_cores)]
-    inputs = torch.zeros(FLAGS.batch_size, 3, 32, 32)
-    target = torch.zeros(FLAGS.batch_size, dtype=torch.int64)
-    xla_model = xm.XlaModel(model, [inputs], loss_fn=F.nll_loss,
-                            target=target, num_cores=FLAGS.num_cores,
-                            devices=devices)
-    optimizer = optim.SGD(xla_model.parameters_list(), lr=lr,
-                          momentum=momentum, weight_decay=5e-4)
+  devices = [':{}'.format(n) for n in range(0, FLAGS.num_cores)]
+  inputs = torch.zeros(FLAGS.batch_size, 3, 32, 32)
+  target = torch.zeros(FLAGS.batch_size, dtype=torch.int64)
+  xla_model = xm.XlaModel(
+      model, [inputs],
+      loss_fn=F.nll_loss,
+      target=target,
+      num_cores=FLAGS.num_cores,
+      devices=devices)
+  optimizer = optim.SGD(
+      xla_model.parameters_list(), lr=lr, momentum=momentum, weight_decay=5e-4)
 
-    for epoch in range(1, FLAGS.num_epochs + 1):
-        xla_model.train(train_loader, optimizer, FLAGS.batch_size,
-                        log_interval=log_interval)
-        if FLAGS.metrics_debug:
-            print(torch_xla._XLAC._xla_metrics_report())
-        accuracy = xla_model.test(test_loader,
-                                  xm.category_eval_fn(F.nll_loss),
-                                  FLAGS.batch_size)
-        xm.update_optimizer_state(optimizer, 'lr', lambda x: x / 1.025)
-    return accuracy
+  for epoch in range(1, FLAGS.num_epochs + 1):
+    xla_model.train(
+        train_loader, optimizer, FLAGS.batch_size, log_interval=log_interval)
+    if FLAGS.metrics_debug:
+      print(torch_xla._XLAC._xla_metrics_report())
+    accuracy = xla_model.test(test_loader, xm.category_eval_fn(F.nll_loss),
+                              FLAGS.batch_size)
+    xm.update_optimizer_state(optimizer, 'lr', lambda x: x / 1.025)
+  return accuracy
 
 
 class TrainCIFAR10(TestCase):
 
-    def tearDown(self):
-        super(TrainCIFAR10, self).tearDown()
-        if FLAGS.tidy:
-            shutil.rmtree(FLAGS.datadir)
+  def tearDown(self):
+    super(TrainCIFAR10, self).tearDown()
+    if FLAGS.tidy:
+      shutil.rmtree(FLAGS.datadir)
 
-    def test_accurracy(self):
-        self.assertGreaterEqual(train_cifar(), FLAGS.target_accuracy)
+  def test_accurracy(self):
+    self.assertGreaterEqual(train_cifar(), FLAGS.target_accuracy)
 
 
 # Run the tests.
--- test/test_train_mnist.py
+++ test/test_train_mnist.py
@@ -16,78 +16,91 @@ import unittest
 
 
 class MNIST(nn.Module):
-    def __init__(self):
-        super(MNIST, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.bn1 = nn.BatchNorm2d(10)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.bn2 = nn.BatchNorm2d(20)
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = self.bn1(x)
-        x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = self.bn2(x)
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+  def __init__(self):
+    super(MNIST, self).__init__()
+    self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+    self.bn1 = nn.BatchNorm2d(10)
+    self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+    self.bn2 = nn.BatchNorm2d(20)
+    self.fc1 = nn.Linear(320, 50)
+    self.fc2 = nn.Linear(50, 10)
+
+  def forward(self, x):
+    x = F.relu(F.max_pool2d(self.conv1(x), 2))
+    x = self.bn1(x)
+    x = F.relu(F.max_pool2d(self.conv2(x), 2))
+    x = self.bn2(x)
+    x = x.view(-1, 320)
+    x = F.relu(self.fc1(x))
+    x = self.fc2(x)
+    return F.log_softmax(x, dim=1)
 
 
 def train_mnist():
-    torch.manual_seed(1)
-    # Training settings
-    lr = 0.01 * FLAGS.num_cores
-    momentum = 0.5
-    log_interval = max(1, int(10 / FLAGS.num_cores))
+  torch.manual_seed(1)
+  # Training settings
+  lr = 0.01 * FLAGS.num_cores
+  momentum = 0.5
+  log_interval = max(1, int(10 / FLAGS.num_cores))
 
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(FLAGS.datadir, train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=FLAGS.batch_size, shuffle=True, num_workers=FLAGS.num_workers)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(FLAGS.datadir, train=False, transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])),
-        batch_size=FLAGS.batch_size, shuffle=True, num_workers=FLAGS.num_workers)
+  train_loader = torch.utils.data.DataLoader(
+      datasets.MNIST(
+          FLAGS.datadir,
+          train=True,
+          download=True,
+          transform=transforms.Compose([
+              transforms.ToTensor(),
+              transforms.Normalize((0.1307,), (0.3081,))
+          ])),
+      batch_size=FLAGS.batch_size,
+      shuffle=True,
+      num_workers=FLAGS.num_workers)
+  test_loader = torch.utils.data.DataLoader(
+      datasets.MNIST(
+          FLAGS.datadir,
+          train=False,
+          transform=transforms.Compose([
+              transforms.ToTensor(),
+              transforms.Normalize((0.1307,), (0.3081,))
+          ])),
+      batch_size=FLAGS.batch_size,
+      shuffle=True,
+      num_workers=FLAGS.num_workers)
 
-    model = MNIST()
+  model = MNIST()
 
-    # Trace the model.
-    devices = [':{}'.format(n) for n in range(0, FLAGS.num_cores)]
-    inputs = torch.zeros(FLAGS.batch_size, 1, 28, 28)
-    target = torch.zeros(FLAGS.batch_size, dtype=torch.int64)
-    xla_model = xm.XlaModel(model, [inputs], loss_fn=F.nll_loss, target=target,
-                            num_cores=FLAGS.num_cores, devices=devices)
-    optimizer = optim.SGD(xla_model.parameters_list(),
-                          lr=lr, momentum=momentum)
+  # Trace the model.
+  devices = [':{}'.format(n) for n in range(0, FLAGS.num_cores)]
+  inputs = torch.zeros(FLAGS.batch_size, 1, 28, 28)
+  target = torch.zeros(FLAGS.batch_size, dtype=torch.int64)
+  xla_model = xm.XlaModel(
+      model, [inputs],
+      loss_fn=F.nll_loss,
+      target=target,
+      num_cores=FLAGS.num_cores,
+      devices=devices)
+  optimizer = optim.SGD(xla_model.parameters_list(), lr=lr, momentum=momentum)
 
-    for epoch in range(1, FLAGS.num_epochs + 1):
-        xla_model.train(train_loader, optimizer, FLAGS.batch_size,
-                        log_interval=log_interval)
-        if FLAGS.metrics_debug:
-            print(torch_xla._XLAC._xla_metrics_report())
-        accuracy = xla_model.test(test_loader,
-                                  xm.category_eval_fn(F.nll_loss),
-                                  FLAGS.batch_size)
-    return accuracy
+  for epoch in range(1, FLAGS.num_epochs + 1):
+    xla_model.train(
+        train_loader, optimizer, FLAGS.batch_size, log_interval=log_interval)
+    if FLAGS.metrics_debug:
+      print(torch_xla._XLAC._xla_metrics_report())
+    accuracy = xla_model.test(test_loader, xm.category_eval_fn(F.nll_loss),
+                              FLAGS.batch_size)
+  return accuracy
 
 
 class TrainMnist(TestCase):
 
-    def tearDown(self):
-        super(TrainMnist, self).tearDown()
-        if FLAGS.tidy:
-            shutil.rmtree(FLAGS.datadir)
+  def tearDown(self):
+    super(TrainMnist, self).tearDown()
+    if FLAGS.tidy:
+      shutil.rmtree(FLAGS.datadir)
 
-    def test_accurracy(self):
-        self.assertGreaterEqual(train_mnist(), FLAGS.target_accuracy)
+  def test_accurracy(self):
+    self.assertGreaterEqual(train_mnist(), FLAGS.target_accuracy)
 
 
 # Run the tests.
