8ece4212eaafc71d790637e3288f331bd4c4b035
Davide Libenzi <dlibenzi@google.com>
2018-12-12 11:03:58 -0800

    Dedicate IO thread pool to non CPU bound closures.

--- third_party/xla_client/thread_pool.cc
+++ third_party/xla_client/thread_pool.cc
@@ -1,5 +1,6 @@
 #include "tensorflow/compiler/xla/xla_client/thread_pool.h"
 
+#include "tensorflow/compiler/xla/xla_client/sys_util.h"
 #include "tensorflow/core/lib/core/threadpool.h"
 #include "tensorflow/core/platform/cpu_info.h"
 #include "tensorflow/core/platform/env.h"
@@ -8,16 +9,25 @@ namespace xla {
 namespace xla_env {
 namespace {
 
-tensorflow::thread::ThreadPool* CreateThreadPool() {
+tensorflow::thread::ThreadPool* CreateThreadPool(const char* name,
+                                                 const char* cfg_env) {
+  int64 num_threads =
+      sys_util::GetEnvInt(cfg_env, tensorflow::port::NumSchedulableCPUs());
   tensorflow::ThreadOptions thread_options;
   return new tensorflow::thread::ThreadPool(
-      tensorflow::Env::Default(), thread_options, "XlaThreadPool",
-      tensorflow::port::NumSchedulableCPUs(),
+      tensorflow::Env::Default(), thread_options, name, num_threads,
       /*low_latency_hint=*/false);
 }
 
 tensorflow::thread::ThreadPool* GetThreadPool() {
-  static tensorflow::thread::ThreadPool* pool = CreateThreadPool();
+  static tensorflow::thread::ThreadPool* pool =
+      CreateThreadPool("XlaThreadPool", "XLA_THREAD_POOL_SIZE");
+  return pool;
+}
+
+tensorflow::thread::ThreadPool* GetIoThreadPool() {
+  static tensorflow::thread::ThreadPool* pool =
+      CreateThreadPool("XlaIoThreadPool", "XLA_IO_THREAD_POOL_SIZE");
   return pool;
 }
 
@@ -25,6 +35,10 @@ tensorflow::thread::ThreadPool* GetThreadPool() {
 
 void ScheduleClosure(std::function<void()> closure) {
   GetThreadPool()->Schedule(std::move(closure));
+}
+
+void ScheduleIoClosure(std::function<void()> closure) {
+  GetIoThreadPool()->Schedule(std::move(closure));
 }
 
 }  // namespace xla_env
