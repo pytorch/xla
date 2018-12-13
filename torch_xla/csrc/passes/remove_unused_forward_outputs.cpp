e09dcb2606c02f0faf7f82ac0d2419c2f7ee1293
Alex Şuhan <asuhan@google.com>
2018-12-13 14:10:38 -0800

    Adjust signature of RemoveUnusedForwardOutputs

--- torch_xla/csrc/passes/remove_unused_forward_outputs.cpp
+++ torch_xla/csrc/passes/remove_unused_forward_outputs.cpp
@@ -10,15 +10,16 @@ namespace {
 
 // Remove an unused input from the backward graph from both the outputs and
 // captured outputs sections of its input.
-void RemoveInputFromBackwardGraph(Gradient& gradient, const size_t output_idx,
-                                  const size_t captured_output_idx) {
-  const auto backward_inputs = gradient.df->inputs();
+void RemoveInputFromBackwardGraph(Gradient* gradient, size_t output_idx,
+                                  size_t captured_output_idx) {
+  const auto backward_inputs = gradient->df->inputs();
   XLA_CHECK_LT(output_idx, backward_inputs.size());
   const Value* grad_output = backward_inputs[output_idx];
   const Value* captured_output = backward_inputs[captured_output_idx];
   // Remove grad_output and captured_output from the inputs of the backward
   // graph.
-  for (auto it = gradient.df->nodes().begin(), end = gradient.df->nodes().end();
+  for (auto it = gradient->df->nodes().begin(),
+            end = gradient->df->nodes().end();
        it != end; ++it) {
     const auto node_inputs = it->inputs();
     if (grad_output) {
@@ -31,7 +32,7 @@ void RemoveInputFromBackwardGraph(Gradient& gradient, const size_t output_idx,
         std::find(node_inputs.begin(), node_inputs.end(), captured_output);
     if (captured_output_it != node_inputs.end()) {
       WithInsertPoint guard(*it);
-      Node* undef = gradient.df->insertNode(gradient.df->createUndefined());
+      Node* undef = gradient->df->insertNode(gradient->df->createUndefined());
       it->replaceInput(captured_output_it - node_inputs.begin(),
                        undef->output());
     }
@@ -41,17 +42,17 @@ void RemoveInputFromBackwardGraph(Gradient& gradient, const size_t output_idx,
   // output_idx because outputs come before captured outputs. Remove the
   // captured_output_idx first to avoid invalidation of indices.
   JIT_ASSERT(captured_output_idx > output_idx);
-  gradient.df->eraseInput(captured_output_idx);
-  gradient.df->eraseInput(output_idx);
+  gradient->df->eraseInput(captured_output_idx);
+  gradient->df->eraseInput(output_idx);
 }
 
 // Remove the unused output specified by node_output_idx from the given node,
 // with subsequent removal from the backward graph input as well.
-void RemoveNodeOutputFromGradient(Node* node, const size_t node_output_idx,
-                                  Gradient& gradient) {
+void RemoveNodeOutputFromGradient(Node* node, size_t node_output_idx,
+                                  Gradient* gradient) {
   const Value* output = node->outputs()[node_output_idx];
   // Find index of this output in forward graph outputs.
-  const auto forward_outputs = gradient.f->outputs();
+  const auto forward_outputs = gradient->f->outputs();
   const auto output_it =
       std::find(forward_outputs.begin(), forward_outputs.end(), output);
   // This output isn't returned from the forward graph, nothing to do.
@@ -63,11 +64,11 @@ void RemoveNodeOutputFromGradient(Node* node, const size_t node_output_idx,
   // Find the captured_output_idx absolute index of the backward graph input to
   // remove. First, position it at the beginning of the captured outputs, right
   // after the outputs of the forward graph and the captureed inputs.
-  size_t captured_output_idx =
-      gradient.df_input_vjps.size() + gradient.df_input_captured_inputs.size();
+  size_t captured_output_idx = gradient->df_input_vjps.size() +
+                               gradient->df_input_captured_inputs.size();
 
   // Remove the given output from the graph outputs.
-  gradient.f->eraseOutput(forward_output_idx);
+  gradient->f->eraseOutput(forward_output_idx);
   // Remove the given output from the node outputs.
   node->eraseOutput(node_output_idx);
 
@@ -75,9 +76,9 @@ void RemoveNodeOutputFromGradient(Node* node, const size_t node_output_idx,
   // remove. Use it to adjust captured_output_idx and update
   // df_input_captured_outputs.
   int df_input_captured_outputs_idx = -1;
-  for (size_t i = 0; i < gradient.df_input_captured_outputs.size(); i++) {
+  for (size_t i = 0; i < gradient->df_input_captured_outputs.size(); i++) {
     if (static_cast<size_t>(forward_output_idx) ==
-        gradient.df_input_captured_outputs[i]) {
+        gradient->df_input_captured_outputs[i]) {
       captured_output_idx += i;
       df_input_captured_outputs_idx = i;
       break;
@@ -85,28 +86,29 @@ void RemoveNodeOutputFromGradient(Node* node, const size_t node_output_idx,
   }
   JIT_ASSERT(df_input_captured_outputs_idx != -1);
   const size_t df_input_captured_outputs_val =
-      gradient.df_input_captured_outputs[df_input_captured_outputs_idx];
+      gradient->df_input_captured_outputs[df_input_captured_outputs_idx];
   // Remove the node from df_input_captured_outputs and adjust references to
   // nodes with higher indices in df_input_captured_outputs.
-  gradient.df_input_captured_outputs.erase(
-      gradient.df_input_captured_outputs.begin() +
+  gradient->df_input_captured_outputs.erase(
+      gradient->df_input_captured_outputs.begin() +
       df_input_captured_outputs_idx);
-  for (size_t i = 0; i < gradient.df_input_captured_outputs.size(); i++) {
-    if (gradient.df_input_captured_outputs[i] > df_input_captured_outputs_val) {
-      --gradient.df_input_captured_outputs[i];
+  for (size_t i = 0; i < gradient->df_input_captured_outputs.size(); i++) {
+    if (gradient->df_input_captured_outputs[i] >
+        df_input_captured_outputs_val) {
+      --gradient->df_input_captured_outputs[i];
     }
   }
 
   // df_input_vjps must be adjusted similarly to df_input_captured_outputs since
   // it also contains output indices which can shift when outputs are removed.
   const auto df_input_vjps_it =
-      std::find(gradient.df_input_vjps.begin(), gradient.df_input_vjps.end(),
+      std::find(gradient->df_input_vjps.begin(), gradient->df_input_vjps.end(),
                 forward_output_idx);
-  JIT_ASSERT(df_input_vjps_it != gradient.df_input_vjps.end());
-  const size_t output_idx = df_input_vjps_it - gradient.df_input_vjps.begin();
+  JIT_ASSERT(df_input_vjps_it != gradient->df_input_vjps.end());
+  const size_t output_idx = df_input_vjps_it - gradient->df_input_vjps.begin();
   const auto df_input_vjps_val = *df_input_vjps_it;
-  gradient.df_input_vjps.erase(df_input_vjps_it);
-  for (auto& df_input_vjps_elem : gradient.df_input_vjps) {
+  gradient->df_input_vjps.erase(df_input_vjps_it);
+  for (auto& df_input_vjps_elem : gradient->df_input_vjps) {
     if (df_input_vjps_elem > df_input_vjps_val) {
       --df_input_vjps_elem;
     }
@@ -121,8 +123,8 @@ void RemoveNodeOutputFromGradient(Node* node, const size_t node_output_idx,
 
 }  // namespace
 
-void RemoveUnusedForwardOutputs(Gradient& gradient) {
-  for (auto it = gradient.f->nodes().begin(), end = gradient.f->nodes().end();
+void RemoveUnusedForwardOutputs(Gradient* gradient) {
+  for (auto it = gradient->f->nodes().begin(), end = gradient->f->nodes().end();
        it != end; ++it) {
     JIT_ASSERT(it->blocks().size() == 0);
     switch (it->kind()) {
