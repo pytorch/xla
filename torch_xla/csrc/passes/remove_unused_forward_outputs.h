80a8da603b2716e6fbdece54e232663eadfaa5bd
Alex Şuhan <asuhan@google.com>
2018-12-13 14:10:38 -0800

    Adjust signature of RemoveUnusedForwardOutputs

--- torch_xla/csrc/passes/remove_unused_forward_outputs.h
+++ torch_xla/csrc/passes/remove_unused_forward_outputs.h
@@ -6,7 +6,7 @@ namespace torch {
 namespace jit {
 
 // Remove outputs from forward graph which are not useful for the XLA lowering.
-void RemoveUnusedForwardOutputs(Gradient& gradient);
+void RemoveUnusedForwardOutputs(Gradient* gradient);
 
 }  // namespace jit
 }  // namespace torch
