1a870ab34c729581434f471a04bc3c03e992eec3
Daniel Sohn <jysohn@google.com>
2018-12-17 16:22:22 -0800

    Update builds to point to newer PyTorch version

--- kokoro/ubuntu/common.sh
+++ kokoro/ubuntu/common.sh
@@ -19,7 +19,7 @@ cd pytorch
 git submodule update --init --recursive
 
 # TODO(jysohn): remove following patching once pytorch JIT bug is fixed
-git checkout 1ca0ec7299b9352123a07525320a04e39d657781
+git checkout d71fac20ebf6b393e464d312b8a38ae1c45c3386
 git apply xla/pytorch.patch
 
 # Execute build
