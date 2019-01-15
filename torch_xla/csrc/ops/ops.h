43f0c84cd56ac487e0e63189a1a5b3020325e154
Davide Libenzi <dlibenzi@google.com>
2019-01-15 14:29:30 -0800

    Added utility APIs to create IR Node instead of replicating make_shared.

---
+++ torch_xla/csrc/ops/ops.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <memory>
+
+#include "ir.h"
+#include "ops/constant.h"
+#include "ops/cross_replica_sum.h"
+#include "ops/device_data.h"
+#include "ops/generic.h"
+#include "ops/scalar.h"
+
+namespace torch_xla {
+namespace ir {
+namespace ops {
+
+inline NodePtr ScalarOp(double value, xla::Shape shape) {
+  return std::make_shared<Scalar>(value, std::move(shape));
+}
+inline NodePtr ScalarOp(double value, xla::PrimitiveType type) {
+  return std::make_shared<Scalar>(value, type);
+}
+
+inline NodePtr ConstantOp(xla::Literal value) {
+  return std::make_shared<Constant>(std::move(value));
+}
+
+inline NodePtr DeviceDataOp(
+    std::shared_ptr<xla::ComputationClient::Data> data) {
+  return std::make_shared<DeviceData>(std::move(data));
+}
+
+inline NodePtr GenericOp(
+    OpKind op, tensorflow::gtl::ArraySlice<const NodeOperand> operands,
+    xla::Shape shape, Generic::LowerFn lower_fn, size_t num_outputs = 1) {
+  return std::make_shared<Generic>(std::move(op), operands, std::move(shape),
+                                   std::move(lower_fn), num_outputs);
+}
+
+inline NodePtr CrossReplicaSumOp(const NodeOperand& operand,
+                                 std::vector<std::vector<xla::int64>> groups) {
+  return std::make_shared<CrossReplicaSum>(operand, std::move(groups));
+}
+
+}  // namespace ops
+}  // namespace ir
+}  // namespace torch_xla
