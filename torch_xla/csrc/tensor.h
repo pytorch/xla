cc20fdf09656a499e31fbc9dfbedb24629b56431
Davide Libenzi <dlibenzi@google.com>
2019-01-15 16:07:33 -0800

    Enable XLA tensor data to persist.

--- torch_xla/csrc/tensor.h
+++ torch_xla/csrc/tensor.h
@@ -93,7 +93,7 @@ class XLATensor {
 
   void detach_() { requires_grad_ = false; }
 
-  at::Tensor toTensor();
+  const at::Tensor& toTensor();
 
   std::shared_ptr<XLATensor> grad() const;
   void SetGradient(std::shared_ptr<XLATensor> grad);
@@ -115,6 +115,8 @@ class XLATensor {
 
   const ir::NodePtr& CurrentIrNode() const;
   ir::NodePtr GetIrNode() const;
+
+  const std::shared_ptr<at::Tensor>& CurrentTensorData() const;
 
   // Makes the data references from the current tensor, point to the ones from
   // the source tensor.
@@ -200,15 +202,22 @@ class XLATensor {
         : ir_node(std::move(ir_node)),
           device(device),
           unique_id(GetNextTensorId()) {}
+    Data(std::shared_ptr<at::Tensor> tensor_data, const Device& device)
+        : tensor_data(std::move(tensor_data)),
+          device(device),
+          unique_id(GetNextTensorId()) {}
 
     std::shared_ptr<xla::ComputationClient::Data> xla_data;
     ir::NodePtr ir_node;
+    std::shared_ptr<at::Tensor> tensor_data;
     Device device;
     xla::int64 unique_id;
     std::shared_ptr<XLATensor> grad;
   };
 
   void SetIrNode(ir::NodePtr ir_node);
+
+  void SetTensorData(std::shared_ptr<at::Tensor> tensor_data);
 
   // We build an XLA graph accumulating XLA operations, but at a given point we
   // need to force a rendering, otherwise the graph can grow without control.
