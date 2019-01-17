6a076fee5676365a4baa423ab6ce17fcaaf8ac2a
Davide Libenzi <dlibenzi@google.com>
2019-01-16 17:21:45 -0800

    Start relaxing the fact that a tensor is either device data or IR node.

--- third_party/xla_client/util.h
+++ third_party/xla_client/util.h
@@ -4,11 +4,33 @@
 #include <memory>
 #include <vector>
 
+#include "absl/types/optional.h"
 #include "tensorflow/core/lib/gtl/array_slice.h"
 #include "tensorflow/core/lib/hash/hash.h"
 
 namespace xla {
 namespace util {
+
+// Allows APIs which might return const references and values, to not be forced
+// to return values in the signature.
+template <typename T>
+class MaybeRef {
+ public:
+  MaybeRef(const T& ref) : ref_(ref) {}
+  MaybeRef(T&& value) : storage_(std::move(value)), ref_(*storage_) {}
+
+  const T& get() const { return ref_; }
+
+  const T& operator*() const { return get(); }
+
+  operator const T&() const { return get(); }
+
+  bool is_stored() const { return storage_.has_value(); }
+
+ private:
+  absl::optional<T> storage_;
+  const T& ref_;
+};
 
 struct MidPolicy {
   size_t operator()(size_t size) const { return size / 2; }
