41728cc22657658db804d8cf39b05b1dc6f76029
Davide Libenzi <dlibenzi@google.com>
2019-01-20 09:06:06 -0800

    Added C++ unittest util file and added namespaces.

--- test/cpp/test_ir.cpp
+++ test/cpp/test_ir.cpp
@@ -1,11 +1,18 @@
 #include <gtest/gtest.h>
 
+#include "cpp_test_util.h"
 #include "ir.h"
 #include "lowering_context.h"
 #include "ops/ops.h"
 #include "ops/scalar.h"
 
+namespace torch_xla {
+namespace cpp_test {
+
 TEST(IrTest, TestScalarCreate) {
-  torch_xla::ir::NodePtr scalar = torch_xla::ir::ops::ScalarOp(1.0, xla::F32);
+  ir::NodePtr scalar = ir::ops::ScalarOp(1.0, xla::F32);
   ASSERT_TRUE(scalar != nullptr);
 }
+
+}  // namespace cpp_test
+}  // namespace torch_xla
