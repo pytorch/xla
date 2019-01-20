24625ab14e9f53b0698bc2020661b6016c468f53
Davide Libenzi <dlibenzi@google.com>
2019-01-20 09:23:42 -0800

    Added C++ test utility to test on all available device types.

--- test/cpp/cpp_test_util.h
+++ test/cpp/cpp_test_util.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <cmath>
+#include <functional>
+
 #include <ATen/ATen.h>
 #include <gtest/gtest.h>
+
+#include "device.h"
 #include "tensor.h"
 #include "torch/csrc/autograd/variable.h"
 
@@ -10,10 +15,14 @@ namespace cpp_test {
 
 static inline void AllClose(at::Tensor tensor, XLATensor& xla_tensor,
                             double rtol = 1e-5, double atol = 1e-8) {
-  EXPECT_TRUE(tensor.allclose(
-      torch::autograd::as_variable_ref(xla_tensor.ToTensor()).data(), rtol,
-      atol));
+  at::Tensor xtensor = xla_tensor.ToTensor();
+  if (xtensor.is_variable()) {
+    xtensor = torch::autograd::as_variable_ref(xtensor).data();
+  }
+  EXPECT_TRUE(tensor.allclose(xtensor, rtol, atol));
 }
+
+void ForEachDevice(const std::function<void(const Device&)>& devfn);
 
 }  // namespace cpp_test
 }  // namespace torch_xla
