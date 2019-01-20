0936051d50122d54a4446680a13a04a3c758aba1
Davide Libenzi <dlibenzi@google.com>
2019-01-20 09:23:42 -0800

    Added C++ test utility to test on all available device types.

--- test/cpp/test_tensor.cpp
+++ test/cpp/test_tensor.cpp
@@ -1,8 +1,5 @@
 #include <gtest/gtest.h>
 
-#include <cmath>
-#include <vector>
-
 #include <ATen/ATen.h>
 #include "cpp_test_util.h"
 #include "tensor.h"
@@ -12,16 +9,17 @@ namespace torch_xla {
 namespace cpp_test {
 
 TEST(TensotTest, TestAdd) {
-  Device device("CPU:0");
   at::Tensor a = at::rand({2, 2}, at::TensorOptions(at::kFloat));
   at::Tensor b = at::rand({2, 2}, at::TensorOptions(at::kFloat));
-
   auto c = a.add(b, 1.0);
-  auto dev_a = XLATensor::Create(a, device, /*requires_grad=*/false);
-  auto dev_b = XLATensor::Create(b, device, /*requires_grad=*/false);
-  auto dev_c = dev_a->add(*dev_b, 1.0);
 
-  AllClose(c, *dev_c);
+  ForEachDevice([&](const Device& device) {
+    auto dev_a = XLATensor::Create(a, device, /*requires_grad=*/false);
+    auto dev_b = XLATensor::Create(b, device, /*requires_grad=*/false);
+    auto dev_c = dev_a->add(*dev_b, 1.0);
+
+    AllClose(c, *dev_c);
+  });
 }
 
 }  // namespace cpp_test
