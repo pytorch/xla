0f4f48da43000b8f8c5507e9033d16121757899f
Ailing Zhang <ailzhang@fb.com>
2019-01-24 22:44:33 -0800

    move exports to a file

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -4,13 +4,11 @@ set -ex
 
 cd /tmp/pytorch/xla
 
-export XLA_USE_XRT=1 XRT_DEVICE_MAP="CPU:0;/job:localservice/replica:0/task:0/device:XLA_CPU:0"
-export XRT_WORKERS="localservice:0;grpc://localhost:40934"
+source ./xla_env
 
 python test/test_operations.py
 python test/test_train_mnist.py --tidy
 
 pushd test/cpp
-export CC=clang-7 CXX=clang++-7
 ./run_tests.sh
 popd
