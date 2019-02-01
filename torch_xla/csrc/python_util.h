d4887eaabcef1d78ad5de10f4ff458d55046effe
Davide Libenzi <dlibenzi@google.com>
2019-02-01 15:32:03 -0800

    Some comments.

--- torch_xla/csrc/python_util.h
+++ torch_xla/csrc/python_util.h
@@ -7,14 +7,19 @@
 
 namespace torch_xla {
 
+// Represents a location within a source code file.
 struct SourceLocation {
   std::string file;
   std::string function;
   int line = -1;
 };
 
+// Extracts the top Python frame which ended up calling into the C++ side. If no
+// such frame is available, nullopt is returned.
 c10::optional<SourceLocation> GetPythonFrameTop();
 
+// Returns the stack track of the Python side. Frame 0 is the closer to the C++
+// side.
 std::vector<SourceLocation> GetPythonFrames();
 
 }  // namespace torch_xla
