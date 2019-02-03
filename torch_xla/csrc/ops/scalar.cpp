9009e985b93f692019d4a720f915b377523b1772
Davide Libenzi <dlibenzi@google.com>
2019-02-02 16:49:37 -0800

    Use at::Scalar instead of double for IR Scalar Node.

--- torch_xla/csrc/ops/scalar.cpp
+++ torch_xla/csrc/ops/scalar.cpp
@@ -10,14 +10,20 @@
 namespace torch_xla {
 namespace ir {
 namespace ops {
+namespace {
 
-Scalar::Scalar(double value, xla::Shape shape)
-    : Node(OpKind(at::prim::Constant), {}, shape),
-      value_(value) {}
+std::ostream& operator<<(std::ostream& ostrm, const at::Scalar& s) {
+  return ostrm << (s.isFloatingPoint() ? s.toDouble() : s.toLong());
+}
 
-Scalar::Scalar(double value, xla::PrimitiveType type)
+}  // namespace
+
+Scalar::Scalar(at::Scalar value, xla::Shape shape)
+    : Node(OpKind(at::prim::Constant), {}, shape), value_(std::move(value)) {}
+
+Scalar::Scalar(at::Scalar value, xla::PrimitiveType type)
     : Node(OpKind(at::prim::Constant), {}, xla::ShapeUtil::MakeShape(type, {})),
-      value_(value) {}
+      value_(std::move(value)) {}
 
 std::string Scalar::ToString() const {
   std::stringstream ss;
@@ -29,34 +35,35 @@ XlaOpVector Scalar::Lower(LoweringContext* loctx) const {
   xla::Literal literal(xla::ShapeUtil::MakeShape(shape().element_type(), {}));
   switch (shape().element_type()) {
     case xla::S8:
-      literal.Set<xla::int8>({}, static_cast<xla::int8>(value_));
+      literal.Set<xla::int8>({}, static_cast<xla::int8>(value_.toChar()));
       break;
     case xla::U8:
-      literal.Set<xla::uint8>({}, static_cast<xla::uint8>(value_));
+      literal.Set<xla::uint8>({}, static_cast<xla::uint8>(value_.toByte()));
       break;
     case xla::S16:
-      literal.Set<xla::int16>({}, static_cast<xla::int16>(value_));
+      literal.Set<xla::int16>({}, static_cast<xla::int16>(value_.toShort()));
       break;
     case xla::U16:
-      literal.Set<xla::uint16>({}, static_cast<xla::uint16>(value_));
+      literal.Set<xla::uint16>({}, static_cast<xla::uint16>(value_.toShort()));
       break;
     case xla::S32:
-      literal.Set<xla::int32>({}, static_cast<xla::int32>(value_));
+      literal.Set<xla::int32>({}, static_cast<xla::int32>(value_.toInt()));
       break;
     case xla::U32:
-      literal.Set<xla::uint32>({}, static_cast<xla::uint32>(value_));
+      literal.Set<xla::uint32>({}, static_cast<xla::uint32>(value_.toInt()));
       break;
     case xla::S64:
-      literal.Set<xla::int64>({}, static_cast<xla::int64>(value_));
+      literal.Set<xla::int64>({}, static_cast<xla::int64>(value_.toLong()));
       break;
     case xla::U64:
-      literal.Set<xla::uint64>({}, static_cast<xla::uint64>(value_));
+      literal.Set<xla::uint64>({}, static_cast<xla::uint64>(value_.toLong()));
       break;
     case xla::F32:
-      literal.Set<float>({}, static_cast<float>(value_));
+      literal.Set<float>({}, static_cast<float>(value_.toDouble()));
       break;
     case xla::BF16:
-      literal.Set<xla::bfloat16>({}, static_cast<xla::bfloat16>(value_));
+      literal.Set<xla::bfloat16>({},
+                                 static_cast<xla::bfloat16>(value_.toDouble()));
       break;
     default:
       XLA_ERROR() << "Unable to lower scalar " << value_ << " of shape "
