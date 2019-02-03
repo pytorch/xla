c1f9818dd2c7ddb3e2b675e2448005d8575beda1
Davide Libenzi <dlibenzi@google.com>
2019-02-03 15:44:07 -0800

    Added convolution backward test.

--- torch_xla/csrc/torch_util.h
+++ torch_xla/csrc/torch_util.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <vector>
 
+#include <ATen/ATen.h>
 #include "module.h"
 #include "tensor.h"
 #include "torch/csrc/jit/pybind_utils.h"
@@ -14,5 +15,10 @@ XlaModule::TensorBatchVector XlaCreateTensorList(const py::tuple& tuple);
 
 // Packs a vector of XLA tensors into a Python tuple, if they are more than one.
 py::object XlaPackTensorList(const XlaModule::TensorBatchVector& outputs);
+
+// Makes a depp copy of an ATEN tensor.
+static inline at::Tensor CopyTensor(const at::Tensor& ref) {
+  return ref.to(ref.options(), /*non_blocking=*/false, /*copy=*/true);
+}
 
 }  // namespace torch_xla
