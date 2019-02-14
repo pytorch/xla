180cb3d0b2057bdcde1da53c9eb15c82365404fb
Alex Şuhan <asuhan@google.com>
2019-02-14 11:24:22 -0800

    Add squeeze to ATen XLA tensor

---
+++ torch_xla/csrc/ops/squeeze.cpp
@@ -0,0 +1,53 @@
+#include "torch_xla/csrc/ops/squeeze.h"
+#include "tensorflow/compiler/xla/xla_client/debug_macros.h"
+#include "tensorflow/compiler/xla/xla_client/util.h"
+#include "torch_xla/csrc/data_ops.h"
+#include "torch_xla/csrc/lowering_context.h"
+#include "torch_xla/csrc/ops/infer_output_shape.h"
+
+namespace torch_xla {
+namespace ir {
+namespace ops {
+namespace {
+
+xla::Shape NodeOutputShape(const Value& input, int dim) {
+  auto lower_for_shape_fn =
+      [dim](tensorflow::gtl::ArraySlice<const xla::XlaOp> operands)
+      -> xla::XlaOp {
+    XLA_CHECK_EQ(operands.size(), 1);
+    if (dim == -1) {
+      return SqueezeAllTrivialDimensions(operands[0]);
+    }
+    XLA_CHECK_GE(dim, 0);
+    return SqueezeTrivialDimension(operands[0], dim);
+  };
+  return InferOutputShape({input.shape()}, lower_for_shape_fn);
+}
+
+}  // namespace
+
+Squeeze::Squeeze(const Value& input, int dim)
+    : Node(ir::OpKind(at::aten::squeeze), {input}, NodeOutputShape(input, dim),
+           /*num_outputs=*/1, xla::util::MHash(dim)),
+      dim_(dim) {}
+
+XlaOpVector Squeeze::Lower(LoweringContext* loctx) const {
+  xla::XlaOp input = loctx->GetOutputOp(operand(0));
+  if (dim_ == -1) {
+    xla::XlaOp output = SqueezeAllTrivialDimensions(input);
+    return ReturnOp(output, loctx);
+  }
+  XLA_CHECK_GE(dim_, 0);
+  xla::XlaOp output = SqueezeTrivialDimension(input, dim_);
+  return ReturnOp(output, loctx);
+}
+
+std::string Squeeze::ToString() const {
+  std::stringstream ss;
+  ss << Node::ToString() << " dim=" << dim_;
+  return ss.str();
+}
+
+}  // namespace ops
+}  // namespace ir
+}  // namespace torch_xla
