1c631871265469a5ea718d83f1e03a625c7369d9
Alex Şuhan <asuhan@google.com>
2019-02-14 15:02:31 -0800

    Add permute to ATen XLA tensor

---
+++ torch_xla/csrc/ops/permute.cpp
@@ -0,0 +1,46 @@
+#include "torch_xla/csrc/ops/permute.h"
+#include "tensorflow/compiler/xla/xla_client/debug_macros.h"
+#include "tensorflow/compiler/xla/xla_client/util.h"
+#include "torch_xla/csrc/lowering_context.h"
+#include "torch_xla/csrc/ops/infer_output_shape.h"
+
+namespace torch_xla {
+namespace ir {
+namespace ops {
+namespace {
+
+xla::Shape NodeOutputShape(const Value& input,
+                           tensorflow::gtl::ArraySlice<const xla::int64> dims) {
+  auto lower_for_shape_fn =
+      [dims](tensorflow::gtl::ArraySlice<const xla::XlaOp> operands)
+      -> xla::XlaOp {
+    XLA_CHECK_EQ(operands.size(), 1);
+    return xla::Transpose(operands[0], dims);
+  };
+  return InferOutputShape({input.shape()}, lower_for_shape_fn);
+}
+
+}  // namespace
+
+Permute::Permute(const Value& input,
+                 tensorflow::gtl::ArraySlice<const xla::int64> dims)
+    : Node(ir::OpKind(at::aten::permute), {input}, NodeOutputShape(input, dims),
+           /*num_outputs=*/1, xla::util::MHash(dims)),
+      dims_(dims.begin(), dims.end()) {}
+
+XlaOpVector Permute::Lower(LoweringContext* loctx) const {
+  xla::XlaOp input = loctx->GetOutputOp(operand(0));
+  xla::XlaOp output = xla::Transpose(input, dims_);
+  return ReturnOp(output, loctx);
+}
+
+std::string Permute::ToString() const {
+  std::stringstream ss;
+  ss << Node::ToString() << ", kernel_size=[" << absl::StrJoin(dims_, ", ")
+     << "]";
+  return ss.str();
+}
+
+}  // namespace ops
+}  // namespace ir
+}  // namespace torch_xla
---
+++ torch_xla/csrc/ops/permute.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "tensorflow/core/lib/gtl/array_slice.h"
+#include "torch_xla/csrc/ir.h"
+
+namespace torch_xla {
+namespace ir {
+namespace ops {
+
+class Permute : public Node {
+ public:
+  Permute(const Value& input,
+          tensorflow::gtl::ArraySlice<const xla::int64> dims);
+
+  XlaOpVector Lower(LoweringContext* loctx) const override;
+
+  std::string ToString() const override;
+
+  const std::vector<xla::int64>& dims() const { return dims_; }
+
+ private:
+  // The permutation of dimensions.
+  std::vector<xla::int64> dims_;
+};
+
+}  // namespace ops
+}  // namespace ir
+}  // namespace torch_xla
