01f0de84a1179a0786d61f5d2f9111866540e30a
Davide Libenzi <dlibenzi@google.com>
2019-02-14 16:51:24 -0800

    WIP: Added aten::mean operation.

--- torch_xla/csrc/reduction.cpp
+++ torch_xla/csrc/reduction.cpp
@@ -19,4 +19,39 @@ xla::XlaOp BuildSum(const torch::jit::Node* node, const xla::XlaOp& operand) {
       XlaHelpers::I64List(dimensions_to_reduce));
 }
 
+xla::XlaOp BuildMean(const xla::XlaOp& input,
+                     tensorflow::gtl::ArraySlice<const xla::int64> dimensions,
+                     bool keep_reduced_dimensions) {
+  xla::Shape input_shape = XlaHelpers::ShapeOfXlaOp(input);
+  xla::XlaOp init_value = XlaHelpers::ScalarValue<float>(
+      0, input_shape.element_type(), input.builder());
+  xla::int64 element_count = 1;
+  std::vector<xla::int64> new_dimensions;
+  size_t idim = 0;
+  for (xla::int64 i = 0; i < input_shape.rank(); ++i) {
+    if (idim < dimensions.size() && dimensions[idim] == i) {
+      element_count *= input_shape.dimensions(i);
+      ++idim;
+      if (keep_reduced_dimensions) {
+        new_dimensions.push_back(1);
+      }
+    } else if (keep_reduced_dimensions) {
+      new_dimensions.push_back(input_shape.dimensions(i));
+    }
+  }
+  xla::XlaOp result = xla::Reduce(
+      input, init_value,
+      XlaHelpers::CreateAddComputation(input_shape.element_type()), dimensions);
+  if (element_count > 1) {
+    xla::XlaOp scale = XlaHelpers::ScalarValue<float>(
+        1.0f / static_cast<float>(element_count), input_shape.element_type(),
+        input.builder());
+    result = xla::Mul(result, scale);
+  }
+  if (keep_reduced_dimensions) {
+    result = xla::Reshape(result, new_dimensions);
+  }
+  return result;
+}
+
 }  // namespace torch_xla
--- torch_xla/csrc/reduction.h
+++ torch_xla/csrc/reduction.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "tensorflow/compiler/xla/client/xla_builder.h"
+#include "tensorflow/core/lib/gtl/array_slice.h"
 #include "torch/csrc/jit/ir.h"
 
 namespace torch_xla {
@@ -8,5 +9,12 @@ namespace torch_xla {
 // Sum the given operand elements along the dimension specified by the "dim"
 // attribute of the node.
 xla::XlaOp BuildSum(const torch::jit::Node* node, const xla::XlaOp& operand);
+
+// Builds a mean by reducing all the dimensions listed in dimensions. If
+// keep_reduced_dimensions is true, the reduced dimensions will be retained,
+// with value 1.
+xla::XlaOp BuildMean(const xla::XlaOp& input,
+                     tensorflow::gtl::ArraySlice<const xla::int64> dimensions,
+                     bool keep_reduced_dimensions);
 
 }  // namespace torch_xla
