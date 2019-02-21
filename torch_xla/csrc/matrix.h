01fb1665e23a585fe19e7877e2edd934c0dbd5fe
Alex Şuhan <asuhan@google.com>
2019-02-21 13:36:01 -0800

    Make Build{Triu, Tril} take xla::int64 diagonal

--- torch_xla/csrc/matrix.h
+++ torch_xla/csrc/matrix.h
@@ -5,9 +5,9 @@
 
 namespace torch_xla {
 
-xla::XlaOp BuildTriu(const xla::XlaOp& input, int diagonal);
+xla::XlaOp BuildTriu(const xla::XlaOp& input, xla::int64 diagonal);
 
-xla::XlaOp BuildTril(const xla::XlaOp& input, int diagonal);
+xla::XlaOp BuildTril(const xla::XlaOp& input, xla::int64 diagonal);
 
 xla::XlaOp BuildDiagonal(const xla::XlaOp& input, xla::int64 offset,
                          xla::int64 dim1, xla::int64 dim2);
