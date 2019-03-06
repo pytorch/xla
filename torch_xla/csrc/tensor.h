747d673fca620aaded1f9d39d053eb6bffa9edfc
Alex Şuhan <asuhan@google.com>
2019-03-06 10:46:48 -0800

    Add in-place transpose to ATen XLA tensor

--- torch_xla/csrc/tensor.h
+++ torch_xla/csrc/tensor.h
@@ -247,6 +247,8 @@ class XLATensor {
 
   static XLATensor t(const XLATensor& input);
 
+  static void t_(XLATensor& input);
+
   // Returns a tensor with the same data and number of elements as input, but
   // with the specified shape.
   static XLATensor reshape(
