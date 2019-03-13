07f06bf16d9e792a9fe2526e92c55bda1be37fc6
Davide Libenzi <dlibenzi@google.com>
2019-03-12 17:27:26 -0700

    Scale gradients CRS to inverse of number of replicas.

--- torch_xla/csrc/ops/cross_replica_sum.cpp
+++ torch_xla/csrc/ops/cross_replica_sum.cpp
@@ -1,6 +1,8 @@
 #include "torch_xla/csrc/ops/cross_replica_sum.h"
 
+#include "absl/strings/str_join.h"
 #include "tensorflow/compiler/xla/xla_client/util.h"
+#include "torch_xla/csrc/cross_replica_reduces.h"
 #include "torch_xla/csrc/lowering_context.h"
 #include "torch_xla/csrc/ops/xla_ops.h"
 
@@ -8,41 +10,28 @@ namespace torch_xla {
 namespace ir {
 namespace ops {
 
-CrossReplicaSum::CrossReplicaSum(const Value& operand,
+CrossReplicaSum::CrossReplicaSum(const Value& operand, double scale,
                                  std::vector<std::vector<xla::int64>> groups)
     : Node(xla_cross_replica_sum, {operand}, operand.shape(),
-           /*num_outputs=*/1, xla::util::MHash(groups)),
+           /*num_outputs=*/1, xla::util::MHash(scale, groups)),
+      scale_(scale),
       groups_(std::move(groups)) {}
 
 std::string CrossReplicaSum::ToString() const {
   std::stringstream ss;
-  ss << Node::ToString() << " groups=(";
+  ss << Node::ToString() << ", scale=" << scale_ << ", groups=(";
   for (size_t i = 0; i < groups_.size(); ++i) {
-    const auto& group = groups_[i];
     ss << (i == 0 ? "(" : ",(");
-    for (size_t j = 0; j < group.size(); ++j) {
-      if (j > 0) {
-        ss << ",";
-      }
-      ss << group[j];
-    }
-    ss << ")";
+    ss << absl::StrJoin(groups_[i], ", ") << ")";
   }
   ss << ")";
   return ss.str();
 }
 
 XlaOpVector CrossReplicaSum::Lower(LoweringContext* loctx) const {
-  std::vector<xla::ReplicaGroup> crs_groups;
-  for (auto& group : groups_) {
-    xla::ReplicaGroup rgroup;
-    for (auto replica_id : group) {
-      rgroup.add_replica_ids(replica_id);
-    }
-    crs_groups.push_back(std::move(rgroup));
-  }
   xla::XlaOp op = loctx->GetOutputOp(operand(0));
-  return ReturnOp(xla::CrossReplicaSum(op, crs_groups), loctx);
+  xla::XlaOp crs = BuildCrossReplicaSum(op, scale_, groups_);
+  return ReturnOp(crs, loctx);
 }
 
 }  // namespace ops
--- torch_xla/csrc/ops/cross_replica_sum.h
+++ torch_xla/csrc/ops/cross_replica_sum.h
@@ -8,14 +8,19 @@ namespace ops {
 
 class CrossReplicaSum : public Node {
  public:
-  CrossReplicaSum(const Value& operand,
+  CrossReplicaSum(const Value& operand, double scale,
                   std::vector<std::vector<xla::int64>> groups);
 
   std::string ToString() const override;
 
   XlaOpVector Lower(LoweringContext* loctx) const override;
 
+  double scale() const { return scale_; }
+
+  const std::vector<std::vector<xla::int64>>& groups() const { return groups_; }
+
  private:
+  double scale_;
   std::vector<std::vector<xla::int64>> groups_;
 };
 
