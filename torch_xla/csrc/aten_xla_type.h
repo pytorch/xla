9ce474a1a1acc9f239ad8610b8a02a94dee40cc3
Alex Şuhan <asuhan@google.com>
2019-03-15 13:49:38 -0700

    Add one_hot to ATen XLA tensor
    It's sufficient to just route it to at::native::one_hot. Still goes back
    to host for range validation of labels, which cannot be avoided.

--- torch_xla/csrc/aten_xla_type.h
+++ torch_xla/csrc/aten_xla_type.h
@@ -701,6 +701,9 @@ class AtenXlaType : public AtenXlaTypeBase {
 
   at::Tensor flip(const at::Tensor& self, at::IntArrayRef dims) const override;
 
+  at::Tensor one_hot(const at::Tensor& self,
+                     int64_t num_classes) const override;
+
   at::Tensor transpose(const at::Tensor& self, int64_t dim0,
                        int64_t dim1) const override;
   at::Tensor& transpose_(at::Tensor& self, int64_t dim0,
