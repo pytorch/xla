3ff502ba0fd42a72e2a3887de6f30152b35e8e0f
Davide Libenzi <dlibenzi@google.com>
2019-03-23 12:43:37 -0700

    Fix XLA tensor views which are not handled by the XLA tensor core.
    Fixes #514

--- torch_xla/csrc/ops/xla_ops.h
+++ torch_xla/csrc/ops/xla_ops.h
@@ -29,6 +29,7 @@ class OpKindWrapper {
 };
 
 extern const OpKindWrapper xla_device_data;
+extern const OpKindWrapper xla_tensor_data;
 extern const OpKindWrapper xla_cross_replica_sum;
 extern const OpKindWrapper xla_cast;
 extern const OpKindWrapper xla_not_supported;
