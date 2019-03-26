39f679ce75da155052290c5683b1c97b59aefee8
Davide Libenzi <dlibenzi@google.com>
2019-03-26 07:27:53 -0700

    Fixes zeros_like() monkey-patch in XLA Python init.
    Fixes #527

--- torch_xla/__init__.py
+++ torch_xla/__init__.py
@@ -5,17 +5,21 @@ torch_zeros_like_builtin = torch.zeros_like
 torch_clone_builtin = torch.clone
 
 
-def _torch_xla_zeros_like(p):
-    import torch_xla
-    if isinstance(p, torch_xla._XLAC.XLATensor):
-        return torch_xla._XLAC.XLATensor(torch.zeros(p.size()), device=p.device())
-    return torch_zeros_like_builtin(p)
+def _torch_xla_zeros_like(p, **kwargs):
+  import torch_xla
+  if isinstance(p, torch_xla._XLAC.XLATensor):
+    return torch_xla._XLAC.XLATensor(
+        torch.zeros(p.size()),
+        dtype=kwargs.get('dtype', p.dtype),
+        device=p.device())
+  return torch_zeros_like_builtin(p, **kwargs)
+
 
 def _torch_xla_clone(p):
-    import torch_xla
-    if isinstance(p, torch_xla._XLAC.XLATensor):
-        return p.clone()
-    return torch_clone_builtin(p)
+  import torch_xla
+  if isinstance(p, torch_xla._XLAC.XLATensor):
+    return p.clone()
+  return torch_clone_builtin(p)
 
 
 torch.zeros_like = _torch_xla_zeros_like
