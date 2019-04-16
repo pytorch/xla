e4fbabd76136852e95dab0f7d05b3173249d2284
Alex Şuhan <asuhan@google.com>
2019-04-16 16:51:10 -0700

    Promote sides of bitwise logical operations

--- torch_xla/csrc/ops/bitwise_ir_ops.cpp
+++ torch_xla/csrc/ops/bitwise_ir_ops.cpp
@@ -12,7 +12,10 @@ Value BitwiseAnd(const Value& node1, const Value& node2) {
   auto lower_fn = [](const Node& node, LoweringContext* loctx) -> XlaOpVector {
     xla::XlaOp op0 = loctx->GetOutputOp(node.operand(0));
     xla::XlaOp op1 = loctx->GetOutputOp(node.operand(1));
-    return node.ReturnOp(op0 & op1, loctx);
+    auto kernel = [](const xla::XlaOp& op0, const xla::XlaOp& op1) {
+      return op0 & op1;
+    };
+    return node.ReturnOp(XlaHelpers::PromotedBinaryOp(op0, op1, kernel), loctx);
   };
   return GenericOp(OpKind(at::aten::__and__), OpList{node1, node2},
                    node1.shape(), std::move(lower_fn));
@@ -22,7 +25,10 @@ Value BitwiseOr(const Value& node1, const Value& node2) {
   auto lower_fn = [](const Node& node, LoweringContext* loctx) -> XlaOpVector {
     xla::XlaOp op0 = loctx->GetOutputOp(node.operand(0));
     xla::XlaOp op1 = loctx->GetOutputOp(node.operand(1));
-    return node.ReturnOp(op0 | op1, loctx);
+    auto kernel = [](const xla::XlaOp& op0, const xla::XlaOp& op1) {
+      return op0 | op1;
+    };
+    return node.ReturnOp(XlaHelpers::PromotedBinaryOp(op0, op1, kernel), loctx);
   };
   return GenericOp(OpKind(at::aten::__or__), OpList{node1, node2},
                    node1.shape(), std::move(lower_fn));
@@ -32,7 +38,10 @@ Value BitwiseXor(const Value& node1, const Value& node2) {
   auto lower_fn = [](const Node& node, LoweringContext* loctx) -> XlaOpVector {
     xla::XlaOp op0 = loctx->GetOutputOp(node.operand(0));
     xla::XlaOp op1 = loctx->GetOutputOp(node.operand(1));
-    return node.ReturnOp(op0 ^ op1, loctx);
+    auto kernel = [](const xla::XlaOp& op0, const xla::XlaOp& op1) {
+      return op0 ^ op1;
+    };
+    return node.ReturnOp(XlaHelpers::PromotedBinaryOp(op0, op1, kernel), loctx);
   };
   return GenericOp(OpKind(at::aten::__xor__), OpList{node1, node2},
                    node1.shape(), std::move(lower_fn));
