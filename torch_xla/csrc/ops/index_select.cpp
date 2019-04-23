a6922d4efe1d20118917d23d2eef39b80db15c81
Alex Şuhan <asuhan@google.com>
2019-04-23 13:56:48 -0700

    Use xla::TorchIndexSelect to lower IndexSelect

--- torch_xla/csrc/ops/index_select.cpp
+++ torch_xla/csrc/ops/index_select.cpp
@@ -1,5 +1,6 @@
 #include "torch_xla/csrc/ops/index_select.h"
 
+#include "tensorflow/compiler/xla/client/lib/slicing.h"
 #include "tensorflow/compiler/xla/xla_client/util.h"
 #include "torch_xla/csrc/helpers.h"
 #include "torch_xla/csrc/lowering_context.h"
@@ -10,30 +11,13 @@ namespace ir {
 namespace ops {
 namespace {
 
-xla::XlaOp LowerIndexSelect(const xla::XlaOp& input, const xla::XlaOp& index,
-                            xla::int64 dim) {
-  // TODO: Use the new xla::TorchIndexSelect() once it shows up.
-  xla::Shape input_shape = XlaHelpers::ShapeOfXlaOp(input);
-  xla::Shape index_shape = XlaHelpers::ShapeOfXlaOp(index);
-  std::vector<xla::int64> slice_sizes = input_shape.dimensions();
-  slice_sizes[dim] = 1;
-  xla::GatherDimensionNumbers gather_dnums;
-  for (xla::int64 i = 0; i < input_shape.rank(); ++i) {
-    if (i != dim) {
-      gather_dnums.add_offset_dims(i);
-    }
-  }
-  gather_dnums.set_index_vector_dim(index_shape.rank());
-  gather_dnums.add_collapsed_slice_dims(dim);
-  gather_dnums.add_start_index_map(dim);
-  return xla::Gather(input, index, gather_dnums, slice_sizes);
-}
-
 xla::Shape NodeOutputShape(const Value& input, const Value& index,
                            xla::int64 dim) {
   auto lower_for_shape_fn =
       [&](tensorflow::gtl::ArraySlice<const xla::XlaOp> operands)
-      -> xla::XlaOp { return LowerIndexSelect(operands[0], operands[1], dim); };
+      -> xla::XlaOp {
+    return xla::TorchIndexSelect(operands[0], operands[1], dim);
+  };
   return InferOutputShape({input.shape(), index.shape()}, lower_for_shape_fn);
 }
 
@@ -49,7 +33,7 @@ IndexSelect::IndexSelect(const Value& input, xla::int64 dim, const Value& index)
 XlaOpVector IndexSelect::Lower(LoweringContext* loctx) const {
   xla::XlaOp input = loctx->GetOutputOp(operand(0));
   xla::XlaOp index = loctx->GetOutputOp(operand(1));
-  return ReturnOp(LowerIndexSelect(input, index, dim_), loctx);
+  return ReturnOp(xla::TorchIndexSelect(input, index, dim_), loctx);
 }
 
 std::string IndexSelect::ToString() const {
