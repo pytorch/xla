ba1d87f20d57c1abf3751926dcbe4efb9b761b04
Davide Libenzi <dlibenzi@google.com>
2019-04-29 06:45:14 -0700

    Track post order size in op-by-op mode.

--- third_party/xla_client/metrics.h
+++ third_party/xla_client/metrics.h
@@ -142,6 +142,13 @@ class Counter {
     __counter->AddValue(value);                 \
   } while (0)
 
+#define XLA_VALUE_METRIC(name, value)                                    \
+  do {                                                                   \
+    static ::xla::metrics::Metric* __metric =                            \
+        new ::xla::metrics::Metric(name, ::xla::metrics::MetricFnValue); \
+    __metric->AddSample(value);                                          \
+  } while (0)
+
 // Creates a report with the current metrics statistics.
 string CreateMetricReport();
 
