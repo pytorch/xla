5672bcea46709c666e295af92be2bacb583dcc1a
Davide Libenzi <dlibenzi@google.com>
2019-04-29 19:49:54 -0700

    Allow the PyTorch include folder to be overridden via environment.

--- scripts/generate_code.sh
+++ scripts/generate_code.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 CDIR="$(cd "$(dirname "$0")" ; pwd -P)"
-XDIR=$CDIR/..
-PTDIR=$XDIR/..
+XDIR="$CDIR/.."
+PTDIR="$XDIR/.."
+if [ -z "$PT_INC_DIR" ]; then
+  PT_INC_DIR="$PTDIR/build/aten/src/ATen"
+fi
 
-python $CDIR/gen.py \
+python "$CDIR/gen.py" \
   --gen_class_mode \
-  --output_folder=$XDIR/torch_xla/csrc \
-  $PTDIR/build/aten/src/ATen/TypeDefault.h \
-  $PTDIR/build/aten/src/ATen/Functions.h \
-  $PTDIR/build/aten/src/ATen/NativeFunctions.h
+  --output_folder="$XDIR/torch_xla/csrc" \
+  "$PT_INC_DIR/TypeDefault.h" \
+  "$PT_INC_DIR/Functions.h" \
+  "$PT_INC_DIR/NativeFunctions.h"
