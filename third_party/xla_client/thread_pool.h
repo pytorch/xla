d581df315156888c4342306d387eed3654fa9af1
Davide Libenzi <dlibenzi@google.com>
2019-05-07 18:52:23 -0700

    Added simple thread pool instead of spawning new threads.

--- third_party/xla_client/thread_pool.h
+++ third_party/xla_client/thread_pool.h
@@ -2,15 +2,34 @@
 #define TENSORFLOW_COMPILER_XLA_XLA_CLIENT_THREAD_POOL_H_
 
 #include <functional>
+#include <memory>
+#include <thread>
 
 namespace xla {
 namespace env {
 
-// Schedules a closure to be run. The closure should not block.
+class Completion {
+ public:
+  class Data;
+
+  explicit Completion(std::shared_ptr<Data> data);
+
+  ~Completion();
+
+  void Wait();
+
+ private:
+  std::shared_ptr<Data> data_;
+};
+
+// Schedules a closure to be run. The closure should not block waiting for other
+// events.
 void ScheduleClosure(std::function<void()> closure);
+Completion ScheduleClosureWithCompletion(std::function<void()> closure);
 
 // Schedules a closure which might wait for IO or other events/conditions.
 void ScheduleIoClosure(std::function<void()> closure);
+Completion ScheduleIoClosureWithCompletion(std::function<void()> closure);
 
 }  // namespace env
 }  // namespace xla
