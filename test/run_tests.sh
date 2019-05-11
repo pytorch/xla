57c1aaeba14eea59063d9a67a932bdf70f2b3eb8
Davide Libenzi <dlibenzi@google.com>
2019-05-11 06:53:39 -0700

    Limit the check graph frequency as well, until the XLA bug has been fixed.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,8 +3,9 @@ set -ex
 CDIR="$(cd "$(dirname "$0")" ; pwd -P)"
 LOGFILE=/tmp/pytorch_py_test.log
 MAX_GRAPH_SIZE=1000
+GRAPH_CHECK_FREQUENCY=100
 
-while getopts 'LM:' OPTION
+while getopts 'LM:C:' OPTION
 do
   case $OPTION in
     L)
@@ -13,11 +14,15 @@ do
     M)
       MAX_GRAPH_SIZE=$OPTARG
       ;;
+    C)
+      GRAPH_CHECK_FREQUENCY=$OPTARG
+      ;;
   esac
 done
 shift $(($OPTIND - 1))
 
 export TRIM_GRAPH_SIZE=$MAX_GRAPH_SIZE
+export TRIM_GRAPH_CHECK_FREQUENCY=$GRAPH_CHECK_FREQUENCY
 
 if [ "$LOGFILE" != "" ]; then
   python "$CDIR/test_operations.py" "$@" 2>&1 | tee $LOGFILE
