7e588326ed01327cb77dd5cb068e4f8a3495e724
Alex Şuhan <asuhan@google.com>
2019-06-20 20:15:08 -0700

    Fix aten_xla_type.h formatting

--- torch_xla/csrc/aten_xla_type.h
+++ torch_xla/csrc/aten_xla_type.h
@@ -1039,13 +1039,14 @@ class AtenXlaType {
       at::IntArrayRef stride, at::IntArrayRef padding);
 
   static std::tuple<at::Tensor, at::Tensor, at::Tensor>
-  conv_transpose2d_backward(
-      const at::Tensor& grad_output, const at::Tensor& self,
-      const at::Tensor& weight, at::IntArrayRef kernel_size,
-      at::IntArrayRef stride, at::IntArrayRef padding,
-      at::IntArrayRef output_padding, at::IntArrayRef dilation,
-      const at::Tensor& columns, const at::Tensor& ones,
-      std::array<bool, 3> output_mask);
+  conv_transpose2d_backward(const at::Tensor& grad_output,
+                            const at::Tensor& self, const at::Tensor& weight,
+                            at::IntArrayRef kernel_size, at::IntArrayRef stride,
+                            at::IntArrayRef padding,
+                            at::IntArrayRef output_padding,
+                            at::IntArrayRef dilation, const at::Tensor& columns,
+                            const at::Tensor& ones,
+                            std::array<bool, 3> output_mask);
 
   static at::Tensor threshold(const at::Tensor& self, at::Scalar threshold,
                               at::Scalar value);
