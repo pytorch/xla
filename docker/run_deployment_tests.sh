52a5e102c83ac0d1e5e8b21252488e86d2147d2e
Daniel Sohn <jysohn@google.com>
2019-06-21 17:21:18 -0700

    Add release_version variable for stable builds

--- docker/run_deployment_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export XRT_DEVICE_MAP="CPU:0;/job:localservice/replica:0/task:0/device:XLA_CPU:0"
-export XRT_WORKERS="localservice:0;grpc://localhost:40934"
-
-time python pytorch/xla/test/test_train_mnist.py
-time bash pytorch/xla/test/run_tests.sh
-time bash pytorch/xla/test/cpp/run_tests.sh
