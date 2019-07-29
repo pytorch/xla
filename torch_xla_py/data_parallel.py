c3fac66190e2733c64f7c8d50ef1ea10ec4ac255
zcain117 <zcain@google.com>
2019-07-29 15:23:47 -0700

    dont crash with one core when checking replication (#854)
    * dont crash with one core when checking replication
    
    * set replication to none in data_parallel
    
    * use len greater than 1
    
    * change only data_parallel

--- torch_xla_py/data_parallel.py
+++ torch_xla_py/data_parallel.py
@@ -181,7 +181,7 @@ class DataParallel(object):
     self._batchdim = batchdim
     self._drop_last = drop_last
     self._native_run = False
-    if self._device_ids:
+    if len(self._device_ids) > 1:
       replication_devices = xm.xla_replication_devices(self._device_ids)
       self._replication = xm.Replication(self._device_ids, replication_devices)
     else:
