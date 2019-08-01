345522b6d3ff974269c57adc7f0cd8d29c8ea879
Jin Young Sohn <jysohn@google.com>
2019-08-01 08:55:07 -0700

    Define classes to be used for distributed training (#864)

---
+++ torch_xla_py/xla_dist.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+"""Tool to distribute training on Cloud TPU Pods."""
+from __future__ import division
+from __future__ import print_function
+
+
+class Worker(object):
+
+  def __init__(self, internal_ip, mach_type, zone):
+    self._internal_ip = internal_ip
+    self._mach_type = mach_type
+    self._zone = zone
+
+
+class ClientWorker(Worker):
+
+  def __init__(self, internal_ip, machine_type, zone, hostname=None):
+    super(ClientWorker, self).__init__(internal_ip, machine_type, zone)
+    self._hostname = hostname
+
+
+class ServiceWorker(Worker):
+  # Same as base Worker ATM.
+  pass
+
+
+class Cluster(object):
+
+  def __init__(self, client_workers, service_workers):
+    """Creates a cluster object.
+
+    Args:
+      client_workers: a list of ClientWorker objects.
+      service_workers: a list of ServiceWorker objects.
+    """
+    self._client_workers = list(client_workers)
+    self._service_workers = list(service_workers)
+
+  def validate(self):
+    """Validates the current cluster configuration.
+
+    Raises:
+      RuntimeError: If the cluster is misconfigured, this validation will
+        raise an error.
+    """
+    raise NotImplementedError()
+
+
+class ClusterResolver(object):
+  """Cluster Resolver for Client VM and Cloud TPU mesh."""
+
+  def __init__(self,
+               tpus,
+               vms=None,
+               zone=None,
+               project=None):
+    """Creates a new ClusterResolver object."""
+
+    assert tpus, "TPU name list must not be empty."
+
+    self._tpus = tpus
+    self._vms = vms
+    self._zone = zone
+    self._project = project
+
+  def get_cluster(self):
+    """Gets client and server side cluster info.
+
+    If a list of vms is not provided at ClusterResolver crate time the current
+    VM's instance group is picked up and we use that to resolve the VM mesh.
+
+    Returns:
+      A Cluster object with both client and server mesh configuration.
+
+    Raises:
+      RuntimeError: If the VM cluster is not healthy. Also if the TPU
+        cluster is not healthy.
+    """
+    raise NotImplementedError()
+
---
+++ torch_xla_py/xla_dist_test.py
@@ -0,0 +1,25 @@
+"""Tests for xla_dist."""
+from __future__ import division
+from __future__ import print_function
+
+import unittest
+import xla_dist
+
+class ClusterTest(unittest.TestCase):
+
+  def test_validate_service_cluster(self):
+    pass
+
+  def test_validate_client_cluster(self):
+    pass
+
+
+class ClusterResolverTest(unittest.TestCase):
+
+  def test_cluster(self):
+    pass
+
+
+if __name__ == '__main__':
+  test = unittest.main()
+  sys.exit(0 if test.result.wasSuccessful() else 1)
