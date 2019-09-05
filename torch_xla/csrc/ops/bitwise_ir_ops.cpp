1aef09308095f16be67b187f2847be343afb7318
Ailing Zhang <ailzhang@fb.com>
2019-09-05 16:27:19 -0700

    remove kernel functions

--- torch_xla/csrc/ops/bitwise_ir_ops.cpp
+++ torch_xla/csrc/ops/bitwise_ir_ops.cpp
@@ -12,10 +12,7 @@ Value BitwiseAnd(const Value& node1, const Value& node2) {
   auto lower_fn = [](const Node& node, LoweringContext* loctx) -> XlaOpVector {
     xla::XlaOp op0 = loctx->GetOutputOp(node.operand(0));
     xla::XlaOp op1 = loctx->GetOutputOp(node.operand(1));
-    auto kernel = [](const xla::XlaOp& op0, const xla::XlaOp& op1) {
-      return op0 & op1;
-    };
-    return node.ReturnOp(kernel(op0, op1), loctx);
+    return node.ReturnOp(op0 & op1, loctx);
   };
   XLA_CHECK_EQ(node1.shape().element_type(), node2.shape().element_type());
   return GenericOp(OpKind(at::aten::__and__), OpList{node1, node2},
@@ -26,10 +23,7 @@ Value BitwiseOr(const Value& node1, const Value& node2) {
   auto lower_fn = [](const Node& node, LoweringContext* loctx) -> XlaOpVector {
     xla::XlaOp op0 = loctx->GetOutputOp(node.operand(0));
     xla::XlaOp op1 = loctx->GetOutputOp(node.operand(1));
-    auto kernel = [](const xla::XlaOp& op0, const xla::XlaOp& op1) {
-      return op0 | op1;
-    };
-    return node.ReturnOp(kernel(op0, op1), loctx);
+    return node.ReturnOp(op0 | op1, loctx);
   };
   XLA_CHECK_EQ(node1.shape().element_type(), node2.shape().element_type());
   return GenericOp(OpKind(at::aten::__or__), OpList{node1, node2},
@@ -40,10 +34,7 @@ Value BitwiseXor(const Value& node1, const Value& node2) {
   auto lower_fn = [](const Node& node, LoweringContext* loctx) -> XlaOpVector {
     xla::XlaOp op0 = loctx->GetOutputOp(node.operand(0));
     xla::XlaOp op1 = loctx->GetOutputOp(node.operand(1));
-    auto kernel = [](const xla::XlaOp& op0, const xla::XlaOp& op1) {
-      return op0 ^ op1;
-    };
-    return node.ReturnOp(kernel(op0, op1), loctx);
+    return node.ReturnOp(op0 ^ op1, loctx);
   };
   XLA_CHECK_EQ(node1.shape().element_type(), node2.shape().element_type());
   return GenericOp(OpKind(at::aten::__xor__), OpList{node1, node2},
