9f6a18ee3094c1bd75f1911cc73802f30fafd832
Ailing <ailzhang@users.noreply.github.com>
2019-09-05 13:17:15 -0700

    better error message for arange (#1002)
    * better error message for arange
    
    * make build happy

--- torch_xla/csrc/ops/ops.cpp
+++ torch_xla/csrc/ops/ops.cpp
@@ -349,6 +349,8 @@ NodePtr ARange(at::Scalar start, at::Scalar end, at::Scalar step,
                at::ScalarType scalar_type) {
   xla::PrimitiveType type = MakeXlaPrimitiveType(scalar_type,
                                                  /*device=*/nullptr);
+  XLA_CHECK(!std::isnan(start.toDouble()) && !std::isnan(end.toDouble()))
+      << "unsupported range: " << start.toDouble() << " -> " << end.toDouble();
   xla::Literal values;
   switch (type) {
     case xla::PrimitiveType::BF16:
