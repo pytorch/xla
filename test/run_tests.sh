7702a27a25a51711e7cbb9b021db5e97412eeae4
Mike Ruberry <mruberry@devfair044.maas>
2019-09-17 09:54:24 -0700

    runs torch xla tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -31,7 +31,9 @@ export TRIM_GRAPH_CHECK_FREQUENCY=$GRAPH_CHECK_FREQUENCY
 if [ "$LOGFILE" != "" ]; then
   python3 "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY 2>&1 | tee $LOGFILE
   python3 "$CDIR/test_mp_replication.py" "$@" 2>&1 | tee $LOGFILE
+  python3 "$CDIR/../../test/test_torch.py" "$@" -v TestTorchDeviceTypeXLA 2>&1 | tee $LOGFILE
 else
   python3 "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
   python3 "$CDIR/test_mp_replication.py" "$@"
+  python3 "$CDIR/../../test/test_torch.py" "$@" -v TestTorchDeviceTypeXLA
 fi
