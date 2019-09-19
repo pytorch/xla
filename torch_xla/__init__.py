1601673221b11c9e6dd47d8e167f835c490caaad
Daniel Sohn <jysohn@google.com>
2019-09-18 20:17:33 -0700

    Remove try-catch version import

--- torch_xla/__init__.py
+++ torch_xla/__init__.py
@@ -1,10 +1,5 @@
 import torch
+from .version import __version__
 import _XLAC
-
-try:
-  from .version import __version__
-except ImportError:
-  # Only wheel installation has torch_xla.version module
-  __version__ = ''
 
 _XLAC._initialize_aten_bindings()
