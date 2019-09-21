71fb134a839b4fcac58c75ff32856f5b48fa7d25
Davide Libenzi <dlibenzi@google.com>
2019-09-21 15:52:29 -0700

    Made XLA to() API support self being a CPU tensor. (#1079)

--- torch_xla/csrc/aten_xla_type.cpp
+++ torch_xla/csrc/aten_xla_type.cpp
@@ -866,9 +866,8 @@ AtenXlaType::convolution_backward_overrideable(
 
 at::Tensor& AtenXlaType::copy_(at::Tensor& self, const at::Tensor& src,
                                bool non_blocking) {
-  c10::optional<XLATensor> self_tensor = bridge::TryGetXlaTensor(self);
-  c10::optional<XLATensor> src_tensor = bridge::TryGetXlaTensor(src);
-
+  auto self_tensor = bridge::TryGetXlaTensor(self);
+  auto src_tensor = bridge::TryGetXlaTensor(src);
   if (!src_tensor) {
     XLA_CHECK(self_tensor);
     self_tensor->SetTensor(CopyTensor(src, self.scalar_type()));
@@ -2759,13 +2758,24 @@ at::Tensor AtenXlaType::threshold_backward(const at::Tensor& grad_output,
 at::Tensor AtenXlaType::to(const at::Tensor& self,
                            const at::TensorOptions& options,
                            bool /* non_blocking */, bool /* copy */) {
-  XLATensor self_tensor = bridge::GetXlaTensor(self);
-  if (options.has_device() && options.device().type() != at::kXLA) {
-    return bridge::XlaToAtenTensor(self_tensor, options);
+  auto self_tensor = bridge::TryGetXlaTensor(self);
+  if (!self_tensor) {
+    XLA_CHECK(options.has_device());
+    at::ScalarType dtype = options.has_dtype()
+                               ? c10::typeMetaToScalarType(options.dtype())
+                               : self.scalar_type();
+    XLATensor xtensor =
+        XLATensor::Create(CopyTensor(self, dtype),
+                          bridge::AtenDeviceToXlaDevice(options.device()));
+    return bridge::AtenFromXlaTensor(xtensor);
   }
-  XlaOptions xla_options(options, self_tensor.GetDevice(), self_tensor.dtype());
+  if (options.has_device() && options.device().type() != at::kXLA) {
+    return bridge::XlaToAtenTensor(*self_tensor, options);
+  }
+  XlaOptions xla_options(options, self_tensor->GetDevice(),
+                         self_tensor->dtype());
   return bridge::AtenFromXlaTensor(
-      XLATensor::to(self_tensor, xla_options.device, xla_options.scalar_type));
+      XLATensor::to(*self_tensor, xla_options.device, xla_options.scalar_type));
 }
 
 at::Tensor AtenXlaType::to(const at::Tensor& self, c10::Device device,
