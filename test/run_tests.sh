e9fa96ecd01f92d9082c5628b87e73d56952eabd
Mike Ruberry <mruberry@devfair044.maas>
2019-09-26 23:05:26 -0700

    adds programmatic test device support and test_indexing.py

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -28,13 +28,18 @@ shift $(($OPTIND - 1))
 export TRIM_GRAPH_SIZE=$MAX_GRAPH_SIZE
 export TRIM_GRAPH_CHECK_FREQUENCY=$GRAPH_CHECK_FREQUENCY
 export XLA_TEST_DIR=$CDIR
+export PYTORCH_TEST_WITH_SLOW=1
 
 if [ "$LOGFILE" != "" ]; then
+  python3 "$CDIR/../../test/test_torch.py" "$@" -v TestTorchDeviceTypeXLA 2>&1 | tee $LOGFILE
+  python3 "$CDIR/../../test/test_indexing.py" "$@" -v TestIndexingXLA 2>&1 | tee $LOGFILE
+  python3 "$CDIR/../../test/test_indexing.py" "$@" -v NumpyTestsXLA 2>&1 | tee $LOGFILE
   python3 "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY 2>&1 | tee $LOGFILE
   python3 "$CDIR/test_mp_replication.py" "$@" 2>&1 | tee $LOGFILE
-  python3 "$CDIR/../../test/test_torch.py" "$@" -v TestTorchDeviceTypeXLA 2>&1 | tee $LOGFILE
 else
+  python3 "$CDIR/../../test/test_torch.py" "$@" -v TestTorchDeviceTypeXLA
+  python3 "$CDIR/../../test/test_indexing.py" "$@" -v TestIndexingXLA
+  python3 "$CDIR/../../test/test_indexing.py" "$@" -v NumpyTestsXLA
   python3 "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
   python3 "$CDIR/test_mp_replication.py" "$@"
-  python3 "$CDIR/../../test/test_torch.py" "$@" -v TestTorchDeviceTypeXLA
 fi
