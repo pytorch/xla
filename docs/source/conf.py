48e612d51b53ea2c2f113718bb66cb7c2c789df4
Mike Ruberry <mruberry@devfair044.maas>
2019-09-30 07:13:41 -0700

    Adds requirements.txt, css, and docs logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import torch
 import torch_xla
-import pytorch_sphinx_theme
+import sphinx_rtd_theme
 
 
 # -- General configuration ------------------------------------------------
@@ -98,15 +98,15 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pytorch_sphinx_theme'
-html_theme_path = [pytorch_sphinx_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    'collapse_navigation': False,
+    'collapse_navigation': True,
     'display_version': True,
     'logo_only': True,
 }
