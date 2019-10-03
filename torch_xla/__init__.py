fcba3bf7ad23f59b3615e8e0b0fc41722658d10b
Daniel Sohn <jysohn@google.com>
2019-10-03 09:56:50 -0700

    Turn on gRPC keepalive
    Fixes hangs on gRPC connections that last longer than 10 minutes.

--- torch_xla/__init__.py
+++ torch_xla/__init__.py
@@ -1,3 +1,12 @@
+import os
+GRPC_OPTIONS = [
+    'grpc.keepalive_time_ms=60000',  # 1 min
+    'grpc.keepalive_timeout_ms=14400000',  # 4 hrs
+    'grpc.http2.max_pings_without_data=0',  # unlimited
+    'grpc.http2.min_ping_interval_without_data_ms=300000',  # 5 min
+]
+os.environ['TF_GRPC_DEFAULT_OPTIONS'] = ','.join(GRPC_OPTIONS)
+
 import torch
 from .version import __version__
 import _XLAC
