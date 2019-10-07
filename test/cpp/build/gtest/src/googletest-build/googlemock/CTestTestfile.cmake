2f5bff3e8a621491f07d327a63da7687976df0c7
Mike Ruberry <mruberry@devfair044.maas>
2019-10-07 15:06:09 -0700

    further simplification

--- test/cpp/build/gtest/src/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /private/home/mruberry/xla/pytorch/xla/test/cpp/build/gtest/src/googletest-src/googlemock
-# Build directory: /private/home/mruberry/xla/pytorch/xla/test/cpp/build/gtest/src/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- test/cpp/build/gtest/src/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /private/home/mruberry/xla/pytorch/xla/test/cpp/build/gtest/src/googletest-src/googletest
-# Build directory: /private/home/mruberry/xla/pytorch/xla/test/cpp/build/gtest/src/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
