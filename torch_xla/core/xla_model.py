0464c37eb59006b1e5ba541f4e2bcc30cbe15d09
Davide Libenzi <dlibenzi@google.com>
2019-10-19 11:32:06 -0700

    Let the caller handle the separate paths to be fed to the xm.save() API in case master_only is False. Cleaned up some unused imports.

--- torch_xla/core/xla_model.py
+++ torch_xla/core/xla_model.py
@@ -1,15 +1,12 @@
 from __future__ import print_function
 
 import collections
-import gc
-from six import itervalues
 import sys
 import os
 import re
 import threading
 import time
 import torch
-import torch.nn as nn
 import torch_xla
 import torch_xla.core.xla_env_vars as xenv
 import torch_xla.debug.metrics_saver as ms
@@ -481,10 +478,13 @@ def save(data, file_or_path, master_only=True):
     data: The input data to be saved. Any nested combination of Python objects
       (list, tuples, sets, dicts, ...).
     file_or_path: The destination for the data saving operation. Either a file
-      path or a Python file object.
+      path or a Python file object. If `master_only` is ``False`` the path or
+      file objects must point to different destinations as otherwise all the
+      writes from the same host will override each other.
     master_only (bool): Whether only the master device should save the data. If
-      False, the `file_or_path` argument must be a path, and the different
-      devices will save data to files with their ordinal as extension.
+      False, the `file_or_path` argument should be a different file or path for
+      each of the ordinals taking part to the replication, otherwise all the
+      replicas on the same host will be writing to the same location.
       Default: True
   """
 
@@ -498,6 +498,4 @@ def save(data, file_or_path, master_only=True):
     if is_master_ordinal():
       torch.save(cpu_data, file_or_path)
   else:
-    assert type(file_or_path) == str
-    file_or_path = '{}.{}'.format(file_or_path, get_ordinal())
     torch.save(cpu_data, file_or_path)
