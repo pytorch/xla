57694d40bb7a64e712add272bbbe4bb4180cab0f
zcain <zcain@google.com>
2019-10-22 20:03:38 +0000

    remove import from multiprocess mnist

--- test/test_train_mp_mnist.py
+++ test/test_train_mp_mnist.py
@@ -17,7 +17,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.tensorboard import SummaryWriter
 from torchvision import datasets, transforms
 import torch_xla
 import torch_xla.debug.metrics as met
