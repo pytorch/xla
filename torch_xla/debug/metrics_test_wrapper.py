caef49eeaed38b9a97ff04dd7e9a0e2cd73a5ca6
zcain <zcain@google.com>
2019-11-06 19:02:05 +0000

    updated metrics test wrapper

--- torch_xla/debug/metrics_test_wrapper.py
+++ torch_xla/debug/metrics_test_wrapper.py
@@ -9,25 +9,34 @@ python metrics_test_wrapper.py --metrics_output_filename="metrics.txt" \
 """
 import argparse
 import os
+import shutil
 import subprocess
 import sys
+import tempfile
 
 import torch_xla.utils.gcsfs as gcsfs
 
 
 _CLOUD_STORAGE_PREFIX = 'gs://'
-_TMP_FILENAME = '/tmp/pytorch_tpu_metrics_tmp'
 _XLA_METRICS_FILE = 'XLA_METRICS_FILE'
 
 
-def _run_subprocess(cmd):
+def _run_subprocess(cmd, distributed=False):
   # Use _XLA_METRICS_FILE to pass the metrics report from the subprocess
-  # to this process.
-  os.environ[_XLA_METRICS_FILE] = _TMP_FILENAME
+  # to this process. Note that distributed training jobs write 1 file per
+  # core.
+  tmp_dir = tempfile.mkdtemp()
+  _, tmp_path = tempfile.mkstemp(dir=tmp_dir)
+  file_to_read = '{}.0'.format(tmp_path) if distributed else tmp_path
+
+  # Run the PyTorch XLA script.
+  os.environ[_XLA_METRICS_FILE] = tmp_path
   sp_return_code = subprocess.call(cmd)
-  with open(_TMP_FILENAME, 'r') as tmp_metrics:
+  with open(file_to_read, 'r') as tmp_metrics:
     metrics = tmp_metrics.read()
-  os.remove(_TMP_FILENAME)
+
+  # Cleanup.
+  shutil.rmtree(tmp_dir)
   del os.environ[_XLA_METRICS_FILE]
 
   # Use only the metrics from the last step.
@@ -35,31 +44,35 @@ def _run_subprocess(cmd):
   return metrics, sp_return_code
 
 
-def _write_to_disk(output_string, output_filename,
-                   service_account_filename=None):
-  if not output_string:
+def _write_to_disk(output_string, output_filename):
+  if not output_filename:
     return
   try:
     if output_filename.find(_CLOUD_STORAGE_PREFIX) == 0:
-      gcsfs.write(output_filename, output_string,
-                  service_account_filename=service_account_filename)
+      gcsfs.write(output_filename, output_string)
     else:
       with open(output_filename, 'w') as outfile:
         outfile.write(output_string)
+    print('Succeeded writing metrics to file: {}'.format(output_filename))
   except Exception as e:
-    print('Failed to write metrics to file: {}'.format(e))
+    print('Failed writing metrics to file: {}'.format(e))
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(
-      description='PyTorch on TPU distrubuted training',
+          description='PyTorch on TPU: Verify metrics and/or save to disk.',
       epilog=('Usage example: metrics_test_wrapper.py '
           '--golden_metrics_filename="gcs://bucket/file" '
           '--metrics_output_filename="gcs://bucket/file" '
           '--service_account_filename="path/to/file" -- python train.py'))
-  parser.add_argument('--golden_metrics_filename', type=str, default=None)
-  parser.add_argument('--metrics_output_filename', type=str, default=None)
-  parser.add_argument('--service_account_filename', type=str, default=None)
+  parser.add_argument('--golden_metrics_filename', type=str, default=None,
+                      help='Read metrics from here and compare to current.')
+  parser.add_argument('--metrics_output_filename', type=str, default=None,
+                      help='If provided, write current metrics here.')
+  parser.add_argument('--service_account_filename', type=str, default=None,
+                      help='Used to authenticate to Google Cloud Storage.')
+  parser.add_argument('--distributed', action='store_true',
+                      help='Whether the subprocess is a distributed job.')
   parser.add_argument(
       'positional',
       nargs='+',
@@ -70,12 +83,14 @@ if __name__ == '__main__':
     raise ValueError('At least one of golden_metrics_filename or '
                      'metrics_output_filename is required.')
 
-  metrics, sp_return_code = _run_subprocess(FLAGS.positional)
+  metrics, sp_return_code = _run_subprocess(FLAGS.positional,
+                                            distributed=FLAGS.distributed)
 
   # Include the params for this invocation when saving metrics.
   output_string = '{}\n\n{}'.format(FLAGS, metrics)
-  _write_to_disk(output_string, FLAGS.metrics_output_filename,
-                 service_account_filename=FLAGS.service_account_filename)
+  if FLAGS.service_account_filename:
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = FLAGS.service_account_filename
+  _write_to_disk(output_string, FLAGS.metrics_output_filename)
 
   if FLAGS.golden_metrics_filename:
     # TODO(zcain): Read golden metrics file and compare to current metrics.
