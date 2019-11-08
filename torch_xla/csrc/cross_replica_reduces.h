f90f233316aff2c9243c223ed8defa4c696679f0
Davide Libenzi <dlibenzi@google.com>
2019-11-08 10:52:04 -0800

    Enable vardic AllReduce to allow XLA to adopt a deterministic scheduling across cores.

--- torch_xla/csrc/cross_replica_reduces.h
+++ torch_xla/csrc/cross_replica_reduces.h
@@ -1,13 +1,16 @@
 #pragma once
 
+#include <vector>
+
 #include "tensorflow/compiler/xla/client/xla_builder.h"
+#include "tensorflow/core/lib/gtl/array_slice.h"
 
 namespace torch_xla {
 
 // Builds a Cross Replica Sum operation on the operand, and scales the result by
 // scale.
-xla::XlaOp BuildCrossReplicaSum(
-    const xla::XlaOp& operand, double scale,
+std::vector<xla::XlaOp> BuildCrossReplicaSum(
+    tensorflow::gtl::ArraySlice<const xla::XlaOp> operands, double scale,
     const std::vector<std::vector<xla::int64>>& groups);
 
 }  // namespace torch_xla
