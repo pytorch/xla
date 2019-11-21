e3312034ece40c39a2ac943abc3e74657f9d9baf
zcain <zcain@google.com>
2019-11-21 15:35:24 +0000

    some work done, temp remove line in init

--- test/metrics_compare_utils_test.py
+++ test/metrics_compare_utils_test.py
@@ -1,8 +1,7 @@
 from __future__ import print_function
 
-import pdb; pdb.set_trace()
-import os
-os.environ['PATH'] = '/usr/share/torch-xla-nightly/pytorch/xla:' + os.environ['PATH']
+import sys; sys.path.insert(0, '/usr/share/torch-xla-nightly/pytorch/xla')
+
 
 import unittest
 
@@ -10,10 +9,11 @@ from torch_xla.debug import metrics_compare_utils as mcu
 
 class MetricsCompareUtilsTest(unittest.TestCase):
 
-  def temp_test(self):
+  #Accumulator: 07s370ms955.021us
+  def test_tempp(self):
     self.assertTrue(mcu._parse_metrics_report("""Metric: TransferFromServerTime
   TotalSamples: 1728
-  Accumulator: 07s370ms955.021us
+  Accumulator: 10GB
   ValueRate: 024ms258.635us / second
   Rate: 16.8665 / second
   Percentiles: 1%=909.826us; 5%=997.067us; 10%=001ms45.729us; 20%=001ms92.220us; 50%=001ms216.204us; 80%=002ms513.601us; 90%=002ms872.090us; 95%=002ms290.191us; 99%=006ms818.808us
--- torch_xla/debug/metrics_compare_utils.py
+++ torch_xla/debug/metrics_compare_utils.py
@@ -1,30 +1,65 @@
 """Logic to compare XLA metrics and identify significant changes."""
 
-_METRIC_NAME_AND_SAMPLES_AND_ACCUMULATOR = \
-  "Metric: (\S+)\s+TotalSamples: (\d+)\s+Accumulator: (\S+)"
-_COUNTER_NAME_AND_VALUE = "Counter: (\S+)\s+Value: (\d+)"
+import re
+
+_METRIC_REGEX = r'Metric: (\S+)\s+TotalSamples: (\d+)\s+Accumulator: (\S+)'
+_COUNTER_REGEX = r'Counter: (\S+)\s+Value: (\d+)'
+#_TIME_FIND_REGEX = r'(?P<days>\d+d)?(?P<hours>\d+h)?(?P<minutes>\d+m)?(?P<seconds>\d+s)?(?P<milliseconds>\d+ms)?(?P<microseconds>[\d.]+us)?'
+_TIME_FIND_REGEX = r'((?P<days>\d+)d)?((?P<hours>\d+)h)?((?P<minutes>\d+)m)?((?P<seconds>\d+)s)?((?P<milliseconds>\d+)ms)?((?P<microseconds>[\d.]+)us)?'
+
+def _accumulator_to_number(accumulator_string):
+  # Converts the various XLA metrics report Accumulator strings into a number
+  # using a single unit. Cases covered:
+  #  1. 01d01h01m01s01ms01.5us --> (float) seconds
+  #  2. TB/GB/MB/KB --> (float) GB
+  # if no letters in string
+  try:
+    return float(accumulator_string)
+  except ValueError as e:
+    pass
+
+  print("NOW WE PARSE")
+  import pdb; pdb.set_trace()
+
+  # Try to parse the string as a duration.
+  m = re.match(_TIME_FIND_REGEX, accumulator_string)
+  if sum([1 for v in m.groups() if v is not None]):
+    gd = m.groupdict()
+    for k, v in gd.items():
+      new_v = 0.0 if v is None else float(v)
+      gd[k] = new_v
+    total_sec = 0.0
+    total_sec += gd.get('days') * 24 * 60 * 60
+    total_sec += gd.get('hours') * 60 * 60
+    total_sec += gd.get('minutes') * 60
+    total_sec += gd.get('seconds')
+    total_sec += gd.get('milliseconds') * 0.001
+    total_sec += gd.get('microseconds') * 1e-6
+    print(accumulator_string)
+    print("TOTAL SEC:{}".format(total_sec))
+    return total_sec
+
+  # Try to parse the string as disk space.
+  print("PARSE AS DISK")
+  
 
 def _parse_metrics_report(report):
   import pdb; pdb.set_trace()
-
   data_points = {}
-  metrics_lines = metrics_data_string.splitlines()
-  for i in range(metrics_lines):
-    line = metrics_lines[i].strip()
-    if line.startswith(_METRICS_LINE_START):
-      metric_name = line.replace(_METRICS_LINE_START, '')
-      i+=1
-      # find TotalSamples line
-      # find Accumulator line
-      # convert to standard metric, e.g. KB/MB/GB/TB to bytes and d/h/m/s/ms/us to ?s?
-      pass
-    elif line.startswith(_COUNTER_LINE_START):
-      counter_name = line.replace(_COUNTER_LINE_START, '')
-      # find counter name
-      # find Value: line
-      data_points['{}_{}'.format(counter_name, 'Value')] = value
-  return data_points
 
+  metrics_matches = re.findall(_METRIC_REGEX, report)
+  for match in metrics_matches:
+    # Each tuple is of form (name, num samples, accumulator).
+    data_points['{}_{}'.format(match[0], 'TotalSamples')] = int(match[1])
+    data_points['{}_{}'.format(
+        match[0], 'Accumulator')] = _accumulator_to_number(match[2])
+  
+  counters_matches = re.findall(_COUNTER_REGEX, report)
+  for match in counters_matches:
+    # Each tuple is of the form (name, counter value).
+    data_points['{}_{}'.format(match[0], 'Value')] = int(match[1])
+
+  return data_points
 
 def get_raw_data_points(metrics_reports):
   """
@@ -41,7 +76,7 @@ def get_raw_data_points(metrics_reports):
     metrics and will look like "CreateCompileHandles_Value" for counters.
   """
   raw_data_points = collections.defaultdict(lambda: [None] * len(metrics_reports))
-  reports = [_parse_metrics_report(report) for report in metrics_reports)]
+  reports = [_parse_metrics_report(report) for report in metrics_reports]
   for report_index in range(len(metrics_reports)):
     for metric_name in report:
       raw_data_points[metric_name][report_index] = reports[report_index].get(metric_name, None)
