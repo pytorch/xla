059a3a8eadf355d9c412c4837cfa611ef941899c
zcain <zcain@google.com>
2019-11-22 00:22:12 +0000

    first working version with test

--- test/metrics_compare_utils_test.py
+++ test/metrics_compare_utils_test.py
@@ -7,19 +7,13 @@ import unittest
 
 from torch_xla.debug import metrics_compare_utils as mcu
 
-class MetricsCompareUtilsTest(unittest.TestCase):
-
-  #Accumulator: 07s370ms955.021us
-  def test_tempp(self):
-    self.assertTrue(mcu._parse_metrics_report("""Metric: TransferFromServerTime
+_REPORT_1 = """Metric: InboundData
   TotalSamples: 1728
   Accumulator: 10GB
-  ValueRate: 024ms258.635us / second
   Rate: 16.8665 / second
-  Percentiles: 1%=909.826us; 5%=997.067us; 10%=001ms45.729us; 20%=001ms92.220us; 50%=001ms216.204us; 80%=002ms513.601us; 90%=002ms872.090us; 95%=002ms290.191us; 99%=006ms818.808us
 Metric: TransferToServerTime
   TotalSamples: 2616
-  Accumulator: 01m29s615ms262.004us
+  Accumulator: 01m29s615ms
   ValueRate: 783ms426.227us / second
   Rate: 24.5054 / second
   Percentiles: 1%=003ms783.790us; 5%=004ms98.116us; 10%=010ms364.458us; 20%=015ms121.974us; 50%=026ms622.656us; 80%=035ms554.304us; 90%=082ms478.207us; 95%=108ms554.247us; 99%=129ms338.132us
@@ -28,8 +22,175 @@ Counter: CachedSyncTensors
 Counter: CreateCompileHandles
   Value: 40
 Counter: CreateDataHandles
-  Value: 407992"""))
+  Value: 407992"""
 
+_REPORT_2 = """--metric:localhost\n\n\nMetric: InboundData
+  TotalSamples: 73216
+  Accumulator: 64.75TB
+Metric: TransferToServerTime
+  TotalSamples: 247016
+  Accumulator: 04d17h11m07s495ms546.299us
+Counter: CachedSyncTensors
+  Value: 1022168
+Counter: CreateCompileHandles
+  Value: 40
+Counter: CreateDataHandles
+  Value: 576462152
+"""
+
+_REPORT_3 = """--root='gs://metric'\n\n\nMetric: InboundData
+  TotalSamples: 73216
+  Accumulator: 64.75GB
+Metric: TransferToServerTime
+  TotalSamples: 247016
+  Accumulator: 1s
+Metric: UniqueMetric
+  TotalSamples: 9000
+  Accumulator: 9000
+Counter: CachedSyncTensors
+  Value: 1022168
+Counter: CreateCompileHandles
+  Value: 40
+Counter: CreateDataHandles
+  Value: 576462152
+Counter: UniqueCounter
+  Value: 9000
+DistractingText
+"""
+
+_REPORT_3_SLIGHTLY_DIFFERENT_VALUES = """distracting text\n\n\nMetric: InboundData
+  TotalSamples: 70000
+  Accumulator: 74.75GB
+Metric: TransferToServerTime
+  TotalSamples: 247016
+  Accumulator: 1s
+Metric: UniqueMetric
+  TotalSamples: 9000
+  Accumulator: 9000
+Counter: CachedSyncTensors
+  Value: 1022168
+Counter: CreateCompileHandles
+  Value: 40
+Counter: CreateDataHandles
+  Value: 576462152
+Counter: UniqueCounter
+  Value: 9999
+DistractingText
+"""
+
+_REPORT_3_WITH_NEW_COUNTERS = """Metric: InboundData
+  TotalSamples: 73216
+  Accumulator: 64.75GB
+Metric: TransferToServerTime
+  TotalSamples: 247016
+  Accumulator: 1s
+Metric: UniqueMetric
+  TotalSamples: 9000
+  Accumulator: 9000
+Counter: CachedSyncTensors
+  Value: 1022168
+Counter: CreateCompileHandles
+  Value: 40
+Counter: CreateDataHandles
+  Value: 576462152
+Counter: UniqueCounter
+  Value: 9000
+Counter: aten::_local_scalar_dense
+  Value: 73216
+Counter: xla::mean
+  Value: 1000
+DistractingText
+"""
+
+
+class MetricsCompareUtilsTest(unittest.TestCase):
+
+  def test_get_data_points_from_metrics_reports(self):
+    correct_dict = {
+        'InboundData__Accumulator_mb': [10000.0, 64750000.0, 64750.0],
+        'InboundData__TotalSamples': [1728, 73216, 73216],
+        'TransferToServerTime__Accumulator_sec': [89.615, 407467.495546299, 1.0],
+        'TransferToServerTime__TotalSamples': [2616, 247016, 247016],
+        'CachedSyncTensors__Value': [11336, 1022168, 1022168],
+        'CreateCompileHandles__Value': [40, 40, 40],
+        'CreateDataHandles__Value': [407992, 576462152, 576462152],
+        'UniqueMetric__Accumulator': [None, None, 9000.0],
+        'UniqueMetric__TotalSamples': [None, None, 9000],
+        'UniqueCounter__Value': [None, None, 9000]
+    }
+    self.assertEqual(mcu.get_data_points_from_metrics_reports(
+        [_REPORT_1, _REPORT_2, _REPORT_3]), correct_dict)
+
+
+  def test_compare_metrics_reports_no_difference(self):
+    data_points = mcu.get_data_points_from_metrics_reports(
+        [_REPORT_3, _REPORT_3, _REPORT_3])
+    metrics_difference_report = mcu.compare_metrics(
+        data_points, _REPORT_3,
+        config={'base_tolerance': 2.0})
+
+    # The latest metrics match the previous ones exactly, so the difference
+    # report should be empty.
+    self.assertEqual(metrics_difference_report, '')
+
+
+  def test_compare_metrics_reports_value_difference_tolerance_loose(self):
+    data_points = mcu.get_data_points_from_metrics_reports(
+        [_REPORT_3, _REPORT_3, _REPORT_3_SLIGHTLY_DIFFERENT_VALUES])
+    metrics_difference_report = mcu.compare_metrics(
+        data_points, _REPORT_3_SLIGHTLY_DIFFERENT_VALUES,
+        config={'base_tolerance': 2.0})
+
+    # Since the tolerance is 2.0, the small differences in values are not
+    # big enough to trigger lines in the difference report.
+    self.assertEqual(metrics_difference_report, '')
+
+
+  def test_compare_metrics_reports_value_difference_tolerance_strict(self):
+    data_points = mcu.get_data_points_from_metrics_reports(
+        [_REPORT_3, _REPORT_3, _REPORT_3_SLIGHTLY_DIFFERENT_VALUES])
+    metrics_difference_report = mcu.compare_metrics(
+        data_points, _REPORT_3_SLIGHTLY_DIFFERENT_VALUES,
+        config={'base_tolerance': 0.0})
+
+    # Since the tolerance is 0.0, even a tiny difference leads to a line in
+    # the metrics_difference_report.
+    expected_report = 'InboundData__Accumulator_mb is outside the expected range using tolerance: 0.0. Lower limit: 68083.33333333333  Upper limit: 68083.33333333333  Actual Value: 74750.0\nInboundData__TotalSamples is outside the expected range using tolerance: 0.0. Lower limit: 72144.0  Upper limit: 72144.0  Actual Value: 70000\nUniqueCounter__Value is outside the expected range using tolerance: 0.0. Lower limit: 9333.0  Upper limit: 9333.0  Actual Value: 9999\n'
+    self.assertEqual(metrics_difference_report, expected_report)
+
+
+  def test_compare_metrics_reports_value_difference_tolerance_custom(self):
+    data_points = mcu.get_data_points_from_metrics_reports(
+        [_REPORT_3, _REPORT_3, _REPORT_3_SLIGHTLY_DIFFERENT_VALUES])
+    metrics_difference_report = mcu.compare_metrics(
+        data_points, _REPORT_3_SLIGHTLY_DIFFERENT_VALUES,
+        config={
+            'base_tolerance': 0.0,
+            'InboundData__Accumulator_mb_tolerance': 2.0,
+            'UniqueCounter__Value_tolerance': 1.5,
+        })
+
+    # 2 of 3 differing values have custom tolerances and therefore should pass.
+    # The third uses the default tolerance of 0.0, so it will generate a line.
+    expected_report = 'InboundData__TotalSamples is outside the expected range using tolerance: 0.0. Lower limit: 72144.0  Upper limit: 72144.0  Actual Value: 70000\n'
+    self.assertEqual(metrics_difference_report, expected_report)
+ 
+
+  def test_compare_metrics_reports_new_counters(self):
+    data_points = mcu.get_data_points_from_metrics_reports(
+        [_REPORT_3, _REPORT_3, _REPORT_3_SLIGHTLY_DIFFERENT_VALUES])
+    metrics_difference_report = mcu.compare_metrics(
+        data_points, _REPORT_3_WITH_NEW_COUNTERS,
+        config={'base_tolerance': 2.0})
+
+    # Since the tolerance is 2.0, the small differences in values are not
+    # big enough to trigger lines in the difference report.
+    expected_report = 'Found new aten counter: aten::_local_scalar_dense__Value: 73216\n'
+    self.assertEqual(metrics_difference_report, expected_report)
+
+
+  
+  
 if __name__ == '__main__':
   test = unittest.main()
   sys.exit(0 if test.result.wasSuccessful() else 1)
--- torch_xla/debug/metrics_compare_utils.py
+++ torch_xla/debug/metrics_compare_utils.py
@@ -1,68 +1,99 @@
 """Logic to compare XLA metrics and identify significant changes."""
 
+import collections
 import re
+
+from numpy import mean
+from numpy import std
 
 _METRIC_REGEX = r'Metric: (\S+)\s+TotalSamples: (\d+)\s+Accumulator: (\S+)'
 _COUNTER_REGEX = r'Counter: (\S+)\s+Value: (\d+)'
-#_TIME_FIND_REGEX = r'(?P<days>\d+d)?(?P<hours>\d+h)?(?P<minutes>\d+m)?(?P<seconds>\d+s)?(?P<milliseconds>\d+ms)?(?P<microseconds>[\d.]+us)?'
 _TIME_FIND_REGEX = r'((?P<days>\d+)d)?((?P<hours>\d+)h)?((?P<minutes>\d+)m)?((?P<seconds>\d+)s)?((?P<milliseconds>\d+)ms)?((?P<microseconds>[\d.]+)us)?'
+_DISK_SIZE_REGEX = r'((?P<petabytes>[\d.]+)PB)?((?P<terabytes>[\d.]+)TB)?((?P<gigabytes>[\d.]+)GB)?((?P<megabytes>[\d.]+)MB)?((?P<kilobytes>[\d.]+)KB)?((?P<bytes>[\d.]+)B)?'
 
-def _accumulator_to_number(accumulator_string):
-  # Converts the various XLA metrics report Accumulator strings into a number
-  # using a single unit. Cases covered:
-  #  1. 01d01h01m01s01ms01.5us --> (float) seconds
-  #  2. TB/GB/MB/KB --> (float) GB
-  # if no letters in string
-  try:
-    return float(accumulator_string)
-  except ValueError as e:
-    pass
 
-  print("NOW WE PARSE")
-  import pdb; pdb.set_trace()
-
-  # Try to parse the string as a duration.
-  m = re.match(_TIME_FIND_REGEX, accumulator_string)
-  if sum([1 for v in m.groups() if v is not None]):
+def _regex_matches_groupdict(regex, target_str):
+  m = re.match(regex, target_str, re.IGNORECASE)
+  if any(m.groups()):  # Regex results will be str or None.
     gd = m.groupdict()
     for k, v in gd.items():
       new_v = 0.0 if v is None else float(v)
       gd[k] = new_v
+    return gd
+  return None
+
+
+def _accumulator_to_number(accumulator_string):
+  # Convert XLA metrics report Accumulator strings into a number using
+  # a standardized unit. Returns the value as float and unit as string.
+  #
+  # Cases covered:
+  #  1. No units
+  #  2. 01d01h01m01s01ms01.5us --> (float) seconds
+  #  3. PB/TB/GB/MB/KB/B --> (float) MB
+
+  # Try to parse the string as a number (no units).
+  try:
+    return float(accumulator_string), ''
+  except ValueError as e:
+    pass
+
+  # Try to parse the string as a duration.
+  time_gd = _regex_matches_groupdict(_TIME_FIND_REGEX, accumulator_string)
+  if time_gd:
     total_sec = 0.0
-    total_sec += gd.get('days') * 24 * 60 * 60
-    total_sec += gd.get('hours') * 60 * 60
-    total_sec += gd.get('minutes') * 60
-    total_sec += gd.get('seconds')
-    total_sec += gd.get('milliseconds') * 0.001
-    total_sec += gd.get('microseconds') * 1e-6
-    print(accumulator_string)
-    print("TOTAL SEC:{}".format(total_sec))
-    return total_sec
+    total_sec += time_gd.get('days') * 24 * 60 * 60
+    total_sec += time_gd.get('hours') * 60 * 60
+    total_sec += time_gd.get('minutes') * 60
+    total_sec += time_gd.get('seconds')
+    total_sec += time_gd.get('milliseconds') * 1e-3
+    total_sec += time_gd.get('microseconds') * 1e-6
+    return total_sec, 'sec'
 
   # Try to parse the string as disk space.
-  print("PARSE AS DISK")
+  disk_gd = _regex_matches_groupdict(_DISK_SIZE_REGEX, accumulator_string)
+  if disk_gd:
+    total_mb = 0.0
+    total_mb += disk_gd.get('petabytes') * 1e9
+    total_mb += disk_gd.get('terabytes') * 1e6
+    total_mb += disk_gd.get('gigabytes') * 1e3
+    total_mb += disk_gd.get('megabytes')
+    total_mb += disk_gd.get('kilobytes') * 1e-3
+    total_mb += disk_gd.get('bytes') * 1e-6
+    return total_mb, 'mb'
+
+  raise ValueError('Unknown accumulator_string format: {}'.format(
+      accumulator_string))
   
 
 def _parse_metrics_report(report):
-  import pdb; pdb.set_trace()
+  # Convert a string metrics report to a dict of parsed values.
   data_points = {}
 
   metrics_matches = re.findall(_METRIC_REGEX, report)
+  # Each match tuple is of form (name, num samples, accumulator string).
   for match in metrics_matches:
-    # Each tuple is of form (name, num samples, accumulator).
-    data_points['{}_{}'.format(match[0], 'TotalSamples')] = int(match[1])
-    data_points['{}_{}'.format(
-        match[0], 'Accumulator')] = _accumulator_to_number(match[2])
+    parsed_value, units = _accumulator_to_number(match[2])
+    
+    # Dict keys will be of form 'MetricName_Accumulator_unit' or
+    # 'MetricName_Accumulator' if units are not given. For TotalSamples,
+    # the units are ommitted since this is just a count of samples.
+    accumulator_key = '{}__{}{}{}'.format(
+        match[0], 'Accumulator', '_' if units else '', units)
+    samples_key = '{}__{}'.format(match[0], 'TotalSamples')
+    data_points[accumulator_key] = parsed_value
+    data_points[samples_key] = int(match[1])
   
   counters_matches = re.findall(_COUNTER_REGEX, report)
+  # Each match tuple is of the form (name, counter value).
   for match in counters_matches:
-    # Each tuple is of the form (name, counter value).
-    data_points['{}_{}'.format(match[0], 'Value')] = int(match[1])
+    data_points['{}__{}'.format(match[0], 'Value')] = int(match[1])
 
   return data_points
 
-def get_raw_data_points(metrics_reports):
-  """
+
+def get_data_points_from_metrics_reports(metrics_reports):
+  """Collect numeric datapoints from a list of metrics reports.
 
   Args:
     metrics_reports(list of strings): List of strings from calls to
@@ -71,35 +102,81 @@ def get_raw_data_points(metrics_reports):
   Returns:
     dict of metric name to list of values, one value from each report in
     metrics_reports. Order of values for each metric in the output dict
-    will match the order of metrics reports in the input. Output dict keys
-    will look like "CompileTime_TotalSamples" and "CompileTime_Accumulator" for
-    metrics and will look like "CreateCompileHandles_Value" for counters.
+    will match the order of metrics reports in the input.
+    
+    For example, if the args were [report1, report2, report3], output might be:
+    {
+      'CompileTime_Accumulator_sec': [50, None, 80],  # missing from report2
+      'InboundData_TotalSamples': [11, 12, 11],
+      'CreateCompileHandles_Value': [20, 20, 19],
+      ...
+    }
   """
-  raw_data_points = collections.defaultdict(lambda: [None] * len(metrics_reports))
-  reports = [_parse_metrics_report(report) for report in metrics_reports]
+  data_points = collections.defaultdict(lambda: [None] * len(metrics_reports))
   for report_index in range(len(metrics_reports)):
-    for metric_name in report:
-      raw_data_points[metric_name][report_index] = reports[report_index].get(metric_name, None)
-  return raw_data_points
-      
+    parsed_report = _parse_metrics_report(metrics_reports[report_index])
+    for metric_name in parsed_report:
+      data_points[metric_name][report_index] = parsed_report.get(
+          metric_name, None)
+  return data_points
 
 
-def compare_metrics(raw_data_points, metrics_report,
-                    config={'base_tolerance': 2}):
-  """
+def _compute_aggregates(data_points):
+  # Compute mean and standard deviation for each key in data_points.
+  aggregates = {}
+  for k, v_list in data_points.items():
+    v_list = [v for v in v_list if v is not None]
+    if v_list:
+      aggregates[k] = {'mean': mean(v_list), 'stddev': std(v_list)}
+  return aggregates
+
+
+def compare_metrics(data_points, metrics_report,
+                    config={'base_tolerance': 2.0}):
+  """Compare metrics_report to historical averages and report differences.
 
   Args:
-    raw_data_points(dict): Output of get_raw_data_points().
+    data_points(dict): Output of get_data_points_from_metrics_reports().
     metrics_report(string): Output of torch_xla metrics_report(). These will
       be compared with the aggregates of raw_data_points.
-    config_dict(dict): Configuration for metrics comparison. Tolerances are
+    config(dict): Configuration for metrics comparison. Tolerances are
       expressed as the number of standard deviations away from the mean.
       You can supply a per-metric tolerance for any aggregated metric name,
-      otherwise base_tolerance will be used.
+      otherwise base_tolerance will be used. Example config:
+      {
+        'base_tolerance': 2.0,
+        'TransferFromServerTime_Accumulator_sec_tolerance': 10.0,
+        ...
+      }
 
   Returns:
     Metrics difference report (string). For any metric that differed between
     metrics_report and the aggregates of raw_data_points, this report will have
     1 line reporting the difference.
   """
-  pass
+  parsed_report = _parse_metrics_report(metrics_report)
+  means_and_stddevs = _compute_aggregates(data_points)
+
+  difference_report = ''
+  for k, v in parsed_report.items():
+    if k not in means_and_stddevs:
+      # Alert if we have new ops falling back to CPU.
+      if k.startswith('aten::'):
+        difference_report += 'Found new aten counter: {}: {}\n'.format(k, v)
+    else:
+      v_mean = means_and_stddevs[k]['mean']
+      v_stddev = means_and_stddevs[k]['stddev']
+
+      # base_tolerance can be overridden for individual metrics.
+      mul = config.get('{}_tolerance'.format(k), None)
+      if mul is None:
+        mul = config['base_tolerance']
+
+      lower_limit = v_mean - (v_stddev * mul)
+      upper_limit = v_mean + (v_stddev * mul)
+      if v < lower_limit or v > upper_limit:
+        difference_report += ('{} is outside the expected range using '
+            'tolerance: {}. Lower limit: {}  Upper limit: {}  '
+            'Actual Value: {}\n'.format(
+                k, mul, lower_limit, upper_limit, v))
+  return difference_report
