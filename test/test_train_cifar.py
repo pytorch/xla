ac5bed60b0e7c8186ea50ac0074f54912c8ce43c
zcain117 <zcain@google.com>
2019-12-02 12:56:07 -0800

    Test max accuracy instead of final accuracy (#1438)
    * assert against max accuracy instead of final accuracy
    
    * save max_accuracy instead of all accuracies
    
    * remove print for new best acc

--- test/test_train_cifar.py
+++ test/test_train_cifar.py
@@ -221,11 +221,13 @@ def train_cifar():
       xm.xla_replication_devices(devices)) if len(devices) > 1 else 1
   num_training_steps_per_epoch = train_dataset_len // (
       FLAGS.batch_size * num_devices)
+  max_accuracy = 0.0
   for epoch in range(1, FLAGS.num_epochs + 1):
     model_parallel(train_loop_fn, train_loader)
     accuracies = model_parallel(test_loop_fn, test_loader)
     accuracy = mean(accuracies)
-    print("Epoch: {}, Mean Accuracy: {:.2f}%".format(epoch, accuracy))
+    max_accuracy = max(accuracy, max_accuracy)
+    print('Epoch: {}, Mean Accuracy: {:.2f}%'.format(epoch, accuracy))
     global_step = (epoch - 1) * num_training_steps_per_epoch
     test_utils.add_scalar_to_summary(writer, 'Accuracy/test', accuracy,
                                      global_step)
@@ -233,7 +235,8 @@ def train_cifar():
       print(met.metrics_report())
 
   test_utils.close_summary_writer(writer)
-  return accuracy
+  print('Max Accuracy: {:.2f}%'.format(accuracy))
+  return max_accuracy
 
 
 class TrainCIFAR10(TestCase):
