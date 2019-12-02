66e1e37e5da05aa665badfa6dd4ee7b922ac17c5
Davide Libenzi <dlibenzi@google.com>
2019-12-02 10:37:51 -0800

    Do not add extra .0 if running single core. (#1437)

--- torch_xla/debug/metrics_saver.py
+++ torch_xla/debug/metrics_saver.py
@@ -5,7 +5,6 @@ import threading
 import torch_xla
 import torch_xla.debug.metrics as met
 
-_STEP_METRICS_FILE = None
 _STEP_METRICS_FILE_LOCK = threading.Lock()
 
 _TLS = threading.local()
@@ -22,17 +21,18 @@ def _extract_metrics_file():
   import torch_xla.core.xla_model as xm
   metrics_file = os.environ.get('XLA_METRICS_FILE', None)
   if metrics_file is not None:
-    ordinal = xm.get_ordinal(defval=-1)
-    if ordinal >= 0:
+    ordinal = xm.get_local_ordinal(defval=-1)
+    if ordinal >= 0 and xm.xrt_world_size() > 1:
       metrics_file = '{}.{}'.format(metrics_file, ordinal)
   return metrics_file
 
 
 def _get_metrics_file():
-  global _STEP_METRICS_FILE
-  if _STEP_METRICS_FILE is None:
-    _STEP_METRICS_FILE = _extract_metrics_file()
-  return _STEP_METRICS_FILE
+  metrics_file = getattr(_TLS, 'metrics_file', '')
+  if metrics_file == '':
+    metrics_file = _extract_metrics_file()
+    _TLS.metrics_file = metrics_file
+  return metrics_file
 
 
 def save_metrics(metrics_file=None):
