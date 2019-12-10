cf3adeb7fd1f17de3546fe86e8746107fa2a2e9d
Davide Libenzi <dlibenzi@google.com>
2019-12-09 23:25:01 -0800

    Enforce a layout for AllReduce and use Current instead of Defau… (#1468)

--- torch_xla/csrc/aten_xla_bridge.h
+++ torch_xla/csrc/aten_xla_bridge.h
@@ -57,6 +57,10 @@ std::string ToXlaString(const c10::Device& device);
 
 c10::Device AtenDefaultDevice();
 
+c10::Device SetCurrentDevice(const c10::Device& device);
+
+c10::Device GetCurrentAtenDevice();
+
 at::Tensor XlaToAtenTensor(XLATensor xla_tensor,
                            const at::TensorOptions& tensor_options);
 
