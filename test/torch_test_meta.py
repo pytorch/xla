1eb1bc61a7c071cf0e0adeff92c32af164b3e5ff
Mike Ruberry <38511765+mruberry@users.noreply.github.com>
2019-12-15 13:24:08 -0800

    Updates test_indexing.py

--- test/torch_test_meta.py
+++ test/torch_test_meta.py
@@ -185,6 +185,9 @@ disabled_torch_tests = {
     'test_boolean_shape_mismatch',  # runtime error
     'test_boolean_indexing_weirdness',  # runtime error
     'test_boolean_indexing_weirdness_tensors',  # runtime error
+    'test_zero_dim_index', # runtime error
+    'test_index_setitem_bools_slices', # runtime error
+    'test_index_scalar_with_bool_mask', # runtime error
 
     # test_nn.py
     # TestNNDeviceType
