a3aad1bafe71bdca2f9e257a1a0db6410b53e976
Davide Libenzi <dlibenzi@google.com>
2019-12-19 16:55:27 -0800

    Refine the logic of assigning the output dynamic dimension of a reshape.

--- torch_xla/csrc/layout_manager.h
+++ torch_xla/csrc/layout_manager.h
@@ -7,7 +7,8 @@
 
 namespace torch_xla {
 
-// Creates a minor-to-major layout from given dimensions.
+// Creates a minor-to-major layout from given dimensions. The dynamic_dimensions
+// slice should be either empty, or of the same size as dimensions.
 xla::Shape MakeTorchTensorLayout(
     tensorflow::gtl::ArraySlice<const xla::int64> dimensions,
     tensorflow::gtl::ArraySlice<const bool> dynamic_dimensions,
