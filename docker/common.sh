6540ef36a7f45a58dbe6cabe5938c941dcccf99f
Davide Libenzi <dlibenzi@google.com>
2020-01-20 07:03:04 -0800

    Fix last place referencing clang-7.

--- docker/common.sh
+++ docker/common.sh
@@ -3,7 +3,7 @@
 function run_deployment_tests() {
   export XRT_DEVICE_MAP="CPU:0;/job:localservice/replica:0/task:0/device:XLA_CPU:0"
   export XRT_WORKERS="localservice:0;grpc://localhost:40934"
-  export CC=clang-7 CXX=clang++-7
+  export CC=clang-8 CXX=clang++-8
 
   time python /pytorch/xla/test/test_train_mnist.py
   time bash /pytorch/xla/test/run_tests.sh
