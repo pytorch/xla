63423da4a69230d9c62ab19db528a6e25da432de
zcain <zcain@google.com>
2020-02-08 01:19:44 +0000

    Write an initial event to Tensorboard when tests start up. This allows us to determine the wall time of a test from the Tensorboard summary alone.

--- torch_xla/test/test_utils.py
+++ torch_xla/test/test_utils.py
@@ -68,7 +68,9 @@ def get_summary_writer(logdir):
   """
   if logdir:
     from tensorboardX import SummaryWriter
-    return SummaryWriter(log_dir=logdir)
+    writer = SummaryWriter(log_dir=logdir)
+    write_to_summary(writer, 0, dict_to_write={'StartupTime': 0})
+    return writer
 
 
 def print_training_update(device, step_num, loss, rate, global_rate):
