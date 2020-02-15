8b569a1293dbfa2b57c93c15b518fdb3833f0f80
Davide Libenzi <dlibenzi@google.com>
2020-02-14 17:06:27 -0800

    Fix bitwise ops shape inference.

--- torch_xla/csrc/ops/bitwise_ir_ops.cpp
+++ torch_xla/csrc/ops/bitwise_ir_ops.cpp
@@ -2,6 +2,7 @@
 
 #include "torch_xla/csrc/helpers.h"
 #include "torch_xla/csrc/lowering_context.h"
+#include "torch_xla/csrc/ops/infer_output_shape.h"
 #include "torch_xla/csrc/ops/ops.h"
 
 namespace torch_xla {
@@ -14,9 +15,16 @@ Value BitwiseAnd(const Value& node1, const Value& node2) {
     xla::XlaOp op1 = loctx->GetOutputOp(node.operand(1));
     return node.ReturnOp(op0 & op1, loctx);
   };
+  auto shape_fn = [&](absl::Span<const xla::XlaOp> operands) -> xla::XlaOp {
+    return operands[0] & operands[1];
+  };
   XLA_CHECK_EQ(node1.shape().element_type(), node2.shape().element_type());
-  return GenericOp(OpKind(at::aten::__and__), OpList{node1, node2},
-                   node1.shape(), std::move(lower_fn));
+  return GenericOp(
+      OpKind(at::aten::__and__), OpList{node1, node2},
+      [&]() {
+        return InferOutputShape({node1.shape(), node2.shape()}, shape_fn);
+      },
+      std::move(lower_fn));
 }
 
 Value BitwiseOr(const Value& node1, const Value& node2) {
@@ -25,9 +33,16 @@ Value BitwiseOr(const Value& node1, const Value& node2) {
     xla::XlaOp op1 = loctx->GetOutputOp(node.operand(1));
     return node.ReturnOp(op0 | op1, loctx);
   };
+  auto shape_fn = [&](absl::Span<const xla::XlaOp> operands) -> xla::XlaOp {
+    return operands[0] | operands[1];
+  };
   XLA_CHECK_EQ(node1.shape().element_type(), node2.shape().element_type());
-  return GenericOp(OpKind(at::aten::__or__), OpList{node1, node2},
-                   node1.shape(), std::move(lower_fn));
+  return GenericOp(
+      OpKind(at::aten::__or__), OpList{node1, node2},
+      [&]() {
+        return InferOutputShape({node1.shape(), node2.shape()}, shape_fn);
+      },
+      std::move(lower_fn));
 }
 
 Value BitwiseXor(const Value& node1, const Value& node2) {
@@ -36,9 +51,16 @@ Value BitwiseXor(const Value& node1, const Value& node2) {
     xla::XlaOp op1 = loctx->GetOutputOp(node.operand(1));
     return node.ReturnOp(op0 ^ op1, loctx);
   };
+  auto shape_fn = [&](absl::Span<const xla::XlaOp> operands) -> xla::XlaOp {
+    return operands[0] ^ operands[1];
+  };
   XLA_CHECK_EQ(node1.shape().element_type(), node2.shape().element_type());
-  return GenericOp(OpKind(at::aten::__xor__), OpList{node1, node2},
-                   node1.shape(), std::move(lower_fn));
+  return GenericOp(
+      OpKind(at::aten::__xor__), OpList{node1, node2},
+      [&]() {
+        return InferOutputShape({node1.shape(), node2.shape()}, shape_fn);
+      },
+      std::move(lower_fn));
 }
 
 }  // namespace ops
