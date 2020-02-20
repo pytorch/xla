d4671d250b27994b324eaf7b23f8ce29bff723d4
Davide Libenzi <dlibenzi@google.com>
2020-02-20 14:36:07 -0800

    Have the mesh size come from configuration to allow rendezvous working with MT parallelism as well. Move the API to xla_model, to allow use from there.

--- test/test_mp_rendezvous.py
+++ test/test_mp_rendezvous.py
@@ -7,7 +7,7 @@ import torch_xla.distributed.xla_multiprocessing as xmp
 def _mp_fn(index):
   ordinal = xm.get_ordinal()
   print('Core {} waiting for rendezvous ...'.format(ordinal))
-  data = xmp.rendezvous('rendezvous_test', 'ORD={}'.format(ordinal))
+  data = xm.rendezvous('rendezvous_test', 'ORD={}'.format(ordinal))
   print('Core {} got rendezvous!'.format(ordinal))
   for i in range(0, len(data)):
     m = re.match(r'ORD=(\d+)', data[i])
