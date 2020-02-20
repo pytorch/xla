2489b5c0b39322659e48dca837e92380b583693e
Jin Young Sohn <jysohn@google.com>
2020-02-20 19:10:31 +0000

    Fix nightly wheel update script

--- scripts/update_torch_wheels.sh
+++ scripts/update_torch_wheels.sh
@@ -5,29 +5,19 @@ set -x
 DIST_BUCKET="gs://tpu-pytorch/wheels"
 TORCH_WHEEL="torch-nightly-cp36-cp36m-linux_x86_64.whl"
 TORCH_XLA_WHEEL="torch_xla-nightly-cp36-cp36m-linux_x86_64.whl"
+TORCHVISION_WHEEL="torch_xla-nightly-cp36-cp36m-linux_x86_64.whl"
 
 [[ ! -z "$1" ]] && conda activate $1
 
 function update_wheels() {
   gsutil cp "$DIST_BUCKET/$TORCH_WHEEL" /tmp/
   gsutil cp "$DIST_BUCKET/$TORCH_XLA_WHEEL" /tmp/
+  gsutil cp "$DIST_BUCKET/$TORCHVISION_WHEEL" /tmp/
   pip install "/tmp/$TORCH_WHEEL"
   pip install "/tmp/$TORCH_XLA_WHEEL"
+  pip install "/tmp/$TORCHVISION_WHEEL"
 
-  rm -f "/tmp/$TORCH_WHEEL" "/tmp/$TORCH_XLA_WHEEL"
-}
-
-function update_torchvision() {
-  # Need to update torchvision too
-  # https://github.com/pytorch/vision/issues/967
-  torchvision_version=$(pip freeze | grep torchvision | awk -F "==" '{print $2}')
-  cd /tmp && \
-    git clone -b "v${torchvision_version}" https://github.com/pytorch/vision.git && \
-    cd vision && \
-    python setup.py install && \
-    cd .. && \
-    rm -rf /tmp/vision
+  rm -f "/tmp/$TORCH_WHEEL" "/tmp/$TORCH_XLA_WHEEL" "/tmp/$TORCHVISION_WHEEL"
 }
 
 update_wheels
-update_torchvision
