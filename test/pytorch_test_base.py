6521cd2ea0b67d7a99a7ffd2efad5bbca753a1a3
JackCaoG <59073027+JackCaoG@users.noreply.github.com>
2020-04-10 10:37:10 -0700

    Disable test_index_fill_xla (#1902)

--- test/pytorch_test_base.py
+++ test/pytorch_test_base.py
@@ -143,8 +143,9 @@ DISABLED_TORCH_TESTS_ANY = {
     'test_randn_xla_float64',  # xla doesn't support manual_seed, as_stride
     'test_rand_xla_float32',  # xla doesn't support manual_seed, as_stride
     'test_rand_xla_float64',  # xla doesn't support manual_seed, as_stride
-    'test_normal_xla_float64',  # AssertionError: 0.22364577306378963 not less than or equal to 0.2
+    'test_normal',  # AssertionError: 0.22364577306378963 not less than or equal to 0.2
     'test_uniform_from_to',  # Checks for error strings.
+    'test_index_fill_xla', # half support
 
     # TestViewOps
     'test_contiguous_nonview',
