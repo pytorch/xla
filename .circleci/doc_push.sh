c17e4954b0f145a2e78993e3f7ddb73ecd71c0b4
Ailing Zhang <ailzhang@fb.com>
2020-04-13 15:45:00 -0700

    Try creating master subfolder.

--- .circleci/doc_push.sh
+++ .circleci/doc_push.sh
@@ -16,18 +16,26 @@ git config --global user.email "torchxla@gmail.com"
 git config --global user.name "torchxlabot"
 GH_PAGES_BRANCH=gh-pages
 GH_PAGES_DIR=gh-pages-tmp
+BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+if [[ "$BRANCH_NAME" == release/* ]]; then
+  SUBDIR_NAME=$BRANCH_NAME
+else
+  SUBDIR_NAME="master"
+fi
 pushd /tmp
 git clone --quiet -b "$GH_PAGES_BRANCH" https://github.com/pytorch/xla.git "$GH_PAGES_DIR"
 pushd $GH_PAGES_DIR
-cp -fR /tmp/pytorch/xla/docs/build/* .
+rm -rf $SUBDIR_NAME
+mkdir -p $SUBDIR_NAME
+cp -fR /tmp/pytorch/xla/docs/build/* $SUBDIR_NAME
 git_status=$(git status --porcelain)
 if [[ $git_status ]]; then
   echo "Doc is updated... Pushing to public"
   echo "${git_status}"
   sudo apt-get -qq update
-  sudo apt-get -qq install expect 
+  sudo apt-get -qq install expect
   git add .
-  
+
   CURRENT_COMMIT=`git rev-parse HEAD`
   COMMIT_MSG="Update doc from commit $CURRENT_COMMIT"
   git commit -m "$COMMIT_MSG"
