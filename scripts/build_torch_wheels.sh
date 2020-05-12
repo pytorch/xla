3b1bee5c690fbffca809eb10eef44e0f91a491f3
Davide Libenzi <dlibenzi@google.com>
2020-05-12 08:52:50 -0700

    Remove cuda/ folder after CUDNN install.

--- scripts/build_torch_wheels.sh
+++ scripts/build_torch_wheels.sh
@@ -49,6 +49,7 @@ function install_cudnn {
   sudo cp cuda/include/cudnn.h /usr/local/cuda/include
   sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
   sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
+  rm -rf cuda
   rm -f "$CUDNN_FILE"
 }
 
