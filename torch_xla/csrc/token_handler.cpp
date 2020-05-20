1e2a17a323be2d5b2423680f495dd3272e46273f
Davide Libenzi <dlibenzi@google.com>
2020-05-19 18:48:18 -0700

    Use gather to slice one element.

--- torch_xla/csrc/token_handler.cpp
+++ torch_xla/csrc/token_handler.cpp
@@ -1,5 +1,7 @@
 #include "torch_xla/csrc/token_handler.h"
 
+#include "tensorflow/compiler/xla/client/lib/constants.h"
+#include "tensorflow/compiler/xla/shape_util.h"
 #include "torch_xla/csrc/convert_ops.h"
 #include "torch_xla/csrc/helpers.h"
 
@@ -8,8 +10,20 @@ namespace {
 
 xla::XlaOp SliceOneToken(xla::XlaOp input) {
   const xla::Shape& input_shape = XlaHelpers::ShapeOfXlaOp(input);
-  for (xla::int64 dim = 0; dim < input_shape.rank(); ++dim) {
-    input = xla::SliceInDim(input, 0, 1, 1, dim);
+  xla::int64 input_rank = input_shape.rank();
+  if (input_rank > 0) {
+    xla::GatherDimensionNumbers dim_numbers;
+    for (xla::int64 i = 0; i < input_rank; ++i) {
+      dim_numbers.add_collapsed_slice_dims(i);
+      dim_numbers.add_start_index_map(i);
+    }
+    dim_numbers.set_index_vector_dim(0);
+
+    std::vector<xla::int64> slice_sizes(input_rank, 1);
+    xla::XlaOp indices = xla::Zeros(
+        input.builder(),
+        xla::ShapeUtil::MakeShape(xla::PrimitiveType::S32, {input_rank}));
+    input = xla::Gather(input, indices, dim_numbers, slice_sizes);
   }
   return input;
 }
