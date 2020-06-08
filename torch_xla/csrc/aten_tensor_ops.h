7a33d58767c0f4234c6312bf346c6e7f86968cc0
Ailing <ailzhang@users.noreply.github.com>
2020-06-08 08:00:12 -0700

    Lower group_norm and its backward. (#2150)

---
+++ torch_xla/csrc/aten_tensor_ops.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <ATen/Context.h>
+#include <ATen/native/BinaryOps.h>
+
+namespace aten_tensor_ops {
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor> native_group_norm(
+    const at::Tensor& input, const at::Tensor& weight, const at::Tensor& bias,
+    int64_t N, int64_t C, int64_t HxW, int64_t group, double eps);
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor> native_group_norm_backward(
+    const at::Tensor& grad_out, const at::Tensor& input, const at::Tensor& mean,
+    const at::Tensor& rstd, const at::Tensor& weight, int64_t N, int64_t C,
+    int64_t HxW, int64_t group, std::array<bool, 3> output_mask);
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor> native_layer_norm(
+    const at::Tensor& input, const at::Tensor& weight, const at::Tensor& bias,
+    int64_t M, int64_t N, double eps);
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor> native_layer_norm_backward(
+    const at::Tensor& grad_out, const at::Tensor& input, const at::Tensor& mean,
+    const at::Tensor& rstd, const at::Tensor& weight, int64_t M, int64_t N,
+    std::array<bool, 3> output_mask);
+
+}  // namespace aten_tensor_ops
