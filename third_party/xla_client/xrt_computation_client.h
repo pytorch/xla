84ae4455b721148b8af2951ffd35cf71fd830557
Davide Libenzi <dlibenzi@google.com>
2020-06-12 15:05:46 -0700

    Fix CI GPU failures with CUDA error 4.

--- third_party/xla_client/xrt_computation_client.h
+++ third_party/xla_client/xrt_computation_client.h
@@ -190,6 +190,8 @@ class XrtComputationClient : public ComputationClient {
 
   std::map<std::string, Metric> GetMetrics() const override;
 
+  void PrepareToExit() override;
+
   static Worker ParseWorker(const std::string& worker);
 
   static std::string GetMultiProcessingDevice();
