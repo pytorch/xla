b3d1347531f7dcfe5a95e440656ba2ef83fe034f
Taylan Bilal <taylanbil@google.com>
2020-06-24 18:32:42 +0000

    Applied suggestion and added descrioption to docstring.

--- torch_xla/core/xla_model.py
+++ torch_xla/core/xla_model.py
@@ -423,30 +423,29 @@ def all_gather(value, dim=0, groups=None):
     value (torch.Tensor): The input tensor.
     dim (int): The gather dimension.
       Default: 0
+    groups (list, optional): A list of list, representing the replica groups for
+      the `all_gather()` operation. Example: `[[0, 1, 2, 3], [4, 5, 6, 7]]`
+        defines two groups, one with the `[0, 1, 2, 3]` replicas and one with
+        the `[4, 5, 6, 7]` replicas. If `None` there will be only one group with
+        all the replicas in it.
   Returns:
     A tensor which has, in the ``dim`` dimension, all the values from the
     participating replicas.
   """
-
-  def get_pad_indices(groups):
-    ordinal = get_ordinal()
-    if groups is None:
-      return ordinal, xrt_world_size() - 1 - ordinal
-    for group in groups:
-      try:
-        i = group.index(ordinal)
-        return i, len(group) - 1 - i
-      except ValueError:
-        continue
-    else:
-      raise ValueError('Variable `groups` does not cover the world_size')
-
   if dim < 0:
     dim = value.dim() + dim
   size = value.size(dim)
   padding = [0] * (2 * value.dim())
+  ordinal = get_ordinal()
+  if groups is None:
+    left, right = ordinal, xrt_world_size() - 1 - ordinal
+  else:
+    ordinals = dict()
+    for g in groups:
+      for i, x in enumerate(g):
+        ordinals[x] = (i, len(g) - 1 - i)
+    left, right = ordinals[ordinal]
   idx = value.dim() - 1 - dim
-  left, right = get_pad_indices(groups)
   padding[2 * idx] = left * size
   padding[2 * idx + 1] = right * size
   return all_reduce(REDUCE_SUM, F.pad(value, padding), groups=groups)
