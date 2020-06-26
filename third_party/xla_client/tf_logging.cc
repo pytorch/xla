0342fc5cd1973b9fb1c1d244a2ccd4742853da6b
Davide Libenzi <dlibenzi@google.com>
2020-06-26 16:55:46 -0700

    Allow cached dataset to stand-alone.

--- third_party/xla_client/tf_logging.cc
+++ third_party/xla_client/tf_logging.cc
@@ -8,7 +8,7 @@ namespace internal {
 void ErrorGenerator::operator&(const std::basic_ostream<char>& oss) const {
   const ErrorSink& sink = dynamic_cast<const ErrorSink&>(oss);
   auto sink_str = sink.str();
-  TF_LOG(ERROR) << sink_str;
+  TF_VLOG(1) << sink_str;
   std::stringstream ess;
   ess << file_ << ":" << line_ << " : " << sink_str;
   // We cannot use AT_ERROR() here, due to layering issues.
