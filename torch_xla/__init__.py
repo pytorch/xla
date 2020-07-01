d7e5dae4a446cec7d2a819d4e2176c7745d81b21
Jin Young Sohn <jysohn@google.com>
2020-07-01 21:02:02 +0000

    Auto select TPU version on Colab/Kaggle for r1.6

--- torch_xla/__init__.py
+++ torch_xla/__init__.py
@@ -1,5 +1,53 @@
+import logging
 import os
 import re
+import socket
+import time
+
+from .version import __version__ as version
+
+
+def _maybe_select_tpu_version():
+  # Setup correct TPU runtime version for Colab and Kaggle.
+
+  def _is_open(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if s.connect_ex((ip, int(port))) == 0:
+      return True
+    return False
+
+  def _wait_for_open(version, timeout=100, interval=10, log=True):
+    tpu_addr = os.environ['TPU_NAME'].split('grpc://')[1]
+    deadline = time.time() + timeout
+
+    while not _is_open(*tpu_addr.split(':')):
+      if log:
+        logging.warning(
+            f'Waiting for TPU to be start up with version pytorch-{version}...')
+      if time.time() > deadline:
+        raise RuntimeError('Timed out waiting for TPU to start up')
+      time.sleep(interval)
+
+    if log:
+      logging.warning(
+          f'TPU has started up successfully with version pytorch-{version}')
+
+  try:
+    import cloud_tpu_client
+    client = cloud_tpu_client.Client()
+    client.configure_tpu_version(f'pytorch-{version}', restart_type='ifNeeded')
+    # client.wait_for_healthy() API doesn't work as we dont have TPU API access
+    _wait_for_open(version)
+  except ImportError:
+    logging.warning((
+        'Not selecting corresponding TPU runtime since cloud_tpu_client is not '
+        'installed. Ignore if not running on Colab/Kaggle TPU.'))
+  except ValueError:
+    pass  # Not on Colab/Kaggle TPU runtime
+  except Exception:
+    # This path is hit, when we get throttled by the verison changer
+    # when we import torch_xla from xmp.spawn-ed processes.
+    _wait_for_open(version, log=False)
 
 
 def _setup_grpc():
@@ -42,6 +90,7 @@ def _setup_default_env():
 
 
 # These needs to be called before the _XLAC module is loaded.
+_maybe_select_tpu_version()
 _setup_default_env()
 _setup_grpc()
 _setup_xla_flags()
@@ -49,7 +98,6 @@ _setup_xla_flags()
 import atexit
 import torch
 from ._patched_functions import _apply_patches
-from .version import __version__
 import _XLAC
 
 
