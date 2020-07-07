e0909cddcad0d719079590c45706602315d6d572
Davide Libenzi <dlibenzi@google.com>
2020-07-07 15:22:58 -0700

    Minor refactoring.

--- torch_xla/utils/utils.py
+++ torch_xla/utils/utils.py
@@ -21,6 +21,19 @@ class Cleaner(object):
     self.func()
 
 
+class LazyProperty(object):
+
+  def __init__(self, gen_fn):
+    self._gen_fn = gen_fn
+
+  @property
+  def value(self):
+    if self._gen_fn is not None:
+      self._value = self._gen_fn()
+      self._gen_fn = None
+    return self._value
+
+
 class TmpFolder(object):
 
   def __init__(self):
