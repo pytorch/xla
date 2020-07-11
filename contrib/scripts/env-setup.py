e2285562516770fee396f6b25801f64ca167dd92
Fabrizio Milo <Mistobaan@users.noreply.github.com>
2020-07-11 09:51:40 -0700

    use the python's executable environment

--- contrib/scripts/env-setup.py
+++ contrib/scripts/env-setup.py
@@ -10,6 +10,7 @@ import re
 import requests
 import subprocess
 import threading
+import sys
 
 VersionConfig = collections.namedtuple('VersionConfig',
                                        ['wheels', 'tpu', 'py_version', 'cuda_version'])
@@ -105,13 +106,13 @@ def install_vm(version, apt_packages, is_root=False):
     apt_cmd.insert(0, 'sudo')
 
   installation_cmds = [
-      ['pip', 'uninstall', '-y', 'torch', 'torchvision'],
+      [sys.executable, '-m', 'pip', 'uninstall', '-y', 'torch', 'torchvision'],
       ['gsutil', 'cp', torch_whl_path, '.'],
       ['gsutil', 'cp', torch_xla_whl_path, '.'],
       ['gsutil', 'cp', torchvision_whl_path, '.'],
-      ['pip', 'install', torch_whl],
-      ['pip', 'install', torch_xla_whl],
-      ['pip', 'install', torchvision_whl],
+      [sys.executable, '-m', 'pip', 'install', torch_whl],
+      [sys.executable, '-m', 'pip', 'install', torch_xla_whl],
+      [sys.executable, '-m', 'pip', 'install', torchvision_whl],
       apt_cmd,
   ]
   for cmd in installation_cmds:
