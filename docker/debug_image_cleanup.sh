ba2ef5d6203b977f024b809efa4b6460f636e450
zcain <zcain@google.com>
2020-09-09 15:17:20 -0700

    Switch to the real 90 day limit and remove the debug ls.

--- docker/debug_image_cleanup.sh
+++ docker/debug_image_cleanup.sh
@@ -1,5 +1,5 @@
 IMAGE="gcr.io/tpu-pytorch/xla_debug"
-DATE=$(date --date='-20 days -18 hours' +"%Y-%m-%dT%H:%M:%S")
+DATE=$(date --date='-90 days' +"%Y-%m-%dT%H:%M:%S")
 
 for digest in $(gcloud container images list-tags ${IMAGE} --limit=999999 --sort-by=TIMESTAMP     --filter="timestamp.datetime < '${DATE}'" --format='get(digest)'); do
   echo $digest
