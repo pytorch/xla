b48b9940f786bb4329be842ac44c3c62c5fb16d1
Ailing <ailzhang@users.noreply.github.com>
2020-09-21 11:15:47 -0700

    Remove override for native_group_norm since we move the composite ker… (#2489)

--- torch_xla/csrc/aten_tensor_ops.h
+++ torch_xla/csrc/aten_tensor_ops.h
@@ -8,11 +8,6 @@ at::Tensor celu(const at::Tensor& self, at::Scalar alpha);
 
 at::Tensor& celu_(at::Tensor& self, at::Scalar alpha);
 
-std::tuple<at::Tensor, at::Tensor, at::Tensor> native_group_norm(
-    const at::Tensor& input, const c10::optional<at::Tensor>& weight,
-    const c10::optional<at::Tensor>& bias, int64_t N, int64_t C, int64_t HxW,
-    int64_t group, double eps);
-
 std::tuple<at::Tensor, at::Tensor, at::Tensor> native_group_norm_backward(
     const at::Tensor& grad_out, const at::Tensor& input, const at::Tensor& mean,
     const at::Tensor& rstd, const c10::optional<at::Tensor>& weight, int64_t N,
