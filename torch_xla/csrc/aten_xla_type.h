d60ab86bd951f824b0e3e5ce62f3f6139f58cd5c
Ailing <ailzhang@users.noreply.github.com>
2020-09-21 13:15:42 -0700

    Revert "Remove override for native_group_norm since we move the composite ker…" (#2504)
    This reverts commit 8025265866c582aa4fafa065cdcb71bc16ed3cfb.

--- torch_xla/csrc/aten_xla_type.h
+++ torch_xla/csrc/aten_xla_type.h
@@ -693,6 +693,11 @@ class AtenXlaType {
                              bool train, double eps,
                              std::array<bool, 3> output_mask);
 
+  static std::tuple<at::Tensor, at::Tensor, at::Tensor> native_group_norm(
+      const at::Tensor& input, const c10::optional<at::Tensor>& weight,
+      const c10::optional<at::Tensor>& bias, int64_t N, int64_t C, int64_t HxW,
+      int64_t group, double eps);
+
   static std::tuple<at::Tensor, at::Tensor, at::Tensor>
   native_group_norm_backward(const at::Tensor& grad_out,
                              const at::Tensor& input, const at::Tensor& mean,
