aa38b6d039ece0f27e3acb6f2f057e94ed0c7fbd
Jin Young Sohn <jysohn@google.com>
2020-09-23 17:15:22 +0000

    Fix VLOGs being disabled
    Our TF_VLOGs had been disabled due to a conflict between
    `TF_CPP_MIN_LOG_LEVEL` and TF_VLOG since TF_VLOG logs with tf::INFO,
      which ends up getting logged nowhere since because of this check:
      https://github.com/tensorflow/tensorflow/blob/efff893c709b585da9771cb3a2997321efef36ae/tensorflow/core/platform/default/logging.cc#L195

--- third_party/xla_client/mesh_service.cc
+++ third_party/xla_client/mesh_service.cc
@@ -309,8 +309,8 @@ MeshClient* MeshClient::Get() {
 MeshClient::MeshClient(const std::string& address) : impl_(new Impl(address)) {
   int64 connect_wait_seconds =
       sys_util::GetEnvInt("XRT_MESH_CONNECT_WAIT", 300);
-  TF_LOG(INFO) << "Waiting to connect to client mesh master ("
-               << connect_wait_seconds << " seconds) " << address;
+  TF_VLOG(1) << "Waiting to connect to client mesh master ("
+             << connect_wait_seconds << " seconds) " << address;
   XLA_CHECK(impl_->channel->WaitForConnected(
       std::chrono::system_clock::now() +
       std::chrono::seconds(connect_wait_seconds)))
