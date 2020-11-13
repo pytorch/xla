58ccf11c361e4ba2fd9d3829ca907c2d037657a1
Ailing <ailzhang@users.noreply.github.com>
2020-11-12 19:52:03 -0800

    Revert "revert empty() signature to TensorOptions version" (#2571)

--- torch_xla/csrc/aten_xla_type.cpp
+++ torch_xla/csrc/aten_xla_type.cpp
@@ -1172,7 +1172,9 @@ at::Tensor AtenXlaType::embedding_dense_backward(const at::Tensor& grad_output,
 }
 
 at::Tensor AtenXlaType::empty(
-    at::IntArrayRef size, const at::TensorOptions& options,
+    at::IntArrayRef size, c10::optional<at::ScalarType> dtype,
+    c10::optional<at::Layout> layout, c10::optional<at::Device> device,
+    c10::optional<bool> pin_memory,
     c10::optional<at::MemoryFormat> /* memory_format */) {
   XLA_FN_COUNTER("xla::");
   // PT empty*() are optimizations to avoid initializing the data when it is
@@ -1181,8 +1183,8 @@ at::Tensor AtenXlaType::empty(
   // avoid going to CPU for it. A common PT pattern is indeed doing empty() plus
   // s_copy_().
   return bridge::AtenFromXlaTensor(XLATensor::full(
-      XlaHelpers::I64List(size), 0, GetXlaDeviceOrCurrent(options.device()),
-      GetScalarTypeOrFloat(c10::typeMetaToScalarType(options.dtype()))));
+      XlaHelpers::I64List(size), 0, GetXlaDeviceOrCurrent(device),
+      GetScalarTypeOrFloat(dtype)));
 }
 
 at::Tensor AtenXlaType::empty_strided(at::IntArrayRef size,
@@ -1192,12 +1194,7 @@ at::Tensor AtenXlaType::empty_strided(at::IntArrayRef size,
                                       c10::optional<at::Device> device,
                                       c10::optional<bool> pin_memory) {
   XLA_FN_COUNTER("xla::");
-  at::TensorOptions options = at::TensorOptions()
-                                  .dtype(dtype)
-                                  .layout(layout)
-                                  .pinned_memory(pin_memory)
-                                  .device(device);
-  at::Tensor t = empty(size, options, c10::nullopt);
+  at::Tensor t = empty(size, dtype, layout, device, pin_memory, c10::nullopt);
   return as_strided(t, size, stride, /*storage_offset=*/0);
 }
 
