3a33d2970250dbbb0efb563a7d553d6be3bbd113
JackCaoG <59073027+JackCaoG@users.noreply.github.com>
2020-11-20 09:55:44 -0800

    Reenable test_kthvalue_xla_ (#2639)

--- test/pytorch_test_base.py
+++ test/pytorch_test_base.py
@@ -186,7 +186,6 @@ DISABLED_TORCH_TESTS_ANY = {
         'test_shift_mem_overlap',  # doesn't raise
         'test_matrix_exp_analytic_xla',  # server side crash
         'test_muldiv_scalar_xla_bfloat16',  # FIXME
-        'test_kthvalue_xla_.*',  # FIXME
         'test_random_from_to_bool',  # doesn't raise
         'test_random_from_to_xla',  # doesn't raise
         'test_random_to_xla',  # doesn't raise
