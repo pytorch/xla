ee7462acab627c41488c2c36b5661c9ea7caf8b1
Jin Young (Daniel) Sohn <jysohn@google.com>
2021-01-20 13:10:52 -0500

    Remote `test_xla_dist` from CI

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -65,7 +65,6 @@ function run_all_tests {
   run_test python3 "$CDIR/test_mp_rendezvous.py"
   run_test python3 "$CDIR/test_mp_save.py"
   run_test python3 "$CDIR/test_mp_mesh_reduce.py"
-  run_test python3 "$CDIR/test_xla_dist.py"
 }
 
 if [ "$LOGFILE" != "" ]; then
