5dc9d9723bb694d3e9f9d2a3f0abdb8af885584a
JackCaoG <jackcao@google.com>
2021-03-30 19:51:55 +0000

    Add stdout file

--- torch_xla/core/xrt_run_server.py
+++ torch_xla/core/xrt_run_server.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
   # Enable the basic logging by defualt
   my_env['TF_CPP_MIN_LOG_LEVEL'] = '0'
   my_env[
-      'TF_CPP_VMODULE'] = 'tpu_configuration_ops=1,tpu_execute_op=1,tpu_compile_op=1,tpu_compile_op_impl=1,tpu_compile_op_common=1,tpu_compile_ops=1,master=1'
+      'TF_CPP_VMODULE'] = 'tpu_configuration_ops=1,tpu_execute_op=1,tpu_compile_op=1,tpu_compile_op_impl=1,tpu_compile_op_common=1,tpu_compile_ops=1,master=1,computation_client=5'
 
   env_vars = list(FLAGS.env) if FLAGS.env else []
   for env_var in env_vars:
@@ -31,13 +31,15 @@ if __name__ == '__main__':
     (env, var) = env_var.split('=', 1)
     my_env[env] = var
 
-  Path("/tmp/tf_server_log").mkdir(parents=True, exist_ok=True)
-  log_file = open(
-      "/tmp/tf_server_log/server_{}.log".format(time.strftime("%Y%m%d-%H%M%S")),
-      "w")
+  Path("/tmp/xrt_server_log").mkdir(parents=True, exist_ok=True)
+  time_str = time.strftime("%Y%m%d-%H%M%S")
+  stderr_file = open("/tmp/xrt_server_log/server_err_{}.log".format(time_str),
+                     "w")
+  stdout_file = open("/tmp/xrt_server_log/server_out_{}.log".format(time_str),
+                     "w")
   subprocess.Popen(
       ["python", "-m", "torch_xla.core._xrt_run_server", FLAGS.port],
-      stdout=subprocess.PIPE,
-      stderr=log_file,
+      stdout=stdout_file,
+      stderr=stderr_file,
       env=my_env,
       start_new_session=True)
