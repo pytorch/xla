c70b792b737a6251300bcc581a6a1fdb97bcdb26
Brian Hirsh <hirsheybar@fb.com>
2021-04-12 12:03:25 -0700

    use in-tree public codegen API.

--- scripts/generate_code.sh
+++ scripts/generate_code.sh
@@ -7,9 +7,6 @@ if [ -z "$PT_INC_DIR" ]; then
   PT_INC_DIR="$PTDIR/build/aten/src/ATen"
 fi
 
-python "$CDIR/gen.py" \
-  --gen_class_mode \
-  --output_folder="$XDIR/torch_xla/csrc" \
-  "$XDIR/torch_xla/csrc/aten_xla_type.h" \
-  "$PTDIR/build/aten/src/ATen/RegistrationDeclarations.h" \
-  "$PT_INC_DIR/Functions.h" \
+python "$PTDIR/tools/codegen/gen_backend_stubs.py" \
+  --output_dir="$XDIR/torch_xla/csrc" \
+  --source_yaml="xla_native_functions.yaml"\
