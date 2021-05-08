0f8877ae77d3cb64e4bc290284a96f5ae712bd4d
JackCaoG <jackcao@google.com>
2021-05-08 04:55:54 +0000

    Fix testOneHot

--- test/cpp/test_aten_xla_tensor.cpp
+++ test/cpp/test_aten_xla_tensor.cpp
@@ -6205,7 +6205,7 @@ TEST_F(AtenXlaTensorTest, TestOneHot) {
   // TODO: PT one_hot impl employs item() which could be eliminated.
   ExpectCounterNotChanged("aten::(?!_local_scalar_dense).*",
                           cpp_test::GetIgnoredCounters());
-  ExpectCounterChanged("xla::scatter_", cpp_test::GetIgnoredCounters());
+  ExpectCounterChanged("xla::scatter_out", cpp_test::GetIgnoredCounters());
 }
 
 TEST_F(AtenXlaTensorTest, TestTranspose) {
