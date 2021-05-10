dfc253dc66005c33f26a8b6658f71c9102e4cab0
Brian Hirsh <hirsheybar@fb.com>
2021-05-10 10:48:06 -0700

    Re-apply the commit: "use in-tree public codegen API. (#2898)"
    This reverts commit b1bfffabae0ada97a5d21240e92935807577fb38.

--- scripts/generate_code.sh
+++ scripts/generate_code.sh
@@ -7,9 +7,9 @@ if [ -z "$PT_INC_DIR" ]; then
   PT_INC_DIR="$PTDIR/build/aten/src/ATen"
 fi
 
-python "$CDIR/gen.py" \
-  --gen_class_mode \
-  --output_folder="$XDIR/torch_xla/csrc" \
-  "$XDIR/torch_xla/csrc/aten_xla_type.h" \
-  "$PTDIR/build/aten/src/ATen/RegistrationDeclarations.h" \
-  "$PT_INC_DIR/Functions.h" \
+pushd $PTDIR
+python -m tools.codegen.gen_backend_stubs \
+  --output_dir="$XDIR/torch_xla/csrc" \
+  --source_yaml="$XDIR/xla_native_functions.yaml"\
+
+popd
