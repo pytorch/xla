b0781d4dc221cb34e0b5c095d95a239c3345b12e
Brian Hirsh <hirsheybar@fb.com>
2021-06-25 06:34:47 -0700

    pass aten_xla_type.cpp to codegen to check for missing kernels. Get codegen to stop build on error

--- scripts/generate_code.sh
+++ scripts/generate_code.sh
@@ -7,9 +7,11 @@ if [ -z "$PT_INC_DIR" ]; then
   PT_INC_DIR="$PTDIR/build/aten/src/ATen"
 fi
 
+set -e
 pushd $PTDIR
 python -m tools.codegen.gen_backend_stubs \
   --output_dir="$XDIR/torch_xla/csrc" \
   --source_yaml="$XDIR/xla_native_functions.yaml"\
+  --impl_path="$XDIR/torch_xla/csrc/aten_xla_type.cpp"\
 
 popd
