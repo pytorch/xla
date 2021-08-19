cce62e13a28862928f8829bccd176aa8d0b80df1
JackCaoG <59073027+JackCaoG@users.noreply.github.com>
2021-08-18 18:49:17 -0700

    Run test_autocast for cpu and gpu on circleCI (#3089)
    * Run test_autopcast for cpu and gpu on circleCI
    
    * linter

--- test/test_autocast.py
+++ test/test_autocast.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument('--verbosity', type=int, default=0)
+parser.add_argument('--verbosity', type=int, default=2)
 FLAGS, leftovers = parser.parse_known_args()
 sys.argv = [sys.argv[0]] + leftovers
 
@@ -51,8 +51,7 @@ class TestAutocastBase(unittest.TestCase):
 
   def setUp(self):
     super(TestAutocastBase, self).setUp()
-    self.autocast_lists = AutocastTestLists(
-        torch.device(xm.xla_device(devkind="GPU")))
+    self.autocast_lists = AutocastTestLists(torch.device(xm.xla_device()))
     self.autocast_unsupported_lists = AutocastTestUnsupportedLists()
     self.skip_list = []
 
