6855abb41a11c56d314c908532e33966403c8c1f
JackCaoG <59073027+JackCaoG@users.noreply.github.com>
2021-08-20 12:09:59 -0700

    Add more debugging message to test_data_type (#3091)

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -85,11 +85,6 @@ function run_all_tests {
   run_test python3 "$CDIR/test_profiler.py"
   run_test python3 "$CDIR/test_ops.py"
   run_downcast_bf16 python3 "$CDIR/test_data_type.py"
-  # `run_use_bf16` should be run after the `run_downcast_bf16` version.
-  # In XLML test there seems to be an env var leakage where `XLA_USE_BF16=1`
-  # will be picked up by `test_data_type.py` (but not the C++ code) when we
-  # run the `run_use_bf16` version and causing test to expect `bf16` for `f64`
-  # data.
   run_use_bf16 python3 "$CDIR/test_data_type.py"
 }
 
--- test/test_data_type.py
+++ test/test_data_type.py
@@ -17,13 +17,13 @@ class XlaDataTypeTest(unittest.TestCase):
 
     hlo_text = torch_xla._XLAC._get_xla_tensors_text([t3])
     device_data_hlo = hlo_text.split('\n')[1]
-    assert 'xla::device_data' in device_data_hlo
+    assert 'xla::device_data' in device_data_hlo, device_data_hlo
     if os.getenv('XLA_USE_BF16') or os.getenv('XLA_DOWNCAST_BF16'):
-      assert 'bf16' in device_data_hlo
+      assert 'bf16' in device_data_hlo, device_data_hlo
     elif os.getenv('XLA_USE_FP16') or os.getenv('XLA_DOWNCAST_FP16'):
-      assert 'f16' in device_data_hlo
+      assert 'f16' in device_data_hlo, device_data_hlo
     else:
-      assert 'f32' in device_data_hlo
+      assert 'f32' in device_data_hlo, device_data_hlo
 
   def test_datatype_f64(self):
     t1 = torch.tensor([2.0, 3.0], dtype=torch.double, device=xm.xla_device())
@@ -33,17 +33,21 @@ class XlaDataTypeTest(unittest.TestCase):
 
     hlo_text = torch_xla._XLAC._get_xla_tensors_text([t3])
     device_data_hlo = hlo_text.split('\n')[1]
-    assert 'xla::device_data' in device_data_hlo
+    assert 'xla::device_data' in device_data_hlo, device_data_hlo
     if os.getenv('XLA_USE_BF16'):
-      assert 'bf16' in device_data_hlo
+      assert 'bf16' in device_data_hlo, device_data_hlo
     elif os.getenv('XLA_USE_FP16'):
-      assert 'f16' in device_data_hlo
+      assert 'f16' in device_data_hlo, device_data_hlo
     elif os.getenv('XLA_DOWNCAST_BF16') or os.getenv('XLA_DOWNCAST_FP16'):
-      assert 'f32' in device_data_hlo
+      assert 'f32' in device_data_hlo, device_data_hlo
     else:
-      assert 'f64' in device_data_hlo
+      assert 'f64' in device_data_hlo, device_data_hlo
 
 
 if __name__ == '__main__':
+  print(f'XLA_USE_BF16: {os.getenv("XLA_USE_BF16")}')
+  print(f'XLA_USE_FP16: {os.getenv("XLA_USE_FP16")}')
+  print(f'XLA_DOWNCAST_BF16: {os.getenv("XLA_DOWNCAST_BF16")}')
+  print(f'XLA_DOWNCAST_FP16: {os.getenv("XLA_DOWNCAST_FP16")}')
   test = unittest.main()
   sys.exit(0 if test.result.wasSuccessful() else 1)
