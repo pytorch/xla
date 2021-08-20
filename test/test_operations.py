81ab49ae718df530a9e009fc97c3c112751760f4
Milad Mohammadi <milad.mo@gmail.com>
2021-08-20 17:52:41 +0000

    resubmitting the sgn() operation test logic changes
    resubmitting the sgn() operation test logic changes

--- test/test_operations.py
+++ test/test_operations.py
@@ -281,28 +281,23 @@ class XlaTestCase(unittest.TestCase):
               b = b.to(torch.int)
 
             diff = a - b
-            if a.is_floating_point():
-              # check that NaNs are in the same locations
-              nan_mask = torch.isnan(a)
-              self.assertTrue(torch.equal(nan_mask, torch.isnan(b)), message)
-              diff[nan_mask] = 0
-              # inf check if allow_inf=True
-              if allow_inf:
-                inf_mask = torch.isinf(a)
-                inf_sign = inf_mask.sign()
-                self.assertTrue(
-                    torch.equal(inf_sign,
-                                torch.isinf(b).sign()), message)
-                diff[inf_mask] = 0
+            # check that NaNs are in the same locations
+            nan_mask = torch.isnan(a)
+            self.assertTrue(torch.equal(nan_mask, torch.isnan(b)), message)
+            diff[nan_mask] = 0
+            # inf check if allow_inf=True
+            if allow_inf:
+              inf_mask = torch.isinf(a)
+              inf_sign = inf_mask.sign()
+              self.assertTrue(
+                  torch.equal(inf_sign,
+                              torch.isinf(b).sign()), message)
+              diff[inf_mask] = 0
             # TODO: implement abs on CharTensor (int8)
             if diff.is_signed() and diff.dtype != torch.int8:
               diff = diff.abs()
-            if a.isnan().any() or b.isnan().any():
-              c = a.isnan() == b.isnan()
-              self.assertTrue(c.all(), message)
-            else:
-              max_err = diff.max()
-              self.assertLessEqual(max_err, prec, message)
+            max_err = diff.max()
+            self.assertLessEqual(max_err, prec, message)
 
       super(XlaTestCase, self).assertEqual(x.is_sparse, y.is_sparse, message)
       super(XlaTestCase, self).assertEqual(x.is_quantized, y.is_quantized,
