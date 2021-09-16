64ea5a0dbceffc3791bfd5cb7b771e0c3dd53f93
Milad Mohammadi <milad.mo@gmail.com>
2021-09-16 03:52:39 +0000

    linter fix

--- test/cpp/test_aten_xla_tensor.cpp
+++ test/cpp/test_aten_xla_tensor.cpp
@@ -10727,9 +10727,10 @@ TEST_F(AtenXlaTensorTest, TestNanToNum) {
     ForEachDevice([&](const torch::Device& device) {
       torch::Tensor xla_input = CopyToDevice(input, device);
       torch::Tensor xla_output = torch::nan_to_num(xla_input);
-      if (bridge::AtenDeviceToXlaDevice(device).hw_type == DeviceType::TPU && scalar_type == torch::kDouble) {
-        //Since TPU converts double to float (unlike CPU), the Inf entries are
-        //expected to be different. Skipping checks for Inf entries.
+      if (bridge::AtenDeviceToXlaDevice(device).hw_type == DeviceType::TPU &&
+          scalar_type == torch::kDouble) {
+        // Since TPU converts double to float (unlike CPU), the Inf entries are
+        // expected to be different. Skipping checks for Inf entries.
         AllEqual(output[0], xla_output[0]);
         AllEqual(output[1], xla_output[1]);
       } else {
@@ -10765,9 +10766,10 @@ TEST_F(AtenXlaTensorTest, TestNanToNumInplace) {
     ForEachDevice([&](const torch::Device& device) {
       torch::Tensor xla_input = CopyToDevice(input_copy, device);
       xla_input.nan_to_num_();
-      if (bridge::AtenDeviceToXlaDevice(device).hw_type == DeviceType::TPU && scalar_type == torch::kDouble) {
-        //Since TPU converts double to float (unlike CPU), the Inf entries are
-        //expected to be different. Skipping checks for Inf entries.
+      if (bridge::AtenDeviceToXlaDevice(device).hw_type == DeviceType::TPU &&
+          scalar_type == torch::kDouble) {
+        // Since TPU converts double to float (unlike CPU), the Inf entries are
+        // expected to be different. Skipping checks for Inf entries.
         AllEqual(input[0], xla_input[0]);
         AllEqual(input[1], xla_input[1]);
       } else {
@@ -10803,9 +10805,10 @@ TEST_F(AtenXlaTensorTest, TestNanToNumOut) {
       torch::Tensor xla_input = CopyToDevice(input, device);
       torch::Tensor xla_output = torch::zeros_like(input);
       torch::nan_to_num_out(xla_output, xla_input);
-      if (bridge::AtenDeviceToXlaDevice(device).hw_type == DeviceType::TPU && scalar_type == torch::kDouble) {
-        //Since TPU converts double to float (unlike CPU), the Inf entries are
-        //expected to be different. Skipping checks for Inf entries.
+      if (bridge::AtenDeviceToXlaDevice(device).hw_type == DeviceType::TPU &&
+          scalar_type == torch::kDouble) {
+        // Since TPU converts double to float (unlike CPU), the Inf entries are
+        // expected to be different. Skipping checks for Inf entries.
         AllEqual(output[0], xla_output[0]);
         AllEqual(output[1], xla_output[1]);
       } else {
