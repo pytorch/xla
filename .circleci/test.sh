4f22b045dae5aff7176b1f94dd5cfa7b65073595
Wonjoo Lee <wonjoo@google.com>
2021-12-22 22:12:21 +0000

    Fix typo

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -9,8 +9,8 @@ PYTORCH_DIR=/tmp/pytorch
 XLA_DIR=$PYTORCH_DIR/xla
 
 sudo apt-get -qq update
-sudo apt-get -qq clang-9
-sudo apt-get -qq clang++-9
+sudo apt-get -qq install clang-9
+sudo apt-get -qq install clang++-9
 
 source "$PYTORCH_DIR/.jenkins/pytorch/common_utils.sh"
 install_torchvision
