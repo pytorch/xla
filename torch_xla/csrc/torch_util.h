a1c71bea90931b6fe86ad19861c384c6851da739
Wonjoo Lee <wonjoo@google.com>
2022-01-28 00:47:56 +0000

    Remove PTXLA OptionalOr definition

--- torch_xla/csrc/torch_util.h
+++ torch_xla/csrc/torch_util.h
@@ -21,11 +21,6 @@ at::Scalar MakeFloatScalar(T value) {
   return at::Scalar(static_cast<double>(value));
 }
 
-template <typename T, typename S>
-T OptionalOr(const c10::optional<S>& value, T defval) {
-  return value ? static_cast<T>(*value) : defval;
-}
-
 // Unwraps tensor to target dtype if it's a wrapped number.
 at::Tensor UnwrapNumber(const at::Tensor& tensor, at::ScalarType dtype);
 
