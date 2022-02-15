fc8eea9c19d1b2cc93751faac8558ccc587b0b35
JackCaoG <59073027+JackCaoG@users.noreply.github.com>
2022-02-15 10:32:08 -0800

    Minor change to Gelu (#3374)

--- torch_xla/csrc/aten_xla_type.cpp
+++ torch_xla/csrc/aten_xla_type.cpp
@@ -16,7 +16,6 @@
 #include "torch_xla/csrc/aten_xla_bridge.h"
 #include "torch_xla/csrc/debug_util.h"
 #include "torch_xla/csrc/device.h"
-#include "torch_xla/csrc/gelu.h"
 #include "torch_xla/csrc/helpers.h"
 #include "torch_xla/csrc/ops/as_strided.h"
 #include "torch_xla/csrc/ops/index_ops.h"
@@ -198,16 +197,6 @@ void DoBinaryOpOut(const at::Tensor& self, const at::Tensor& other,
       GetBinaryOperands(self, UnwrapNumber(other, dtype));
   XLATensor out_tensor = bridge::GetXlaTensor(out);
   bin_op_out(operands.first, operands.second, out_tensor);
-}
-
-GeluType GetXlaGeluType(const c10::string_view approximate) {
-  if (approximate == "none") {
-    return GeluType::None;
-  } else if (approximate == "tanh") {
-    return GeluType::Tanh;
-  } else {
-    XLA_ERROR() << "Unknown gelu type: " << approximate;
-  }
 }
 
 }  // namespace
@@ -1521,7 +1510,7 @@ at::Tensor XLANativeFunctions::gelu(const at::Tensor& self,
                                     c10::string_view approximate) {
   XLA_FN_COUNTER("xla::");
   return bridge::AtenFromXlaTensor(
-      XLATensor::gelu(bridge::GetXlaTensor(self), GetXlaGeluType(approximate)));
+      XLATensor::gelu(bridge::GetXlaTensor(self), approximate));
 }
 
 at::Tensor XLANativeFunctions::gelu_backward(const at::Tensor& grad,
@@ -1529,8 +1518,7 @@ at::Tensor XLANativeFunctions::gelu_backward(const at::Tensor& grad,
                                              c10::string_view approximate) {
   XLA_FN_COUNTER("xla::");
   return bridge::AtenFromXlaTensor(XLATensor::gelu_backward(
-      bridge::GetXlaTensor(grad), bridge::GetXlaTensor(self),
-      GetXlaGeluType(approximate)));
+      bridge::GetXlaTensor(grad), bridge::GetXlaTensor(self), approximate));
 }
 
 at::Tensor XLANativeFunctions::ger(const at::Tensor& self,
