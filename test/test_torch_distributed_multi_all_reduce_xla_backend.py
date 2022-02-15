841e0d3cab9555ce6ca03de5e0498c1310a01ba2
Junmin Hao <86754787+hjm-aws@users.noreply.github.com>
2022-02-15 14:04:23 -0800

    Add XLA backend for torch.distributed (#3339)
    * Add XLA backend for torch.distributed
    
    * Add XLA backend multiprocessing tests.
    
    * Linter fixes.
    
    * Address Jack's comments.
    
    * Fix multiprocessing tests: forgot to import the backend.
    
    * Addressing Shen and Jack's comments.
    
    * Fix a search/replace error.
    
    * Fix typo in test_mp_all_gather_xla_backend to make it real.
    
    * Use new reduce_scatter output param and use tensor.copy_ for all_gather result tensor to avoid graph execution.
    
    * Lint fix.
    
    * Fix TODO(alias).
    
    * Add XRT_WORKERS and XRT_DEVICE_MAP setting back to the unit test as we do not aim to exercise GPU spicific code in the unit test.
    
    * Lint fix.
    
    * Skip XLA backend unit tests for GPU/TPU.
    
    * Address Jack's comments.
    
    * rename tests according to Jack's comment.

---
+++ test/test_torch_distributed_multi_all_reduce_xla_backend.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import torch
+import torch_xla
+import torch_xla.core.xla_model as xm
+import torch_xla.distributed.xla_multiprocessing as xmp
+import torch_xla.distributed.xla_backend
+import torch.distributed as dist
+
+
+def _mp_fn(index):
+  device = xm.xla_device()
+  if xm.xla_device_hw(device) in ('TPU', 'GPU'):
+    world_size = xm.xrt_world_size()
+    rank = xm.get_ordinal()
+
+    dist.init_process_group('xla', world_size=world_size, rank=rank)
+
+    num_all_reduces = 20
+    xinputs_list = []
+    for i in range(num_all_reduces):
+      inputs = torch.ones((2, 3)) * i
+      xinputs = inputs.to(device)
+      xinputs_list.append(xinputs)
+      dist.all_reduce(xinputs)
+    for i in range(num_all_reduces):
+      expected = torch.ones((2, 3)) * i * world_size
+      xinputs = xinputs_list[i]
+      assert torch.all(
+          xinputs.cpu() == expected), f'trial {i}, {xinputs} != {expected}'
+  else:
+    print(
+        'Default device {} is not a TPU or GPU device'.format(device),
+        file=sys.stderr)
+
+
+if __name__ == '__main__':
+  xmp.spawn(_mp_fn, args=())
---
+++ test/test_torch_distributed_reduce_scatter_xla_backend.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import torch
+import torch_xla
+import torch_xla.core.xla_model as xm
+import torch_xla.distributed.xla_multiprocessing as xmp
+import torch_xla.distributed.xla_backend
+import torch.distributed as dist
+
+
+def _mp_fn(index):
+  device = xm.xla_device()
+  if xm.xla_device_hw(device) in ('TPU', 'GPU'):
+    world_size = xm.xrt_world_size()
+    rank = xm.get_ordinal()
+
+    dist.init_process_group('xla', world_size=world_size, rank=rank)
+
+    input_size = (32, 3)
+    inputs = torch.ones(input_size).split(input_size[0] // world_size)
+    output = torch.zeros_like(inputs[0])
+    xinputs = [i.to(device) for i in inputs]
+    xoutput = output.to(device)
+    dist.reduce_scatter(xoutput, xinputs)
+    expected = torch.ones_like(inputs[0]) * world_size
+    assert torch.all(xoutput.cpu() == expected), f'{xoutput} != {expected}'
+  else:
+    print(
+        'Default device {} is not a TPU or GPU device'.format(device),
+        file=sys.stderr)
+
+
+if __name__ == '__main__':
+  xmp.spawn(_mp_fn, args=())
