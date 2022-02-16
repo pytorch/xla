d43202330b0c7eb3b24629971e46a2914b8e3564
Yeounoh Chung <yeounoh@google.com>
2022-02-16 11:14:48 -0800

    Update circleci base image with TF version bump (#3355)
    * Pin tf to 98fdeb774845c0396ed4d35b0ac4665b64ee2919
    * Use absl::Hash instead of ir::Literal::Hash
    * Requires libtpu-nightly 0.1.dev20220128
    * Additional PyTorch requirements for build
    * Consolidate conda envs to base
    * Install debugging package and bump up the build image tag

---
+++ .circleci/docker/install_valgrind.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+mkdir valgrind_build && cd valgrind_build
+VALGRIND_VERSION=3.16.1
+wget https://ossci-linux.s3.amazonaws.com/valgrind-${VALGRIND_VERSION}.tar.bz2
+tar -xjf valgrind-${VALGRIND_VERSION}.tar.bz2
+cd valgrind-${VALGRIND_VERSION}
+./configure --prefix=/usr/local
+make -j6
+sudo make install
+cd ../../
+rm -rf valgrind_build
+alias valgrind="/usr/local/bin/valgrind"
