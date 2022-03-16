033be7cfa81e3748e376bc2fa8220864f7a7fbbf
Yeounoh Chung <yeounoh@google.com>
2022-03-16 21:40:03 +0000

    Add testing probe for partitioner

--- torch_xla/experimental/xla_sharded_tensor.py
+++ torch_xla/experimental/xla_sharded_tensor.py
@@ -137,8 +137,8 @@ class XLAShardedTensor(torch.Tensor):
 
   @property
   def sharding_spec(self):
-    # TODO: check if global_tensor is an XLA tensor.
-    return torch_xla._XLAC._xla_get_sharding_spec(self.global_tensor)
+    # TODO: check if global_tensor is an XLA tensor
+    return torch_xla._XLAC._get_xla_sharding_spec(self.global_tensor)
 
   def __repr__(self):
     return f"XLAShardedTensor({self.global_tensor})"
