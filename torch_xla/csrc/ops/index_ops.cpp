45948b4b2162116e399bb7a3f62eb30c0aae5a7d
Wonjoo Lee <wonjoo@google.com>
2022-03-17 07:09:32 +0000

    Revert "Migrate to upstream NodePtr"
    This reverts commit bb201c33d65fff5e2d222430d0891b4fc8cd62a2.

--- torch_xla/csrc/ops/index_ops.cpp
+++ torch_xla/csrc/ops/index_ops.cpp
@@ -240,9 +240,8 @@ CanonicalIndexInfo GetCanonicalIndexInfo(
 }
 
 ir::Value EnsureRank1(const ir::Value& index) {
-  const ir::Node* casted = dynamic_cast<const ir::Node*>(index.node.get());
-  XLA_CHECK_LE(casted->shape().rank(), 1);
-  return casted->shape().rank() == 0
+  XLA_CHECK_LE(index->shape().rank(), 1);
+  return index->shape().rank() == 0
              ? ir::MakeNode<ir::ops::Expand>(index, std::vector<int64_t>{1})
              : index;
 }
