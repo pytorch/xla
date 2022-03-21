4612153f9125e90ea741d70c34d5872f1cc1b220
Wonjoo Lee <wonjoo@google.com>
2022-03-21 00:28:17 -0700

    Migrate to upstream torch::lazy::BackendDevice (#3383)
    * Migrate to upstream torch::lazy::BackendDevice
    
    * Run linter
    
    * Update DeviceType initialization
    
    * Update compare method
    
    * Run linter again
    
    * Add operators back to device.cpp

--- torch_xla/csrc/device.h
+++ torch_xla/csrc/device.h
@@ -4,18 +4,32 @@
 #include <string>
 
 #include "tensorflow/compiler/xla/xla_client/util.h"
+#include "torch/csrc/lazy/backend/backend_device.h"
 #include "torch/csrc/lazy/core/hash.h"
 #include "torch/csrc/lazy/core/util.h"
 
 namespace torch_xla {
 
-enum class DeviceType { CPU, GPU, TPU };
+enum class TorchXLADeviceType { CPU, GPU, TPU };
 
-struct Device {
+struct DeviceType : public torch::lazy::BackendDeviceType {
+  TorchXLADeviceType hw_type;
+
+  DeviceType(TorchXLADeviceType torch_xla_device_type) {
+    hw_type = torch_xla_device_type;
+    type = static_cast<int>(hw_type);
+  }
+};
+
+struct Device : public torch::lazy::BackendDevice {
   Device() = default;
   explicit Device(const std::string& device_spec);
-  Device(DeviceType hw_type, int ordinal)
-      : hw_type(hw_type), ordinal(ordinal) {}
+  Device(DeviceType device_type, int ordinal)
+      : torch::lazy::BackendDevice(
+            std::make_shared<torch::lazy::BackendDeviceType>(device_type),
+            ordinal),
+        device_type(device_type),
+        ordinal(ordinal) {}
 
   bool operator==(const Device& other) const { return compare(other) == 0; }
 
@@ -24,25 +38,20 @@ struct Device {
   bool operator<(const Device& rhs) const { return compare(rhs) < 0; }
 
   int compare(const Device& rhs) const {
-    if (hw_type != rhs.hw_type) {
-      return hw_type < rhs.hw_type ? -1 : +1;
+    if (device_type.hw_type != rhs.device_type.hw_type) {
+      return device_type.hw_type < rhs.device_type.hw_type ? -1 : +1;
     }
     return ordinal < rhs.ordinal ? -1 : (ordinal > rhs.ordinal ? +1 : 0);
   }
 
   std::string ToString() const;
 
-  friend std::ostream& operator<<(std::ostream& os, const Device& device) {
-    os << device.ToString();
-    return os;
-  }
-
   size_t hash() const {
-    return torch::lazy::StdHashCombine(torch::lazy::GetEnumValue(hw_type),
-                                       ordinal + 1);
+    return torch::lazy::StdHashCombine(
+        torch::lazy::GetEnumValue(device_type.hw_type), ordinal + 1);
   }
 
-  DeviceType hw_type = DeviceType::CPU;
+  DeviceType device_type = DeviceType(TorchXLADeviceType::CPU);
   int ordinal = 0;
 };
 
