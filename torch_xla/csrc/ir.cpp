c26a0c9f8f5e92bf0a9acb60b021054a6d4f0f8e
Wonjoo Lee <wonjoo@google.com>
2022-03-30 01:48:07 +0000

    u

--- torch_xla/csrc/ir.cpp
+++ torch_xla/csrc/ir.cpp
@@ -249,8 +249,9 @@ std::vector<torch::lazy::SourceLocation> Node::GetFrameInfo() {
   // At the time of writing, retrieving Python frames costs from 1us up to 20us.
   // This per IR Node. Since it is not unreasonable to have a many hundreds of
   // IR Node, this can be a multi-millisecond cost, which is not negligible.
-  static bool wants_frames = xla::sys_util::GetEnvBool("XLA_IR_DEBUG", false);
-  return wants_frames ? torch::lazy::GetPythonFrames()
+  FLAGS_torch_lazy_ir_debug = xla::sys_util::GetEnvBool("XLA_IR_DEBUG", false);
+
+  return FLAGS_torch_lazy_ir_debug ? torch::lazy::GetPythonFrames()
                       : std::vector<torch::lazy::SourceLocation>();
 }
 
