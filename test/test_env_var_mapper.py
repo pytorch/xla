9da005ef753f86af87febccb5f222b0bbf8bb332
Wonjoo Lee <wonjoo@google.com>
2022-04-06 00:32:12 +0000

    Run linter again

--- test/test_env_var_mapper.py
+++ test/test_env_var_mapper.py
@@ -12,18 +12,17 @@ def check_env_flag(name, default=''):
 
 
 class EnvVarMapperTest(unittest.TestCase):
-    
-    def test_xla_ir_debug(self):
-      xla_device = xm.xla_device()
-      t = torch.tensor([2.0, 3.0], dtype=torch.float, device=xla_device)
-      xla_tensors_report = torch_xla._XLAC._xla_tensors_report(0, str(xla_device))
-      if check_env_flag('XLA_IR_DEBUG'):
-        assert 'Frames' in xla_tensors_report
-      else:
-        assert 'Frames' not in xla_tensors_report
+
+  def test_xla_ir_debug(self):
+    xla_device = xm.xla_device()
+    t = torch.tensor([2.0, 3.0], dtype=torch.float, device=xla_device)
+    xla_tensors_report = torch_xla._XLAC._xla_tensors_report(0, str(xla_device))
+    if check_env_flag('XLA_IR_DEBUG'):
+      assert 'Frames' in xla_tensors_report
+    else:
+      assert 'Frames' not in xla_tensors_report
 
 
 if __name__ == '__main__':
   test = unittest.main()
   sys.exit(0 if test.result.wasSuccessful() else 1)
-
