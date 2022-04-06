dfb04e48b3ee176880e99bc14e6d422ae3384c59
Wonjoo Lee <wonjoo@google.com>
2022-04-06 12:36:29 -0700

    Add test_env_var_mapper.py test (#3473)
    * Add test_env_var_mapper.py test
    
    * Run linter and remove debugging code
    
    * Run linter again
    
    * Rename test

---
+++ test/test_env_var_mapper.py
@@ -0,0 +1,28 @@
+import os
+
+import torch
+import torch_xla
+import torch_xla.core.xla_model as xm
+import torch_xla.utils.utils as xu
+import unittest
+
+
+def check_env_flag(name, default=''):
+  return os.getenv(name, default).upper() in ['ON', '1', 'YES', 'TRUE', 'Y']
+
+
+class EnvVarMapperTest(unittest.TestCase):
+
+  def test_xla_ir_debug(self):
+    xla_device = xm.xla_device()
+    t = torch.tensor([2.0, 3.0], dtype=torch.float, device=xla_device)
+    xla_tensors_report = torch_xla._XLAC._xla_tensors_report(0, str(xla_device))
+    if check_env_flag('XLA_IR_DEBUG'):
+      assert 'Frames' in xla_tensors_report
+    else:
+      assert 'Frames' not in xla_tensors_report
+
+
+if __name__ == '__main__':
+  test = unittest.main()
+  sys.exit(0 if test.result.wasSuccessful() else 1)
