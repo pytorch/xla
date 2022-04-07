91b6cd369992af7b6c8dc723f72df4078b4453c0
Will Cromar <wcromar@google.com>
2022-04-07 21:38:17 +0000

    Remove some deps.

--- third_party/xla_client/pjrt_computation_client.cc
+++ third_party/xla_client/pjrt_computation_client.cc
@@ -8,7 +8,6 @@
 #include "tensorflow/compiler/xla/pjrt/cpu_device.h"
 #include "tensorflow/compiler/xla/pjrt/pjrt_client.h"
 #include "tensorflow/compiler/xla/shape.h"
-#include "tensorflow/compiler/xla/statusor.h"
 #include "tensorflow/compiler/xla/xla_client/computation_client.h"
 #include "tensorflow/compiler/xla/xla_client/debug_macros.h"
 #include "tensorflow/compiler/xla/xla_client/env_vars.h"
