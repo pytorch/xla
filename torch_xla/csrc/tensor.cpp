bf5be920927cbc21f5290ee44d905f12e7eba390
Milad Mohammadi <milad.mo@gmail.com>
2022-04-21 22:20:01 +0000

    cleaner condition

--- torch_xla/csrc/tensor.cpp
+++ torch_xla/csrc/tensor.cpp
@@ -996,19 +996,17 @@ std::vector<xla::ComputationClient::DataPtr> XLATensor::GatherTensorsXlaData(
 void XLATensor::TensorCollectionBarrier(SyncTensorCollection* coll) {
   std::string invalid_device(
       "Unknown0"); /* Temp solution to idetify unassigned devices */
-  if (coll->device.ToString().compare(invalid_device) == 0) {
+  if (coll->device.ToString().compare(invalid_device) == 0 || coll->unlocker.size() > 0) {
     return;
   }
-  if (coll->unlocker.size() == 0) {
-    TF_VLOG(4) << "Waiting on device barrier for device " << coll->device
-               << " ...";
-    {
-      XLA_TIMED("DeviceLockWait");
-      coll->unlocker = LockDevices({coll->device});
-    }
-    TF_VLOG(4) << "Waiting on device barrier for device " << coll->device
-               << " done!";
+  TF_VLOG(4) << "Waiting on device barrier for device " << coll->device
+             << " ...";
+  {
+    XLA_TIMED("DeviceLockWait");
+    coll->unlocker = LockDevices({coll->device});
   }
+  TF_VLOG(4) << "Waiting on device barrier for device " << coll->device
+             << " done!";
 }
 
 std::vector<at::Tensor> XLATensor::GetTensorsOpByOp(
