c54c8d460f442b9a97991ad8acf54d02a5a0652d
lyh271596 <lyh271596@alibaba-inc.com>
2022-04-26 11:46:18 +0800

    rebase

--- test/cpp/test_aten_xla_tensor.cpp
+++ test/cpp/test_aten_xla_tensor.cpp
@@ -11098,6 +11098,8 @@ TEST_F(AtenXlaTensorTest, TestRoll) {
       }
     }
   }
+  ExpectCounterNotChanged("aten::.*", cpp_test::GetIgnoredCounters());
+  ExpectCounterChanged("xla::roll", cpp_test::GetIgnoredCounters());
 }
 
 }  // namespace cpp_test
