72593a37ba2ab94c3f37e5e21ce5ab83b5143e3e
Milad Mohammadi <milad.mo@gmail.com>
2022-04-26 23:09:40 +0000

    reverted tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -81,14 +81,14 @@ function run_async_rng {
 }
 
 function run_all_tests {
-  #run_dynamic python3 "$CDIR/../../test/test_view_ops.py" "$@" -v TestViewOpsXLA
-  #run_test python3 "$CDIR/../../test/test_torch.py" "$@" -v TestTorchDeviceTypeXLA
-  #run_dynamic python3 "$CDIR/../../test/test_torch.py" "$@" -v TestDevicePrecisionXLA
-  #run_test python3 "$CDIR/../../test/test_torch.py" "$@" -v TestTensorDeviceOpsXLA
-  #run_test python3 "$CDIR/../../test/test_indexing.py" "$@" -v TestIndexingXLA
-  #run_test python3 "$CDIR/../../test/test_indexing.py" "$@" -v NumpyTestsXLA
-  #run_dynamic python3 "$CDIR/../../test/test_nn.py" "$@" -v TestNNDeviceTypeXLA
-  #run_dynamic python3 "$CDIR/../../test/test_type_promotion.py" "$@" -v TestTypePromotionXLA
+  run_dynamic python3 "$CDIR/../../test/test_view_ops.py" "$@" -v TestViewOpsXLA
+  run_test python3 "$CDIR/../../test/test_torch.py" "$@" -v TestTorchDeviceTypeXLA
+  run_dynamic python3 "$CDIR/../../test/test_torch.py" "$@" -v TestDevicePrecisionXLA
+  run_test python3 "$CDIR/../../test/test_torch.py" "$@" -v TestTensorDeviceOpsXLA
+  run_test python3 "$CDIR/../../test/test_indexing.py" "$@" -v TestIndexingXLA
+  run_test python3 "$CDIR/../../test/test_indexing.py" "$@" -v NumpyTestsXLA
+  run_dynamic python3 "$CDIR/../../test/test_nn.py" "$@" -v TestNNDeviceTypeXLA
+  run_dynamic python3 "$CDIR/../../test/test_type_promotion.py" "$@" -v TestTypePromotionXLA
   run_dynamic python3 "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
   run_opbyop python3 "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
   run_eager_debug python3 "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
