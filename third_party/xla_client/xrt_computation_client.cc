b8ae6e9ed43d7753da65a45b603ba4cfabae416e
Yeounoh Chung <yeounoh@google.com>
2022-05-07 23:46:15 +0000

    is_spmd device assignment for xrt_computation

--- third_party/xla_client/xrt_computation_client.cc
+++ third_party/xla_client/xrt_computation_client.cc
@@ -1070,52 +1070,66 @@ const std::string& XrtComputationClient::TorchDeviceToXrtDevice(
 
 std::unique_ptr<xrt::XLAComputation> XrtComputationClient::CreateXrtComputation(
     const XlaComputation& computation, absl::Span<const std::string> devices,
-    const Shape* output_shape) const {
+    const Shape* output_shape, bool is_spmd) const {
   std::unique_ptr<xrt::XLAComputation> xrt_computation(
       new xrt::XLAComputation());
   auto config = xrt_computation->mutable_config();
-  if (device.kind == "TPU") {
-    // TODO: this used to be hard-coded to 1; now overlaying
-    // replicas and shards, co-existing across devices.
+  if (device.kind == "TPU" && is_spmd) {
+    // TODO(yeounoh) num_cores_per_replica is no longer hard-coded to 1;
+    // setting the number of replicas to 1 for GSPMD test
+    config->set_num_replicas(1);
     config->set_num_cores_per_replica(devices.size());
+    // Model-parallel computation requires num_cores_per_repica
+    // computation devices
+    auto device_assignment = config->mutable_device_assignment();
     for (int64_t i = 0; i < devices.size(); ++i) {
       ProgramShapeProto* per_core_program_shape =
           config->add_per_core_program_shape();
       *per_core_program_shape =
           computation.GetProgramShape().ValueOrDie().ToProto();
-    }
-  } else {
-    config->set_num_cores_per_replica(1);
-  }
 
-  // TODO: current design assumes that all devices would participate in
-  // replication. This affects the groupings of replica groups.
-  if (devices.size() > 1) {
-    auto device_assignment = config->mutable_device_assignment();
-    auto computation_device = device_assignment->add_computation_devices();
-    for (int64_t i = 0; i < devices.size(); ++i) {
+      auto computation_device = device_assignment->add_computation_devices();
       Device device(devices[i]);
       auto replica_device = computation_device->add_replica_devices();
-      if (device.kind == "TPU") {
-        const std::string& xrt_device = TorchDeviceToXrtDevice(devices[i]);
-        const auto& core_coords = GetDeviceMeshCoords(xrt_device);
-        for (auto coord : core_coords) {
-          replica_device->add_value(coord);
-        }
-      } else if (device.kind == "GPU") {
-        // For GPU use X,Y,Z=0 and CORE=GPU_ORDINAL (where GPU_ORDINAL is the
-        // global ordinal value).
-        replica_device->add_value(0);
-        replica_device->add_value(0);
-        replica_device->add_value(0);
-        replica_device->add_value(device.ordinal);
-      } else {
-        XLA_ERROR() << "Unsupported replication device type: " << device.kind;
+      const std::string& xrt_device = TorchDeviceToXrtDevice(devices[i]);
+      const auto& core_coords = GetDeviceMeshCoords(xrt_device);
+      for (auto coord : core_coords) {
+        replica_device->add_value(coord);
       }
     }
-    config->set_num_replicas(devices.size());
+
+  } else {
+    // TODO(yeounoh) existing scheme assumes that all devices would participate
+    // in replication. This affects the groupings of replica groups.
+    config->set_num_cores_per_replica(1);
+    if (devices.size() > 1) {
+      auto device_assignment = config->mutable_device_assignment();
+      auto computation_device = device_assignment->add_computation_devices();
+      for (int64_t i = 0; i < devices.size(); ++i) {
+        Device device(devices[i]);
+        auto replica_device = computation_device->add_replica_devices();
+        if (device.kind == "TPU") {
+          const std::string& xrt_device = TorchDeviceToXrtDevice(devices[i]);
+          const auto& core_coords = GetDeviceMeshCoords(xrt_device);
+          for (auto coord : core_coords) {
+            replica_device->add_value(coord);
+          }
+        } else if (device.kind == "GPU") {
+          // For GPU use X,Y,Z=0 and CORE=GPU_ORDINAL (where GPU_ORDINAL is the
+          // global ordinal value).
+          replica_device->add_value(0);
+          replica_device->add_value(0);
+          replica_device->add_value(0);
+          replica_device->add_value(device.ordinal);
+        } else {
+          XLA_ERROR() << "Unsupported replication device type: " << device.kind;
+        }
+      }
+      config->set_num_replicas(devices.size());
+    }
   }
-  // TODO: Program shape for the whole computation
+
+  // TODO(yeounoh) Program shape for the whole computation
   *config->mutable_program_shape() =
       computation.GetProgramShape().ValueOrDie().ToProto();
   if (output_shape != nullptr) {
