b92feb5a7d3391081a6ee9b5c8566005911155b2
Yeounoh Chung <yeounoh@google.com>
2022-05-17 03:45:06 +0000

    Unset CC & CXX for tensorflow build

--- build_torch_xla_libs.sh
+++ build_torch_xla_libs.sh
@@ -61,6 +61,10 @@ if [ "$CMD" == "clean" ]; then
 else
   # Temporary patch until tensorflow update bazel requirement to 5.2.0
   echo "6e54699884cfad49d4e8f6dd59a4050bc95c4edf" > third_party/tensorflow/.bazelversion
+  # We can remove this after https://github.com/bazelbuild/bazel/issues/15359 is resolved
+  unset CC
+  unset CXX
+
   cp -r -u -p $THIRD_PARTY_DIR/xla_client $THIRD_PARTY_DIR/tensorflow/tensorflow/compiler/xla/
 
   pushd $THIRD_PARTY_DIR/tensorflow
