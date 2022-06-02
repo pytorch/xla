47c9290238266ae585f42ef0eb279792d41eba68
JackCaoG <jackcao@google.com>
2022-06-02 21:32:25 +0000

    Install ninja for torchaudio build

--- scripts/build_torch_wheels.sh
+++ scripts/build_torch_wheels.sh
@@ -96,6 +96,10 @@ function install_bazel() {
   export PATH="$PATH:$HOME/bin"
 }
 
+function install_ninja() {
+  sudo apt-get install ninja-build
+}
+
 function debian_version {
   local VER
   if ! sudo apt-get install -y lsb-release > /dev/null 2>&1 ; then
@@ -124,6 +128,7 @@ function install_req_packages() {
   sudo apt-get -y install python-pip git curl libopenblas-dev vim apt-transport-https ca-certificates wget procps
   maybe_install_cuda
   install_bazel
+  install_ninja
 }
 
 function install_gcloud() {
