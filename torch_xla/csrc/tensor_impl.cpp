e3eab13d823ea3469269961bd184422ee8a2fe57
Nikolay Korovaiko <korovaikon@gmail.com>
2022-06-07 11:02:32 -0700

    fix clang-format

--- torch_xla/csrc/tensor_impl.cpp
+++ torch_xla/csrc/tensor_impl.cpp
@@ -110,7 +110,8 @@ at::IntArrayRef XLATensorImpl::sizes_custom() const {
 
 c10::SymIntArrayRef XLATensorImpl::sym_sizes_custom() const {
   auto sizes = sizes_custom();
-  return c10::SymIntArrayRef(reinterpret_cast<const c10::SymInt*>(sizes.data()), sizes.size());
+  return c10::SymIntArrayRef(reinterpret_cast<const c10::SymInt*>(sizes.data()),
+                             sizes.size());
 }
 
 at::IntArrayRef XLATensorImpl::strides_custom() const {
