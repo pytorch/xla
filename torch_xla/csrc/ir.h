14f42364cc475586687128fa8457a17980fe6f4d
Wonjoo Lee <wonjoo@google.com>
2022-06-07 18:27:04 +0000

    Remove torch_xla::ScopePusher and related functions

--- torch_xla/csrc/ir.h
+++ torch_xla/csrc/ir.h
@@ -139,16 +139,6 @@ class XlaNode : public torch::lazy::Node {
   const xla::OpSharding* output_sharding_ = nullptr;
 };
 
-// RAII data structure to be used a stack variable to enter a new IR scope. IR
-// scope names will appear in the IR and will help identifying the source of the
-// single IR nodes.
-struct ScopePusher {
-  explicit ScopePusher(const std::string& name);
-  ~ScopePusher();
-
-  static void ResetScopes();
-};
-
 inline std::ostream& operator<<(std::ostream& stream, const XlaNode& node) {
   stream << node.ToString();
   return stream;
