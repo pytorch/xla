4f32b140902ad8d06f5780dd953bafb0c007aa14
Milad Mohammadi <milad.mo@gmail.com>
2022-06-13 23:53:15 +0000

    linter

--- torch_xla/csrc/ops/dynamic_ir.cpp
+++ torch_xla/csrc/ops/dynamic_ir.cpp
@@ -7,18 +7,10 @@
 
 namespace torch_xla {
 
-// DimensionNode::DimensionNode(torch::lazy::OpKind op, OpList operands,
-//                              torch::lazy::hash_t hash_seed)
-//     : XlaNode(op, operands, xla::ShapeUtil::MakeShape(xla::S32, {}),
-//               /*num_outputs=*/1,
-//               /*hash_seed*/ hash_seed) {}
-
-// std::string DimensionNode::ToString() const { return "DimensionNode"; }
-
 SizeNode::SizeNode(XlaValue input, size_t dim)
-    : XlaNode(
-          torch::lazy::OpKind{c10::Symbol::fromQualString("aten::size")},
-          {input}, xla::ShapeUtil::MakeShape(xla::S32, {}), 1, torch::lazy::MHash(dim)),
+    : XlaNode(torch::lazy::OpKind{c10::Symbol::fromQualString("aten::size")},
+              {input}, xla::ShapeUtil::MakeShape(xla::S32, {}), 1,
+              torch::lazy::MHash(dim)),
       dim_(dim){};
 
 XlaOpVector SizeNode::Lower(LoweringContext* loctx) const {
@@ -33,13 +25,14 @@ int64_t SizeNode::getStaticValue() const {
 std::string SizeNode::ToString() const { return "SizeNode"; }
 
 SizeAdd::SizeAdd(XlaValue a, XlaValue b)
-    : XlaNode(
-          torch::lazy::OpKind{c10::Symbol::fromQualString("aten::add")},
-          {a, b}, xla::ShapeUtil::MakeShape(xla::S32, {}), 1){};
+    : XlaNode(torch::lazy::OpKind{c10::Symbol::fromQualString("aten::add")},
+              {a, b}, xla::ShapeUtil::MakeShape(xla::S32, {}), 1){};
 
 int64_t SizeAdd::getStaticValue() const {
-  return dynamic_cast<const torch::lazy::DimensionNode*>(operand(0).node)->getStaticValue() +
-         dynamic_cast<const torch::lazy::DimensionNode*>(operand(1).node)->getStaticValue();
+  return dynamic_cast<const torch::lazy::DimensionNode*>(operand(0).node)
+             ->getStaticValue() +
+         dynamic_cast<const torch::lazy::DimensionNode*>(operand(1).node)
+             ->getStaticValue();
 }
 
 std::string SizeAdd::ToString() const { return "SizeAdd"; }
@@ -53,13 +46,14 @@ XlaOpVector SizeAdd::Lower(LoweringContext* loctx) const {
 }
 
 SizeMul::SizeMul(XlaValue a, XlaValue b)
-    : XlaNode(
-          torch::lazy::OpKind{c10::Symbol::fromQualString("aten::mul")},
-          {a, b}, xla::ShapeUtil::MakeShape(xla::S32, {}), 1){};
+    : XlaNode(torch::lazy::OpKind{c10::Symbol::fromQualString("aten::mul")},
+              {a, b}, xla::ShapeUtil::MakeShape(xla::S32, {}), 1){};
 
 int64_t SizeMul::getStaticValue() const {
-  return dynamic_cast<const torch::lazy::DimensionNode*>(operand(0).node)->getStaticValue() *
-         dynamic_cast<const torch::lazy::DimensionNode*>(operand(1).node)->getStaticValue();
+  return dynamic_cast<const torch::lazy::DimensionNode*>(operand(0).node)
+             ->getStaticValue() *
+         dynamic_cast<const torch::lazy::DimensionNode*>(operand(1).node)
+             ->getStaticValue();
 }
 
 std::string SizeMul::ToString() const { return "SizeMul"; }
@@ -73,17 +67,17 @@ XlaOpVector SizeMul::Lower(LoweringContext* loctx) const {
 }
 
 SizeDiv::SizeDiv(XlaValue a, XlaValue b)
-    : XlaNode(
-          torch::lazy::OpKind{c10::Symbol::fromQualString("aten::div")},
-          {a, b}, xla::ShapeUtil::MakeShape(xla::S32, {}), 1){};
+    : XlaNode(torch::lazy::OpKind{c10::Symbol::fromQualString("aten::div")},
+              {a, b}, xla::ShapeUtil::MakeShape(xla::S32, {}), 1){};
 
 int64_t SizeDiv::getStaticValue() const {
-  XLA_CHECK(
-      dynamic_cast<const torch::lazy::DimensionNode*>(operand(1).node)->getStaticValue() !=
-      0)
+  XLA_CHECK(dynamic_cast<const torch::lazy::DimensionNode*>(operand(1).node)
+                ->getStaticValue() != 0)
       << "Can't divide a dimension by zero";
-  return dynamic_cast<const torch::lazy::DimensionNode*>(operand(0).node)->getStaticValue() /
-         dynamic_cast<const torch::lazy::DimensionNode*>(operand(1).node)->getStaticValue();
+  return dynamic_cast<const torch::lazy::DimensionNode*>(operand(0).node)
+             ->getStaticValue() /
+         dynamic_cast<const torch::lazy::DimensionNode*>(operand(1).node)
+             ->getStaticValue();
 }
 
 std::string SizeDiv::ToString() const { return "SizeDiv"; }
