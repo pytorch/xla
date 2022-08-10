a9d175acfc5f63f4bb92651dadfd8290a9b1df47
MiladM <milad.mo@gmail.com>
2022-08-09 21:24:56 -0700

    added TPU_ML_PLATFORM variable (#3846)

--- torch_xla/__init__.py
+++ torch_xla/__init__.py
@@ -58,6 +58,7 @@ def _setup_default_env():
   _set_missing_env('TF_CPP_MIN_LOG_LEVEL', '1')
   _set_missing_env('GRPC_VERBOSITY', 'ERROR')
   _set_missing_env('ALLOW_MULTIPLE_LIBTPU_LOAD', '1')
+  _set_missing_env('TPU_ML_PLATFORM', 'PyTorch/XLA')
   if server_is_alive():
     _set_missing_env('XRT_START_LOCAL_SERVER', '0')
 
