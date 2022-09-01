b7d15c1a9f6089ad9675e619c5f56ce454a4a80e
Yeounoh Chung <yeounoh@google.com>
2022-09-01 13:51:01 -0700

    [SPMD] Refactor XLAShardedTensor & unit tests.

--- torch_xla/experimental/xla_sharded_tensor.py
+++ torch_xla/experimental/xla_sharded_tensor.py
@@ -48,6 +48,76 @@ class XLAShardedTensor(torch.Tensor):
   # Note: we can consider returning a callback or even define
   # sharding at XLAShardedTensor construction after pjrt migration.
   local_shards: List[XLAShard] = None
+  # A logical device topology, each element describes
+  # a number of devices in the corresponding axis.
+  # NOTE: we could use more specific device-rank mapping, e.g., ShardingSpec,
+  # if needed. The change shouldn't be difficult, or create another constructor.
+  mesh_shape: Tuple[int]  # TODO: create a wrapper for named axes
+  # Specifies how each input rank is sharded (index to mesh_shape)
+  # or replicated (None). For example, we can shard an 8x10 tensor
+  # 4-way row-wise, and replicate column-wise.
+  # >> input = torch.randn(8, 10)
+  # >> mesh_shape = (4, 2)
+  # >> assert np.prod(mesh_shape) == xm.xrt_world_size()
+  # >> partition_spec = (0, None)
+  # >> assert len(input.shape) == len(partition_spec)
+  partition_spec: Tuple[int, None]
+
+  __slots__ = ['global_tensor']
+
+  @staticmethod
+  def __new__(cls, elem: torch.Tensor, *args, **kwargs):
+    # TODO(yeounoh) wrapper can take different arguments
+    r = torch.Tensor._make_wrapper_subclass(  # type: ignore[attr-defined]
+        cls,
+        elem.size(),
+        strides=elem.stride(),
+        storage_offset=elem.storage_offset(),
+        dtype=elem.dtype,
+        layout=elem.layout,
+        device=elem.device,
+        requires_grad=kwargs.get("requires_grad", False))
+    r.global_tensor = elem.detach() if r.requires_grad else elem
+    return r
+
+  @property
+  def sharding_spec(self):
+    # TODO(yeounoh) `torch_xla._XLAC._get_xla_sharding_spec(self.global_tensor)`
+    # is broken after ltc migration, needs a further investigation.
+    return NotImplemented
+
+  @property
+  def shards(self):
+    # Return a list of local shards
+    return NotImplemented
+
+  def __repr__(self):
+    return f"XLAShardedTensor({self.global_tensor})"
+
+  @classmethod
+  def __torch_dispatch__(cls, func, types, args=(), kwargs=None):
+    """
+      The dispatcher allows the unwrapped torch.Tensor to re-dispatched to the
+      `xla` backend as XlaTensor, and the XlaTensor with an associated sharding spec
+      to be received and wrapped as XLAShardedTensor.
+    """
+
+  # XLAShardedTensor behaves like a unpartitioned,
+  # combined tensor on the host machine. When user annotates,
+  # this is simply set to the input tensor. When an XLA partitioned
+  # output tensor returns (or sharding propagated intermediate tensors)
+  # as XLAShardedTensor, the backend gathers global data across devices
+  # and materialize and set `global_tensor` on the host; the actual device
+  # data still remain on individual device as sharded or replicated.
+  # Note: we should drop this reference, and force all gather on each access.
+  global_tensor: torch.Tensor
+  # Shards on the devices are materialized/available after the lazy
+  # execution of the SPMDPartitioned HLO graph; otherwise,
+  # local_shards is set to `None`. Each XLAShard points to
+  # torch.Tensor (xla::device_data).
+  # Note: we can consider returning a callback or even define
+  # sharding at XLAShardedTensor construction after pjrt migration.
+  local_shards: List[XLAShard] = None
 
   __slots__ = ['global_tensor']
 
--- torch_xla/experimental/xla_sharding.py
+++ torch_xla/experimental/xla_sharding.py
@@ -7,21 +7,17 @@ import numpy as np
 from typing import Tuple, Union
 
 
-# torch_xla.distributed.xla_sharding
 def mark_sharding(t: Union[torch.Tensor,
                            XLAShardedTensor], mesh_shape: Tuple[int],
                   partition_spec: Tuple[Union[int, None]]) -> XLAShardedTensor:
   """
     Annotates the tensor provided with XLA partition spec. Internally,
     it annotates the corresponding XLATensor as sharded for the XLA SpmdPartitioner pass.
-
     Args:
         t (Union[torch.Tensor, XLAShardedTensor]): input tensor to be annotated with partition_sepc.
-
         mesh_shape (Tuple[Union[int, None]]): A int tuple describing the logical topology
         of the device mesh, and each element describes the number of devices in
         the corresponding axis.
-
         partition_spec (Tuple[int, None]): A tuple of device_mesh dimension index or `None`.
         This specifies how each input rank is sharded (index to mesh_shape) or replicated (None).
         For example, we can shard an 8x10 tensor 4-way row-wise, and replicate column-wise.
@@ -30,46 +26,44 @@ def mark_sharding(t: Union[torch.Tensor,
         >> assert np.prod(mesh_shape) == xm.xrt_world_size()
         >> partition_spec = (0, None)
         >> assert len(input.shape) == len(partition_spec)
-
     Examples
     —------------------------------
     mesh_shape = (4, 2)
     input = torch.randn(8, 32).to(xm.xla_device())
     # 4-way data parallel
     input = xs.mark_sharding(input, mesh_shape, (0, None))
-
     linear = nn.Linear(32, 10).to(xm.xla_device())
     # 2-way model parallel
     linear.weight = xs.mark_sharding(linear.weight, device_mesh, (None, 1))
-
     output = linear(input)
     # full replication
     output = xs.mark_sharding(output, device_mesh, (None, None))
-    """
+  """
   num_devices = xm.xrt_world_size()
   assert np.prod(mesh_shape) == num_devices, \
     f"{mesh_shape} is not mappable over {num_devices} devices."
   assert all((d >= 0 and d < len(mesh_shape)) for d in partition_spec if d), \
     f"partition_spec ({partition_spec}) contains out of bound index into mesh_shape."
-  # We might allow {len(partition_spec)} <= {len(t.shape)},
-  # where the unspecified ranks are replicated.
+  # TODO(yeounoh) allow unspecified ranks (len(partition_spec) <= len(t.shape)),
+  # for replication. For now, all input rank sharding should be specified.
   assert len(t.shape) == len(partition_spec), \
     f"Partition spec length ({len(partition_spec)}) is not equal to the input rank ({len(t.shape)})."
 
   device_ids = np.array(range(num_devices))
-  tile_assignment = list(device_ids.reshape(mesh_shape))
+  tile_assignment = device_ids.reshape(mesh_shape).tolist()
 
-  manual = False
-  replicated = False
+  manual, replicated, partial = False, False, False
   if all(d is None for d in partition_spec):
-    # TODO(yeounoh) support partial replication
     replicated = True
+  elif any(d is None for d in partition_spec):
+    partial = True
+
+  # TODO(yeounoh) suport partially replicated sharding.
+  assert not partial, "Partial replication is currently not supported."
 
   if isinstance(t, XLAShardedTensor):
-    # Update sharding annotation
     torch_xla._XLAC._xla_mark_sharding(t.global_tensor, tile_assignment,
                                        replicated, manual)
-    return t  #  XLAShardedTensor
-
+    return t
   torch_xla._XLAC._xla_mark_sharding(t, tile_assignment, replicated, manual)
   return XLAShardedTensor(t)
