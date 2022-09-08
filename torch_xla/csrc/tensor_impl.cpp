dcb71e92dc0d642c231b49c9aa4da8dc46e3bd0d
Edward Z. Yang <ezyang@fb.com>
2022-09-07 19:26:02 -0700

    Remove unnecessary virtual method.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch_xla/csrc/tensor_impl.cpp
+++ torch_xla/csrc/tensor_impl.cpp
@@ -129,12 +129,6 @@ c10::SymInt XLATensorImpl::sym_numel_custom() const {
   return prod;
 }
 
-c10::SymIntArrayRef XLATensorImpl::sym_sizes() const {
-  // it isn't strictly necessary to delegate to `sym_sizes_custom`
-  // however, it's consistent with pytorch core
-  return sym_sizes_custom();
-}
-
 at::IntArrayRef XLATensorImpl::strides_custom() const {
   const_cast<XLATensorImpl*>(this)->SetupSizeProperties();
   return strides_default();
