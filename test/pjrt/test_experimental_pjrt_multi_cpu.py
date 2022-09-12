2942f63561f40d75ee1f9622f550d8ef848ceaca
Will Cromar <wcromar@google.com>
2022-09-12 23:20:10 +0000

    Update tests

--- test/pjrt/test_experimental_pjrt_multi_cpu.py
+++ test/pjrt/test_experimental_pjrt_multi_cpu.py
@@ -23,18 +23,16 @@ class TestExperimentalPjrtMultiCpu(parameterized.TestCase):
     os.environ.pop(xenv.CPU_NUM_DEVICES, None)
     os.environ.pop(xenv.PJRT_CPU_ASYNC_CLIENT, None)
 
-    expected = {0: {0: torch.device('xla:0'),}}
+    expected = {0: torch.device('xla:0')}
     devices_per_process = pjrt.run_multiprocess(xm.xla_device)
     self.assertDictEqual(devices_per_process, expected)
 
   def test_multi_cpu_devices(self):
     expected = {
-        0: {
-            0: torch.device('xla:0'),
-            1: torch.device('xla:1'),
-            2: torch.device('xla:2'),
-            3: torch.device('xla:3')
-        }
+        0: torch.device('xla:0'),
+        1: torch.device('xla:1'),
+        2: torch.device('xla:2'),
+        3: torch.device('xla:3'),
     }
 
     devices_per_process = pjrt.run_multiprocess(xm.xla_device)
@@ -44,18 +42,14 @@ class TestExperimentalPjrtMultiCpu(parameterized.TestCase):
                                   ('pjrt', pjrt.global_ordinal))
   def test_global_ordinal(self, ordinal_func):
     results = pjrt.run_multiprocess(ordinal_func)
-    values = list(
-        itertools.chain.from_iterable(row.values() for row in results.values()))
-    self.assertListEqual(sorted(values), [0, 1, 2, 3])
+    self.assertListEqual(sorted(results.values()), [0, 1, 2, 3])
 
   @parameterized.named_parameters(('xla_model', xm.get_local_ordinal),
                                   ('pjrt', pjrt.local_ordinal))
   def test_local_ordinal(self, ordinal_func):
     # TODO(wcromar): add multiprocess tests
     results = pjrt.run_multiprocess(ordinal_func)
-    values = list(
-        itertools.chain.from_iterable(row.values() for row in results.values()))
-    self.assertListEqual(sorted(values), [0, 1, 2, 3])
+    self.assertListEqual(sorted(results.values()), [0, 1, 2, 3])
 
   @staticmethod
   def _multi_cpu_backwards():
@@ -90,13 +84,11 @@ class TestExperimentalPjrtMultiCpu(parameterized.TestCase):
     })
 
     expected = {
-        0: {
-            i: {
-                'forward_ordinal': i,
-                'backward_ordinal': i,
-                'device': f'xla:{i}'
-            } for i in range(4)
-        }
+        i: {
+            'forward_ordinal': i,
+            'backward_ordinal': i,
+            'device': f'xla:{i}'
+        } for i in range(4)
     }
     results = pjrt.run_multiprocess(self._multi_cpu_backwards)
 
