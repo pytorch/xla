43fc48324c6c76cfe4d0db6bf4b31ebcffca3567
JackCaoG <jackcao@google.com>
2022-09-14 01:17:25 +0000

    update dynamic test

--- test/cpp/test_ir.cpp
+++ test/cpp/test_ir.cpp
@@ -11,6 +11,7 @@
 #include "torch_xla/csrc/ops/scalar.h"
 #include "torch_xla/csrc/ops/select.h"
 #include "torch_xla/csrc/ops/unselect.h"
+#include "torch_xla/csrc/ops/update_slice.h"
 
 namespace torch_xla {
 namespace cpp_test {
@@ -123,14 +124,23 @@ TEST(IrTest, TestSizeNode) {
 }
 
 TEST(IrTest, TestSizeNodeDynamic) {
+  // t1 = torch.tensor(0).expand(10,10)
+  // t1[0][0] = 1
+  // res = t1.non_zero()
   torch::lazy::Value scalar_value =
       torch::lazy::Value(ScalarOp(0.0, xla::F32), 0);
+  torch::lazy::Value scalar_value_1 =
+      torch::lazy::Value(ScalarOp(1.0, xla::F32), 0);
   std::vector<int64_t> target_size = {10, 10};
   torch::lazy::NodePtr expand_node =
       torch::lazy::MakeNode<Expand>(scalar_value, target_size);
   torch::lazy::Value expand_value = torch::lazy::Value(expand_node, 0);
+  std::vector<int64_t> base_indeies = {0, 0};
+  torch::lazy::NodePtr slice_node = torch::lazy::MakeNode<UpdateSlice>(
+      expand_value, scalar_value_1, base_indeies);
+  torch::lazy::Value slice_value = torch::lazy::Value(slice_node, 0);
   torch::lazy::NodePtr nonzero_node =
-      torch::lazy::MakeNode<NonZero>(expand_value);
+      torch::lazy::MakeNode<NonZero>(slice_value);
 
   torch::lazy::NodePtr size_node_nonzero_0 =
       torch::lazy::MakeNode<SizeNode>(nonzero_node, 0);
@@ -144,7 +154,7 @@ TEST(IrTest, TestSizeNodeDynamic) {
           size_node_nonzero_1);
 
   EXPECT_EQ(dim_node_0->getStaticValue(), 100);
-  EXPECT_EQ(dim_node_0->getDynamicValue(), 0);
+  EXPECT_EQ(dim_node_0->getDynamicValue(), 1);
   EXPECT_EQ(dim_node_1->getStaticValue(), 2);
   EXPECT_EQ(dim_node_1->getDynamicValue(), 2);
 }
