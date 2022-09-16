9ecf48f17b3780576f0db690c0380d88ccf3f783
Jiewen Tan <jwtan@google.com>
2022-09-16 07:33:33 +0000

    Add XRT testcase

---
+++ test/test_ddp.py
@@ -0,0 +1,16 @@
+from absl.testing import absltest, parameterized
+import torch_xla.distributed.xla_multiprocessing as xmp
+import pjrt.distributed_util as util
+
+
+class TestXrtDistributedDataParallel(parameterized.TestCase):
+  @staticmethod
+  def _ddp_correctness(self, rank):
+    util.ddp_correctness(None)
+
+  def test_ddp_correctness(self):
+    xmp.spawn(util.ddp_correctness, args=())
+
+
+if __name__ == "__main__":
+  absltest.main()
