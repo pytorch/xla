88461915429fb3352652d77fa1651785b6e54ecd
Jiewen Tan <jwtan@google.com>
2022-09-16 08:22:23 +0000

    Fix test_ddp.py

--- test/test_ddp.py
+++ test/test_ddp.py
@@ -5,11 +5,11 @@ import pjrt.distributed_util as util
 
 class TestXrtDistributedDataParallel(parameterized.TestCase):
   @staticmethod
-  def _ddp_correctness(self, rank):
+  def _ddp_correctness(rank):
     util.ddp_correctness(None)
 
   def test_ddp_correctness(self):
-    xmp.spawn(util.ddp_correctness, args=())
+    xmp.spawn(self._ddp_correctness, args=())
 
 
 if __name__ == "__main__":
