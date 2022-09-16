848222aff52c986bc8e0f290ee87b7751dda282d
Jiewen Tan <jwtan@google.com>
2022-09-16 04:32:57 +0000

    Remove some imports

--- test/pjrt/test_ddp.py
+++ test/pjrt/test_ddp.py
@@ -1,13 +1,7 @@
 from absl.testing import absltest, parameterized
-from absl import logging
-import copy
-import torch
-import torch.distributed as dist
 import torch.nn as nn
-import torch.optim as optim
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch_xla.core.xla_model as xm
-import torch_xla.distributed.xla_backend
 from torch_xla.experimental import pjrt
 import distributed_util as util
 
