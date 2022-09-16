4ba775e7dc4bcbdf1ec0224d731059690e1d828b
Jiewen Tan <jwtan@google.com>
2022-09-16 04:20:38 +0000

    Make PJRT work on V4

--- test/pjrt/test_ddp.py
+++ test/pjrt/test_ddp.py
@@ -9,16 +9,24 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 import torch_xla.core.xla_model as xm
 import torch_xla.distributed.xla_backend
 from torch_xla.experimental import pjrt
-from .. import distributed_util as util
+import distributed_util as util
 
 
 class TestPjRtDistributedDataParallel(parameterized.TestCase):
 
+  @staticmethod
+  def _ddp_init(init_file: str):
+    util.init_xla_backend(init_file)
+
+    device = xm.xla_device()
+    model = nn.Linear(10, 10).to(device)
+    ddp_model = DDP(model)
+
   def test_ddp_init(self):
-    pjrt.run_multiprocess(util.ddp_init, self.create_tempfile().full_path)
+    pjrt.run_multiprocess(self._ddp_init, self.create_tempfile().full_path)
 
   def test_ddp_correctness(self):
-    pjrt.run_multiprocess(self.ddp_correctness,
+    pjrt.run_multiprocess(util.ddp_correctness,
                           self.create_tempfile().full_path)
 
 
