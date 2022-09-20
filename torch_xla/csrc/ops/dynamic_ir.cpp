4de0e6ef3b75e269411110fc2aeae5633df8292d
JackCaoG <59073027+JackCaoG@users.noreply.github.com>
2022-09-20 09:54:42 -0700

    Cache dynamic ir result (#4008)
    * Cache the result of getDynamicvalue for SizeNode
    
    * Update counter test
    
    * fix test
    
    * Fix comment

--- torch_xla/csrc/ops/dynamic_ir.cpp
+++ torch_xla/csrc/ops/dynamic_ir.cpp
@@ -35,13 +35,18 @@ SizeNode::SizeNode(torch::lazy::Value input, size_t dim)
 };
 
 int64_t SizeNode::getDynamicValue() const {
+  if (dynamic_value_computed_) {
+    XLA_COUNTER("CachedSizeNodeValue", 1);
+    return runtime_size_;
+  }
   torch::lazy::NodePtr cloned =
       torch::lazy::MakeNode<SizeNode>(operands_[0], dim_);
   std::vector<XLATensorPtr> dummy_size_tensors = {
       XLATensor::Create(cloned, *GetDefaultDevice(), at::ScalarType::Long)};
-  // TODO: cache the result
   std::vector<at::Tensor> res = XLATensor::GetTensors(&dummy_size_tensors);
-  return res[0].sum().item().toInt();
+  runtime_size_ = res[0].item().toInt();
+  dynamic_value_computed_ = true;
+  return runtime_size_;
 }
 
 XlaOpVector SizeNode::Lower(LoweringContext* loctx) const {
