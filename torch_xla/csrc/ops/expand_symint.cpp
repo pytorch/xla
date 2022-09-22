31825f3d5ad7f09f7f7ec8201b79c55a33102de9
Milad Mohammadi <milad.mo@gmail.com>
2022-09-22 11:16:04 -0700

    Support `expand.symint` with real symbolic symint (#3889)
    * Add expand.symint
    
    * linter
    
    * addressed some of the comments
    
    * made major change to the code architecture
    
    * linter
    
    * Update ExpandSymintNode
    
    * Add ExpandSymIntSymbolic
    
    * Add ExpandSymIntSymbolic
    
    * Add TestExpandSymIntDynamic
    
    * linter
    
    * Fix review comments
    
    * Update test_aten_xla_tensor.cpp
    
    * update comment
    
    Co-authored-by: JackCaoG <jackcao@google.com>
    Co-authored-by: JackCaoG <59073027+JackCaoG@users.noreply.github.com>

---
+++ torch_xla/csrc/ops/expand_symint.cpp
@@ -0,0 +1,72 @@
+#include "torch_xla/csrc/ops/expand_symint.h"
+
+#include "absl/strings/str_join.h"
+#include "tensorflow/compiler/xla/client/lib/constants.h"
+#include "tensorflow/compiler/xla/xla_client/debug_macros.h"
+#include "torch/csrc/lazy/core/helpers.h"
+#include "torch/csrc/lazy/core/util.h"
+#include "torch_xla/csrc/data_ops.h"
+#include "torch_xla/csrc/lowering_context.h"
+#include "torch_xla/csrc/ops/infer_output_shape.h"
+
+namespace torch_xla {
+namespace {
+
+xla::Shape NodeOutputShape(const torch::lazy::Value& input,
+                           const std::vector<int64_t> upper_bounds,
+                           const std::vector<bool> dynamic_dims) {
+  return xla::ShapeUtil::MakeShape(GetXlaShape(input).element_type(),
+                                   {upper_bounds}, {dynamic_dims});
+}
+
+std::vector<torch::lazy::Value> GetValues(
+    const torch::lazy::Value& input,
+    const std::vector<torch::lazy::NodePtr>& dimensions) {
+  std::vector<torch::lazy::Value> values;
+  values.reserve(dimensions.size() + 1);
+  values.push_back(input);
+  for (torch::lazy::NodePtr dim : dimensions) {
+    if (dim) {
+      // Dimension Node only exist for symbolic dimension.
+      values.push_back(torch::lazy::Value(dim, 0));
+    }
+  }
+  return values;
+}
+
+}  // namespace
+
+ExpandSymInt::ExpandSymInt(const torch::lazy::Value& input,
+                           const SymIntElements& size_elements)
+    : XlaNode(torch::lazy::OpKind(at::aten::expand),
+              GetValues(input, size_elements.GetSizeNodes()),
+              [&]() {
+                return NodeOutputShape(input, size_elements.GetUpperBounds(),
+                                       size_elements.GetDynamicDims());
+              },
+              /*num_outputs=*/1,
+              torch::lazy::MHash(size_elements.GetUpperBounds(),
+                                 size_elements.GetDynamicDims())),
+      upper_bounds_(size_elements.GetUpperBounds()),
+      dynamic_dims_(size_elements.GetDynamicDims()) {}
+
+XlaOpVector ExpandSymInt::Lower(LoweringContext* loctx) const {
+  xla::XlaOp input = loctx->GetOutputOp(operand(0));
+  std::vector<xla::XlaOp> size_ops;
+  for (int i = 1; i < operands().size(); i++) {
+    size_ops.push_back(loctx->GetOutputOp(operand(i)));
+  }
+  xla::XlaOp output = SetDimensionSizes(BuildExpand(input, upper_bounds_),
+                                        size_ops, dynamic_dims_);
+  return ReturnOp(output, loctx);
+}
+
+std::string ExpandSymInt::ToString() const {
+  std::stringstream ss;
+  ss << XlaNode::ToString() << ", size=(" << absl::StrJoin(upper_bounds_, ", ")
+     << ")"
+     << ", dynamic_dims=(" << absl::StrJoin(dynamic_dims_, ", ") << ")";
+  return ss.str();
+}
+
+}  // namespace torch_xla
---
+++ torch_xla/csrc/ops/expand_symint.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+
+#include "torch_xla/csrc/ir.h"
+#include "torch_xla/csrc/torch_util.h"
+
+namespace torch_xla {
+
+class ExpandSymInt : public XlaNode {
+ public:
+  ExpandSymInt(const torch::lazy::Value& input,
+               const SymIntElements& size_elements);
+
+  std::string ToString() const override;
+
+  XlaOpVector Lower(LoweringContext* loctx) const override;
+
+  const std::vector<int64_t>& size() const { return upper_bounds_; };
+
+  const bool IsDynamic(int index) const { return dynamic_dims_[index]; };
+
+ private:
+  std::vector<int64_t> upper_bounds_;
+  std::vector<bool> dynamic_dims_;
+};
+
+}  // namespace torch_xla
