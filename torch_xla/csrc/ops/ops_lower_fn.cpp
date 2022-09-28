75283e9398800d0c3941f84e9a90e810586f9cb6
JackCaoG <59073027+JackCaoG@users.noreply.github.com>
2022-09-28 10:14:01 -0700

    Revert "Update amax/amin/norm signatures with int[*]? dim  (#4035)" (#4059)
    This reverts commit 2cdfa5efd74bee35dea5c034f79ce6a4d40034a5.

--- torch_xla/csrc/ops/ops_lower_fn.cpp
+++ torch_xla/csrc/ops/ops_lower_fn.cpp
@@ -85,24 +85,12 @@ torch_xla::XlaOpVector AllDim::Lower(LoweringContext* loctx) const {
 
 torch_xla::XlaOpVector Amax::Lower(LoweringContext* loctx) const {
   xla::XlaOp input = loctx->GetOutputOp(operand(0));
-  absl::Span<const int64_t> dims;
-  if (dim.has_value()) {
-    dims = dim.value();
-  } else {
-    dims = {XlaHelpers::ShapeOfXlaOp(input).rank()};
-  }
-  return ReturnOp(BuildMaxInDims(input, dims, keepdim), loctx);
+  return ReturnOp(BuildMaxInDims(input, dim, keepdim), loctx);
 }
 
 torch_xla::XlaOpVector Amin::Lower(LoweringContext* loctx) const {
   xla::XlaOp input = loctx->GetOutputOp(operand(0));
-  absl::Span<const int64_t> dims;
-  if (dim.has_value()) {
-    dims = dim.value();
-  } else {
-    dims = {XlaHelpers::ShapeOfXlaOp(input).rank()};
-  }
-  return ReturnOp(BuildMinInDims(input, dims, keepdim), loctx);
+  return ReturnOp(BuildMinInDims(input, dim, keepdim), loctx);
 }
 
 torch_xla::XlaOpVector Any::Lower(LoweringContext* loctx) const {
