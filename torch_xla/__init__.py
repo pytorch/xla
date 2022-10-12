afaab1f65eb85a723c66df34fba59d9ef8be87ec
Will Cromar <wcromar@google.com>
2022-10-12 13:50:46 -0700

    [PJRT] Enable PJRT C API option (#4077)
    * Enable PJRT C API option
    
    * Add patch for `pjrt_c_api_client.cc`
    
    * Remove obsolete comment.
    
    * Only use `on_device_shape()` with C API

--- torch_xla/__init__.py
+++ torch_xla/__init__.py
@@ -109,10 +109,9 @@ from .version import __version__
 logger.info(
     'Letting libtpu.so load fail during _XLAC import. libtpu.so will be loaded '
     'from `libtpu` Python package when the ComputationClient is created.')
-# _tpu_vm_init() will update TPU_LIBRARY_PATH to Python package, if available
-os.environ['TPU_LIBRARY_PATH'] = '/dev/null'
+os.environ['TPU_LOAD_LIBRARY'] = '0'
 import _XLAC
-del os.environ['TPU_LIBRARY_PATH']
+del os.environ['TPU_LOAD_LIBRARY']
 
 _tpu_vm_init()
 
