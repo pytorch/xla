0c4f35a6d9fde9fb816159da2703aaa235858eea
Nick Korovaiko <korovaikon@gmail.com>
2022-10-13 10:36:49 -0700

    Adding bool operator and a test (#4080)
    * Adding bool operator and a test
    
    * linter
    
    Co-authored-by: Milad Mohammadi <milad.mo@gmail.com>

---
+++ test/test_dynamic_shapes.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import make_blobs
+import torch, torch_xla
+import torch_xla.core.xla_model as xm
+import unittest
+
+dev = xm.xla_device()
+
+
+class TestDynamicShapes(unittest.TestCase):
+
+  def test_wrap(self):
+    a1 = torch.tensor([[1, 0, 0, 5, 0, 6]], device=dev)
+    a2 = torch.nonzero(a1)
+    a3 = a2.shape[0] + 3  # tests wrap
+    self.assertIsInstance(a3, torch.SymIntNode)
+
+  def test_wrap(self):
+    a1 = torch.tensor([[1, 0, 0, 5, 0, 6]], device=dev)
+    a2 = torch.nonzero(a1)
+    self.assertTrue(a2.shape[0] == 3)
+
+
+if __name__ == '__main__':
+  test = unittest.main()
+  sys.exit(0 if test.result.wasSuccessful() else 1)
