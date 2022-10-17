9c0fb95d2d179689a3da91ea1a9d96616c1041ee
Wonjoo Lee <wonjoo@google.com>
2022-10-17 23:23:31 +0000

    Update tests

--- test/cpp/test_lazy.cpp
+++ test/cpp/test_lazy.cpp
@@ -12,7 +12,7 @@ class LazyTest : public TorchXlaTest {};
 
 TEST_F(LazyTest, TestXlaShapeToLazyWithF64) {
   int64_t dimensions[] = {1};
-  bool dynamic_dimensions[] = {false, false};
+  bool dynamic_dimensions[] = {false};
   absl::Span<const int64_t> xla_dimensions =
       absl::Span<const int64_t>(dimensions);
   absl::Span<const bool> xla_dynamic_dimensions =
@@ -33,7 +33,7 @@ TEST_F(LazyTest, TestXlaShapeToLazyWithF64) {
 
 TEST_F(LazyTest, TestXlaShapeToLazyWithPred) {
   int64_t dimensions[] = {1};
-  bool dynamic_dimensions[] = {false, false};
+  bool dynamic_dimensions[] = {false};
   absl::Span<const int64_t> xla_dimensions =
       absl::Span<const int64_t>(dimensions);
   absl::Span<const bool> xla_dynamic_dimensions =
@@ -54,7 +54,7 @@ TEST_F(LazyTest, TestXlaShapeToLazyWithPred) {
 
 TEST_F(LazyTest, TestXlaShapeToLazyWithU64) {
   int64_t dimensions[] = {1};
-  bool dynamic_dimensions[] = {false, false};
+  bool dynamic_dimensions[] = {false};
   absl::Span<const int64_t> xla_dimensions =
       absl::Span<const int64_t>(dimensions);
   absl::Span<const bool> xla_dynamic_dimensions =
@@ -75,7 +75,7 @@ TEST_F(LazyTest, TestXlaShapeToLazyWithU64) {
 
 TEST_F(LazyTest, TestXlaShapeToLazyWithMultipleDimensions) {
   int64_t dimensions[] = {2, 1, 3};
-  bool dynamic_dimensions[] = {false, false};
+  bool dynamic_dimensions[] = {false, false, false};
   absl::Span<const int64_t> xla_dimensions =
       absl::Span<const int64_t>(dimensions);
   absl::Span<const bool> xla_dynamic_dimensions =
@@ -96,7 +96,7 @@ TEST_F(LazyTest, TestXlaShapeToLazyWithMultipleDimensions) {
 
 TEST_F(LazyTest, TestXlaShapeToLazyWithDynamicDimensions) {
   int64_t dimensions[] = {2, 1, 3};
-  bool dynamic_dimensions[] = {true, false};
+  bool dynamic_dimensions[] = {true, false, true};
   absl::Span<const int64_t> xla_dimensions =
       absl::Span<const int64_t>(dimensions);
   absl::Span<const bool> xla_dynamic_dimensions =
@@ -116,6 +116,20 @@ TEST_F(LazyTest, TestXlaShapeToLazyWithDynamicDimensions) {
   EXPECT_EQ(lazy_dynamic_dimensions.value(),
             std::vector<bool>(std::begin(dynamic_dimensions),
                               std::end(dynamic_dimensions)));
+}
+
+TEST_F(LazyTest, TestXlaShapeToLazyWithUnsupportedPrimitiveType) {
+  int64_t dimensions[] = {1};
+  bool dynamic_dimensions[] = {false};
+  absl::Span<const int64_t> xla_dimensions =
+      absl::Span<const int64_t>(dimensions);
+  absl::Span<const bool> xla_dynamic_dimensions =
+      absl::Span<const bool>(dynamic_dimensions);
+  std::vector<xla::Shape> xla_tuple_shapes = std::vector<xla::Shape>();
+  xla::Shape xla_shape = xla::Shape(xla::PrimitiveType::TUPLE, xla_dimensions,
+                                    xla_dynamic_dimensions, xla_tuple_shapes);
+
+  EXPECT_THROW(XlaHelpers::ConvertXlaShapeToLazy(xla_shape), std::runtime_error);
 }
 
 }  // namespace cpp_test
