af82f3c08bffa5ef0212446ef0f39ed9986703d8
Wonjoo Lee <wonjoo@google.com>
2022-10-27 08:58:57 +0000

    Override gen() in GenXlaLazyIr

--- scripts/gen_lazy_tensor.py
+++ scripts/gen_lazy_tensor.py
@@ -29,7 +29,6 @@ def is_boolean_dtype(lazy_type):
 
 @dataclass(frozen=True)
 class GenXlaLazyIR(GenLazyIR):
-
   def lowering_function(self, f: Union[NativeFunctionsGroup,
                                        NativeFunction]) -> str:
     return f"""torch_xla::XlaOpVector Lower(LoweringContext* loctx) const override;"""
@@ -61,10 +60,106 @@ class GenXlaLazyIR(GenLazyIR):
     scalar_hashes = ", ".join([f"{a.name}" for a in scalar_args])
 
     return f"""{self.node_base}(torch::lazy::OpKind({aten_symbol(schema)}),
-              {{{base_ctor_value_args}}}, std::move(shapes),
+              {{{base_ctor_value_args}}},
               [&]() {{ return {schema.node_name}OutputShape({shape_fn_inputs}); }},
               /* num_outputs */ {len(schema.returns)},
               torch::lazy::MHash({scalar_hashes}))"""
+
+  def gen(self, schema: LazyIrSchema) -> List[str]:
+      opkind = schema.opkind or aten_symbol(schema)
+
+      # for now, we just want one IR class decl and soon after also the method defs
+      # and we use the functional version not out/inplace.
+      all_args = schema.filtered_args()
+      value_args = schema.filtered_args(values=True, scalars=False)
+      scalar_args = schema.filtered_args(values=False, scalars=True)
+
+      ctor_args = [f"const {i.lazy_type.cpp_type()}& {i.name}" for i in all_args]
+      reuse_ctor_args = ", ".join(ctor_args)
+      # if schema.properties.ShapePrecompute:
+      #     ctor_args.append("std::vector<wonjoo>&& shapes")
+      node_ctor_args = ", ".join(ctor_args)
+
+      scalar_initializers = ",\n        ".join(
+          [
+              # This code is just special casing the mapping from string_view -> strings
+              f"{a.name}({a.name}.has_value() ? c10::make_optional(std::string(*{a.name})) : c10::nullopt)"
+              if a.lazy_type.cpp_type() == "c10::optional<c10::string_view>"
+              else f"{a.name}({a.name})"
+              for a in scalar_args
+          ]
+      )
+      if len(scalar_initializers):
+          scalar_initializers = f",\n        {scalar_initializers}"
+      scalar_decls = "\n  ".join(
+          [
+              f"std::string {a.name};"
+              if a.lazy_type.cpp_type() == "c10::string_view"
+              else f"c10::optional<std::string> {a.name};"
+              if a.lazy_type.cpp_type() == "c10::optional<c10::string_view>"
+              else f"{a.lazy_type.cpp_type()} {a.name};"
+              for a in scalar_args
+          ]
+      )
+      optional_values = [
+          arg.name
+          for arg in schema.filtered_args(values=True, scalars=False)
+          if isinstance(arg.lazy_type, OptionalCType)
+      ]
+      has_optional_decls = "\n  ".join(
+          [f"bool has_{value}: 1;" for value in optional_values]
+      )
+      has_optional_defs = "\n    ".join(
+          [f"has_{value} = !!{value};" for value in optional_values]
+      )
+      members_to_string = []
+      for arg in scalar_args:
+          if isinstance(arg.lazy_type, OptionalCType):
+              members_to_string.append(
+                  f"""if ({arg.name}.has_value()) {{
+    ss << ", {arg.name}=" << {arg.name}.value();
+  }} else {{
+    ss << ", {arg.name}=null";
+  }}"""
+              )
+          else:
+              members_to_string.append(f'ss << ", {arg.name}=" << {arg.name};')
+      members_to_string_str = "\n    ".join(members_to_string)
+
+      return [
+          f"""\
+class {schema.node_name} : public {self.node_base} {{
+public:
+static torch::lazy::OpKind ClassOpKind() {{
+  return torch::lazy::OpKind({opkind});
+}}
+
+{schema.node_name}({node_ctor_args})
+    : {self.node_base_ctor_call(schema)}{scalar_initializers}
+{{
+  {has_optional_defs}
+}}
+
+std::string ToString() const override {{
+  std::stringstream ss;
+  ss << {self.node_base}::ToString();
+  {members_to_string_str}
+  return ss.str();
+}}
+
+{self.create_function(schema, reuse_ctor_args)}
+
+{self.can_be_reused_function(schema, reuse_ctor_args)}
+
+{self.lowering_function(schema)}
+
+{scalar_decls}
+{has_optional_decls}
+
+}};
+
+""",
+      ]
 
 
 @dataclass(frozen=True)
@@ -94,7 +189,6 @@ if __name__ == '__main__':
       node_base_hdr="torch_xla/csrc/generated_file_include.h",
       tensor_class="torch_xla::XLATensor",
       tensor_class_hdr="torch_xla/csrc/tensor.h",
-      shape_class="xla::Shape",
       shape_inference_hdr=shape_inference_hdr,
       lazy_ir_generator=GenXlaLazyIR,
       native_func_definition_generator=GenXlaLazyNativeFuncDefinition,
