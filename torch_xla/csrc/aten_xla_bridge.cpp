684db591d7c0c49f8e9630a1f9fbb3669401daf7
Brian Hirsh <hirsheybar@fb.com>
2022-11-04 10:35:58 -0700

    POC of functionalization integration

--- torch_xla/csrc/aten_xla_bridge.cpp
+++ torch_xla/csrc/aten_xla_bridge.cpp
@@ -172,8 +172,12 @@ std::vector<at::Tensor> XlaCreateTensorList(const at::ITensorListRef& tensors) {
   for (size_t i = 0, defined_pos = 0; i < tensors.size(); ++i) {
     if (to_translate[i]) {
       auto tensor = defined_aten_xla_tensors[defined_pos++];
+<<<<<<< HEAD
       XLA_CHECK(!at::functionalization::impl::isFunctionalTensor(tensor))
           << "Expected non-functional tensor!";
+=======
+      XLA_CHECK(!at::functionalization::impl::isFunctionalTensor(tensor)) << "Expected non-functional tensor!";
+>>>>>>> POC of functionalization integration
       // This function is responsible for returning CPU tensors.
       // So we do not want to wrap the outputs into FunctionalTensorWrappers.
       aten_xla_tensors[i] = tensor;
@@ -349,14 +353,14 @@ at::Tensor XlaToAtenTensor(XLATensorPtr xla_tensor,
 
 at::Tensor AtenFromXlaTensor(XLATensorPtr xla_tensor) {
   if (xla_tensor) {
-    auto out =
-        at::Tensor(c10::make_intrusive<XLATensorImpl>(std::move(xla_tensor)));
+    auto out = at::Tensor(c10::make_intrusive<XLATensorImpl>(
+                          std::move(xla_tensor)));
     // See Note [Lazy Tensor Functionalization]
     if (c10::impl::tls_local_dispatch_key_set().excluded_.has(
             c10::DispatchKey::Functionalize)) {
-      // Invariant: if the functionalization key is in the exclude set, then
-      // we're expected to return an ordinary tensor, which will be "lifted"
-      // into a functional wrapper later.
+      // Invariant: if the functionalization key is in the exclude set, then we're
+      // expected to return an ordinary tensor, which will be "lifted" into a
+      // functional wrapper later.
       return out;
     } else {
       auto wrapped = at::functionalization::impl::to_functional_tensor(out);
