b096c5cf7be124cdbd957d239265c1b62ec1e5b5
Yeounoh Chung <yeounoh@google.com>
2022-11-08 14:48:52 -0800

    [SPMD] Mesh to support custom device order. (#4162)

--- test/test_xla_sharding.py
+++ test/test_xla_sharding.py
@@ -11,25 +11,48 @@ import torch_xla.core.xla_model as xm
 import torch_xla.utils.utils as xu
 import torch_xla.experimental.xla_sharding as xs
 from torch_xla.experimental.xla_sharded_tensor import XLAShardedTensor
+from torch_xla.experimental.pjrt import using_pjrt
 
 
-@unittest.skipIf((os.getenv('PJRT_DEVICE') == "") or (
-    xm.get_xla_supported_devices("GPU") is not None
-), "PyTorch/XLA SPMD requires PJRT_DEVICE={CPU, TPU}, GPU is currently not supported."
-                )
+@unittest.skipIf(not using_pjrt() or xm.get_xla_supported_devices("GPU"),
+                 f"Requires PJRT_DEVICE set to `TPU` or `CPU`.")
 class XlaShardingTest(unittest.TestCase):
 
+  n_devices = 0
+  device_ids = None
+
+  @classmethod
+  def setUpClass(cls):
+    cls.n_devices = len(xm.get_xla_supported_devices())
+    cls.device_ids = np.array(range(cls.n_devices))
+
+  def _get_mesh(self, mesh_shape, device_ids=None):
+    if device_ids is None:
+      device_ids = self.device_ids
+    assert len(device_ids) == self.n_devices
+    return xs.Mesh(device_ids, mesh_shape)
+
   def test_xla_sharded_tensor(self):
-    n_devices = xm.xrt_world_size()
-    mesh_shape = (1, n_devices)
     partition_spec = (0, 1)
     xt1 = torch.tensor([[1, 2, 3, 4, 5, 6, 7, 8]],
                        dtype=torch.float,
                        device=xm.xla_device())
-    xst1 = xs.mark_sharding(xt1, mesh_shape, partition_spec)
+    xst1 = xs.mark_sharding(xt1, self._get_mesh((1, self.n_devices)),
+                            partition_spec)
 
     # TODO(244003536) add more tests for XLAShardedTensror.
     self.assertTrue(isinstance(xst1, XLAShardedTensor))
+
+  def test_custom_tile_assignment(self):
+    xt = torch.randn(10, 20).to(device=xm.xla_device())
+    mesh_shape = (1, self.n_devices)
+    device_ids = np.flip(self.device_ids)
+    mesh = self._get_mesh(mesh_shape, device_ids)
+    xs.mark_sharding(xt, mesh, (0, 1))
+    annotation = '{devices=[1,%d]%s}' % (self.n_devices, ','.join([
+        str(i) for i in reversed(range(self.n_devices))
+    ])) if self.n_devices > 1 else '{maximal device=0}'
+    self.assertEqual(annotation, torch_xla._XLAC._get_xla_sharding_spec(xt))
 
   def test_mark_sharding_2d(self):
     t1 = torch.randn(1, 128, device='cpu')
@@ -38,11 +61,10 @@ class XlaShardingTest(unittest.TestCase):
 
     xt1 = t1.to(xm.xla_device())
     xt2 = t2.to(xm.xla_device())
-    n_devices = xm.xrt_world_size()
-    xs.mark_sharding(xt1, (1, n_devices), (0, 1))
-    annotation = '{devices=[1,%d]%s}' % (n_devices, ','.join(
-        [str(i)
-         for i in range(n_devices)])) if n_devices > 1 else '{maximal device=0}'
+    xs.mark_sharding(xt1, self._get_mesh((1, self.n_devices)), (0, 1))
+    annotation = '{devices=[1,%d]%s}' % (self.n_devices, ','.join([
+        str(i) for i in range(self.n_devices)
+    ])) if self.n_devices > 1 else '{maximal device=0}'
     self.assertEqual(annotation, torch_xla._XLAC._get_xla_sharding_spec(xt1))
 
     actual = (xt1 @ xt2.T).cpu()
@@ -53,11 +75,11 @@ class XlaShardingTest(unittest.TestCase):
     expected = t + t
 
     xt = t.to(xm.xla_device())
-    n_devices = xm.xrt_world_size()
-    xs.mark_sharding(xt, (1, 1, 1, n_devices), (0, 1, 2, 3))
-    annotation = '{devices=[1,1,1,%d]%s}' % (n_devices, ','.join(
-        [str(i)
-         for i in range(n_devices)])) if n_devices > 1 else '{maximal device=0}'
+    xs.mark_sharding(xt, self._get_mesh((1, 1, 1, self.n_devices)),
+                     (0, 1, 2, 3))
+    annotation = '{devices=[1,1,1,%d]%s}' % (self.n_devices, ','.join([
+        str(i) for i in range(self.n_devices)
+    ])) if self.n_devices > 1 else '{maximal device=0}'
     self.assertEqual(annotation, torch_xla._XLAC._get_xla_sharding_spec(xt))
 
     actual = (xt + xt).cpu()
@@ -65,16 +87,16 @@ class XlaShardingTest(unittest.TestCase):
 
   def test_clear_sharding(self):
     xt = torch.randn(2, 4, 8, 16).to(xm.xla_device())
-    n_devices = xm.xrt_world_size()
-    xs.mark_sharding(xt, (1, 1, 1, n_devices), (0, 1, 2, 3))
+    xs.mark_sharding(xt, self._get_mesh((1, 1, 1, self.n_devices)),
+                     (0, 1, 2, 3))
     self.assertTrue(torch_xla._XLAC._get_xla_sharding_spec(xt))
     xs.clear_sharding(xt)
     self.assertFalse(torch_xla._XLAC._get_xla_sharding_spec(xt))
 
   def test_deep_copy(self):
     xt = torch.randn(2, 4, 8, 16).to(xm.xla_device())
-    n_devices = xm.xrt_world_size()
-    xs.mark_sharding(xt, (1, 1, 1, n_devices), (0, 1, 2, 3))
+    xs.mark_sharding(xt, self._get_mesh((1, 1, 1, self.n_devices)),
+                     (0, 1, 2, 3))
     xt2 = copy.deepcopy(xt)
     self.assertEqual(
         torch_xla._XLAC._get_xla_sharding_spec(xt),
