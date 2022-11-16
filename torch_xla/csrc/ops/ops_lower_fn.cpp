121dbd4cf0c9aa1b18a0c979a64f936dadab8a9b
ManfeiBai <manfei@google.com>
2022-11-16 06:35:02 +0000

    clear again again

--- torch_xla/csrc/ops/ops_lower_fn.cpp
+++ torch_xla/csrc/ops/ops_lower_fn.cpp
@@ -133,16 +133,16 @@ torch_xla::XlaOpVector Atanh::Lower(LoweringContext* loctx) const {
 }
 
 torch_xla::XlaOpVector Baddbmm::Lower(LoweringContext* loctx) const {
-  xla::XlaOp xla_self = loctx->GetOutputOp(operand(0));
-  xla::XlaOp xla_batch1 = loctx->GetOutputOp(operand(1));
-  xla::XlaOp xla_batch2 = loctx->GetOutputOp(operand(2));
-  xla::XlaOp xla_beta = loctx->GetOutputOp(operand(3));
-  xla::XlaOp xla_alpha = loctx->GetOutputOp(operand(4));
-  std::tie(xla_batch1, xla_batch2) =
-      XlaHelpers::PromoteValues(xla_batch1, xla_batch2);
+  xla::XlaOp xla_lhs = loctx->GetOutputOp(operand(0));
+  xla::XlaOp xla_rhs = loctx->GetOutputOp(operand(1));
+  xla::XlaOp xla_bias = loctx->GetOutputOp(operand(2));
+  xla::XlaOp xla_product_multiplier = loctx->GetOutputOp(operand(3));
+  xla::XlaOp xla_bias_multiplier = loctx->GetOutputOp(operand(4));
+  std::tie(xla_lhs, xla_rhs) =
+      XlaHelpers::PromoteValues(xla_lhs, xla_rhs);
 
-  return ReturnOp(BuildMatMulWithMultiplier(xla_batch1, xla_batch2, xla_self,
-                                            xla_alpha, xla_beta),
+  return ReturnOp(BuildMatMulWithMultiplier(xla_lhs, xla_rhs, xla_bias,
+                                            xla_product_multiplier, xla_bias_multiplier),
                   loctx);
 }
 
