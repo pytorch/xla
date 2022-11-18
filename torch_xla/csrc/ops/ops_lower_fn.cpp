f272554446d3ffae8003692dceb06cdeca7c443a
ManfeiBai <manfei@google.com>
2022-11-18 20:28:45 +0000

    format

--- torch_xla/csrc/ops/ops_lower_fn.cpp
+++ torch_xla/csrc/ops/ops_lower_fn.cpp
@@ -133,10 +133,6 @@ torch_xla::XlaOpVector Atanh::Lower(LoweringContext* loctx) const {
 }
 
 torch_xla::XlaOpVector Baddbmm::Lower(LoweringContext* loctx) const {
-  // return input->CreateFrom(torch::lazy::MakeNode<Baddbmm>(
-  //     input->GetIrValue(), batch1->GetIrValue(), batch2->GetIrValue(),
-  //     bias_multiplier, product_multiplier));
-  // // self, batch1, batch2, beta, alpha
   xla::XlaOp xla_self = loctx->GetOutputOp(operand(0));
   xla::XlaOp xla_batch1 = loctx->GetOutputOp(operand(1));
   xla::XlaOp xla_batch2 = loctx->GetOutputOp(operand(2));
@@ -144,13 +140,6 @@ torch_xla::XlaOpVector Baddbmm::Lower(LoweringContext* loctx) const {
   xla::XlaOp xla_alpha = loctx->GetOutputOp(operand(4));
   std::tie(xla_batch1, xla_batch2) =
       XlaHelpers::PromoteValues(xla_batch1, xla_batch2);
-
-  LOG(WARNING) << "opes_lower_fn.cpp(((((((((((((((((((((((((((((((";
-  LOG(WARNING) << "xla_self      " << xla_self;
-  LOG(WARNING) << "xla_batch1    " << xla_batch1;
-  LOG(WARNING) << "xla_batch2    " << xla_batch2;
-  LOG(WARNING) << "xla_bate      " << xla_beta;
-  LOG(WARNING) << "xla_alpha     " << xla_alpha;
 
   return ReturnOp(BuildMatMulWithMultiplier(xla_batch1, xla_batch2, xla_self,
                                             xla_alpha, xla_beta),
