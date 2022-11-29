2f004e818f6a329ef5ee08b41f77ccf402a0f426
Xiongfei Wei <isaacwxf23@gmail.com>
2022-11-29 23:19:57 +0000

    improve the model by making the weights in the linear layer deterministic.

--- test/dynamic_shape_model_test.py
+++ test/dynamic_shape_model_test.py
@@ -1,4 +1,3 @@
-from sklearn.datasets import make_blobs
 import torch
 import torch_xla.core.xla_model as xm
 
@@ -8,13 +7,18 @@ dev = xm.xla_device()
 # SIMPLE NN MODEL
 class Feedforward(torch.nn.Module):
         def __init__(self, input_size, hidden_size):
-            super(Feedforward, self).__init__()
+            super().__init__()
             self.input_size = input_size
             self.hidden_size  = hidden_size
             self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
+            self.fc1.weight.data.fill_(0.01)
+            self.fc1.bias.data.fill_(0.01)
             self.relu = torch.nn.ReLU()
             self.fc2 = torch.nn.Linear(self.hidden_size, 1)
+            self.fc2.weight.data.fill_(0.01)
+            self.fc2.bias.data.fill_(0.01)
             self.sigmoid = torch.nn.Sigmoid()
+
         def forward(self, x):
             hidden = self.fc1(x)
             relu = self.relu(hidden)
@@ -22,17 +26,11 @@ class Feedforward(torch.nn.Module):
             output = self.sigmoid(output)
             return output
 
-# CREATE FAKE TRAIN DATA
 num_features = 2
-x_train, y_train = make_blobs(n_samples=40, n_features=num_features, cluster_std=1.5, shuffle=True)
-x_train = torch.Tensor(x_train)
-y_train = torch.Tensor(y_train)
-
-# CREATE FAKE TEST DATA
 num_test_samples = 5
-x_test = torch.ones_like(torch.empty(num_test_samples, num_features))
+x_test = torch.ones(num_test_samples, num_features)
 x_test[0][0] = 0
-y_test = torch.ones_like(torch.empty(num_test_samples*2))
+y_test = torch.ones(num_test_samples*2)
 y_test[0] = 0
 
 x_test = x_test.to(dev)
@@ -41,16 +39,19 @@ y_test = y_test.to(dev)
 y_test = torch.nonzero(y_test.int()).float().squeeze()
 
 # MODEL SETUP
-model = Feedforward(2, 10).to(dev)
+hidden_size = 10
+model = Feedforward(num_features, hidden_size).to(dev)
 criterion = torch.nn.BCELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
 
 # RUN THE FWD PASS
+# print(model)
 model.eval()
-y_pred = model(x_test)
-before_train = criterion(y_pred.squeeze(), y_test)
-xm.mark_step()
-print('Test loss before training' , before_train.item())
+with torch.no_grad():
+    y_pred = model(x_test)
+    before_train = criterion(y_pred.squeeze(), y_test)
+    xm.mark_step()
+    print('Test loss before training' , before_train.item())
 
 # DISABLE PYTHON DISPATCHER FLAG
 del pd
+
