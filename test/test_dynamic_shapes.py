95be5acbfc141c2ce914337c9f470e2bd012b10f
Xiongfei Wei <isaacwxf23@gmail.com>
2022-12-01 01:14:46 +0000

    Added more test for SizeAdd

--- test/test_dynamic_shapes.py
+++ test/test_dynamic_shapes.py
@@ -12,12 +12,13 @@ class TestDynamicShapes(unittest.TestCase):
     size2 = 2
     t1 = torch.zeros([size1, size2], device=dev)
     t1[3][0] = 1
+    t1[3][1] = 1
     # t2 has size [<=10, 2]
     t2 = torch.nonzero(t1)
     t5 = torch.ones(1, device=dev)
-    t5.expand(t2.size(0))
-    t5_cpu = t5.cpu()
-    self.assertEqual(t5_cpu.shape[0], 1)
+    t6 = t5.expand(t2.size(0))
+    t6_cpu = t6.cpu()
+    self.assertEqual(t6_cpu.shape[0], 2)
 
   def test_simple_expand_on_2d_tensor(self):
     size1 = 5
@@ -64,6 +65,7 @@ class TestDynamicShapes(unittest.TestCase):
     # t2 has size [<=10, 2]
     t2 = torch.nonzero(t1)
     # Create a SizeAdd IR node.
+    # t2.shape[1] generates a SizeConstant node.
     dyn_size = t2.shape[0] + t2.shape[1]
     # Exercises SizeAdd::getDynamicValue.
     dynamic_size = int(dyn_size)
@@ -71,8 +73,10 @@ class TestDynamicShapes(unittest.TestCase):
     # Exercise SizeAdd::getStaticValue.
     self.assertEqual(str(dyn_size), '<=12')
     t3 = torch.ones(1, device=dev)
+    # Exercise SizeAdd::Lower.
     t4 = t3.expand(dyn_size)
     self.assertEqual(t4.size(0), 3)
+    print(torch_xla._XLAC._get_xla_tensors_text([t4]))
 
 
 if __name__ == '__main__':
