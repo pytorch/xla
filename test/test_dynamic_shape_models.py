300640cddaa3d44e4a57827cec3bc04cdbfeb19f
Xiongfei Wei <isaacwxf23@gmail.com>
2022-12-03 00:22:07 +0000

    fix pr comments

--- test/test_dynamic_shape_models.py
+++ test/test_dynamic_shape_models.py
@@ -33,61 +33,36 @@ class Feedforward(torch.nn.Module):
 
 class TestDynamicShapeModels(unittest.TestCase):
 
-  def test_forward_pass_nn_model_correctness(self):
+  def test_forward_pass_dynamic_input_correctness(self):
     losses = []
     for dev in [torch.device('cpu'), xla_dev]:
       num_features = 2
       num_test_samples = 5
-      x_test = torch.ones(num_test_samples, num_features)
-      x_test[0][0] = 0
-      y_test = torch.ones(num_test_samples * 2)
-      y_test[0] = 0
+      x_test, y_test = self.create_dynamic_test_data(num_test_samples, num_features, dev)
 
-      x_test = x_test.to(dev)
-      x_test = torch.nonzero(x_test.int()).float()
-      y_test = y_test.to(dev)
-      y_test = torch.nonzero(y_test.int()).float().squeeze()
-
-      # MODEL SETUP
-      hidden_size = 10
-      model = Feedforward(num_features, hidden_size).to(dev)
+      model = Feedforward(num_features, hidden_size=10).to(dev)
       criterion = torch.nn.BCELoss()
 
-      # RUN THE FWD PASS
-      # print(model)
       model.eval()
       with torch.no_grad():
         y_pred = model(x_test)
         before_train = criterion(y_pred.squeeze(), y_test)
         xm.mark_step()
         losses.append(before_train.item())
-        print('Test loss before training', before_train.item())
 
     np.testing.assert_allclose(losses[0], losses[1], rtol=1e-2, atol=1e-2)
 
-  def test_forward_pass_nn_model_compile_once(self):
-    met.clear_counters()
+  def test_forward_pass_dynamic_input_compile_once(self):
+    met.clear_metrics()
     losses = []
-    for _ in range(2):
+    for _ in range(10):
       num_features = 2
       num_test_samples = 5
-      x_test = torch.ones(num_test_samples, num_features)
-      x_test[0][0] = 0
-      y_test = torch.ones(num_test_samples * 2)
-      y_test[0] = 0
+      x_test, y_test = self.create_dynamic_test_data(num_test_samples, num_features, xla_dev)
 
-      x_test = x_test.to(xla_dev)
-      x_test = torch.nonzero(x_test.int()).float()
-      y_test = y_test.to(xla_dev)
-      y_test = torch.nonzero(y_test.int()).float().squeeze()
-
-      # MODEL SETUP
-      hidden_size = 10
-      model = Feedforward(num_features, hidden_size).to(xla_dev)
+      model = Feedforward(num_features, hidden_size=10).to(xla_dev)
       criterion = torch.nn.BCELoss()
 
-      # RUN THE FWD PASS
-      # print(model)
       model.eval()
       with torch.no_grad():
         y_pred = model(x_test)
@@ -98,6 +73,18 @@ class TestDynamicShapeModels(unittest.TestCase):
     np.testing.assert_equal(met.metric_data('CompileTime')[0], 1)
     print('xw32 met.metric_data("CompileTime")=',
           met.metric_data('CompileTime'))
+
+  def create_dynamic_test_data(self, num_test_samples, num_features, device):
+    x_test = torch.ones(num_test_samples, num_features)
+    x_test[0][0] = 0
+    y_test = torch.ones(num_test_samples * 2)
+    y_test[0] = 0
+
+    x_test_xla = x_test.to(device)
+    x_test_nonzero_dev = torch.nonzero(x_test_xla.int()).float()
+    y_test_xla = y_test.to(device)
+    y_test_nonzero_dev = torch.nonzero(y_test_xla.int()).float().squeeze() 
+    return x_test_nonzero_dev, y_test_nonzero_dev
 
 
 if __name__ == '__main__':
