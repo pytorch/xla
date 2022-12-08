6e21a0bb47f6d302fc056d6119c96c061eadd161
Wonjoo Lee <wonjoo@google.com>
2022-12-08 08:24:14 +0000

    Update XLATensorImpl to extend LTCTensorImpl

--- torch_xla/csrc/tensor_impl.cpp
+++ torch_xla/csrc/tensor_impl.cpp
@@ -56,60 +56,17 @@ C10_REGISTER_GUARD_IMPL(XLA, XLAGuardImpl);
 
 }  // namespace
 
-XLATensorImpl::XLATensorImpl(XLATensor&& tensor)
-    : c10::TensorImpl(c10::DispatchKeySet{c10::DispatchKey::XLA,
-                                          c10::DispatchKey::AutogradXLA},
-                      GetTypeMeta(tensor),
-                      bridge::XlaDeviceToAtenDevice(tensor.GetDevice())),
-      tensor_(c10::make_intrusive<XLATensor>(std::move(tensor))) {
+XLATensorImpl::XLATensorImpl(XLATensorPtr tensor)
+    : torch::lazy::LTCTensorImpl(tensor, c10::DispatchKey::XLA, 
+        c10::DispatchKey::AutogradXLA, GetTypeMeta(*tensor), 
+        bridge::XlaDeviceToAtenDevice(tensor->GetDevice())) {
+  tensor_ = c10::make_intrusive<XLATensor>(std::move(*tensor));
   is_non_overlapping_and_dense_ = false;
   set_custom_sizes_strides(SizesStridesPolicy::CustomSizes);
 }
 
-XLATensorImpl::XLATensorImpl(XLATensor& tensor)
-    : XLATensorImpl(XLATensor(tensor)) {}
-
-XLATensorImpl::XLATensorImpl(XLATensorPtr tensor)
-    : XLATensorImpl(XLATensor(*tensor)) {}
-
 void XLATensorImpl::set_tensor(XLATensorPtr xla_tensor) {
   tensor_ = c10::make_intrusive<XLATensor>(std::move(*xla_tensor));
-  generation_ = 0;
-}
-
-c10::intrusive_ptr<c10::TensorImpl> XLATensorImpl::shallow_copy_and_detach(
-    const c10::VariableVersion& version_counter,
-    bool allow_tensor_metadata_change) const {
-  auto impl = c10::make_intrusive<XLATensorImpl>(tensor_);
-  copy_tensor_metadata(
-      /*src_impl=*/this,
-      /*dest_impl=*/impl.get(),
-      /*version_counter=*/version_counter,
-      /*allow_tensor_metadata_change=*/allow_tensor_metadata_change);
-  return impl;
-}
-
-c10::intrusive_ptr<c10::TensorImpl> XLATensorImpl::shallow_copy_and_detach(
-    c10::VariableVersion&& version_counter,
-    bool allow_tensor_metadata_change) const {
-  auto impl = c10::make_intrusive<XLATensorImpl>(tensor_);
-  copy_tensor_metadata(
-      /*src_impl=*/this,
-      /*dest_impl=*/impl.get(),
-      /*version_counter=*/std::move(version_counter),
-      /*allow_tensor_metadata_change=*/allow_tensor_metadata_change);
-  return impl;
-}
-
-void XLATensorImpl::shallow_copy_from(
-    const c10::intrusive_ptr<TensorImpl>& impl) {
-  XLATensorImpl* xla_impl = dynamic_cast<XLATensorImpl*>(impl.get());
-  copy_tensor_metadata(
-      /*src_impl=*/xla_impl,
-      /*dest_impl=*/this,
-      /*version_counter=*/version_counter(),
-      /*allow_tensor_metadata_change=*/allow_tensor_metadata_change());
-  xla_impl->tensor_->ShallowCopyTo(tensor_);
   generation_ = 0;
 }
 
@@ -146,12 +103,6 @@ int64_t XLATensorImpl::dim_custom() const {
 int64_t XLATensorImpl::numel_custom() const {
   const_cast<XLATensorImpl*>(this)->SetupSizeProperties();
   return numel_default();
-}
-
-bool XLATensorImpl::is_contiguous_custom(at::MemoryFormat memory_format) const {
-  // Only check that the storage is already contiguous.
-  XLA_CHECK(is_contiguous_) << "Non-contiguous storage for XLA tensor";
-  return true;
 }
 
 void XLATensorImpl::SetupSizeProperties() {
@@ -204,9 +155,5 @@ caffe2::TypeMeta XLATensorImpl::GetTypeMeta(const XLATensor& tensor) {
 void XLATensorImpl::AtenInitialize() {
   // ATEN specific initialization calls placed below.
 }
-
-const at::Storage& XLATensorImpl::storage() const { return tensor_->Storage(); }
-
-bool XLATensorImpl::has_storage() const { return tensor_->Storage(); }
 
 }  // namespace torch_xla
--- torch_xla/csrc/tensor_impl.h
+++ torch_xla/csrc/tensor_impl.h
@@ -4,33 +4,20 @@
 #include <c10/core/Storage.h>
 #include <c10/core/TensorImpl.h>
 
+#include "torch/csrc/lazy/core/tensor_impl.h"
 #include "torch_xla/csrc/tensor.h"
 
 namespace torch_xla {
 
 // Tensor implementation class used to be fed to the at::Tensor.
 // Its scope is just to handle an XLATensor.
-class XLATensorImpl : public c10::TensorImpl {
+class XLATensorImpl : public torch::lazy::LTCTensorImpl {
  public:
-  explicit XLATensorImpl(XLATensor&& tensor);
-  explicit XLATensorImpl(XLATensor& tensor);
   explicit XLATensorImpl(XLATensorPtr tensor);
 
   XLATensorPtr& tensor() { return tensor_; }
 
   void set_tensor(XLATensorPtr xla_tensor);
-
-  void force_refresh_sizes() { generation_ = 0; }
-
-  c10::intrusive_ptr<TensorImpl> shallow_copy_and_detach(
-      const c10::VariableVersion& version_counter,
-      bool allow_tensor_metadata_change) const override;
-
-  c10::intrusive_ptr<TensorImpl> shallow_copy_and_detach(
-      c10::VariableVersion&& version_counter,
-      bool allow_tensor_metadata_change) const override;
-
-  void shallow_copy_from(const c10::intrusive_ptr<TensorImpl>& impl) override;
 
   at::IntArrayRef sizes_custom() const override;
   c10::SymIntArrayRef sym_sizes_custom() const override;
@@ -40,12 +27,6 @@ class XLATensorImpl : public c10::TensorImpl {
   int64_t dim_custom() const override;
 
   int64_t numel_custom() const override;
-
-  bool is_contiguous_custom(at::MemoryFormat memory_format) const override;
-
-  const at::Storage& storage() const override;
-
-  bool has_storage() const override;
 
   static void AtenInitialize();
 
