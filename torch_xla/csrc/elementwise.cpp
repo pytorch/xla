456d1962614d605ac41f385481fbbb7320e5f82f
Yanming Wang <yanmwang@amazon.com>
2022-12-12 18:55:01 +0000

    Improve numerical stability of torch.sigmoid

--- torch_xla/csrc/elementwise.cpp
+++ torch_xla/csrc/elementwise.cpp
@@ -238,14 +238,7 @@ xla::XlaOp BuildPrelu(xla::XlaOp input, xla::XlaOp weight) {
   return xla::Select(xla::Gt(input, zero), input, product);
 }
 
-xla::XlaOp BuildSigmoid(xla::XlaOp input) {
-  const xla::Shape& shape = XlaHelpers::ShapeOfXlaOp(input);
-  xla::XlaOp half = XlaHelpers::ScalarValue<float>(0.5, shape.element_type(),
-                                                   input.builder());
-  return half + half * xla::Tanh(half * input);
-  // xla::XlaOp one = xla::One(input.builder(), shape.element_type());
-  // return one / (one + xla::Exp(-input));
-}
+xla::XlaOp BuildSigmoid(xla::XlaOp input) { return xla::Logistic(input); }
 
 xla::XlaOp BuildSiLUBackward(xla::XlaOp grad_output, xla::XlaOp input) {
   const xla::Shape& shape = XlaHelpers::ShapeOfXlaOp(input);
