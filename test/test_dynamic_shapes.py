13629d650e82ba676b2fc4376cf6185fb432459c
iefgnoix <isaacwxf23@gmail.com>
2022-12-12 19:38:21 -0600

    Add one more symint expand test. (#4291)
    * Add one more symint expand test.
    
    * fixed the test.

--- test/test_dynamic_shapes.py
+++ test/test_dynamic_shapes.py
@@ -19,6 +19,36 @@ class TestDynamicShapes(unittest.TestCase):
     t5_cpu = t5.cpu()
     self.assertEqual(t5_cpu.shape[0], 1)
 
+  def test_simple_expand_on_2d_tensor(self):
+    size1 = 5
+    size2 = 2
+    t1 = torch.zeros([size1, size2], device=dev)
+    t1[3][0] = 1
+    t1[3][1] = 1
+    # t2 has size [<=10, 2]
+    t2 = torch.nonzero(t1)
+    t3 = torch.ones(1, size2, device=dev)
+
+    # varargs
+    t4 = t3.expand(t2.shape[0], t2.shape[1])
+    self.assertEqual(t4.shape[0], 2)
+    self.assertEqual(t4.shape[1], size2)
+
+    # shape list
+    t4 = t3.expand((t2.shape[0], t2.shape[1]))
+    self.assertEqual(t4.shape[0], 2)
+    self.assertEqual(t4.shape[1], size2)
+
+    # mixed python symints and ints
+    t4 = t3.expand(t2.shape[0], size2)
+    self.assertEqual(t4.shape[0], 2)
+    self.assertEqual(t4.shape[1], size2)
+
+    # mixed python symints and ints in a list
+    t4 = t3.expand((t2.shape[0], size2))
+    self.assertEqual(t4.shape[0], 2)
+    self.assertEqual(t4.shape[1], size2)
+
   def test_wrap(self):
     a1 = torch.tensor([[1, 0, 0, 5, 0, 6]], device=dev)
     a2 = torch.nonzero(a1)
