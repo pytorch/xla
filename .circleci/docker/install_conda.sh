9db3c3703e2412e23d01e59a7dce1340b730ec69
JackCaoG <59073027+JackCaoG@users.noreply.github.com>
2022-12-15 10:21:00 -0800

    Revert "Revert "Install sympy to ci base docker image (#4337)" (#4339)"
    This reverts commit 40bd7d6f0c33fd92b1022c1bc2d7bbadc9a2ee66.

--- .circleci/docker/install_conda.sh
+++ .circleci/docker/install_conda.sh
@@ -49,6 +49,7 @@ function install_and_setup_conda() {
   /usr/bin/yes | pip install psutil
   /usr/bin/yes | pip install unittest-xml-reporting
   /usr/bin/yes | pip install pytest
+  /usr/bin/yes | pip install sympy
 
 }
 
