[PJRT] Fix TPU local ordinal calculation (#4334)

test/pjrt/test_experimental_pjrt_tpu.py
@@ -1,6 +1,7 @@
 import concurrent.futures
 import os
 from typing import Dict
+from unittest import mock
 import requests
 
 import numpy as np
@@ -19,9 +20,6 @@ class TestExperimentalPjrtTpu(parameterized.TestCase):
   def setUp(self):
     pjrt.set_device_type('TPU')
 
-    os.environ.pop(xenv.TPU_VISIBLE_CHIPS, None)
-    os.environ.pop(xenv.TPU_PROCESS_BOUNDS, None)
-
     try:
       tpu_env = tpu.get_tpu_env()
       self.accelerator_type = tpu_env['ACCELERATOR_TYPE']
@@ -33,6 +31,10 @@ class TestExperimentalPjrtTpu(parameterized.TestCase):
     # The main process must not initialize the ComputationClient, otherwise
     # sub-processes will not be able to initialize the client witht the correct
     # settings.
+
+  def tearDown(self) -> None:
+    os.environ.pop(xenv.TPU_VISIBLE_CHIPS, None)
+    os.environ.pop(xenv.TPU_PROCESS_BOUNDS, None)
 
   def test_xla_devices_multiprocess(self):
     accelerator_devices = {
@@ -102,8 +104,12 @@ class TestExperimentalPjrtTpu(parameterized.TestCase):
     self.assertDictEqual(devices, expected)
 
   @staticmethod
-  def _fail_on_nonfirst_device(i):
-    assert i == 0, f"the device index {i} must be 0 in nprocs=1"
+  def _fail_on_nonfirst_device():
+
+    def _assert(i):
+      assert i == 0, f"the device index {i} must be 0 in nprocs=1"
+
+    xmp.spawn(_assert, nprocs=1)
 
   def test_xla_devices_single_process_one_chip_one_device_spawn(self):
     accelerators = ['v3-8', 'v4-8']
@@ -111,7 +117,10 @@ class TestExperimentalPjrtTpu(parameterized.TestCase):
     if self.accelerator_type not in accelerators:
       raise NotImplementedError('Test not implemented for {}'.format(
           self.accelerator_type))
-    xmp.spawn(self._fail_on_nonfirst_device, nprocs=1)
+
+    # Avoid initializing the TPU client in the parent process
+    with concurrent.futures.ProcessPoolExecutor(max_workers=1) as executor:
+      executor.submit(self._fail_on_nonfirst_device).result()
 
   def test_default_xla_devices(self):
     accelerator_num_devices = {
@@ -165,6 +174,31 @@ class TestExperimentalPjrtTpu(parameterized.TestCase):
     results = pjrt._run_multiprocess(ordinal_func)
     values = list(results.values())
     self.assertListEqual(sorted(values), list(range(expected_num_devices)))
+
+  @staticmethod
+  def _local_ordinal_with_discontiguous_global_ordinal_v4():
+    # Actual set of global ordinals from one v4-128 host
+    global_ordinals = [58, 59, 62, 63]
+    new_global_ordinal = global_ordinals[pjrt.global_ordinal()]
+
+    with mock.patch.object(
+        pjrt, 'global_ordinal', return_value=new_global_ordinal):
+      return pjrt.local_ordinal()
+
+  @absltest.skipIf(tpu.version() < 4, "Not implemented")
+  def test_local_ordinal_with_discontiguous_global_ordinal_v4(self):
+    results = pjrt._run_multiprocess(
+        self._local_ordinal_with_discontiguous_global_ordinal_v4)
+    self.assertSameElements(results.values(), [0, 1, 2, 3])
+
+  @absltest.skipIf(tpu.version() < 4, "Not implemented")
+  def test_local_ordinal_with_discontiguous_global_ordinal_v4_threaded(self):
+    os.environ[xenv.TPU_PROCESS_BOUNDS] = '1,1,1'
+    os.environ[xenv.TPU_VISIBLE_CHIPS] = '0,1,2,3'
+
+    results = pjrt._run_multiprocess(
+        self._local_ordinal_with_discontiguous_global_ordinal_v4)
+    self.assertSameElements(results.values(), [0, 1, 2, 3])
 
   @staticmethod
   def _spawn_threads() -> Dict[int, torch.device]: