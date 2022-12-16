ecaad228eebc3492507b04a3090d4ba44c9b0fd8
Wonjoo Lee <wonjoo@google.com>
2022-12-16 21:06:49 +0000

    Run linter

--- torch_xla/csrc/aten_xla_type.cpp
+++ torch_xla/csrc/aten_xla_type.cpp
@@ -54,7 +54,9 @@ at::Tensor to_meta(const at::Tensor& tensor) {
   // undefined tensors can't be converted to the meta device, since they don't
   // have sizes/strides
   // std::cout << "WONJOO: at aten_xla_type.cpp, to_meta1" << std::endl;
-  // std::cout << "WONJOO: at aten_xla_type.cpp, to_meta2, tensor_is_functional=" << at::functionalization::impl::isFunctionalTensor(tensor) << std::endl;
+  // std::cout << "WONJOO: at aten_xla_type.cpp, to_meta2,
+  // tensor_is_functional=" <<
+  // at::functionalization::impl::isFunctionalTensor(tensor) << std::endl;
   if (!tensor.defined()) return tensor;
   auto out = at::native::empty_strided_meta_symint(
       tensor.sym_sizes(), tensor.sym_strides(),
@@ -675,7 +677,8 @@ at::Tensor XLANativeFunctions::as_strided_scatter(
     const at::Tensor& base, const at::Tensor& mutated_view,
     at::IntArrayRef size, at::IntArrayRef stride,
     c10::optional<int64_t> storage_offset) {
-  // std::cout << "WONJOO: at aten_xla_type.cpp, as_strided_scatter1" << std::endl;
+  // std::cout << "WONJOO: at aten_xla_type.cpp, as_strided_scatter1" <<
+  // std::endl;
   TORCH_LAZY_FN_COUNTER("xla::");
   auto base_ = bridge::GetXlaTensor(base);
   auto xsize = XlaHelpers::I64List(size);
@@ -1029,7 +1032,8 @@ at::Tensor XLANativeFunctions::cumsum(const at::Tensor& self, int64_t dim,
 // TODO(alanwaketan): Let's rewrite a without reusing other native functions.
 at::Tensor XLANativeFunctions::detach_copy(const at::Tensor& self) {
   TORCH_LAZY_FN_COUNTER("xla::");
-  auto new_tensor = empty_symint(self.sym_sizes(), c10::nullopt, c10::nullopt, c10::nullopt, c10::nullopt, c10::nullopt);
+  auto new_tensor = empty_symint(self.sym_sizes(), c10::nullopt, c10::nullopt,
+                                 c10::nullopt, c10::nullopt, c10::nullopt);
   return _copy_from(self, new_tensor, true);
 }
 
@@ -1401,7 +1405,9 @@ at::Tensor& XLANativeFunctions::index_put_(
     at::Tensor& self, const c10::List<c10::optional<at::Tensor>>& indices,
     const at::Tensor& values, bool accumulate) {
   // std::cout << "WONJOO: at aten_xla_type.cpp, input_put_1" << std::endl;
-  // std::cout << "WONJOO: at aten_xla_type.cpp, input_put_2, self.is_functional=" << at::functionalization::impl::isFunctionalTensor(self) << std::endl;
+  // std::cout << "WONJOO: at aten_xla_type.cpp, input_put_2,
+  // self.is_functional=" <<
+  // at::functionalization::impl::isFunctionalTensor(self) << std::endl;
   TORCH_LAZY_FN_COUNTER("xla::");
   XLA_CHECK(self.scalar_type() == values.scalar_type());
   CanonicalIndexInfo canonical_index_info =
@@ -1420,7 +1426,9 @@ at::Tensor& XLANativeFunctions::index_put_(
       canonical_index_info.start_dim,
       bridge::GetOrCreateXlaTensor(values, *device), accumulate,
       canonical_index_info.result_permutation);
-// std::cout << "WONJOO: at aten_xla_type.cpp, input_put_3, self.is_functional=" << at::functionalization::impl::isFunctionalTensor(self) << std::endl;
+  // std::cout << "WONJOO: at aten_xla_type.cpp, input_put_3,
+  // self.is_functional=" <<
+  // at::functionalization::impl::isFunctionalTensor(self) << std::endl;
   return self;
 }
 
