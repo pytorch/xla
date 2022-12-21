ce19d129fd1fd74e801c77e233eda1ff00a80494
Jiewen Tan <jwtan@google.com>
2022-12-21 01:46:24 +0000

    Route embeeding to functionalize pass

--- torch_xla/csrc/aten_xla_type.cpp
+++ torch_xla/csrc/aten_xla_type.cpp
@@ -3538,6 +3538,19 @@ at::Tensor XLANativeFunctions::_euclidean_dist(const at::Tensor& x1,
       _euclidean_dist)>::call(x1, x2);
 }
 
+at::Tensor XLANativeFunctions::embedding_symint(const at::Tensor& weight,
+                                                const at::Tensor& indices,
+                                                c10::SymInt padding_idx,
+                                                bool scale_grad_by_freq,
+                                                bool sparse) {
+  TORCH_LAZY_FN_COUNTER("xla::");
+  // TODO: for now route to native, which dispatches supported XLA operations.
+  // We need to make use of the TPU embedding core here eventually.
+  return at::functionalization::functionalize_aten_op_symint<ATEN_OP(
+      embedding)>::call(weight, indices, padding_idx,
+                                      scale_grad_by_freq, sparse);
+}
+
 at::Tensor XLANativeFunctions::_euclidean_dist(const at::Tensor & x1, const at::Tensor & x2) {
   return at::functionalization::functionalize_aten_op<ATEN_OP(
       _euclidean_dist)>::call(x1, x2);
