36d7ecfae1c468963aee69aba698a3956d11d776
Wonjoo Lee <wonjoo@google.com>
2022-12-21 08:17:44 +0000

    Lower zeros_symint

--- torch_xla/csrc/aten_xla_type.cpp
+++ torch_xla/csrc/aten_xla_type.cpp
@@ -3328,4 +3328,9 @@ at::Tensor XLANativeFunctions::slice_backward(const at::Tensor& grad_output,
       slice_backward)>::call(grad_output, input_sizes, dim, start, end, step);
 }
 
+at::Tensor XLANativeFunctions::zeros_symint(c10::SymIntArrayRef size, c10::optional<at::ScalarType> dtype, c10::optional<at::Layout> layout, c10::optional<at::Device> device, c10::optional<bool> pin_memory) {
+  return at::functionalization::functionalize_aten_op_symint<ATEN_OP(
+      zeros)>::call(size, dtype, layout, device, pin_memory);
+}
+
 }  // namespace torch_xla
