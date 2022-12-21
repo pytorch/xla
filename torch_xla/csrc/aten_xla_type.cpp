b15ee3e4d3e3ac1886f9c5958fb8295ea85ff8e5
Wonjoo Lee <wonjoo@google.com>
2022-12-21 10:20:39 +0000

    Lower permute.Tensor

--- torch_xla/csrc/aten_xla_type.cpp
+++ torch_xla/csrc/aten_xla_type.cpp
@@ -3343,6 +3343,11 @@ at::Tensor XLANativeFunctions::_cdist_forward(
       bridge::GetXlaTensor(x1), bridge::GetXlaTensor(x2), p));
 }
 
+at::Tensor XLANativeFunctions::permute(const at::Tensor & self, at::IntArrayRef dims) {
+  return at::functionalization::functionalize_aten_op<ATEN_OP(
+      permute)>::call(self, dims);
+}
+
 at::Tensor XLANativeFunctions::zeros_symint(c10::SymIntArrayRef size, c10::optional<at::ScalarType> dtype, c10::optional<at::Layout> layout, c10::optional<at::Device> device, c10::optional<bool> pin_memory) {
   return at::functionalization::functionalize_aten_op_symint<ATEN_OP(
       zeros)>::call(size, dtype, layout, device, pin_memory);
