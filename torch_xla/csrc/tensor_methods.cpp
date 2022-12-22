acb1058d360abf4dbeca2467752398e4a4e2365b
JackCaoG <jackcao@google.com>
2022-12-22 05:20:14 +0000

    Update usage of GetIrValueForScalar

--- torch_xla/csrc/tensor_methods.cpp
+++ torch_xla/csrc/tensor_methods.cpp
@@ -871,7 +871,7 @@ XLATensorPtr cat(absl::Span<const XLATensorPtr> tensors, int64_t dim,
 XLATensorPtr cdist_forward(const XLATensorPtr& x1, const XLATensorPtr& x2,
                            double p) {
   torch::lazy::Value exponent_node =
-      XLATensor::GetIrValueForScalar(p, x1->GetDevice());
+      XLAGraphExecutor::Get()->GetIrValueForScalar(p, x1->GetDevice());
   torch::lazy::NodePtr node = torch::lazy::MakeNode<CdistForward>(
       x1->GetIrValue(), x2->GetIrValue(), exponent_node,
       /*use_hamming=*/p == 0.0,
