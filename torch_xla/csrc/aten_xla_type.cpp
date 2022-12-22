af42112d5ff7909b03a668c5479ae5843a1bce04
Jiewen Tan <jwtan@google.com>
2022-12-22 00:34:42 +0000

    Revert "Lower zeros_symint"
    This reverts commit da5dc0241f04f426df0b7923a3969e7c54cf8494.

--- torch_xla/csrc/aten_xla_type.cpp
+++ torch_xla/csrc/aten_xla_type.cpp
@@ -3360,9 +3360,4 @@ at::Tensor XLANativeFunctions::permute(const at::Tensor & self, at::IntArrayRef 
       permute)>::call(self, dims);
 }
 
-at::Tensor XLANativeFunctions::zeros_symint(c10::SymIntArrayRef size, c10::optional<at::ScalarType> dtype, c10::optional<at::Layout> layout, c10::optional<at::Device> device, c10::optional<bool> pin_memory) {
-  return at::functionalization::functionalize_aten_op_symint<ATEN_OP(
-      zeros)>::call(size, dtype, layout, device, pin_memory);
-}
-
 }  // namespace torch_xla
