30f34808256aef0312a4e7f825296078bae36216
Wonjoo Lee <wonjoo@google.com>
2022-12-21 21:05:56 -0800

    Update CI test scripts to allow continuing on test failures (#4385)

--- .circleci/common.sh
+++ .circleci/common.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+# See Note [Keep Going]
+CONTINUE_ON_ERROR=false
+if [[ "$CONTINUE_ON_ERROR" != "1" ]]; then
+  set +e
+fi
+
 # System default cmake 3.10 cannot find mkl, so point it to the right place.
 # CMAKE_PREFIX_PATH will default to (in this order):
 # 1. CMAKE_PREFIX_PATH (if it exists)
