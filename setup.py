04ec21cff28b35990f6e7df9c779c8c0b3f297b6
JackCaoG <59073027+JackCaoG@users.noreply.github.com>
2022-12-22 15:01:35 -0800

    2.0 (#4392)

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ def get_git_head_sha(base_dir):
 
 
 def get_build_version(xla_git_sha):
-  version = os.getenv('TORCH_XLA_VERSION', '1.14')
+  version = os.getenv('TORCH_XLA_VERSION', '2.0.0')
   if _check_env_flag('VERSIONED_XLA_BUILD', default='0'):
     try:
       version += '+' + xla_git_sha[:7]
