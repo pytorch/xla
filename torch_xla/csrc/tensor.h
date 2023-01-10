cebb812f7ff67c5774b85787f359a2e66cfc53d4
Xiongfei Wei <isaacwxf23@gmail.com>
2023-01-10 05:21:44 +0000

    remove unwanted lines.

--- torch_xla/csrc/tensor.h
+++ torch_xla/csrc/tensor.h
@@ -34,13 +34,10 @@ class TORCH_API XLASymNodeImpl : public c10::SymNodeImpl {
   bool is_float() override;
   c10::SymNode add(const c10::SymNode& other) override;
   c10::SymNode sub(const c10::SymNode& other) override;
-  c10::SymNode sub(const c10::SymNode& other) override;
   c10::SymNode mul(const c10::SymNode& other) override;
   c10::SymNode truediv(const c10::SymNode& other) override;
   c10::SymNode pow(const c10::SymNode& other) override;
   c10::SymNode truediv(const c10::SymNode& other) override;
-  c10::SymNode pow(const c10::SymNode& other) override;
-  c10::SymNode floordiv(const c10::SymNode& other) override;
   c10::SymNode mod(const c10::SymNode& other) override;
   c10::SymNode eq(const c10::SymNode& other) override;
   c10::SymNode ne(const c10::SymNode& other) override;
@@ -54,14 +51,16 @@ class TORCH_API XLASymNodeImpl : public c10::SymNodeImpl {
   c10::SymNode min(const c10::SymNode& other) override;
   c10::SymNode max(const c10::SymNode& other) override;
   c10::SymNode clone() override;
-  c10::SymNode sym_float()  override;
+  c10::SymNode sym_float() override;
   c10::SymNode wrap_int(int64_t num) override;
-
-  torch::lazy::NodePtr node() { return node_; }
+  c10::SymNode wrap_float(double num) override;
+  int64_t guard_int(const char* file, int64_t line) override;
+  double guard_float(const char* file, int64_t line) override;
+  int64_t int_() override;
+  bool bool_() override;
   std::string str() override;
 
-  bool bool_() override;
-  int64_t int_() override;
+  torch::lazy::NodePtr node() { return node_; }
 
  private:
   torch::lazy::NodePtr node_;
