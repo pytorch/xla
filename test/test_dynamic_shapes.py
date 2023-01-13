45dd7464b4124627107ef1b793eefd1779650039
Jiewen Tan <jwtan@google.com>
2023-01-13 07:21:12 +0000

    Skip test_sizeAdd

--- test/test_dynamic_shapes.py
+++ test/test_dynamic_shapes.py
@@ -83,6 +83,7 @@ class TestDynamicShapes(test_utils.XlaTestCase):
     a3 = a2.shape[0] + 3  # tests wrap
     self.assertIsInstance(a3, torch.SymInt)
 
+  @unittest.skip("fails with functionalization")
   def test_sizeAdd(self):
     size1 = 5
     size2 = 2
