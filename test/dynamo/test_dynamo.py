edc4cf729deefac5f1c1b569bb27909d01e62a1c
Wonjoo Lee <wonjoo@google.com>
2023-01-17 13:32:13 -0800

    Re-enable dynamo tests (#4454)

--- test/dynamo/test_dynamo.py
+++ test/dynamo/test_dynamo.py
@@ -23,7 +23,6 @@ class DynamoInferenceBasicTest(unittest.TestCase):
   def fn_simple_dynamo(self, x, y):
     return self.fn_simple(x, y)
 
-  @unittest.skip("fails with functionalization")
   def test_simple_model(self):
     device = xm.xla_device()
     x = torch.tensor(100.0)
@@ -44,7 +43,6 @@ class DynamoInferenceBasicTest(unittest.TestCase):
     res_cpu_3 = self.fn_simple(x + y, y * 3)
     assert torch.allclose(res_cpu_3, res_xla_dynamo_3.cpu())
 
-  @unittest.skip("fails with functionalization")
   def test_resnet18(self):
     device = xm.xla_device()
     batch_size = xu.getenv_as('BATCH_SIZE', int, defval=4)
@@ -98,7 +96,14 @@ class DynamoTrainingBasicTest(unittest.TestCase):
     loss.backward()
     return pred
 
+<<<<<<< HEAD
   @unittest.skip("fails with functionalization")
+=======
+  @dynamo.optimize('aot_torchxla_trace_once')
+  def run_model_with_dynamo(self, model, data, target):
+    return self.train_model(model, data, target)
+
+>>>>>>> Re-enable dynamo tests (#4454)
   def test_simple_model(self):
     torch._dynamo.reset()
     device = xm.xla_device()
@@ -125,7 +130,6 @@ class DynamoTrainingBasicTest(unittest.TestCase):
     assert torch.allclose(res_cpu_3, res_xla_dynamo_3.cpu())
     assert torch.allclose(input.grad, xla_input.grad.cpu())
 
-  @unittest.skip("fails with functionalization")
   def test_resnet18(self):
     torch._dynamo.reset()
     met.clear_counters()
