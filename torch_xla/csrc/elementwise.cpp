f60127d1e1981596b125040a8e9743dfaa0d576b
Wonjoo Lee <wonjoo@google.com>
2023-01-18 09:03:20 +0000

    Lower _prelu_kernel to fix prelu cpp tests

--- torch_xla/csrc/elementwise.cpp
+++ torch_xla/csrc/elementwise.cpp
@@ -228,13 +228,8 @@ xla::XlaOp BuildPrelu(xla::XlaOp input, xla::XlaOp weight) {
   const xla::Shape& input_shape = XlaHelpers::ShapeOfXlaOp(input);
   const xla::Shape& weight_shape = XlaHelpers::ShapeOfXlaOp(weight);
 
-  int64_t weight_num = xla::ShapeUtil::ElementsIn(weight_shape);
-  int64_t broadcast_dim = weight_num == 1 ? 0 : 1;
-
   xla::XlaOp zero = xla::Zero(input.builder(), input_shape.element_type());
-  xla::XlaOp broadcasted_weight =
-      xla::BroadcastInDim(weight, input_shape.dimensions(), {broadcast_dim});
-  xla::XlaOp product = xla::Mul(input, broadcasted_weight);
+  xla::XlaOp product = xla::Mul(input, weight);
 
   return xla::Select(xla::Gt(input, zero), input, product);
 }
