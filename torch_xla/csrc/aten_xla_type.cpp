3db05c1ffd50fdaee4c7bcc0245c593004e3f792
Jiewen Tan <jwtan@google.com>
2023-01-19 07:30:24 +0000

    Adds copy_ as the auto generated copy_ sucks

--- torch_xla/csrc/aten_xla_type.cpp
+++ torch_xla/csrc/aten_xla_type.cpp
@@ -456,6 +456,13 @@ at::Tensor& XLANativeFunctions::_amp_update_scale_(at::Tensor& current_scale,
   return current_scale;
 }
 
+at::Tensor& XLANativeFunctions::copy_(at::Tensor& self,
+                                    const at::Tensor& src, bool non_blocking) {
+  TORCH_LAZY_FN_COUNTER("xla::");
+  _copy_from(src, self, non_blocking);
+  return self;
+}
+
 at::Tensor XLANativeFunctions::copy(const at::Tensor& self,
                                     const at::Tensor& src, bool non_blocking) {
   TORCH_LAZY_FN_COUNTER("xla::");
