65a4f2c7b81a9eb1134484de3a4087bbf9aa3529
Jiewen Tan <jwtan@google.com>
2023-01-18 23:11:20 -0800

    [Functionalization] Fix test_simple_expand_on_2d_tensor (#4452)
    Summary:
    This pull request fixes TestDynamicShapes. test_simple_expand_on_2d_tensor by:
    1. enabling python dispatcher in the test such that dynamic ops are decomposed correctly,
    2. implementing missing sym size ops, ge and clone.
    
    Test Plan:
    XLA_EXPERIMENTAL="nonzero:masked_select:masked_scatter" python test/test_dynamic_shapes.py -v TestDynamicShapes.test_simple_expand_on_2d_tensor
    
    Fixes #4448

--- test/test_dynamic_shapes.py
+++ test/test_dynamic_shapes.py
@@ -24,9 +24,8 @@ class TestDynamicShapes(unittest.TestCase):
     t6 = t5.expand(t2.size(0))
     self.assertIn('<=10', torch_xla._XLAC._get_xla_tensors_text([t6]))
     t6_cpu = t6.cpu()
-    self.assertEqual(t6_cpu.shape[0], 2)
+    self.assertEqual(t6_cpu.shape[0], 2)  # 10 instead of 2
 
-  @unittest.skip("fails with functionalization")
   def test_simple_expand_on_2d_tensor(self):
     size1 = 5
     size2 = 2
@@ -57,6 +56,10 @@ class TestDynamicShapes(unittest.TestCase):
     self.assertEqual(t4.shape[0], 2)
     self.assertEqual(t4.shape[1], size2)
 
+    # size_clone should be called as part of decomposition from
+    # the python dispatcher.
+    self.assertGreater(met.counter_value("xla::size_clone"), 0)
+
   def test_simple_expand_add_dimension(self):
     size1 = 5
     size2 = 2
@@ -82,7 +85,6 @@ class TestDynamicShapes(unittest.TestCase):
     a3 = a2.shape[0] + 3  # tests wrap
     self.assertIsInstance(a3, torch.SymInt)
 
-  @unittest.skip("fails with functionalization")
   def test_sizeAdd(self):
     size1 = 5
     size2 = 2
@@ -141,6 +143,40 @@ class TestDynamicShapes(unittest.TestCase):
     self.assertEqual(t2.shape[0], 3)
     self.assertIsInstance(t2.shape[1], int)
     self.assertEqual(t2.shape[1], 2)
+
+  def test_sizeGe(self):
+    met.clear_all()
+
+    size1 = 5
+    size2 = 2
+    t1 = torch.zeros([size1, size2], device=dev)
+    t1[3][0] = 1
+    # t2 has size [<=10, 2]
+    t2 = torch.nonzero(t1)
+    # Create a SizeAdd IR node.
+    # t2.shape[1] generates a SizeConstant node.
+    dyn_size = t2.shape[0] >= t2.shape[1]
+    self.assertGreater(met.counter_value("xla::size_ge"), 0)
+    # Exercises SizeGe::getDynamicValue.
+    dynamic_size = int(dyn_size)
+    self.assertEqual(dynamic_size, 0)
+
+  def test_sizeLt(self):
+    met.clear_all()
+
+    size1 = 5
+    size2 = 2
+    t1 = torch.zeros([size1, size2], device=dev)
+    t1[3][0] = 1
+    # t2 has size [<=10, 2]
+    t2 = torch.nonzero(t1)
+    # Create a SizeAdd IR node.
+    # t2.shape[1] generates a SizeConstant node.
+    dyn_size = t2.shape[0] < t2.shape[1]
+    self.assertGreater(met.counter_value("xla::size_lt"), 0)
+    # Exercises SizeLt::getDynamicValue.
+    dynamic_size = int(dyn_size)
+    self.assertEqual(dynamic_size, 1)
 
 
 if __name__ == '__main__':
