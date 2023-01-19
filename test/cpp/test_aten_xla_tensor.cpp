e4c4e893fdccab65fbf845c7d0720229496bcda8
Wonjoo Lee <wonjoo@google.com>
2023-01-19 00:31:27 +0000

    Lower diag_embed to fix diagflat cpp tests

--- test/cpp/test_aten_xla_tensor.cpp
+++ test/cpp/test_aten_xla_tensor.cpp
@@ -9391,7 +9391,7 @@ TEST_F(AtenXlaTensorTest, TestDiagFlat) {
     });
 
     ExpectCounterNotChanged("aten::.*", cpp_test::GetIgnoredCounters());
-    ExpectCounterChanged("xla::diag", cpp_test::GetIgnoredCounters());
+    ExpectCounterChanged("xla::diag_embed", cpp_test::GetIgnoredCounters());
   }
 }
 
