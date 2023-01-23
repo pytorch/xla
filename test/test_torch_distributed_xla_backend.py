24b1f651627b66cbc17844fd28bbf9c56bc00d0d
Jiewen Tan <jwtan@google.com>
2023-01-23 11:25:30 -0800

    [Functionalization] Fix torch.distributed tests (#4477)
    Summary:
    This pull request tries to fix the following two tests:
    1. test_allgather: allgather uses copy_ internally to update the input and copy_ being decomposed into copy which we need to lower.
    2. test_send: send uses t.data to update t’s data which is not supported in functionalization. See functorch.functionalize doesn't allow t.data update pytorch#91093. After switching to copy_, it looks like the problem is fixed.
    
    Test Plan:
    CI.

--- test/test_torch_distributed_xla_backend.py
+++ test/test_torch_distributed_xla_backend.py
@@ -136,7 +136,6 @@ class XlaBackendTest(unittest.TestCase):
     # purge all computations attached the device.
     xm.mark_step()
 
-  @unittest.skip("fails with functionalization")
   def test_allgather(self):
     device = xm.xla_device()
     tensor = torch.arange(2, device=device) + 1 + 2 * dist.get_rank()
@@ -188,7 +187,6 @@ class XlaBackendTest(unittest.TestCase):
     # purge all computations attached the device.
     xm.mark_step()
 
-  @unittest.skip("fails with functionalization")
   def test_send(self):
     device = xm.xla_device()
     tensor = torch.arange(2, device=device) + 1 + 2 * dist.get_rank()
