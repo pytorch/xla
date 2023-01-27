7277888f4839d26d954b61a32002c08104fff0a0
Jiewen Tan <jwtan@google.com>
2023-01-27 13:34:04 -0800

    [Functionalization] Re-enable some of the dynamic shape test (#4518)
    Summary:
    The dynamic shape tests are broken because of upstream changes that decompose dynamic ops into different ways. Adds new lowering to fix that.
    
    Corresponding upstream changes for SymBool: pytorch/pytorch#92149.
    
    Test Plan:
    CI

--- test/test_dynamic_shapes.py
+++ test/test_dynamic_shapes.py
@@ -11,7 +11,6 @@ dev = xm.xla_device()
 
 class TestDynamicShapes(unittest.TestCase):
 
-  @unittest.skip("Needs to lower ne")
   def test_simple_expand(self):
     size1 = 5
     size2 = 2
@@ -26,7 +25,6 @@ class TestDynamicShapes(unittest.TestCase):
     t6_cpu = t6.cpu()
     self.assertEqual(t6_cpu.shape[0], 2)
 
-  @unittest.skip("Needs to lower ne")
   def test_simple_expand_on_2d_tensor(self):
     size1 = 5
     size2 = 2
@@ -68,7 +66,6 @@ class TestDynamicShapes(unittest.TestCase):
     a3 = a2.shape[0] + 3  # tests wrap
     self.assertIsInstance(a3, torch.SymInt)
 
-  @unittest.skip("Needs to lower ne")
   def test_sizeAdd(self):
     size1 = 5
     size2 = 2
@@ -120,6 +117,23 @@ class TestDynamicShapes(unittest.TestCase):
     dyn_size = t2.shape[0] < t2.shape[1]
     self.assertGreater(met.counter_value("xla::size_lt"), 0)
     # Exercises SizeLt::getDynamicValue.
+    dynamic_size = int(dyn_size)
+    self.assertEqual(dynamic_size, 1)
+
+  def test_sizeNe(self):
+    met.clear_all()
+
+    size1 = 5
+    size2 = 2
+    t1 = torch.zeros([size1, size2], device=dev)
+    t1[3][0] = 1
+    # t2 has size [<=10, 2]
+    t2 = torch.nonzero(t1)
+    # Create a SizeAdd IR node.
+    # t2.shape[1] generates a SizeConstant node.
+    dyn_size = t2.shape[0] != t2.shape[1]
+    self.assertGreater(met.counter_value("xla::size_ne"), 0)
+    # Exercises SizeNe::getDynamicValue.
     dynamic_size = int(dyn_size)
     self.assertEqual(dynamic_size, 1)
 
