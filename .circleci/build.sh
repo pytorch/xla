5866d4cd187adb37041a0bd136ff22a97de9c841
Goran Petrovic <stgpetrovic@gmail.com>
2023-01-31 10:27:02 +0000

    Merge with pjrt client test build changes.

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -35,8 +35,6 @@ checkout_torch_pin_if_available
 
 install_deps_pytorch_xla $XLA_DIR $USE_CACHE
 
-apply_patches
-
 python -c "import fcntl; fcntl.fcntl(1, fcntl.F_SETFL, 0)"
 
 python setup.py install
