267130743d38164d7eac1571c8e58f05a1557c97
Goran Petrovic <stgpetrovic@gmail.com>
2023-01-31 22:35:17 +0000

    Format test and plugin

--- test/cpp/test_xla_sharding.cpp
+++ test/cpp/test_xla_sharding.cpp
@@ -5,9 +5,9 @@
 #include <iostream>
 
 #include "cpp_test_util.h"
+#include "tensorflow/compiler/xla/xla_data.pb.h"
 #include "third_party/xla_client/env_vars.h"
 #include "third_party/xla_client/sys_util.h"
-#include "tensorflow/compiler/xla/xla_data.pb.h"
 #include "torch_xla/csrc/aten_xla_bridge.h"
 #include "torch_xla/csrc/helpers.h"
 #include "torch_xla/csrc/tensor.h"
