0be5a0534181c63dac901701c6e9516974aca868
iefgnoix <isaacwxf23@gmail.com>
2023-02-02 14:12:35 -0600

    Implement missing XLASymNodeImpl::Sub (#4551)
    * implementing xlasymnodeimpl::sub
    
    * the code buillds fine and the test passed.
    
    * fix lnter
    
    * change opkind to be more specific
    
    * check counter in the test.
    
    * fix linter
    
    * unify the op kind
    
    * fix linter
    
    * fix a failing test.

--- torch_xla/csrc/tensor.cpp
+++ torch_xla/csrc/tensor.cpp
@@ -639,8 +639,13 @@ c10::SymNode XLASymNodeImpl::add(const c10::SymNode& other) {
 }
 
 c10::SymNode XLASymNodeImpl::sub(const c10::SymNode& other) {
-  XLA_CHECK(false) << "XLASymNodeImpl::" << __FUNCTION__
-                   << " has not been implemented.";
+  TORCH_LAZY_FN_COUNTER("xla::size_");
+
+  torch_xla::XLASymNodeImpl* p_other =
+      dynamic_cast<XLASymNodeImpl*>(other.get());
+  torch::lazy::NodePtr n_sub =
+      torch::lazy::MakeNode<SizeSub>(node(), p_other->node());
+  return c10::make_intrusive<XLASymNodeImpl>(n_sub);
 }
 
 c10::SymNode XLASymNodeImpl::mul(const c10::SymNode& other) {
