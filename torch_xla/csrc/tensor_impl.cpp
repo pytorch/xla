222049bc37b09f424949a28292fa3e79350dbc01
Xiongfei Wei <isaacwxf23@gmail.com>
2023-02-07 23:06:36 +0000

    fix linter

--- torch_xla/csrc/tensor_impl.cpp
+++ torch_xla/csrc/tensor_impl.cpp
@@ -114,7 +114,8 @@ void XLATensorImpl::shallow_copy_from(
 }
 
 at::IntArrayRef XLATensorImpl::sizes_custom() const {
-  XLA_CHECK(!has_symbolic_sizes_strides_) << "Cannot call sizes() on an XLA tensor with symbolic sizes/strides";
+  XLA_CHECK(!has_symbolic_sizes_strides_)
+      << "Cannot call sizes() on an XLA tensor with symbolic sizes/strides";
   const_cast<XLATensorImpl*>(this)->SetupSizeProperties();
   return sizes_default();
 }
