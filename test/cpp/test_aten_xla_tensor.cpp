c0a05a820c74be240b52c3f6bdfa8ceefe5e878e
Wonjoo Lee <wonjoo@google.com>
2023-02-07 15:10:10 -0800

    Lower masked_scatter to fix masked_scatter op test in xla/test/test_ops.py (#4438)
    * Lower masked_scatter instead of masked_scatter_ and update broadcasting logic
    
    * Run linter

--- test/cpp/test_aten_xla_tensor.cpp
+++ test/cpp/test_aten_xla_tensor.cpp
@@ -5371,8 +5371,7 @@ TEST_F(AtenXlaTensorTest, TestMaskedScatter) {
       // calls.
       ExpectCounterNotChanged("aten::.*", cpp_test::GetIgnoredCounters());
     }
-    ExpectCounterChanged("xla::masked_scatter_",
-                         cpp_test::GetIgnoredCounters());
+    ExpectCounterChanged("xla::masked_scatter", cpp_test::GetIgnoredCounters());
     ResetCounters();
   });
 }
