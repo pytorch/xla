0c88622588bd87717c7b02d0519fc5692dcce557
Xiongfei Wei <isaacwxf23@gmail.com>
2023-02-09 21:33:04 +0000

    re-enable all failing tests

--- test/cpp/test_aten_xla_tensor.cpp
+++ test/cpp/test_aten_xla_tensor.cpp
@@ -5312,7 +5312,6 @@ TEST_F(AtenXlaTensorTest, TestOneIndexTransfer) {
 }
 
 TEST_F(AtenXlaTensorTest, TestNonzero) {
-  GTEST_SKIP() << "Needs additional DS support after functionalization";
   torch::Tensor a = torch::zeros({4, 2}, torch::TensorOptions(torch::kFloat));
   a[0][1] = 1.0;
   a[1][0] = 2.0;
