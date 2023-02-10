7de406a2478a4703a44403f94d6510530ae30ef0
Yanming Wang <yanmwang@amazon.com>
2023-02-10 10:10:15 -0800

    Fix fp64 div issue (#4595)
    * Add test for div by f64 bug
    
    * Add torch_pin
    
    * Delete .torch_pin
    
    ---------
    
    Co-authored-by: JackCaoG <59073027+JackCaoG@users.noreply.github.com>

--- test/test_operations_hlo.py
+++ test/test_operations_hlo.py
@@ -50,6 +50,16 @@ class TestOperationsHlo(unittest.TestCase):
       assert 'f32' in root_hlo
       assert 'f64' not in root_hlo
 
+  def test_div_by_f64(self):
+    mod = torch.nn.MultiheadAttention(768, 12, batch_first=True)
+    mod.to(xm.xla_device())
+    a = torch.rand(1, 512, 768).to(xm.xla_device())
+    b, _ = mod(a, a, a, need_weights=False)
+    b.sum().backward()
+    hlo_text = torch_xla._XLAC._get_xla_tensors_text(
+        [p.grad for p in mod.parameters() if p.requires_grad])
+    assert 'f64' not in hlo_text
+
 
 if __name__ == '__main__':
   torch.set_default_tensor_type('torch.FloatTensor')
