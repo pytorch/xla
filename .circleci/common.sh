bc007b547771bb257d538fe862bde20c41cc729e
Goran Petrovic <stgpetrovic@gmail.com>
2023-02-21 18:16:22 +0000

    Add comments about local python

--- .circleci/common.sh
+++ .circleci/common.sh
@@ -112,7 +112,11 @@ function install_deps_pytorch_xla() {
 function build_torch_xla() {
   XLA_DIR=$1
   pushd "$XLA_DIR"
-  python setup.py install
+
+  # Passing `--action_env=PATH=$PATH` makes bazel use the configured python version
+  # with required packages installed; otherwise it might find a system version with no packages
+  # in case of `conda` or `virtualenv`.
+  EXTRA_BAZEL_FLAGS="--action_env PATH=$PATH" python setup.py install
   popd
 }
 
