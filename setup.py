8d1682c34b77793111b9e8564cd27c162893e858
manfeibai <manfei@google.com>
2023-02-22 22:30:29 +0000

    update with bazel

--- setup.py
+++ setup.py
@@ -287,6 +287,8 @@ for ipath in [
     'bazel-xla',
     'bazel-bin/external/org_tensorflow/',
     'bazel-xla/external/org_tensorflow/',
+    'bazel-bin/external/org_openxla/',
+    'bazel-xla/external/org_openxla/',
     'bazel-xla/external/com_github_grpc_grpc/include',
     'bazel-xla/external/com_google_protobuf/src',
     'bazel-xla/external/eigen_archive',
