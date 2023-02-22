5b66324a0f80cc812f03bd3266d8f7c4cdaf2fea
Jiewen Tan <jwtan@google.com>
2023-02-22 20:38:42 +0000

    Revert "Turn on keep going"
    This reverts commit 2ded7ca92c528c6906aff3db9675c22eb32f60bd.

--- .circleci/common.sh
+++ .circleci/common.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # See Note [Keep Going]
-CONTINUE_ON_ERROR=True
+CONTINUE_ON_ERROR=false
 if [[ "$CONTINUE_ON_ERROR" == "1" ]]; then
   set +e
 fi
--- test/cpp/run_tests.sh
+++ test/cpp/run_tests.sh
@@ -10,7 +10,7 @@ BAZEL_REMOTE_CACHE="0"
 BAZEL_VERB="test"
 
 # See Note [Keep Going]
-CONTINUE_ON_ERROR=True
+CONTINUE_ON_ERROR=false
 if [[ "$CONTINUE_ON_ERROR" == "1" ]]; then
   set +e
 fi
