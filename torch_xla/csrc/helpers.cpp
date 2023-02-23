4ce7f14ca74d9e3c98b29a79b945f1ee5f84a51b
Xiongfei Wei <isaacwxf23@gmail.com>
2023-02-23 18:25:30 +0000

    fix build error

--- torch_xla/csrc/helpers.cpp
+++ torch_xla/csrc/helpers.cpp
@@ -269,8 +269,8 @@ XlaHelpers::GetDynamicReshapeInfo(const xla::Shape& input_shape,
     int64_t dynamic_dimension = -1;
     int64_t out_size = 1;
     for (int64_t i = 0; i < output_sizes.size(); ++i) {
-      XLA_CHECK_LE(out_size, size_at_dyndim / input_shape.dimensions(
-                                                  input_dynamic_dimension))
+      XLA_CHECK_LE(out_size, size_prod_until_dyndim / input_shape.dimensions(
+                                                  input_dyndim_idx))
           << "Unable to map dynamic dimension of shape " << input_shape
           << " to output sizes (" << absl::StrJoin(output_sizes, ", ") << ")";
       out_size *= output_sizes[i];
