bdfe5f9a6b68f672a94dc3f84b3c37cd75da3972
Goran Petrovic <stgpetrovic@gmail.com>
2023-02-23 12:19:18 +0000

    Fix build flags

--- build_torch_xla_libs.sh
+++ build_torch_xla_libs.sh
@@ -34,24 +34,22 @@ if [[ "$XLA_BAZEL_VERBOSE" == "1" ]]; then
   VERBOSE="-s"
 fi
 
-if [[ "$TPUVM_MODE" == "1" ]]; then
-  if [[ "$BAZEL_REMOTE" == "1" ]]; then
-    OPTS+=(--config=rbe_cpu_linux_py39)
-  else
-    OPTS+=(--config=cuda)
-  fi
-fi
-
 MAX_JOBS=
 if [[ ! -z "$BAZEL_JOBS" ]]; then
   MAX_JOBS="--jobs=$BAZEL_JOBS"
 fi
 
+if [[ "$TPUVM_MODE" == "1" ]]; then
+  OPTS+=(--config=tpu)
+  if [[ "$BAZEL_REMOTE" == "1" ]]; then
+    OPTS+=(--config=rbe_cpu_linux_py39)
+  fi
+fi
+
 if [[ "$XLA_CUDA" == "1" ]]; then
+  OPTS+=(--config=cuda)
   if [[ "$BAZEL_REMOTE" == "1" ]]; then
     OPTS+=(--config=rbe_linux_cuda11.8_nvcc_py3.9)
-  else
-    OPTS+=(--config=cuda)
   fi
 fi
 
