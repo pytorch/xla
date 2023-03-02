d91736efb1871b87ffbd8c675c02eb7899dc89fe
David Dunleavy <ddunleavy@google.com>
2023-03-02 21:45:11 +0000

    Update formatting

--- third_party/xla_client/debug_macros.h
+++ third_party/xla_client/debug_macros.h
@@ -7,20 +7,13 @@
 
 #define XLA_ERROR() TF_ERROR_STREAM()
 #define XLA_CHECK(c) TF_CHECK(c) << "\n" << tsl::CurrentStackTrace()
-#define XLA_CHECK_OK(c) \
-  TF_CHECK_OK(c) << "\n" << tsl::CurrentStackTrace()
-#define XLA_CHECK_EQ(a, b) \
-  TF_CHECK_EQ(a, b) << "\n" << tsl::CurrentStackTrace()
-#define XLA_CHECK_NE(a, b) \
-  TF_CHECK_NE(a, b) << "\n" << tsl::CurrentStackTrace()
-#define XLA_CHECK_LE(a, b) \
-  TF_CHECK_LE(a, b) << "\n" << tsl::CurrentStackTrace()
-#define XLA_CHECK_GE(a, b) \
-  TF_CHECK_GE(a, b) << "\n" << tsl::CurrentStackTrace()
-#define XLA_CHECK_LT(a, b) \
-  TF_CHECK_LT(a, b) << "\n" << tsl::CurrentStackTrace()
-#define XLA_CHECK_GT(a, b) \
-  TF_CHECK_GT(a, b) << "\n" << tsl::CurrentStackTrace()
+#define XLA_CHECK_OK(c) TF_CHECK_OK(c) << "\n" << tsl::CurrentStackTrace()
+#define XLA_CHECK_EQ(a, b) TF_CHECK_EQ(a, b) << "\n" << tsl::CurrentStackTrace()
+#define XLA_CHECK_NE(a, b) TF_CHECK_NE(a, b) << "\n" << tsl::CurrentStackTrace()
+#define XLA_CHECK_LE(a, b) TF_CHECK_LE(a, b) << "\n" << tsl::CurrentStackTrace()
+#define XLA_CHECK_GE(a, b) TF_CHECK_GE(a, b) << "\n" << tsl::CurrentStackTrace()
+#define XLA_CHECK_LT(a, b) TF_CHECK_LT(a, b) << "\n" << tsl::CurrentStackTrace()
+#define XLA_CHECK_GT(a, b) TF_CHECK_GT(a, b) << "\n" << tsl::CurrentStackTrace()
 
 template <typename T>
 T ConsumeValue(xla::StatusOr<T>&& status) {
