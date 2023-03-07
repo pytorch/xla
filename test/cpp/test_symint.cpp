a46ee8cb5bb0a162a831bf9d2c84e7377d1c4842
JackCaoG <59073027+JackCaoG@users.noreply.github.com>
2023-03-06 17:18:05 -0800

    fix linter (#4725)

--- test/cpp/test_symint.cpp
+++ test/cpp/test_symint.cpp
@@ -15,8 +15,8 @@ namespace torch_xla {
 namespace cpp_test {
 
 static c10::SymInt make_symint(const torch::lazy::NodePtr& p) {
-  return c10::SymInt(
-      static_cast<c10::SymNode>(c10::make_intrusive<XLASymNodeImpl>(p, PyType::INT)));
+  return c10::SymInt(static_cast<c10::SymNode>(
+      c10::make_intrusive<XLASymNodeImpl>(p, PyType::INT)));
 }
 
 TEST(SymintTest, TestStaticSymint) {
@@ -213,7 +213,8 @@ TEST(SymintTest, TestXLASymNodeImplStr) {
   torch::lazy::Value expand_value = torch::lazy::Value(expand_node, 0);
   torch::lazy::NodePtr size_node =
       torch::lazy::MakeNode<SizeNode>(expand_value, 0);
-  c10::SymNode symint_node = c10::make_intrusive<XLASymNodeImpl>(size_node, PyType::INT);
+  c10::SymNode symint_node =
+      c10::make_intrusive<XLASymNodeImpl>(size_node, PyType::INT);
   ASSERT_EQ(symint_node.get()->str(), "<=2");
 }
 
