add720fcb5fbc18fccc2cb57b3ed2d8afa9523b8
Edward Z. Yang <ezyang@fb.com>
2023-03-06 19:41:27 -0500

    Manually track type of SymNode (#4722)
    * Manually track type of SymNode
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    * lint
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    * buildfix
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    * pin
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    * lint
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    * fix cpp test
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    * More build fixes
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    * lintfix
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    * remove pin
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    ---------
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- test/cpp/test_aten_xla_tensor.cpp
+++ test/cpp/test_aten_xla_tensor.cpp
@@ -5313,8 +5313,8 @@ TEST_F(AtenXlaTensorTest, TestExpandSymIntStatic) {
 }
 
 static c10::SymInt make_symint(const torch::lazy::NodePtr& p) {
-  return c10::SymInt(
-      static_cast<c10::SymNode>(c10::make_intrusive<XLASymNodeImpl>(p)));
+  return c10::SymInt(static_cast<c10::SymNode>(
+      c10::make_intrusive<XLASymNodeImpl>(p, PyType::INT)));
 }
 
 TEST_F(AtenXlaTensorTest, TestEye) {
