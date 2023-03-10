63faa0dcdd474ad93719d499b4470915a1a48e56
Yeounoh Chung <yeounoh@google.com>
2023-03-10 10:00:35 -0800

    Generate CI coverage metric in lcov format and upload (#4682)
    * Reformat coverage megric into LCOV format
    
    * Enable source coverage by installed torch_xla source
    
    * Run all tests for coverage
    
    * Delete test/utils/run_test_covereage.sh

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -45,13 +45,24 @@ export PYTORCH_TEST_WITH_SLOW=1
 export XLA_DUMP_FATAL_STACK=1
 export CPU_NUM_DEVICES=4
 
+TORCH_XLA_DIR=$(cd ~; dirname "$(python -c 'import torch_xla; print(torch_xla.__file__)')")
+COVERAGE_FILE="$CDIR/../.coverage"
+
+function run_coverage {
+  if [ "$USE_COVERAGE" != "0" ]; then
+    coverage run --source="$TORCH_XLA_DIR" -p "$@"
+  else
+    python3 "$@"
+  fi
+}
+
 function run_test {
   echo "Running in PjRt runtime: $@"
   if [ -x "$(command -v nvidia-smi)" ]; then
-    PJRT_DEVICE=GPU "$@"
+    PJRT_DEVICE=GPU run_coverage "$@"
   else
     # TODO(darisoy): run these tests with multiple CPU devices, this fails due to TF issue.
-    PJRT_DEVICE=CPU CPU_NUM_DEVICES=1 "$@"
+    PJRT_DEVICE=CPU CPU_NUM_DEVICES=1 run_coverage "$@"
   fi
 }
 
@@ -97,9 +108,9 @@ function run_xla_backend_mp {
 
 function run_xrt {
   if [ -x "$(command -v nvidia-smi)" ]; then
-    GPU_NUM_DEVICES=2 "$@"
+    GPU_NUM_DEVICES=2 run_coverage "$@"
   else
-    XRT_DEVICE_MAP="CPU:0;/job:localservice/replica:0/task:0/device:XLA_CPU:0" XRT_WORKERS="localservice:0;grpc://localhost:$(shuf -i 40701-40999 -n 1)" "$@"
+    XRT_DEVICE_MAP="CPU:0;/job:localservice/replica:0/task:0/device:XLA_CPU:0" XRT_WORKERS="localservice:0;grpc://localhost:$(shuf -i 40701-40999 -n 1)" run_coverage "$@"
   fi
 }
 
@@ -125,74 +136,74 @@ function run_torchrun {
 function run_xrt_tests {
   # For features not supported in PJRT
   echo "Running XRT tests"
-  run_opbyop python3 "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
-  run_async_scalar python3 "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
-  run_torchrun python3 "$CDIR/test_allreduce_torchrun.py"
+  run_opbyop  "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
+  run_async_scalar  "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
+  run_torchrun  "$CDIR/test_allreduce_torchrun.py"
 }
 
 function run_op_tests {
-  run_dynamic python3 "$CDIR/../../test/test_view_ops.py" "$@" -v TestViewOpsXLA
-  run_test python3 "$CDIR/../../test/test_torch.py" "$@" -v TestTorchDeviceTypeXLA
-  run_dynamic python3 "$CDIR/../../test/test_torch.py" "$@" -v TestDevicePrecisionXLA
-  run_test python3 "$CDIR/../../test/test_torch.py" "$@" -v TestTensorDeviceOpsXLA
-  run_test python3 "$CDIR/../../test/test_indexing.py" "$@" -v TestIndexingXLA
-  run_test python3 "$CDIR/../../test/test_indexing.py" "$@" -v NumpyTestsXLA
-  run_dynamic python3 "$CDIR/../../test/test_nn.py" "$@" -v TestNNDeviceTypeXLA
-  run_dynamic python3 "$CDIR/../../test/nn/test_dropout.py" "$@" -v TestDropoutNNDeviceTypeXLA
-  run_dynamic python3 "$CDIR/../../test/nn/test_pooling.py" "$@" -v TestPoolingNNDeviceTypeXLA
-  run_dynamic python3 "$CDIR/../../test/nn/test_embedding.py" "$@" -v TestEmbeddingNNDeviceTypeXLA
-  run_dynamic python3 "$CDIR/../../test/nn/test_convolution.py" "$@" -v TestConvolutionNNDeviceTypeXLA
-  run_dynamic python3 "$CDIR/../../test/nn/test_multihead_attention.py" "$@" -v TestMultiheadAttentionNNDeviceTypeXLA
-  run_dynamic python3 "$CDIR/../../test/test_type_promotion.py" "$@" -v TestTypePromotionXLA
-  run_dynamic python3 "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
-  run_dynamic python3 "$CDIR/test_dynamic_shapes.py"
-  run_dynamic python3 "$CDIR/test_dynamic_shape_models.py" "$@" --verbosity=$VERBOSITY
-  run_eager_debug python3 "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
-  run_test python3 "$CDIR/test_grad_checkpoint.py"
-  run_test python3 "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
-  run_test python3 "$CDIR/test_async_closures.py"
-  run_test python3 "$CDIR/test_xla_dist.py"
-  run_test python3 "$CDIR/test_profiler.py"
-  run_test python3 "$CDIR/test_ops.py"
-  run_test python3 "$CDIR/test_metrics.py"
-  run_test python3 "$CDIR/test_zero1.py"
-  run_test python3 "$CDIR/dynamo/test_dynamo_integrations_util.py"
-  run_test python3 "$CDIR/dynamo/test_dynamo.py"
-  run_test python3 "$CDIR/dynamo/test_bridge.py"
-  run_test python3 "$CDIR/dynamo/test_num_output.py"
-  run_save_tensor_file python3 "$CDIR/dynamo/test_dynamo_graph_dump.py"
-  run_downcast_bf16 python3 "$CDIR/test_data_type.py"
-  run_use_bf16 python3 "$CDIR/test_data_type.py"
-  run_xla_ir_debug python3 "$CDIR/test_env_var_mapper.py"
-  run_xla_hlo_debug python3 "$CDIR/test_env_var_mapper.py"
-  run_test python3 "$CDIR/pjrt/test_experimental_pjrt.py"
-  run_test python3 "$CDIR/pjrt/test_experimental_tpu.py"
-  run_test python3 "$CDIR/pjrt/test_ddp.py"
-  run_test python3 "$CDIR/pjrt/test_mesh_service.py"
-  run_test python3 "$CDIR/spmd/test_xla_sharding.py"
-  run_test python3 "$CDIR/spmd/test_xla_virtual_device.py"
-  run_test python3 "$CDIR/test_operations_hlo.py" "$@" --verbosity=$VERBOSITY
-  run_test python3 "$CDIR/test_input_output_aliases.py"
-  run_test python3 "$CDIR/test_torch_distributed_xla_backend.py"
+  run_dynamic "$CDIR/../../test/test_view_ops.py" "$@" -v TestViewOpsXLA
+  run_test "$CDIR/../../test/test_torch.py" "$@" -v TestTorchDeviceTypeXLA
+  run_dynamic "$CDIR/../../test/test_torch.py" "$@" -v TestDevicePrecisionXLA
+  run_test "$CDIR/../../test/test_torch.py" "$@" -v TestTensorDeviceOpsXLA
+  run_test "$CDIR/../../test/test_indexing.py" "$@" -v TestIndexingXLA
+  run_test "$CDIR/../../test/test_indexing.py" "$@" -v NumpyTestsXLA
+  run_dynamic "$CDIR/../../test/test_nn.py" "$@" -v TestNNDeviceTypeXLA
+  run_dynamic "$CDIR/../../test/nn/test_dropout.py" "$@" -v TestDropoutNNDeviceTypeXLA
+  run_dynamic "$CDIR/../../test/nn/test_pooling.py" "$@" -v TestPoolingNNDeviceTypeXLA
+  run_dynamic "$CDIR/../../test/nn/test_embedding.py" "$@" -v TestEmbeddingNNDeviceTypeXLA
+  run_dynamic "$CDIR/../../test/nn/test_convolution.py" "$@" -v TestConvolutionNNDeviceTypeXLA
+  run_dynamic "$CDIR/../../test/nn/test_multihead_attention.py" "$@" -v TestMultiheadAttentionNNDeviceTypeXLA
+  run_dynamic "$CDIR/../../test/test_type_promotion.py" "$@" -v TestTypePromotionXLA
+  run_dynamic "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
+  run_dynamic "$CDIR/test_dynamic_shapes.py"
+  run_dynamic "$CDIR/test_dynamic_shape_models.py" "$@" --verbosity=$VERBOSITY
+  run_eager_debug  "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
+  run_test "$CDIR/test_grad_checkpoint.py"
+  run_test "$CDIR/test_operations.py" "$@" --verbosity=$VERBOSITY
+  run_test "$CDIR/test_async_closures.py"
+  run_test "$CDIR/test_xla_dist.py"
+  run_test "$CDIR/test_profiler.py"
+  run_test "$CDIR/test_ops.py"
+  run_test "$CDIR/test_metrics.py"
+  run_test "$CDIR/test_zero1.py"
+  run_test "$CDIR/dynamo/test_dynamo_integrations_util.py"
+  run_test "$CDIR/dynamo/test_dynamo.py"
+  run_test "$CDIR/dynamo/test_bridge.py"
+  run_test "$CDIR/dynamo/test_num_output.py"
+  run_save_tensor_file "$CDIR/dynamo/test_dynamo_graph_dump.py"
+  run_downcast_bf16 "$CDIR/test_data_type.py"
+  run_use_bf16 "$CDIR/test_data_type.py"
+  run_xla_ir_debug "$CDIR/test_env_var_mapper.py"
+  run_xla_hlo_debug "$CDIR/test_env_var_mapper.py"
+  run_test "$CDIR/pjrt/test_experimental_pjrt.py"
+  run_test "$CDIR/pjrt/test_experimental_tpu.py"
+  run_test "$CDIR/pjrt/test_ddp.py"
+  run_test "$CDIR/pjrt/test_mesh_service.py"
+  run_test "$CDIR/spmd/test_xla_sharding.py"
+  run_test "$CDIR/spmd/test_xla_virtual_device.py"
+  run_test "$CDIR/test_operations_hlo.py" "$@" --verbosity=$VERBOSITY
+  run_test "$CDIR/test_input_output_aliases.py"
+  run_test "$CDIR/test_torch_distributed_xla_backend.py"
 }
 
 function run_mp_op_tests {
-  run_test python3 "$CDIR/test_mp_replication.py"
-  run_test python3 "$CDIR/test_mp_all_to_all.py"
-  run_test python3 "$CDIR/test_mp_collective_permute.py"
-  run_test python3 "$CDIR/test_mp_all_gather.py"
-  run_test python3 "$CDIR/test_mp_reduce_scatter.py"
-  run_test python3 "$CDIR/test_mp_distributed_mm.py"
-  run_test python3 "$CDIR/test_mp_save.py"
-  run_test python3 "$CDIR/test_mp_mesh_reduce.py"
-  run_test python3 "$CDIR/test_mp_sync_batch_norm.py"
-  run_xla_backend_mp python3 "$CDIR/test_torch_distributed_all_gather_xla_backend.py"
-  run_xla_backend_mp python3 "$CDIR/test_torch_distributed_all_reduce_xla_backend.py"
-  run_xla_backend_mp python3 "$CDIR/test_torch_distributed_multi_all_reduce_xla_backend.py"
-  run_xla_backend_mp python3 "$CDIR/test_torch_distributed_reduce_scatter_xla_backend.py"
-  run_xla_backend_mp python3 "$CDIR/test_ddp.py"
-  run_xla_backend_mp python3 "$CDIR/test_fsdp_auto_wrap.py"
-  run_xla_backend_mp python3 "$CDIR/test_torch_distributed_fsdp_meta.py"
+  run_test "$CDIR/test_mp_replication.py"
+  run_test "$CDIR/test_mp_all_to_all.py"
+  run_test "$CDIR/test_mp_collective_permute.py"
+  run_test "$CDIR/test_mp_all_gather.py"
+  run_test "$CDIR/test_mp_reduce_scatter.py"
+  run_test "$CDIR/test_mp_distributed_mm.py"
+  run_test "$CDIR/test_mp_save.py"
+  run_test "$CDIR/test_mp_mesh_reduce.py"
+  run_test "$CDIR/test_mp_sync_batch_norm.py"
+  run_xla_backend_mp "$CDIR/test_torch_distributed_all_gather_xla_backend.py"
+  run_xla_backend_mp "$CDIR/test_torch_distributed_all_reduce_xla_backend.py"
+  run_xla_backend_mp "$CDIR/test_torch_distributed_multi_all_reduce_xla_backend.py"
+  run_xla_backend_mp "$CDIR/test_torch_distributed_reduce_scatter_xla_backend.py"
+  run_xla_backend_mp "$CDIR/test_ddp.py"
+  run_xla_backend_mp "$CDIR/test_fsdp_auto_wrap.py"
+  run_xla_backend_mp "$CDIR/test_torch_distributed_fsdp_meta.py"
 }
 
 function run_tests {
