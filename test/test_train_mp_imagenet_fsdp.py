cc3d82dc36dcc8013d7732ebe4610f5aa21f13aa
Jiewen Tan <jwtan@google.com>
2023-03-13 21:43:01 +0000

    Move add_step_closure out from build_graph

--- test/test_train_mp_imagenet_fsdp.py
+++ test/test_train_mp_imagenet_fsdp.py
@@ -333,10 +333,9 @@ def train_imagenet():
           tracker.add(FLAGS.batch_size)
           if lr_scheduler:
             lr_scheduler.step()
-          if step % FLAGS.log_steps == 0:
-            xm.add_step_closure(
-                _train_update,
-                args=(device, step, loss, tracker, epoch, writer))
+        if step % FLAGS.log_steps == 0:
+          xm.add_step_closure(
+              _train_update, args=(device, step, loss, tracker, epoch, writer))
 
   def test_loop_fn(loader, epoch):
     total_samples, correct = 0, 0
