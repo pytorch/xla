7262bd518cb25d3dd4139c959fc0ad3bf0bbabb5
manfeibai <manfei@google.com>
2023-03-14 23:48:58 +0000

    delete CreateXrtMetricReport

--- third_party/xla_client/metrics_reader.cc
+++ third_party/xla_client/metrics_reader.cc
@@ -27,51 +27,11 @@ MetricFnInfo GetMetricRenderInfo(const Percentile& percentile) {
   }
 }
 
-std::string CreateXrtMetricReport(
-    const std::map<std::string, Metric>& xrt_metrics) {
-  std::stringstream ss;
-  for (const auto& name_metric : xrt_metrics) {
-    if (name_metric.second.percentile) {
-      const Percentile& percentile = *name_metric.second.percentile;
-      MetricFnInfo minfo = GetMetricRenderInfo(percentile);
-      ss << "Metric: " << name_metric.first << std::endl;
-      ss << "  TotalSamples: " << percentile.total_samples << std::endl;
-      ss << "  Accumulator: "
-         << minfo.repr_fn(percentile.accumulator * minfo.scale) << std::endl;
-      ss << "  Mean: " << minfo.repr_fn(percentile.mean * minfo.scale)
-         << std::endl;
-      ss << "  StdDev: " << minfo.repr_fn(percentile.stddev * minfo.scale)
-         << std::endl;
-
-      uint64_t delta_time = percentile.end_nstime - percentile.start_nstime;
-      if (delta_time > 0) {
-        double count_sec = 1e6 * (static_cast<double>(percentile.num_samples) /
-                                  (delta_time / 1000.0));
-        ss << "  Rate: " << count_sec << " / second" << std::endl;
-      }
-
-      ss << "  Percentiles: ";
-      for (size_t i = 0; i < percentile.points.size(); ++i) {
-        if (i > 0) {
-          ss << "; ";
-        }
-        ss << percentile.points[i].percentile
-           << "%=" << minfo.repr_fn(percentile.points[i].value * minfo.scale);
-      }
-      ss << std::endl;
-    } else if (name_metric.second.int64_value) {
-      ss << "Counter: " << name_metric.first << std::endl;
-      ss << "  Value: " << *name_metric.second.int64_value << std::endl;
-    }
-  }
-  return ss.str();
-}
-
 }  // namespace
 
 std::string CreateMetricReport(
     const std::map<std::string, Metric>& xrt_metrics) {
-  return metrics::CreateMetricReport() + CreateXrtMetricReport(xrt_metrics);
+  return metrics::CreateMetricReport();
 }
 
 std::string CreateMetricReport(const std::vector<std::string>& counter_names,
