ee7792b0435b352400ffc610c05cb2214604cd0b
manfeibai <manfei@google.com>
2023-03-20 18:36:27 +0000

    delete xla_dist one

--- test/pjrt/test_experimental_pjrt.py
+++ test/pjrt/test_experimental_pjrt.py
@@ -57,9 +57,7 @@ class TestExperimentalPjrt(parameterized.TestCase):
   }, True), ('pjrt_gpu', {
       'PJRT_DEVICE': 'GPU',
       'GPU_NUM_DEVICES': '4'
-  }, True), ('xla_dist_worker', {
-      'PJRT_DEVICE': 'TPU',
-  }, False))
+  }, True))
   def test_pjrt_default_device(self, env_vars, expect_using_pjrt):
     with mock.patch.dict(os.environ, env_vars, clear=True):
       # Print a warningif we had to select a default runtime
