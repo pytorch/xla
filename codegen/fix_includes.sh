95fd7f2505c31c64bb0e0b9f89c5b89f33e89ef5
Goran Petrovic <stgpetrovic@gmail.com>
2023-03-27 19:57:58 +0000

    Update documentation

--- codegen/fix_includes.sh
+++ codegen/fix_includes.sh
@@ -4,6 +4,8 @@
 sed -i 's#<torch_xla/csrc/tensor.h>#"torch_xla/csrc/tensor.h"#' $@
 
 # remove the runfiles-prefix used in codegen for pytorch
+# `torchgen` generates relative includes path and does not support customizing the root,
+# so we have to fix them up.
 sed -i 's#bazel-out/k8-opt/bin/codegen/lazy_tensor_generator.runfiles/torch/##' $@
 
 # use the generated files that are in the compilation unit
