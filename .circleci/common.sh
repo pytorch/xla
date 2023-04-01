ee736021eb4e388ee544b0440c9e7bc7ac8747ea
steventk-g <107513673+steventk-g@users.noreply.github.com>
2023-04-01 12:42:28 -0700

    Fix copy

--- .circleci/common.sh
+++ .circleci/common.sh
@@ -182,7 +182,7 @@ function run_torch_xla_tests() {
         ls /tmp/pytorch/xla/build/temp.linux-x86_64-cpython-38/torch_xla/csrc
         chmod +x /tmp/pytorch/xla/test/cpp/get_coverage.sh
         lcov --directory /tmp/pytorch/xla/build/temp.linux-x86_64-cpython-38/torch_xla/csrc --base-directory . --gcov-tool /tmp/pytorch/xla/test/cpp/get_coverage.sh --capture -o cpp_lcov.info
-        cp cpp_lcov.info htmlcov/
+        cp cpp_lcov.info htmlcov/cpp_lcov.info
         mv htmlcov ~/
       fi
     popd
