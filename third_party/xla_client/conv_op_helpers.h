e753707ba755972d0e97abec2790c897776144af
manfeibai <manfei@google.com>
2023-04-01 06:56:42 +0000

    delete some tensorflow

--- third_party/xla_client/conv_op_helpers.h
+++ third_party/xla_client/conv_op_helpers.h
@@ -27,16 +27,55 @@ limitations under the License.
 
 namespace xla {
 
+// Padding: the padding we apply to the input tensor along the rows and columns
+// dimensions. This is usually used to make sure that the spatial dimensions do
+// not shrink when we progress with convolutions. Three types of padding are
+// supported:
+//   VALID: No padding is carried out.
+//   SAME: The pad value is computed so that the output will have the same
+//         dimensions as the input.
+//   EXPLICIT: The user specifies the pad values in the explicit_paddings
+//             attribute.
+// The padded area is typically zero-filled. For pooling ops, the padded area is
+// instead ignored. For max pool, this is equivalent to padding with -infinity.
+enum Padding {
+  VALID = 1,     // No padding.
+  SAME = 2,      // Input and output layers have the same size.
+  EXPLICIT = 3,  // Padding is explicitly specified
+};
+
 // ConvOpAttrs contains all of the metadata necessary to specify an XLA
 // convolution.
 struct ConvOpAttrs {
+//   // Constructs a ConvOpAttrs, reading most of the attributes from `ctx`.
+//   static StatusOr<ConvOpAttrs> Create(int num_spatial_dims, bool depthwise,
+//                                       OpKernelConstruction* ctx);
+  bool depthwise;
+  int num_spatial_dims;
+  std::vector<tsl::int32> dilations;
+  std::vector<tsl::int32> strides;
+  Padding padding;
+  std::vector<tsl::int64> explicit_paddings;
+  ConvolutionDimensionNumbers data_format; // or use TensorFormat from `https://github.com/tensorflow/tensorflow/blob/3fb52f183ff77b71e8de558b03ec92aa3011d447/tensorflow/core/util/tensor_format.h#L37`
+  // ConvolutionDimensionNumbers is from `tensorflow/compiler/xla/xla_data.proto`
+};
+
+// ConvOpAttrs contains all of the metadata necessary to specify a TF or XLA
+// convolution.
+struct ConvOpAttrs {
+  // Constructs a ConvOpAttrs, reading most of the attributes from `ctx`.
+  static StatusOr<ConvOpAttrs> Create(int num_spatial_dims, bool depthwise,
+                                      OpKernelConstruction* ctx);
+
   bool depthwise;
   int num_spatial_dims;
   std::vector<int32> dilations;
   std::vector<int32> strides;
-  std::vector<int64> explicit_paddings;
-  ConvolutionDimensionNumbers data_format;
+  Padding padding;
+  std::vector<int64_t> explicit_paddings;
+  TensorFormat data_format;
 };
+
 
 // Computes the convolution with the given input, filter and attributes. Errors
 // returned by this function and the ones below are tagged with "type_string",
