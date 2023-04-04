b468530d34c3fb697e55133131c6ffbffa63a087
Mateusz Lewko <mateusz.lewko@gmail.com>
2023-04-04 13:05:18 +0000

    Put ARG just before run statement

--- docker/experimental/ansible/development.Dockerfile
+++ docker/experimental/ansible/development.Dockerfile
@@ -3,7 +3,6 @@
 # running PyTorch and PyTorch/XLA. The image doesn't contain any source code.
 ARG python_version=3.8
 ARG debian_version=buster
-ARG ansible_vars
 
 FROM python:${python_version}-${debian_version}
 
--- docker/experimental/terraform_modules/xla_docker_build/xla_docker_build.tf
+++ docker/experimental/terraform_modules/xla_docker_build/xla_docker_build.tf
@@ -65,7 +65,8 @@ locals {
             [for arg_key, arg_val in var.build_args : "--build-arg=${arg_key}=${arg_val}"],
 
             # Pass Ansible variables as JSON object, to make sure that
-            # types are preserved (e.g. boolean).
+            # types are preserved (e.g. boolean),
+            # see https://docs.ansible.com/ansible/latest/playbook_guide/playbooks_variables.html#key-value-format.
             ["--build-arg=ansible_vars=${jsonencode(var.ansible_vars)}"],
 
             # Pass Ansible variables as JSON object, to make sure that
