e7ae7d318add3f43045c9dbd8a3c076be2f6fd33
manfeibai <manfei@google.com>
2023-04-05 17:55:24 +0000

    delete #include tensorflow/core/framework/tensor_shape.h

--- torch_xla/csrc/convolution.cpp
+++ torch_xla/csrc/convolution.cpp
@@ -2,7 +2,6 @@
 
 #include "tensorflow/compiler/tf2xla/kernels/conv_op_helpers.h"
 #include "tensorflow/compiler/xla/client/lib/constants.h"
-#include "tensorflow/core/framework/tensor_shape.h"
 #include "tensorflow/core/kernels/conv_grad_ops.h"
 #include "third_party/xla_client/debug_macros.h"
 #include "torch_xla/csrc/helpers.h"
