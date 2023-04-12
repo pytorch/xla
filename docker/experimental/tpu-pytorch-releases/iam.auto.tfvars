a485536443e05081db29e6a4140edb22988c81b8
Mateusz Lewko <mateusz.lewko@gmail.com>
2023-04-12 17:32:40 +0000

    Configure IAMs by adding admins

--- docker/experimental/tpu-pytorch-releases/iam.auto.tfvars
+++ docker/experimental/tpu-pytorch-releases/iam.auto.tfvars
@@ -1,4 +1,5 @@
-project_iam_admins = [
+project_admins = [
+  "group:cloud-tpus-grpadm@twosync.google.com",
   "user:mlewko@google.com",
   "user:goranpetrovic@google.com",
   "user:jackcao@google.com",
--- docker/experimental/tpu-pytorch-releases/iam.tf
+++ docker/experimental/tpu-pytorch-releases/iam.tf
@@ -1,14 +1,22 @@
 data "google_project" "project" {}
 
-variable "project_iam_admins" {
+variable "project_admins" {
   type    = list(string)
   default = []
 }
 
 resource "google_project_iam_member" "project_iam_admins" {
-  for_each = toset(var.project_iam_admins)
+  for_each = toset(var.project_admins)
 
   project = data.google_project.project.project_id
   role    = "roles/resourcemanager.projectIamAdmin"
+  member  = each.value
+}
+
+resource "google_project_iam_member" "storage_admins" {
+  for_each = toset(var.project_admins)
+
+  project = data.google_project.project.project_id
+  role    = "roles/storage.admin"
   member  = each.value
 }
