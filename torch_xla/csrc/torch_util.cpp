e7a523bd4cbc64245476e1720009127040b91a42
Edward Z. Yang <ezyang@fb.com>
2023-04-14 10:02:13 -0700

    Update for SymInt API changes
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch_xla/csrc/torch_util.cpp
+++ torch_xla/csrc/torch_util.cpp
@@ -9,22 +9,21 @@
 namespace torch_xla {
 
 void SymIntElements::AddSymIntNodeElements(c10::SymInt& size) {
-  if (size.is_symbolic()) {
+  if (auto s = size.maybe_as_int()) {
+    size_nodes_.push_back(nullptr);
+    upper_bounds_.push_back(*s);
+    dynamic_dims_.push_back(false);
+  } else {
     // c10::SymInt --(convert)--> c10::SymIntNode --(cast)-->
     // lazy::SymIntNodeImpl
     // --(get)--> lazy::NodePtr --(cast)--> lazy::DimensionNode
-    c10::SymNode symbolicIntNode = size.toSymNodeImpl();
-    auto* lazySymNode = dynamic_cast<XLASymNodeImpl*>(symbolicIntNode.get());
+    auto* lazySymNode = dynamic_cast<XLASymNodeImpl*>(size.toSymNodeImplUnowned());
     torch::lazy::NodePtr size_node = lazySymNode->node();
     std::shared_ptr<torch::lazy::DimensionNode> dimension_node =
         std::dynamic_pointer_cast<torch::lazy::DimensionNode>(size_node);
     size_nodes_.push_back(size_node);
     upper_bounds_.push_back(dimension_node->getStaticValue());
     dynamic_dims_.push_back(dimension_node->isSymbolic());
-  } else {
-    size_nodes_.push_back(nullptr);
-    upper_bounds_.push_back(size.expect_int());
-    dynamic_dims_.push_back(size.is_symbolic());
   }
 }
 
