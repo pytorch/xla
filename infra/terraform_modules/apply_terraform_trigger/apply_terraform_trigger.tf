6f4ce61f814da4fd93fc5d95db50e84466f5deac
Mateusz Lewko <mateusz.lewko@gmail.com>
2023-04-21 13:15:45 +0000

    Add apply_terraform_trigger for -releases project

--- infra/terraform_modules/apply_terraform_trigger/apply_terraform_trigger.tf
+++ infra/terraform_modules/apply_terraform_trigger/apply_terraform_trigger.tf
@@ -1,20 +1,50 @@
+variable "config_directory" {
+  type = string
+}
+
+variable "include_files" {
+  type    = list(string)
+  default = []
+}
+
+variable "branch" {
+  type = string
+}
+
+variable "description" {
+  default = ""
+}
+
+variable "worker_pool_id" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
 module "cloud_build" {
   source = "../build_trigger"
 
-  name                    = "provision-terraform"
-  github_repo             = "pytorch/xla"
-  scheduler_account_email = var.scheduler_account_email
-  timeout_minutes         = var.timeout_minutes
-  worker_pool_id          = var.worker_pool_id
-  description             = var.description
-  location                = var.location
+  name            = var.name
+  github_repo     = "pytorch/xla"
+  timeout_minutes = 60
+  worker_pool_id  = var.worker_pool_id
+  description     = var.description
 
-  trigger_on_push     = var.trigger_on_push
-  trigger_on_schedule = var.trigger_on_schedule
+  trigger_on_push = { branch = var.branch }
 
-  steps = concat(
-    local.fetch_ansible_build_config,
-    local.build_and_push_docker_image_steps,
-    length(var.wheels_srcs) > 0 ? local.collect_and_publish_wheels_steps : []
-  )
+  steps = [
+    {
+      id         = "apply_terraform"
+      name       = "hashicorp/terraform:latest"
+      entrypoint = "sh"
+      args = [
+        "-c", join(" ", [
+          "terraform -chdir=${var.config_directory} init &&",
+          "terraform -chdir=${var.config_directory} apply -auto-approve",
+        ])
+      ]
+    },
+  ]
 }
