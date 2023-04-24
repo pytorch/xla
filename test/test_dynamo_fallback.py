c12315528f56883d8844568d7225e34efe952c89
seanlatias <seanlatias@gmail.com>
2023-04-24 13:12:23 +0000

    add tests

---
+++ test/test_dynamo_fallback.py
@@ -0,0 +1,42 @@
+import torch
+import torch_xla
+import torch_xla.core.xla_model as xm
+import torch._dynamo as dynamo
+import unittest
+
+
+class XlaDynamoFallbackTest(unittest.TestCase):
+
+  def test_operator_fallback(self):
+
+    @dynamo.optimize("torchxla_trace_once")
+    def fn_fallback(M, mat1, mat2):
+      A = torch.cummin(M, 1)
+      B = torch.add(A[0], mat1)
+      return torch.add(B, mat2)
+
+    M = torch.randn(5, 10, device=xm.xla_device())
+    mat1 = torch.randn(5, 10, device=xm.xla_device())
+    mat2 = torch.randn(5, 10, device=xm.xla_device())
+
+    res = fn_fallback(M, mat1, mat2)
+
+  def test_operator_operand_fallback(self):
+
+    @dynamo.optimize("torchxla_trace_once")
+    def fn_fallback(M, mat1, mat2, beta):
+      # xla currently only support alpha and beta == 1
+      A = torch.addmm(M, mat1, mat2, beta=beta)
+      B = torch.add(A, mat1)
+      return torch.add(B, mat1)
+
+    M = torch.randn(2, 3, device=xm.xla_device())
+    mat1 = torch.randn(2, 3, device=xm.xla_device())
+    mat2 = torch.randn(3, 3, device=xm.xla_device())
+
+    res = fn_fallback(M, mat1, mat2, 0.5)
+
+
+if __name__ == '__main__':
+  test = unittest.main()
+  sys.exit(0 if test.result.wasSuccessful() else 1)
