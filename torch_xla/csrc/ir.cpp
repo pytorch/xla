0cf761e8d2b93fc4fe12938290a21993c5639144
JackCaoG <jackcao@google.com>
2023-04-24 20:17:06 +0000

    Increase the default XLA_IR_SHAPE_CACHE_SIZE to accomunate the larger model size

--- torch_xla/csrc/ir.cpp
+++ torch_xla/csrc/ir.cpp
@@ -21,7 +21,7 @@ using ShapeCache =
 
 ShapeCache* GetShapeCache() {
   static int64_t shape_cache_size =
-      xla::sys_util::GetEnvInt("XLA_IR_SHAPE_CACHE_SIZE", 4096);
+      xla::sys_util::GetEnvInt("XLA_IR_SHAPE_CACHE_SIZE", 12288);
   static ShapeCache* cache = new ShapeCache(shape_cache_size);
   return cache;
 }
