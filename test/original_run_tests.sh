eea1a135e75b6b21e298c56a4d9fdda140079a5e
root <root@vm-manfei-05031312pm.c.bct-staging-vmx.internal>
2023-05-08 19:19:33 +0000

    python op tests

--- test/original_run_tests.sh
+++ test/original_run_tests.sh
@@ -218,7 +218,10 @@ function run_mp_op_tests {
 }
 
 function run_tests {
-  run_torch_op_tests
+  run_xla_op_tests
+  if [[ "$XLA_SKIP_TORCH_OP_TESTS" != "1" ]]; then
+    run_torch_op_tests
+  fi
   if [[ "$XLA_SKIP_MP_OP_TESTS" != "1" ]]; then
     run_mp_op_tests
   fi
