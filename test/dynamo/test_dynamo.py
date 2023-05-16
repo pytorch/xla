136539de564649e3a188516b9ab09c3733879001
Wonjoo Lee <wonjoo@example.com>
2023-05-16 21:19:10 +0000

    Fix metrics in test_dynamo.py

--- test/dynamo/test_dynamo.py
+++ test/dynamo/test_dynamo.py
@@ -93,9 +93,11 @@ class DynamoInferenceBasicTest(unittest.TestCase):
       output_cpu = resnet18(data.cpu())
       self.assertTrue(
           torch.allclose(output_cpu, output.cpu(), rtol=1e-05, atol=1e-05))
-    # We only expect one graph for the resnet18 inference.
-    self.assertEqual(met.metric_data('CompileTime')[0], 1)
-    self.assertEqual(met.metric_data('ExecuteTime')[0], sample_count)
+    # One graph for fetching the fallback ops.
+    # Another graph for the resnet18 inference.
+    self.assertEqual(met.metric_data('CompileTime')[0], 2)
+    # Again, +1 offset in ExecuteTime for fetching the fallback ops.
+    self.assertEqual(met.metric_data('ExecuteTime')[0], sample_count + 1)
     self.assertEqual(
         met.metric_data('RunCachedGraphInputData')[0], sample_count)
     self.assertEqual(
@@ -188,13 +190,14 @@ class DynamoTrainingBasicTest(unittest.TestCase):
       # TODO(JackCaoG): Understand why `data.grad` is a pending IR starting
       # from second iteration instead of a `DeviceData`
       # torch.allclose(data.grad.cpu(), cpu_data.grad)
-    # Graph 1: forward
+    # Graph 1: forward 
     # Graph 2: backward
     # Graph 3: sync input for backward
     # Graph 4: sync input for backward (TODO(JackCaoG) understand why there are two graphs)
-    self.assertEqual(met.metric_data('CompileTime')[0], 4)
-    # We execute 3 grphs per step.
-    self.assertEqual(met.metric_data('ExecuteTime')[0], sample_count * 3)
+    # In total, 4 compilations for graphs mentioned above and 2 compilation for fetching fallback ops for each forward/backward graph. 
+    self.assertEqual(met.metric_data('CompileTime')[0], 6)
+    # We execute 3 graphs per step. And +1 offset for fetching fallback ops. 
+    self.assertEqual(met.metric_data('ExecuteTime')[0], sample_count * 3 + 1)
     # one for each forward and one for each backward
     self.assertEqual(
         met.metric_data('RunCachedGraphInputData')[0], sample_count * 2)
@@ -298,9 +301,10 @@ class DynamoTrainingOptimizerTest(unittest.TestCase):
     # Graph 3: optimizer
     # Graph 4: sync input for backward
     # Graph 5: sync input for backward (TODO(JackCaoG) understand why there are two graphs)
-    self.assertEqual(met.metric_data('CompileTime')[0], 5)
-    # We execute 4 grphs per step when optimizer is enabled.
-    self.assertEqual(met.metric_data('ExecuteTime')[0], sample_count * 4)
+    # In total, 5 compilations for graphs mentioned above and 3 compilations for fetching fallback ops for each forward/backward/optimizer graph. 
+    self.assertEqual(met.metric_data('CompileTime')[0], 8)
+    # We execute 4 graphs per step when optimizer is enabled. And +2 offset for fetching fallback ops. One during forward/backward graphs and another one during optimizer graph. 
+    self.assertEqual(met.metric_data('ExecuteTime')[0], sample_count * 4 + 2)
     # one for each forward, backward and optimizer
     self.assertEqual(
         met.metric_data('RunCachedGraphInputData')[0], sample_count * 3)
