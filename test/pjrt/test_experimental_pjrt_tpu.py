ba78c0e4420306388ff2d5fa3ed51c90346273f6
Mohit Khatwani <mohitkhatwani@google.com>
2023-05-28 01:40:32 +0000

    test for _global_device_attributes

--- test/pjrt/test_experimental_pjrt_tpu.py
+++ test/pjrt/test_experimental_pjrt_tpu.py
@@ -205,6 +205,17 @@ class TestExperimentalPjrtTpu(parameterized.TestCase):
       self.assertIsInstance(device['core_on_chip'], int)
 
   @staticmethod
+  def _global_device_attributes():
+    return pjrt._global_device_attributes()
+
+  def test_global_device_attributes(self):
+    result = pjrt._run_multiprocess(self._global_device_attributes())
+    for device in result.values():
+      self.assertCountEqual(['coords', 'core_on_chip'], list(device.keys()))
+      self.assertIsInstance(device['coords'], list)
+      self.assertIsInstance(device['core_on_chip'], int) 
+
+  @staticmethod
   def _execute_time_metric():
     # Initialize the client before starting the timer.
     xm.xla_device()
