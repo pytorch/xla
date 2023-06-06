986098bba2b665c080e9a824644ef3a2c94631ef
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2023-06-06 09:02:40 -0700

    Update convolution.cpp

--- torch_xla/csrc/convolution.cpp
+++ torch_xla/csrc/convolution.cpp
@@ -20,8 +20,8 @@ namespace {
 xla::ConvolutionDimensionNumbers MakeConvolutionDimensionNumbers(
     torch_xla::XLATensorFormat data_format, int num_spatial_dims) {
   int num_dims = num_spatial_dims + 2;
-  int batch_dimension = xla::GetTensorBatchDimIndex(num_dims, data_format);
-  int feature_dimension = xla::GetTensorFeatureDimIndex(num_dims, data_format);
+  int batch_dimension = GetTensorBatchDimIndex(num_dims, data_format);
+  int feature_dimension = GetTensorFeatureDimIndex(num_dims, data_format);
   xla::ConvolutionDimensionNumbers conv_dim_numbers;
   for (int spatial_dim = 0; spatial_dim < num_spatial_dims; ++spatial_dim) {
     conv_dim_numbers.add_input_spatial_dimensions(
