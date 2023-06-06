41d392009e92609c7b2fa4baaaca0cb9c3628dcb
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2023-06-05 23:41:36 -0700

    Update ThreePadding

--- torch_xla/csrc/convolution.cpp
+++ torch_xla/csrc/convolution.cpp
@@ -148,7 +148,7 @@ ConvOpAttrs MakeConvOpAttrs(
   conv_op_attrs.strides = {1, 1};
   std::copy(spatial_stride.begin(), spatial_stride.end(),
             std::back_inserter(conv_op_attrs.strides));
-  conv_op_attrs.padding = tensorflow::Padding::EXPLICIT;
+  conv_op_attrs.padding = ThreePadding::EXPLICIT;
   // https://github.com/tensorflow/tensorflow/blob/ec81825aaf7e848d9f8ddffdf1e0d20aebe9172c/tensorflow/core/util/padding.cc#L40
   // explicit_padding requires to have (spatial_dims + 2) * 2 elements
   conv_op_attrs.explicit_paddings.resize(4);
