0a6e1659019938c3a1095281f716104712b02d0d
Yeounoh Chung <yeounoh@google.com>
2023-06-09 00:49:14 -0700

    Set replicated output false (#5149)
    * [SPMD] index local shards correctly in the execution arguments for replication.
    * [SPMD] support partial replication with xs.ShardingSpec
    * [SPMD] output sharding by default & update ShardingType commnet

--- test/spmd/test_xla_sharding.py
+++ test/spmd/test_xla_sharding.py
@@ -183,24 +183,28 @@ class BasicShardingTest(test_xla_sharding_base.XlaShardingTest):
 
   def test_mark_sharding_partial(self):
     device = xm.xla_device()
-    xt1 = torch.randn(4, 4).to(xm.xla_device())
-    xt2 = torch.randn(4, 4).to(xm.xla_device())
-    expected = (xt1 @ xt2).cpu()
+    t1 = torch.randn(4, 4).to(xm.xla_device())
+    t2 = torch.randn(4, 4).to(xm.xla_device())
+    expected = (t1 @ t2).cpu()
 
     # Shard along two axes if four or more devices are available
     z_dim = 2 if self.n_devices >= 4 else 1
     mesh = self._get_mesh((z_dim, self.n_devices // z_dim))
-    xs.mark_sharding(xt1, mesh, (0, None))
+    xt1 = xs.mark_sharding(t1, mesh, (0, None))
 
     # partial replication requires >1 devices; otherwise, it's replicated.
     if self.n_devices > 1:
       # xt1 is sharded `z_dim`-way, replicated `n_devices/z_dim`-way.
       self.assertTrue('last_tile_dim_replicate' in
-                      torch_xla._XLAC._get_xla_sharding_spec(xt1))
+                      torch_xla._XLAC._get_xla_sharding_spec(t1))
       self.assertTrue('[%d,1,%d]' %
                       (z_dim, self.n_devices //
-                       z_dim) in torch_xla._XLAC._get_xla_sharding_spec(xt1))
-    actual = (xt1 @ xt2).cpu()
+                       z_dim) in torch_xla._XLAC._get_xla_sharding_spec(t1))
+    # replicated group should share the same data content.
+    if (self.n_devices // z_dim) > 1:
+      shards = xt1.local_shards
+      self.assertTrue(torch.allclose(shards[0].data, shards[1].data))
+    actual = (xt1 @ t2).cpu()
     self.assertTrue(torch.allclose(expected, actual))
 
   def test_partial_replication_addmm(self):
