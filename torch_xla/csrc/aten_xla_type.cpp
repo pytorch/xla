464b967a80d6d6c835473d48b6b7090d053f9f6c
iefgnoix <isaacwxf23@gmail.com>
2023-06-12 14:21:49 -0700

    Throw an error when both operands of torch.add have dynamic dimensions (#5157)
    * Err when both operands of torch.add are dynamic.
    
    * fix typo/
    
    * fix test failure

--- torch_xla/csrc/aten_xla_type.cpp
+++ torch_xla/csrc/aten_xla_type.cpp
@@ -638,6 +638,11 @@ at::Tensor XLANativeFunctions::add(const at::Tensor& self,
                                    const at::Tensor& other,
                                    const at::Scalar& alpha) {
   TORCH_LAZY_FN_COUNTER("xla::");
+  XLA_CHECK(!(tensor_has_dym_dim(self) && tensor_has_dym_dim(other)))
+      << "Both operands of torch.add cannot have dynamic dimensions at the "
+         "same time. This is not "
+         "supported in PyTorch/XLA.";
+
   at::native::alpha_check(at::result_type(self, other), alpha);
   return DoBinaryOp(self, other,
                     [&](const XLATensorPtr& xself, const XLATensorPtr& xother,
