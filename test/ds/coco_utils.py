ce308f2cbad5a53005a11ecc3ade2caa672c395c
Xiongfei Wei <isaacwxf23@gmail.com>
2023-06-13 23:47:14 +0000

    ran linter

--- test/ds/coco_utils.py
+++ test/ds/coco_utils.py
@@ -2,76 +2,79 @@ import torch
 from pycocotools import mask as coco_mask
 from torchvision.transforms import functional as F
 
+
 def convert_coco_poly_to_mask(segmentations, height, width):
-    masks = []
-    for polygons in segmentations:
-        rles = coco_mask.frPyObjects(polygons, height, width)
-        mask = coco_mask.decode(rles)
-        if len(mask.shape) < 3:
-            mask = mask[..., None]
-        mask = torch.as_tensor(mask, dtype=torch.uint8)
-        mask = mask.any(dim=2)
-        masks.append(mask)
-    if masks:
-        masks = torch.stack(masks, dim=0)
-    else:
-        masks = torch.zeros((0, height, width), dtype=torch.uint8)
-    return masks
+  masks = []
+  for polygons in segmentations:
+    rles = coco_mask.frPyObjects(polygons, height, width)
+    mask = coco_mask.decode(rles)
+    if len(mask.shape) < 3:
+      mask = mask[..., None]
+    mask = torch.as_tensor(mask, dtype=torch.uint8)
+    mask = mask.any(dim=2)
+    masks.append(mask)
+  if masks:
+    masks = torch.stack(masks, dim=0)
+  else:
+    masks = torch.zeros((0, height, width), dtype=torch.uint8)
+  return masks
+
 
 class ConvertCocoPolysToMask:
-    def __call__(self, image, target):
-        w, h = image.size
 
-        image_id = target[0]["image_id"] if target else []
-        image_id = torch.tensor([image_id])
+  def __call__(self, image, target):
+    w, h = image.size
 
-        anno = target
+    image_id = target[0]["image_id"] if target else []
+    image_id = torch.tensor([image_id])
 
-        anno = [obj for obj in anno if obj["iscrowd"] == 0]
+    anno = target
 
-        boxes = [obj["bbox"] for obj in anno]
-        # guard against no boxes via resizing
-        boxes = torch.as_tensor(boxes, dtype=torch.float32).reshape(-1, 4)
-        boxes[:, 2:] += boxes[:, :2]
-        boxes[:, 0::2].clamp_(min=0, max=w)
-        boxes[:, 1::2].clamp_(min=0, max=h)
+    anno = [obj for obj in anno if obj["iscrowd"] == 0]
 
-        classes = [obj["category_id"] for obj in anno]
-        classes = torch.tensor(classes, dtype=torch.int64)
+    boxes = [obj["bbox"] for obj in anno]
+    # guard against no boxes via resizing
+    boxes = torch.as_tensor(boxes, dtype=torch.float32).reshape(-1, 4)
+    boxes[:, 2:] += boxes[:, :2]
+    boxes[:, 0::2].clamp_(min=0, max=w)
+    boxes[:, 1::2].clamp_(min=0, max=h)
 
-        segmentations = [obj["segmentation"] for obj in anno]
-        masks = convert_coco_poly_to_mask(segmentations, h, w)
+    classes = [obj["category_id"] for obj in anno]
+    classes = torch.tensor(classes, dtype=torch.int64)
 
-        keypoints = None
-        if anno and "keypoints" in anno[0]:
-            keypoints = [obj["keypoints"] for obj in anno]
-            keypoints = torch.as_tensor(keypoints, dtype=torch.float32)
-            num_keypoints = keypoints.shape[0]
-            if num_keypoints:
-                keypoints = keypoints.view(num_keypoints, -1, 3)
+    segmentations = [obj["segmentation"] for obj in anno]
+    masks = convert_coco_poly_to_mask(segmentations, h, w)
 
-        keep = (boxes[:, 3] > boxes[:, 1]) & (boxes[:, 2] > boxes[:, 0])
-        boxes = boxes[keep]
-        classes = classes[keep]
-        masks = masks[keep]
-        if keypoints is not None:
-            keypoints = keypoints[keep]
+    keypoints = None
+    if anno and "keypoints" in anno[0]:
+      keypoints = [obj["keypoints"] for obj in anno]
+      keypoints = torch.as_tensor(keypoints, dtype=torch.float32)
+      num_keypoints = keypoints.shape[0]
+      if num_keypoints:
+        keypoints = keypoints.view(num_keypoints, -1, 3)
 
-        target = {}
-        target["boxes"] = boxes
-        target["labels"] = classes
-        target["masks"] = masks
-        target["image_id"] = image_id
-        if keypoints is not None:
-            target["keypoints"] = keypoints
+    keep = (boxes[:, 3] > boxes[:, 1]) & (boxes[:, 2] > boxes[:, 0])
+    boxes = boxes[keep]
+    classes = classes[keep]
+    masks = masks[keep]
+    if keypoints is not None:
+      keypoints = keypoints[keep]
 
-        # for conversion to coco api
-        area = torch.tensor([obj["area"] for obj in anno])
-        iscrowd = torch.tensor([obj["iscrowd"] for obj in anno])
-        target["area"] = area
-        target["iscrowd"] = iscrowd
+    target = {}
+    target["boxes"] = boxes
+    target["labels"] = classes
+    target["masks"] = masks
+    target["image_id"] = image_id
+    if keypoints is not None:
+      target["keypoints"] = keypoints
 
-        # Convert image from PIL to tensor
-        image = F.pil_to_tensor(image)
-        image = F.convert_image_dtype(image)
-        return image, target
+    # for conversion to coco api
+    area = torch.tensor([obj["area"] for obj in anno])
+    iscrowd = torch.tensor([obj["iscrowd"] for obj in anno])
+    target["area"] = area
+    target["iscrowd"] = iscrowd
+
+    # Convert image from PIL to tensor
+    image = F.pil_to_tensor(image)
+    image = F.convert_image_dtype(image)
+    return image, target
--- test/ds/test_mask_rcnn_models.py
+++ test/ds/test_mask_rcnn_models.py
@@ -24,74 +24,96 @@ from torchvision.datasets.coco import CocoDetection
 
 # silence some spam
 from pycocotools import coco
+
 coco.print = lambda *args: None
 
 CURRENT_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 # Note[xw32]: need to copy the coco2017-minimal folder from torch benchmark to pytorch/xla/test/ds/data/.
-DATA_DIR = os.path.join(CURRENT_DIR, "data", "coco2017-minimal") # xw modified.
-assert os.path.exists(DATA_DIR), "Couldn't find coco2017 minimal data dir, please run install.py again."
+DATA_DIR = os.path.join(CURRENT_DIR, "data", "coco2017-minimal")  # xw modified.
+assert os.path.exists(
+    DATA_DIR
+), "Couldn't find coco2017 minimal data dir, please run install.py again."
 COCO_DATA_KEY = "coco_2017_val_100"
 COCO_DATA = {
-    "coco_2017_val_100": ("coco/val2017", "coco/annotations/instances_val2017_100.json")
+    "coco_2017_val_100":
+        ("coco/val2017", "coco/annotations/instances_val2017_100.json")
 }
 
 pd = torch._C._EnablePythonDispatcher()
 xla_dev = xm.xla_device()
 
+
 def _collate_fn(batch):
-    return tuple(zip(*batch))
+  return tuple(zip(*batch))
+
 
 def _prefetch(loader, device):
-    items = []
-    for images, targets in loader:
-        images = list(image.to(device) for image in images)
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-        items.append((images, targets))
-    return items
+  items = []
+  for images, targets in loader:
+    images = list(image.to(device) for image in images)
+    targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+    items.append((images, targets))
+  return items
+
 
 class Model():
-    DEFAULT_TRAIN_BSIZE = 4
-    DEFAULT_EVAL_BSIZE = 4
-    NUM_OF_BATCHES = 1
+  DEFAULT_TRAIN_BSIZE = 4
+  DEFAULT_EVAL_BSIZE = 4
+  NUM_OF_BATCHES = 1
 
-    def __init__(self, test, device, batch_size=None, extra_args=[]):
-        print('xw32 calling vision_maskrcnn.__init__')
-        self.model = torchvision.models.detection.maskrcnn_resnet50_fpn(
-            weights=torchvision.models.detection.MaskRCNN_ResNet50_FPN_Weights.COCO_V1
-        ).to(self.device)
-        
-        # setup optimizer
-        # optimizer parameters copied from
-        # https://github.com/pytorch/vision/blob/30f4d108319b0cd28ae5662947e300aad98c32e9/references/detection/train.py#L77
-        lr = 0.02
-        momentum = 0.9
-        weight_decay = 1e-4
-        params = [p for p in self.model.parameters() if p.requires_grad]
-        self.optimizer = torch.optim.SGD(params, lr=lr, momentum=momentum, weight_decay=weight_decay)
+  def __init__(self, test, device, batch_size=None, extra_args=[]):
+    print('xw32 calling vision_maskrcnn.__init__')
+    self.model = torchvision.models.detection.maskrcnn_resnet50_fpn(
+        weights=torchvision.models.detection.MaskRCNN_ResNet50_FPN_Weights.
+        COCO_V1).to(self.device)
 
-        transforms = ConvertCocoPolysToMask()
-        dataset = CocoDetection(root=os.path.join(DATA_DIR, COCO_DATA[COCO_DATA_KEY][0]),
-                                annFile=os.path.join(DATA_DIR, COCO_DATA[COCO_DATA_KEY][1]),
-                                transforms=transforms)
-        sampler = torch.utils.data.SequentialSampler(dataset)
+    # setup optimizer
+    # optimizer parameters copied from
+    # https://github.com/pytorch/vision/blob/30f4d108319b0cd28ae5662947e300aad98c32e9/references/detection/train.py#L77
+    lr = 0.02
+    momentum = 0.9
+    weight_decay = 1e-4
+    params = [p for p in self.model.parameters() if p.requires_grad]
+    self.optimizer = torch.optim.SGD(
+        params, lr=lr, momentum=momentum, weight_decay=weight_decay)
 
-        self.data_loader = _prefetch(torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, sampler=sampler, collate_fn=_collate_fn), self.device)
+    transforms = ConvertCocoPolysToMask()
+    dataset = CocoDetection(
+        root=os.path.join(DATA_DIR, COCO_DATA[COCO_DATA_KEY][0]),
+        annFile=os.path.join(DATA_DIR, COCO_DATA[COCO_DATA_KEY][1]),
+        transforms=transforms)
+    sampler = torch.utils.data.SequentialSampler(dataset)
 
-    def train(self):
-        self.model.train()
-        for _batch_id, (images, targets) in zip(range(self.NUM_OF_BATCHES), self.data_loader):
-            loss_dict = self.model(images, targets)
-            losses = sum(loss for loss in loss_dict.values())
-            self.optimizer.zero_grad()
-            losses.backward()
-            xm.optimizer_step(self.optimizer)
+    self.data_loader = _prefetch(
+        torch.utils.data.DataLoader(
+            dataset,
+            batch_size=self.batch_size,
+            sampler=sampler,
+            collate_fn=_collate_fn), self.device)
+
+  def train(self):
+    self.model.train()
+    for _batch_id, (images, targets) in zip(
+        range(self.NUM_OF_BATCHES), self.data_loader):
+      loss_dict = self.model(images, targets)
+      losses = sum(loss for loss in loss_dict.values())
+      self.optimizer.zero_grad()
+      losses.backward()
+      xm.optimizer_step(self.optimizer)
+
+  def eval(self) -> Tuple[torch.Tensor]:
+    self.model.eval()
+    with torch.no_grad():
+      for _batch_id, (images, _targets) in zip(
+          range(self.NUM_OF_BATCHES), self.data_loader):
+        out = self.model(images)
+    out = list(map(lambda x: x.values(), out))
+    return tuple(itertools.chain(*out))
 
 
-    def eval(self) -> Tuple[torch.Tensor]:
-        self.model.eval()
-        with torch.no_grad():
-            for _batch_id, (images, _targets) in zip(range(self.NUM_OF_BATCHES), self.data_loader):
-                out = self.model(images)
-        out = list(map(lambda x: x.values(), out))
-        return tuple(itertools.chain(*out))
-
+if __name__ == '__main__':
+  assert os.environ['XLA_EXPERIMENTAL'] != ''
+  test = unittest.main(verbosity=FLAGS.verbosity, exit=False)
+  # DISABLE PYTHON DISPATCHER FLAG
+  del pd
+  sys.exit(0 if test.result.wasSuccessful() else 1)
