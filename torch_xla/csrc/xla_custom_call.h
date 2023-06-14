674afb00bfbfead09f3a05c4ffac68aab4171c13
Meghan <cowanmeg@gogle.com>
2023-06-14 19:54:55 +0000

    gpu custom example

--- torch_xla/csrc/xla_custom_call.h
+++ torch_xla/csrc/xla_custom_call.h
@@ -3,8 +3,13 @@
 
 #include "tensorflow/compiler/xla/service/custom_call_target_registry.h"
 
+#include <cuda.h>
+
 void argmin_custom(void* out, const void** in);
 XLA_REGISTER_CUSTOM_CALL_TARGET(argmin_custom, "Host");
 
+void do_custom_gpu_call(CUstream stream, void** buffers,
+                    const char* opaque, size_t opaque_len);
+XLA_REGISTER_CUSTOM_CALL_TARGET(do_custom_gpu_call, "CUDA");
 
 #endif  // XLA_TORCH_XLA_CSRC_XLA_CUSTOM_CALL_H_
