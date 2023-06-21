9fbed292a298c5e9566f3278494b87e3c1f30cf6
manfeibaigithub <manfei@google.com>
2023-06-21 21:56:20 +0000

    raise error

--- torch_xla/core/xla_model.py
+++ torch_xla/core/xla_model.py
@@ -140,7 +140,7 @@ def get_ordinal(defval=0):
   if runtime.using_pjrt():
     return runtime.global_ordinal()
 
-  print("Please set PJRT_DEVICE")
+  raise Exception('Please set PJRT_DEVICE')
   return xu.getenv_as(xenv.ORDINAL, int, defval=defval)
 
 
