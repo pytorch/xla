04e892fb17d73e72c400396071e70182a1e8370a
Mateusz Lewko <mateusz.lewko@gmail.com>
2023-06-26 19:37:24 +0200

    Add IAM roles for cloudbuild_editors (#5251)

--- infra/tpu-pytorch-releases/iam.tf
+++ infra/tpu-pytorch-releases/iam.tf
@@ -5,6 +5,11 @@ variable "project_admins" {
   default = []
 }
 
+variable "cloudbuild_editors" {
+  type    = list(string)
+  default = []
+}
+
 resource "google_project_iam_member" "project_iam_admins" {
   for_each = toset(var.project_admins)
 
@@ -18,5 +23,13 @@ resource "google_project_iam_member" "storage_admins" {
 
   project = data.google_project.project.project_id
   role    = "roles/storage.admin"
+  member  = each.value
+}
+
+resource "google_project_iam_member" "cloudbuild_editor" {
+  for_each = toset(var.cloudbuild_editors)
+
+  project = data.google_project.project.project_id
+  role    = "roles/cloudbuild.builds.editor"
   member  = each.value
 }
