4ceda77b28b0bc7e4a71e1110d092309c556570a
Siyuan Liu <lsiyuan@google.com>
2023-06-28 15:35:17 +0000

    fix condition in coverage test

--- scripts/run_bazel_coverage.sh
+++ scripts/run_bazel_coverage.sh
@@ -3,13 +3,13 @@ export XRT_DEVICE_MAP="CPU:0;/job:localservice/replica:0/task:0/device:XLA_CPU:0
 export XRT_WORKERS="localservice:0;grpc://localhost:40934"
 export XLA_EXPERIMENTAL="nonzero:masked_select"
 
+BAZEL_REMOTE_CACHE_CONFIG="--config=remote_cache --remote_default_exec_properties=cache-silo-key=cache-silo-coverage"
 if [ ! -z "$GCLOUD_SERVICE_KEY_FILE" ]; then
   file_size=$(stat -c%s "$GCLOUD_SERVICE_KEY_FILE")
   if [ "$file_size" -le 1 ]; then
     BAZEL_REMOTE_CACHE_CONFIG=""
   fi
 fi
-BAZEL_REMOTE_CACHE_CONFIG="--config=remote_cache --remote_default_exec_properties=cache-silo-key=cache-silo-coverage"
 
 bazel coverage $BAZEL_REMOTE_CACHE_CONFIG //...
 cp "$(bazel info output_path)/_coverage/_coverage_report.dat" /tmp/cov_xrt.dat
