25bfe37c146cecba3e039e70463f171911d02747
Will Cromar <wcromar@google.com>
2023-06-30 18:08:53 +0000

    Make TPU detection more robust

--- torch_xla/runtime.py
+++ torch_xla/runtime.py
@@ -35,7 +35,7 @@ def _maybe_select_default_device():
   logging.warning('PJRT is now the default runtime. For more information, see '
                   'https://github.com/pytorch/xla/blob/master/docs/pjrt.md')
   # Check for libtpu _and_ the TPU device
-  if torch_xla._found_libtpu and os.path.exists('/dev/accel0'):
+  if torch_xla._found_libtpu and tpu.num_available_chips() > 0:
     logging.warning('libtpu.so and TPU device found. Setting PJRT_DEVICE=TPU.')
     os.environ[xenv.PJRT_DEVICE] = 'TPU'
   # TODO(wcromar): Detect GPU device
