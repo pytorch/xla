f87555030a400d13f79838aff41a30540f1385dc
JackCaoG <jackcao@google.com>
2023-07-05 21:50:37 +0000

    fix test_non_tensor_scalar

--- test/spmd/test_xla_virtual_device.py
+++ test/spmd/test_xla_virtual_device.py
@@ -79,6 +79,9 @@ class VirtualDeviceTest(test_xla_sharding_base.XlaShardingTest):
 
   def test_non_tensor_scalar(self):
     sharding_spec = xs.ShardingSpec(self._get_mesh((1, self.n_devices)), (0, 1))
+    # TODO(JackCaoG)currently, execution will only happen if there is at least one
+    # tensor on non-spmd:0 device.
+    t1 = torch.randn(3, 3, device=xm.xla_device())
     # tensor will have device as `SPMD:0` in c++
     xt1 = xm.send_cpu_data_to_device([torch.randn(3, 3)],
                                      xm.xla_device(),
@@ -86,6 +89,7 @@ class VirtualDeviceTest(test_xla_sharding_base.XlaShardingTest):
     # we will transfer 0.5 as a device_data to the 'SPMD:0' device, need to make sure
     # that virtual device can handle this case.
     xt2 = xt1 / 0.5
+    xm.mark_step(wait=True)
     torch.allclose(xt2.cpu(), xt1.cpu() / 0.5)
 
   def test_mark_step_on_virtual_device(self):
@@ -99,7 +103,7 @@ class VirtualDeviceTest(test_xla_sharding_base.XlaShardingTest):
                                      xm.xla_device(),
                                      input_sharding=sharding_spec)[0]
     xt2 = xt1 / 0.5
-    xm.mark_step()
+    xm.mark_step(wait=True)
     # after mark_step, xt2 should be materalized
     self.assertNotIn('aten::div',
                      torch_xla._XLAC._get_xla_tensor_debug_info(xt2))
