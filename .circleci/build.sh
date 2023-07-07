fcab05508d306b6af003a4192babd9fa67914143
Siyuan Liu <lsiyuan@google.com>
2023-07-07 23:19:56 +0000

    remove sccache from build script

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -11,21 +11,21 @@ clone_pytorch $PYTORCH_DIR $XLA_DIR
 # Use bazel cache
 USE_CACHE=1
 
-SCCACHE="$(which sccache)"
-if [ -z "${SCCACHE}" ]; then
-  echo "Unable to find sccache..."
-  exit 1
-fi
+# SCCACHE="$(which sccache)"
+# if [ -z "${SCCACHE}" ]; then
+#   echo "Unable to find sccache..."
+#   exit 1
+# fi
 
-if which sccache > /dev/null; then
-  # Save sccache logs to file
-  sccache --stop-server || true
-  rm ~/sccache_error.log || true
-  SCCACHE_ERROR_LOG=~/sccache_error.log RUST_LOG=sccache::server=error sccache --start-server
+# if which sccache > /dev/null; then
+#   # Save sccache logs to file
+#   sccache --stop-server || true
+#   rm ~/sccache_error.log || true
+#   SCCACHE_ERROR_LOG=~/sccache_error.log RUST_LOG=sccache::server=error sccache --start-server
 
-  # Report sccache stats for easier debugging
-  sccache --zero-stats
-fi
+#   # Report sccache stats for easier debugging
+#   sccache --zero-stats
+# fi
 
 rebase_pull_request_on_target_branch
 
@@ -43,7 +43,7 @@ python -c "import fcntl; fcntl.fcntl(1, fcntl.F_SETFL, 0)"
 
 python setup.py install
 
-sccache --show-stats
+# sccache --show-stats
 
 source $XLA_DIR/xla_env
 export GCLOUD_SERVICE_KEY_FILE="$XLA_DIR/default_credentials.json"
