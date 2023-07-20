87e7dbc92030e972ad8793a0bd3db12ae23102e5
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2023-07-20 16:11:16 -0700

    Update test_dynamo.py

--- test/dynamo/test_dynamo.py
+++ test/dynamo/test_dynamo.py
@@ -111,7 +111,7 @@ class DynamoInferenceBasicTest(unittest.TestCase):
       output = dynamo_resnet18(data)
       output_cpu = resnet18(data.cpu())
       self.assertTrue(
-          torch.allclose(output_cpu, output.cpu(), rtol=1e-05, atol=1e-05))
+          torch.allclose(output_cpu, output.cpu(), rtol=1e-05, atol=1e-08))
     # We only expect one graph for the resnet18 inference.
     self.assertEqual(met.metric_data('CompileTime')[0], 1)
     self.assertEqual(met.metric_data('ExecuteTime')[0], sample_count)
@@ -231,7 +231,7 @@ class DynamoTrainingBasicTest(unittest.TestCase):
     res_xla_dynamo = self.fn_simple_dynamo(xla_input)
     self.assertIn('xla::nll_loss_backward', met.counter_names())
     self.assertTrue(torch.allclose(res_cpu, res_xla_dynamo.cpu()))
-    self.assertTrue(torch.allclose(input.grad, xla_input.grad.cpu()))
+    self.assertTrue(torch.allclose(input.grad, xla_input.grad.cpu(), rtol=1e-05, atol=1e-08))
     # verifiy that tracing is skipped in following runs
     xla_input.grad = None
     met.clear_counters()
@@ -279,7 +279,7 @@ class DynamoTrainingBasicTest(unittest.TestCase):
       cpu_output = self.train_model(resnet18, cpu_data, cpu_target)
       self.assertTrue(
           torch.allclose(
-              xla_output.cpu(), cpu_output.cpu(), rtol=1e-05, atol=1e-05))
+              xla_output.cpu(), cpu_output.cpu(), rtol=1e-05, atol=1e-08))
       # TODO(JackCaoG): Understand why `data.grad` is a pending IR starting
       # from second iteration instead of a `DeviceData`
       # torch.allclose(data.grad.cpu(), cpu_data.grad)
@@ -343,7 +343,7 @@ class DynamoTrainingOptimizerTest(unittest.TestCase):
       res_cpu = self.fn_simple(input, optimizer)
       res_xla_dynamo = self.fn_simple_dynamo(xla_input, xla_optimizer)
       assert torch.allclose(res_cpu, res_xla_dynamo.cpu())
-      assert torch.allclose(input.grad, xla_input.grad.cpu())
+      assert torch.allclose(input.grad, xla_input.grad.cpu(), rtol=1e-05, atol=1e-08)
       assert torch.allclose(input, xla_input.cpu())
 
   def test_resnet18(self):
