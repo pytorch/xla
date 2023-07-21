e35295ddb75067fdc0a3531161068c2174689be8
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2023-07-20 17:43:25 -0700

    Update test_dynamo.py

--- test/dynamo/test_dynamo.py
+++ test/dynamo/test_dynamo.py
@@ -277,9 +277,6 @@ class DynamoTrainingBasicTest(unittest.TestCase):
       cpu_data.requires_grad = True
       cpu_target = target.detach().cpu()
       cpu_output = self.train_model(resnet18, cpu_data, cpu_target)
-      print("----------------------------------280--------------------------")
-      print(xla_output.cpu())
-      print(cpu_output.cpu())
       self.assertTrue(
           torch.allclose(
               xla_output.cpu(), cpu_output.cpu(), rtol=1e-01, atol=1e-01))
