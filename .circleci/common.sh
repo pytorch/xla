19cc221f7f78649590878a7838f1c04b667a06e5
Siyuan Liu <lsiyuan@google.com>
2023-07-25 04:10:32 +0000

    remove env print in CI script

--- .circleci/common.sh
+++ .circleci/common.sh
@@ -131,7 +131,6 @@ function run_torch_xla_tests() {
   fi
   export PYTORCH_TESTING_DEVICE_ONLY_FOR="xla"
   export CXX_ABI=$(python -c "import torch;print(int(torch._C._GLIBCXX_USE_CXX11_ABI))")
-  env
 
   pushd $XLA_DIR
     echo "Running Python Tests"
