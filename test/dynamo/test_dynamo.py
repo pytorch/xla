79804dac3a09864ea1b4159137b2e469c8961d22
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2023-07-27 10:47:01 -0700

    Update test_dynamo.py

--- test/dynamo/test_dynamo.py
+++ test/dynamo/test_dynamo.py
@@ -233,21 +233,21 @@ class DynamoTrainingBasicTest(unittest.TestCase):
     res_xla_dynamo = self.fn_simple_dynamo(xla_input)
     self.assertIn('xla::nll_loss_backward', met.counter_names())
     self.assertTrue(torch.allclose(res_cpu, res_xla_dynamo.cpu()))
-    self.assertTrue(torch.allclose(input.grad, xla_input.grad.cpu(), rtol=1e-04, atol=1e-04))
+    self.assertTrue(torch.allclose(input.grad, xla_input.grad.cpu(), rtol=1e-05, atol=1e-05))
     # verifiy that tracing is skipped in following runs
     xla_input.grad = None
     met.clear_counters()
     res_xla_dynamo_2 = self.fn_simple_dynamo(xla_input)
     self.assertNotIn('xla::nll_loss_backward', met.counter_names())
     self.assertTrue(torch.allclose(res_cpu, res_xla_dynamo_2.cpu()))
-    self.assertTrue(torch.allclose(input.grad, xla_input.grad.cpu(), rtol=1e-04, atol=1e-04))
+    self.assertTrue(torch.allclose(input.grad, xla_input.grad.cpu(), rtol=1e-05, atol=1e-05))
     # verify that dynamo can handle different inputs
     input.grad = None
     xla_input.grad = None
     res_xla_dynamo_3 = self.fn_simple_dynamo(xla_input * 2)
     res_cpu_3 = self.fn_simple(input * 2)
     self.assertTrue(torch.allclose(res_cpu_3, res_xla_dynamo_3.cpu()))
-    self.assertTrue(torch.allclose(input.grad, xla_input.grad.cpu(), rtol=1e-04, atol=1e-04))
+    self.assertTrue(torch.allclose(input.grad, xla_input.grad.cpu(), rtol=1e-05, atol=1e-05))
 
   def test_resnet18(self):
     torch._dynamo.reset()
