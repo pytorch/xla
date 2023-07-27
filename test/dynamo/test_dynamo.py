b6fcee934e8c3ced73be89157b548ecef87eeb94
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2023-07-27 13:21:47 -0700

    Update test_dynamo.py

--- test/dynamo/test_dynamo.py
+++ test/dynamo/test_dynamo.py
@@ -108,6 +108,10 @@ class DynamoInferenceBasicTest(unittest.TestCase):
     for data, _ in loader:
       dynamo_resnet18 = torch.compile(
           xla_resnet18, backend='torchxla_trace_once')
+      print("data")
+      print(data)
+      print("data.cpu()")
+      print(data.cpu())
       output = dynamo_resnet18(data)
       output_cpu = resnet18(data.cpu())
       self.assertTrue(
