9e6b61707e217333cfec1c2ff605c1e2d9766669
Jiewen Tan <jwtan@google.com>
2023-08-08 17:12:51 +0000

    tmp

--- torch_xla/csrc/init_python_bindings.cpp
+++ torch_xla/csrc/init_python_bindings.cpp
@@ -1125,6 +1125,7 @@ void InitXlaModuleBindings(py::module m) {
       for (auto const& [name, value] : attributes) {
         dict[py::str(name)] = py::cast(value);
       }
+      dict[py::str("name")] = device;
       list.push_back(dict);
     }
     return list;
