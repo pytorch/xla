6cf0446d442fb0b2b6ff22d0c442292fc6b6216b
JackCaoG <59073027+JackCaoG@users.noreply.github.com>
2023-08-14 14:50:36 -0700

    Move the C++ test build to CI build job instead of test job (#5442)

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -47,7 +47,8 @@ sccache --show-stats
 
 source $XLA_DIR/xla_env
 export GCLOUD_SERVICE_KEY_FILE="$XLA_DIR/default_credentials.json"
-export SILO_NAME='cache-silo-ci'  # cache bucket for CI
+export SILO_NAME='cache-silo-ci-gcc-11'  # cache bucket for CI
+export BUILD_CPP_TESTS='1'
 build_torch_xla $XLA_DIR
 
 popd
