dac2fa202c51fcf36a3cd4539cc6c3928df8792f
King <kingrj@amazon.com>
2023-08-18 13:57:49 -0700

    fixing spacing

--- torch_xla/csrc/ops/ops_xla_shape_fn.cpp
+++ torch_xla/csrc/ops/ops_xla_shape_fn.cpp
@@ -429,10 +429,10 @@ xla::Shape GluOutputShape(const torch::lazy::Value& input, int64_t dim) {
   absl::Span<const int64_t> inp_dimensions = input_shape.dimensions();
   std::vector<int64_t> output_sizes(std::begin(inp_dimensions),
                                     std::end(inp_dimensions));
- 
+                                    
   // Output shape is always half the input shape on the specified dimension
   output_sizes[dim] = inp_dimensions[dim] / 2;
- 
+
   return xla::ShapeUtil::MakeShape(input_shape.element_type(), output_sizes);
 }
 
