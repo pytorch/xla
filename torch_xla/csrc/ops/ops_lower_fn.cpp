a969e627567bfb979893d668f6c18e97abb2e391
King <kingrj@amazon.com>
2023-08-18 15:21:37 -0700

    fixing spacing

--- torch_xla/csrc/ops/ops_lower_fn.cpp
+++ torch_xla/csrc/ops/ops_lower_fn.cpp
@@ -357,7 +357,7 @@ torch_xla::XlaOpVector Glu::Lower(LoweringContext* loctx) const {
   absl::Span<const int64_t> inp_dimensions = input_shape.dimensions();
   int64_t split_size = inp_dimensions[ldim] / 2;
 
-  // Split the input tensor into two parts, take sigmoid of RHS and multiple 
+  // Split the input tensor into two parts, take sigmoid of RHS and multiple
   // element-wise
   xla::XlaOp a = xla::SliceInDim(xla_input, 0, split_size, 1, ldim);
   xla::XlaOp b =
