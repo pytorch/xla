184ad58aedbe1f0c7984322278585ded4cf0f45b
Nik Waldron <waldronn@amazon.com>
2023-08-23 19:43:32 +0000

    Add python bindings for turning device cache on and off, and special scalar handling

--- torch_xla/csrc/init_python_bindings.cpp
+++ torch_xla/csrc/init_python_bindings.cpp
@@ -1605,7 +1605,19 @@ void InitXlaModuleBindings(py::module m) {
   });
   m.def("_get_ir_debug", [] () {
     return FLAGS_torch_lazy_ir_debug;
+  });
+  m.def("_set_xla_handle_special_scalars", [](bool handle_special_scalars) {
+    FLAGS_torch_lazy_handle_special_scalars = handle_special_scalars;
+  });
+  m.def("_get_xla_handle_special_scalars", []() {
+    return FLAGS_torch_lazy_handle_special_scalars;
+  });
+  m.def("_set_xla_enable_device_data_cache", [](bool enable_device_data_cache) {
+    FLAGS_torch_lazy_enable_device_data_cache = enable_device_data_cache;
   });  
+  m.def("_get_xla_enable_device_data_cache", []() {
+    return FLAGS_torch_lazy_enable_device_data_cache;
+  });    
   m.def("_replace_xla_tensor",
         [](at::Tensor& self, const at::Tensor& source) -> at::Tensor& {
           return XLANativeFunctions::set_(self, source);
