b68929a2ce5d37fd7c643e5c10d92c4c685e9069
Han Qi <hanq@google.com>
2023-08-26 23:29:56 +0000

    Fetch keys

--- .kokoro/build_and_run_stablehlo_tests.sh
+++ .kokoro/build_and_run_stablehlo_tests.sh
@@ -3,5 +3,6 @@ set -ex
 DEBIAN_FRONTEND=noninteractive
 XLA_DIR="${KOKORO_ARTIFACTS_DIR}/github/xla"
 cd $XLA_DIR
+cp $KOKORO_KEYSTORE_DIR ./keystore_content -r
 docker build . -f=.kokoro/Dockerfile -t xladocker
-docker run -it xladocker
+docker -v "$KOKORO_KEYSTORE_DIR:/tmp/keystore" run xladocker
