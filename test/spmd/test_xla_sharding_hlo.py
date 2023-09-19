37c629ada81ea4b8bc53d1a6626a3eddfac75355
JackCaoG <jackcao@google.com>
2023-09-19 22:05:09 +0000

    add test

--- test/spmd/test_xla_sharding_hlo.py
+++ test/spmd/test_xla_sharding_hlo.py
@@ -10,6 +10,7 @@ import torch_xla
 import torch_xla.runtime as xr
 import torch_xla.core.xla_model as xm
 import torch_xla.experimental.xla_sharding as xs
+import torch_xla.debug.metrics as met
 
 import test_xla_sharding_base
 
@@ -31,6 +32,37 @@ class BasicShardingTest(test_xla_sharding_base.XlaShardingTest):
     hlo = torch_xla._XLAC._get_xla_tensors_hlo([t3])
     if self.n_devices > 1:
       self.assertIn('all-reduce', hlo)
+
+  def test_xla_replicated_input_output_alias(self):
+    xla_device = xm.xla_device()
+    t1 = torch.randn(8, 8).to(xla_device)
+    t2 = torch.randn(8, 8).to(xla_device)
+
+    # check in place op aliasing.
+    t3 = t1 + t2
+    t1 *= 2.0
+    t2 += 2.0
+    xm.mark_step()
+    # expected post-optimization HLO with "XLA_FLAGS=--xla_dump_to=/tmp/hlo_dir"
+    # HloModule SyncTensorsGraph.18, is_scheduled=true, input_output_alias={ {0}: (0, {}, may-alias), {1}: (1, {}, may-alias) }, entry_computation_layout={(f32[8,8]{1,0:T(8,128)}, f32[8,8]{1,0:T(8,128)}, f32[]{:T(128)})->(f32[8,8]{1,0:T(8,128)}, f32[8,8]{1,0:T(8,128)}, f32[8,8]{1,0:T(8,128)})}, allow_spmd_sharding_propagation_to_output={true}
+    self.assertEqual(met.metric_data("InputOutputAliasCount")[1], 2.0)
+
+  def test_xla_sharded_input_output_alias(self):
+    xla_device = xm.xla_device()
+    t1 = torch.randn(8, 8).to(xla_device)
+    t2 = torch.randn(8, 8).to(xla_device)
+
+    xs.mark_sharding(t1, self._get_mesh((1, self.n_devices)), (0, 1))
+    xs.mark_sharding(t2, self._get_mesh((1, self.n_devices)), (0, 1))
+
+    # check in place op aliasing.
+    t3 = t1 + t2
+    t1 *= 2.0
+    t2 += 2.0
+    xm.mark_step()
+    # expected post-optimization HLO with "XLA_FLAGS=--xla_dump_to=/tmp/hlo_dir"
+    # HloModule SyncTensorsGraph.18, is_scheduled=true, input_output_alias={ {0}: (0, {}, may-alias), {1}: (1, {}, may-alias) }, entry_computation_layout={(f32[8,8]{1,0:T(8,128)}, f32[8,8]{1,0:T(8,128)}, f32[]{:T(128)})->(f32[8,8]{1,0:T(8,128)}, f32[8,8]{1,0:T(8,128)}, f32[8,8]{1,0:T(8,128)})}, allow_spmd_sharding_propagation_to_output={true}
+    self.assertEqual(met.metric_data("InputOutputAliasCount")[1], 2.0)
 
 
 if __name__ == '__main__':
