3fe6e21622f0fe481274aed96285f5f554f804f1
Sandeep Dasgupta <sdasgup@google.com>
2023-09-20 00:24:28 +0000

    augment test.py with conversion to tflite saved model

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import torch
 import torch_xla
 from torch_xla.core import xla_model as xm
-
+from typing import Tuple, Type, Callable, Union, List
+from torch_xla import tf_saved_model_integration
 
 device = xm.xla_device()
 
@@ -13,7 +14,28 @@ torch_xla._XLAC._xla_mark_dynamic(input, 0)
 result = model(input)
 hlo_content = torch_xla._XLAC._get_xla_tensors_hlo([result])
 print(hlo_content)
-print(xm.get_stablehlo([result]))                                                                                                                                                                                                                                                                                                                  ####multiply(same axis dynamic)##a = torch.tensor([[1, 2], [2, 4]], device = device)##torch_xla._XLAC._xla_mark_dynamic(a, 0)##b = torch.tensor([[1, 2], [2, 4]], device = device)##torch_xla._XLAC._xla_mark_dynamic(b, 0)##c = a * b##hlo_content = torch_xla._XLAC._get_xla_tensors_hlo([c])##print(hlo_content)##print(xm.get_stablehlo([c]))
+print(xm.get_stablehlo([result]))
+export_filename = "resnet18_saved_model"
+
+### The folllowing line crashes with
+## 2023-09-20 00:15:55.270486: F ./torch_xla/csrc/runtime/debug_macros.h:20] Non-OK-status: status.status() status: UNIMPLEMENTED: CustomCall "stablehlo.dynamic_broadcast_in_dim" is not supported to have a dynamic dimension
+## *** Begin stack trace ***
+##         tsl::CurrentStackTrace[abi:cxx11]()
+##         std::unique_ptr<xla::PjRtLoadedExecutable, std::default_delete<xla::PjRtLoadedExecutable> > ConsumeValue<std::unique_ptr<xla::PjRtLoadedExecutable, std::default_delete<xla::PjRtLoadedExecutable> > >(absl::lts_20230125::StatusOr<std::unique_ptr<xla::PjRtLoadedExecutable, std::default_delete<xla::PjRtLoadedExecutable> > >&&)
+##         torch_xla::runtime::PjRtComputationClient::Compile(std::vector<torch_xla::runtime::ComputationClient::CompileInstance, std::allocator<torch_xla::runtime::ComputationClient::CompileInstance> >)
+##         torch_xla::XLAGraphExecutor::Compile(std::vector<c10::intrusive_ptr<torch_xla::XLATensor, c10::detail::intrusive_target_default_null_type<torch_xla::XLATensor> >, std::allocator<c10::intrusive_ptr<torch_xla::XLATensor, c10::detail::intrusive_target_default_null_type<torch_xla::XLATensor> > > > const&, absl::lts_20230125::Span<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const>, torch::lazy::LazyGraphExecutor::SyncTensorCollection const&, torch::lazy::LazyGraphExecutor::PostOrderData*, std::vector<torch::lazy::Value, std::allocator<torch::lazy::Value> > const&)
+##         torch_xla::XLAGraphExecutor::SyncTensorsGraphInternal(std::vector<c10::intrusive_ptr<torch_xla::XLATensor, c10::detail::intrusive_target_default_null_type<torch_xla::XLATensor> >, std::allocator<c10::intrusive_ptr<torch_xla::XLATensor, c10::detail::intrusive_target_default_null_type<torch_xla::XLATensor> > > >*, ab
+## sl::lts_20230125::Span<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const>, torch::lazy::LazyGraphExecutor::SyncTensorsConfig const&, bool)
+##         torch_xla::XLAGraphExecutor::SyncTensorsGraph(std::vector<c10::intrusive_ptr<torch_xla::XLATensor, c10::detail::intrusive_target_default_null_type<torch_xla::XLATensor> >, std::allocator<c10::intrusive_ptr<torch_xla::XLATensor, c10::detail::intrusive_target_default_null_type<torch_xla::XLATensor> > > >*, absl::lts_
+## 20230125::Span<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const>, bool, bool, bool)
+##         torch_xla::XLAGraphExecutor::SyncLiveTensorsGraph(torch::lazy::BackendDevice const*, c10::ArrayRef<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > >, bool)
+
+tf_saved_model_integration.save_torch_module_as_tf_saved_model(model, (input,), export_filename)
+
+
+
+####multiply(same axis dynamic)##a = torch.tensor([[1, 2], [2, 4]], device = device)##torch_xla._XLAC._xla_mark_dynamic(a, 0)##b = torch.tensor([[1, 2], [2, 4]], device = device)##torch_xla._XLAC._xla_mark_dynamic(b, 0)##c = a * b##hlo_content = torch_xla._XLAC._get_xla_tensors_hlo([c])##print(hlo_content)##print(xm.get_stablehlo([c]))
+
 
 # ## multiply (same axis dynamic)
 # a = torch.tensor([[1,2],[2,4]], device=device)
