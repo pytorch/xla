e92d2a006b86f6b35ac487e2ba1b147c2a1caf68
Yeounoh Chung <yeounoh@google.com>
2023-09-22 01:41:06 -0700

    Update nccl2 version

--- .circleci/docker/install_nccl.sh
+++ .circleci/docker/install_nccl.sh
@@ -4,10 +4,7 @@ set -e
 set -x
 
 function install_nccl() {
-  sudo apt-add-repository ppa:graphics-drivers/ppa
-  sudo apt update
-  sudo apt install libnccl2=2.18.3-1+cuda12.0.1 libnccl-dev=2.18.3-1+cuda12.0.1_libnccl-2.18.3
-  ncclinfo
+  sudo apt-get install -y --allow-change-held-packages libnccl2 libnccl-dev
 }
 
 install_nccl
