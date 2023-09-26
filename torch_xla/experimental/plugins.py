5936932175425b62ed2b074d450914b41b205d6c
Will Cromar <wcromar@google.com>
2023-09-26 22:33:05 +0000

    docstrings

--- torch_xla/experimental/plugins.py
+++ torch_xla/experimental/plugins.py
@@ -8,21 +8,36 @@ class DevicePlugin:
   """
 
   def library_path(self) -> str:
+    """Path to PJRT plugin binary."""
     raise NotImplementedError()
 
   def host_index(self) -> int:
+    """Index of the current host."""
     return 0
 
   def configure_single_process(self):
+    """Configure this process to run with world_size 1 for debugging."""
     raise NotImplementedError()
 
   def configure_multiprocess(self, local_rank, local_world_size):
+    """Configure device topology for running in a multiprocess context.
+
+    This is called when processes are being initialized by `xmp.spawn` or
+    `torchrun`. Typically, each process should be assigned a different physical
+    device from the host.
+    """
     pass
 
   def physical_chip_count():
+    """The number of physical chips available on this host.
+
+    This is the number of processes we expect to be created by `xmp.spawn` or
+    for `torchrun`.
+    """
     return 1
 
   def shutdown():
+    """Performs any necessary cleanup for this device."""
     pass
 
 
