52b5ac8cca418a518be9472b06ddc583641a3bc2
Siyuan Liu <lsiyuan@google.com>
2023-09-27 19:57:35 +0000

    clean up

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-# Only used in sccache
-# source ./env
 source .circleci/common.sh
 PYTORCH_DIR=/tmp/pytorch
 XLA_DIR=$PYTORCH_DIR/xla
@@ -11,22 +9,6 @@ clone_pytorch $PYTORCH_DIR $XLA_DIR
 
 # Use bazel cache
 USE_CACHE=1
-
-# SCCACHE="$(which sccache)"
-# if [ -z "${SCCACHE}" ]; then
-#   echo "Unable to find sccache..."
-#   exit 1
-# fi
-
-# if which sccache > /dev/null; then
-#   # Save sccache logs to file
-#   sccache --stop-server || true
-#   rm ~/sccache_error.log || true
-#   SCCACHE_ERROR_LOG=~/sccache_error.log RUST_LOG=sccache::server=error sccache --start-server
-
-#   # Report sccache stats for easier debugging
-#   sccache --zero-stats
-# fi
 
 rebase_pull_request_on_target_branch
 
@@ -45,7 +27,6 @@ python -c "import fcntl; fcntl.fcntl(1, fcntl.F_SETFL, 0)"
 export USE_CUDA=0
 python setup.py install
 
-# sccache --show-stats
 
 source $XLA_DIR/xla_env
 export GCLOUD_SERVICE_KEY_FILE="$XLA_DIR/default_credentials.json"
