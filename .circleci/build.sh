36f1eaf58c6d593f9837cb60359d8725efbf0c4d
Siyuan Liu <lsiyuan@google.com>
2023-09-27 20:55:32 +0000

    re-enable sccache, update workdir, cache name

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+source ./env
 source .circleci/common.sh
 PYTORCH_DIR=/tmp/pytorch
 XLA_DIR=$PYTORCH_DIR/xla
@@ -9,6 +10,22 @@ clone_pytorch $PYTORCH_DIR $XLA_DIR
 
 # Use bazel cache
 USE_CACHE=1
+
+SCCACHE="$(which sccache)"
+if [ -z "${SCCACHE}" ]; then
+  echo "Unable to find sccache..."
+  exit 1
+fi
+
+if which sccache > /dev/null; then
+  # Save sccache logs to file
+  sccache --stop-server || true
+  rm ~/sccache_error.log || true
+  SCCACHE_ERROR_LOG=~/sccache_error.log RUST_LOG=sccache::server=error sccache --start-server
+
+  # Report sccache stats for easier debugging
+  sccache --zero-stats
+fi
 
 rebase_pull_request_on_target_branch
 
@@ -27,10 +44,11 @@ python -c "import fcntl; fcntl.fcntl(1, fcntl.F_SETFL, 0)"
 export USE_CUDA=0
 python setup.py install
 
+sccache --show-stats
 
 source $XLA_DIR/xla_env
 export GCLOUD_SERVICE_KEY_FILE="$XLA_DIR/default_credentials.json"
-export SILO_NAME='cache-silo-ci-dev-container-python38-bullseye'  # cache bucket for CI
+export SILO_NAME='cache-silo-ci-dev-3.8_cuda_12.1'  # cache bucket for CI
 export BUILD_CPP_TESTS='1'
 export TF_CUDA_COMPUTE_CAPABILITIES="sm_50,sm_70,sm_75,compute_80,$TF_CUDA_COMPUTE_CAPABILITIES"
 build_torch_xla $XLA_DIR
