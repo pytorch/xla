b288f46748d3793ad296ce3340366426ddedb01b
Xiongfei Wei <isaacwxf23@gmail.com>
2023-10-02 21:38:15 +0000

    added a ll_reduce test

--- test/pjrt/test_torchrun.py
+++ test/pjrt/test_torchrun.py
@@ -30,6 +30,38 @@ class TestTorchrun(absltest.TestCase):
     expected = torch.arange(0, expected_world_size, step=1, dtype=torch.float32)
     torch.testing.assert_close(result.cpu(), expected)
 
+  def test_xm_all_reduce(self):
+    # The test is inspired by https://pytorch.org/docs/stable/distributed.html#torch.distributed.all_reduce
+    dist.init_process_group('xla', init_method='xla://')
+
+    dist_world_size = xu.getenv_as('WORLD_SIZE', int)
+    devices_per_thread = xr.addressable_device_count()
+    expected_world_size = dist_world_size * devices_per_thread
+    tensors = [torch.arange(2, dtype=torch.int64) + 1 + 2 * r for r in range(expected_world_size)]
+    expected = sum(tensors)
+
+    xla_tensor = torch.arange(2, dtype=torch.int64, device=xm.xla_device()) + 1 + 2 * dist.get_rank()
+    res = xm.all_reduce(xm.REDUCE_SUM, xla_tensor)
+    xm.mark_step()
+
+    torch.testing.assert_close(res.cpu(), expected)
+
+  def test_all_reduce(self):
+    # The test is inspired by https://pytorch.org/docs/stable/distributed.html#torch.distributed.all_reduce
+    dist.init_process_group('xla', init_method='xla://')
+
+    dist_world_size = xu.getenv_as('WORLD_SIZE', int)
+    devices_per_thread = xr.addressable_device_count()
+    expected_world_size = dist_world_size * devices_per_thread
+    tensors = [torch.arange(2, dtype=torch.int64) + 1 + 2 * r for r in range(expected_world_size)]
+    expected = sum(tensors)
+
+    xla_tensor = torch.arange(2, dtype=torch.int64, device=xm.xla_device()) + 1 + 2 * dist.get_rank()
+    dist.all_reduce(xla_tensor, op=dist.ReduceOp.SUM)
+    xm.mark_step()
+
+    torch.testing.assert_close(xla_tensor.cpu(), expected)
+
 
 if __name__ == '__main__':
   if not dist.is_torchelastic_launched():
