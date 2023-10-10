911ce9b5f6eee8411b588f5d05e5758dfae2f7cc
jonb377 <jonbolin@google.com>
2023-10-09 18:14:29 -0700

    Initial commit for CheckpointManager (#5678)
    * Initial commit for CheckpointManager
    
    * Update documentation for async
    
    * Fix typo

--- torch_xla/experimental/distributed_checkpoint/__init__.py
+++ torch_xla/experimental/distributed_checkpoint/__init__.py
@@ -1,6 +1,8 @@
+from .manager import CheckpointManager
 from .planners import SPMDSavePlanner, SPMDLoadPlanner
 
 __all__ = [
+    "CheckpointManager",
     "SPMDSavePlanner",
     "SPMDLoadPlanner",
 ]
