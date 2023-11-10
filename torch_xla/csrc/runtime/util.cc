ab212cfd6be1c0dac6ea2ea070188d5ca0eb2122
Will Cromar <wcromar@google.com>
2023-11-10 17:12:31 +0000

    remove util.cc

--- torch_xla/csrc/runtime/util.cc
+++
@@ -1,9 +0,0 @@
-#include "torch_xla/csrc/runtime/util.h"
-
-#include <sstream>
-
-namespace torch_xla {
-namespace runtime {
-namespace util {}  // namespace util
-}  // namespace runtime
-}  // namespace torch_xla
