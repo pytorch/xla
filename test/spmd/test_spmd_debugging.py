d5eed0bf8b4f89bf751bf68d4dac026d1fc6dcdb
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2023-11-13 01:59:20 -0800

    Update test_spmd_debugging.py

--- test/spmd/test_spmd_debugging.py
+++ test/spmd/test_spmd_debugging.py
@@ -1,7 +1,10 @@
-import os
 import sys
-import numpy as np
+
 import unittest
+from unittest.mock import patch
+import math
+import numpy as np
+import os
 
 import torch
 import torch_xla
@@ -10,25 +13,22 @@ import torch_xla.core.xla_model as xm
 import torch_xla.experimental.xla_sharding as xs
 from torch_xla.experimental.xla_sharded_tensor import XLAShardedTensor
 from torch_xla.experimental.xla_sharding import Mesh
-import torch_xla.distributed.spmd
 from torch_xla.distributed.spmd.debugging import visualize_tensor_sharding
 
 
-# @unittest.skipIf(not using_pjrt() or xm.get_xla_supported_devices("GPU"),
-#                  f"Requires PJRT_DEVICE set to `TPU` or `CPU`.")
-# class XlaShardingTest(unittest.TestCase):
-
 import test_xla_sharding_base
 
+@unittest.skipIf(not using_pjrt() or xm.get_xla_supported_devices("GPU")
+                 or xm.get_xla_supported_devices("CPU"),
+                 f"Requires PJRT_DEVICE set to `TPU`.")
 class DebuggingSpmdTest(test_xla_sharding_base.XlaShardingTest):
 
   @classmethod
   def setUpClass(cls):
-    os.environ["XLA_USE_SPMD"] = "1"
+    xr.use_spmd()# os.environ["XLA_USE_SPMD"] = "1"
     super().setUpClass()
 
   def test_debugging_spmd_single_host_tiled(self):
-    # xr.use_spmd()
     device = xm.xla_device()
     num_devices = xr.global_runtime_device_count()
     mesh_shape = (2, num_devices // 2)
@@ -45,7 +45,6 @@ class DebuggingSpmdTest(test_xla_sharding_base.XlaShardingTest):
 
 
   def test_single_host_partial_replication():
-    # xr.use_spmd()
     device = xm.xla_device()
     num_devices = xr.global_runtime_device_count()
     mesh_shape = (2, num_devices // 2)
@@ -63,7 +62,6 @@ class DebuggingSpmdTest(test_xla_sharding_base.XlaShardingTest):
 
 
   def test_single_host_replicated():
-    # xr.use_spmd()
     device = xm.xla_device()
     num_devices = xr.global_runtime_device_count()
     mesh_shape = (2, num_devices // 2)
@@ -72,19 +70,12 @@ class DebuggingSpmdTest(test_xla_sharding_base.XlaShardingTest):
 
     partition_spec_replicated = (None, None)
     t = torch.randn(8, 32, device=device)
-    # xs.mark_sharding(t, mesh, (0, None))
     xs.mark_sharding(t, mesh, partition_spec_replicated)
     sharding = torch_xla._XLAC._get_xla_sharding_spec(t)
     print("sharding is: ")
     print(sharding)
     print("then print: ")
     visualize_tensor_sharding(t)
-
-
-
-# test_single_host_tiled()
-# test_single_host_partial_replication()
-# test_single_host_replicated()
 
 if __name__ == '__main__':
   test = unittest.main()
