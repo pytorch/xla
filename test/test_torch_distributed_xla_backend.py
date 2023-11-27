01e689469cbde32e8ccd1406f45549ab9ab33872
Shashwat Srijan <119712013+sssrijan-amazon@users.noreply.github.com>
2023-11-27 15:27:45 -0800

    Fix linting error

--- test/test_torch_distributed_xla_backend.py
+++ test/test_torch_distributed_xla_backend.py
@@ -136,7 +136,8 @@ class XlaBackendTest(parameterized.TestCase):
     hlo = torch_xla._XLAC._get_xla_tensors_hlo([output])
     hlo_matches(hlo, reduce_scatter_pattern)
 
-  @skipIf(xr.device_type() == 'CPU', "UNIMPLEMENTED: ReduceScatter is not implemented on CPU.")
+  @skipIf(xr.device_type() == 'CPU',
+          "UNIMPLEMENTED: ReduceScatter is not implemented on CPU.")
   def test_reduce_scatter_coalesced(self):
     device = xm.xla_device()
     tensor = torch.arange(2, device=device) + 1 + 2 * dist.get_rank()
