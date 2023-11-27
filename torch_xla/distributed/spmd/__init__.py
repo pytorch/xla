2a0b9412bf61efe2324b686559b31cec667e82a5
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2023-11-27 15:20:19 -0800

    Update __init__.py

--- torch_xla/distributed/spmd/__init__.py
+++ torch_xla/distributed/spmd/__init__.py
@@ -3,7 +3,7 @@ from .xla_sharding import (Mesh, HybridMesh, ShardingType, ShardingSpec,
                            XLAPatchedLinear, mark_sharding, clear_sharding,
                            wrap_if_sharded, xla_patched_nn_linear_forward)
 from .api import xla_distribute_tensor, xla_distribute_module
-# from .debugging import visualize_tensor_sharding
+from .debugging import visualize_tensor_sharding
 
 __all__ = [
     "XLAShard",
