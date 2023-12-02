6cf2a63f14d970dc997dbcf28033c9e569f4178d
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2023-12-01 17:17:14 -0800

    Update test_spmd_debugging.py

--- test/spmd/test_spmd_debugging.py
+++ test/spmd/test_spmd_debugging.py
@@ -15,8 +15,8 @@ import torch_xla.utils.utils as xu
 import torch_xla.core.xla_env_vars as xenv
 import torch_xla.core.xla_model as xm
 import torch_xla.distributed.spmd as xs
-from torch_xla.experimental.xla_sharded_tensor import XLAShardedTensor
-from torch_xla.experimental.xla_sharding import Mesh
+from torch_xla.distributed.spmd import XLAShardedTensor
+from torch_xla.distributed.spmd import Mesh
 
 import test_xla_sharding_base
 
