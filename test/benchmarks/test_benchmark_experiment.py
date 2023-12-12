7425bed5ee78bf991c7bc5821d1698ac0c6f4bf7
Frederik Gossen <frgossen@google.com>
2023-12-12 10:38:15 -0500

    Remove unused experiment name, which was always

--- test/benchmarks/test_benchmark_experiment.py
+++ test/benchmarks/test_benchmark_experiment.py
@@ -6,11 +6,10 @@ from benchmark_experiment import BenchmarkExperiment
 class BenchmarkExperimentTest(unittest.TestCase):
 
   def test_to_dict(self):
-    be = BenchmarkExperiment("some name", "cpu", "PJRT", "some xla_flags",
-                             "openxla", "train", "123")
+    be = BenchmarkExperiment("cpu", "PJRT", "some xla_flags", "openxla",
+                             "train", "123")
     actual = be.to_dict()
-    self.assertEqual(8, len(actual))
-    self.assertEqual("some name", actual["experiment_name"])
+    self.assertEqual(7, len(actual))
     self.assertEqual("cpu", actual["accelerator"])
     self.assertTrue("accelerator_model" in actual)
     self.assertEqual("PJRT", actual["xla"])
