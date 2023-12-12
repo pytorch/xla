4723d96320eeb2b8cf2be75f7d3e21df84c7789c
Will Cromar <wcromar@google.com>
2023-12-12 13:09:42 -0800

    Fix bad error message when `PjRtComputationClient` throws exception (#5946)

--- torch_xla/csrc/runtime/runtime.cc
+++ torch_xla/csrc/runtime/runtime.cc
@@ -8,35 +8,29 @@
 
 namespace torch_xla {
 namespace runtime {
-namespace {
 
 std::atomic<bool> g_computation_client_initialized(false);
 
-ComputationClient* CreateClient() {
-  bool was_initialized = g_computation_client_initialized.exchange(true);
-  XLA_CHECK(!was_initialized) << "ComputationClient already initialized";
-  if (sys_util::GetEnvBool("XLA_DUMP_FATAL_STACK", false)) {
-    tsl::testing::InstallStacktraceHandler();
-  }
-
-  ComputationClient* client;
-
-  if (sys_util::GetEnvString(env::kEnvPjRtDevice, "") != "") {
-    client = new PjRtComputationClient();
-  } else {
-    g_computation_client_initialized = false;
-    XLA_ERROR() << "$PJRT_DEVICE is not set." << std::endl;
-  }
-
-  XLA_CHECK(client != nullptr);
-
-  return client;
-}
-
-}  // namespace
-
 ComputationClient* GetComputationClient() {
-  static auto client = std::unique_ptr<ComputationClient>(CreateClient());
+  static std::unique_ptr<ComputationClient> client = []() {
+    if (sys_util::GetEnvBool("XLA_DUMP_FATAL_STACK", false)) {
+      tsl::testing::InstallStacktraceHandler();
+    }
+
+    std::unique_ptr<ComputationClient> client;
+
+    if (sys_util::GetEnvString(env::kEnvPjRtDevice, "") != "") {
+      client = std::make_unique<PjRtComputationClient>();
+    } else {
+      XLA_ERROR() << "$PJRT_DEVICE is not set." << std::endl;
+    }
+
+    XLA_CHECK(client);
+
+    g_computation_client_initialized = true;
+    return client;
+  }();
+
   return client.get();
 }
 
