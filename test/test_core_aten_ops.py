3a8396408dde5b9b94510ea724ecdbc8017706e3
Chunnien Chan <121328115+chunnienc@users.noreply.github.com>
2023-12-14 22:50:01 -0800

    Update test_core_aten_ops.py

--- test/test_core_aten_ops.py
+++ test/test_core_aten_ops.py
@@ -2858,7 +2858,6 @@ class AtenOpTest(unittest.TestCase):
     kwargs = dict()
     run_export_and_compare(self, torch.ops.aten.minimum, args, kwargs)
 
-  @unittest.skip
   def test_aten_minimum_2(self):
     args = (
         torch.randint(0, 10, (10, 10)).to(torch.int32),
