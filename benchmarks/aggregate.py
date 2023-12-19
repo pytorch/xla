pytorch/xla

commit bd2ec6f8ccb13915ad120e53a772000e38db8381

benchmarks/aggregate: read JSONL files directly (#6191)
his gets rid of the intermediate step of going through results_analyzer.

The test CSV files were converted to JSONL with this Perl script:
```
use warnings;
use strict;
use Text::CSV;

die if @ARGV == 0;
my $filename = $ARGV[0];

my @headers = ();
my %h = ();
my $csv = Text::CSV->new({ sep_char => ',' });
open(my $fh, '<', $filename) or die $!;
while (my $line = <$fh>) {
  chomp $line;
  if (@headers == 0) {
    @headers = split(',', $line);
    for (my $i = 0; $i < @headers; $i++) {
      $h{$headers[$i]} = $i;
    }
    next;
  }
  if (!$csv->parse($line)) {
    print STDERR "Cannot parse line: $line\n";
    next;
  }
  my @f = $csv->fields();
  my $model_name = $f[$h{model_name}];
  my $accelerator_model = $f[$h{accelerator_model}];
  my $xla = $f[$h{xla}] ? '"' . $f[$h{xla}] . '"' : "null";
  my $dynamo = $f[$h{dynamo}];
  my $test = $f[$h{test}];
  my $batch_size = $f[$h{batch_size}] || "null";
  my $median = $f[$h{median_total_time}];
  my $total_time = $median ? "[15.150130984999123, $median]" : "null";
  my $timestamp = $f[$h{timestamp}];
  print "{\"model\": {\"suite_name\": \"torchbench\", \"model_name\": \"$model_name\"}, \"experiment\": {\"experiment_name\": \"run_all\", \"accelerator\": \"cuda\", \"accelerator_model\": \"$accelerator_model\", \"xla\": $xla, \"xla_flags\": null, \"dynamo\": \"$dynamo\", \"test\": \"$test\", \"batch_size\": $batch_size}, \"repeat\": 2, \"iterations_per_run\": 1, \"metrics\": {\"total_time\": $total_time, \"per_iter_time\": $total_time}, \"outputs_file\": null, \"timestamp\": $timestamp}\n";
}
close($fh);
```

benchmarks/aggregate.py
@@ -1,8 +1,8 @@
-"""Processes .csv result files and aggregates them."""
+"""Processes .jsonl result files and aggregates them."""
 
 import argparse
-import csv
 from datetime import datetime
+import json
 import logging
 import os
 import re
@@ -22,7 +22,7 @@ except ImportError:
 
 logger = logging.getLogger(__name__)
 
-_test_to_csv_field_name = {
+_test_to_field_name = {
     'inference': 'eval',
     'training': 'train',
 }
@@ -51,49 +51,47 @@ def skip_model(args, model_name: str):
 
 
 def process_file(args, results_map: Dict[str, Any], filename: str):
-  with open(filename) as check_header_file:
-    try:
-      has_header = csv.Sniffer().has_header(check_header_file.read(1024))
-    except csv.Error:
-      logger.error('Cannot read CSV in %s, skipping.', filename)
-      return
-    if not has_header:
-      logger.error('Cannot interpret %s: missing headers.', filename)
-      return
-  fields = (
-      'model_name',
-      'accelerator_model',
-      'dynamo',
-      'test',
-      'batch_size',
-      'median_total_time',
-  )
+  fields = {
+      'experiment': ['accelerator_model', 'batch_size', 'dynamo', 'test'],
+      'metrics': [],
+      'model': ['model_name'],
+      'timestamp': [],
+  }
   with open(filename) as read_file:
-    reader = csv.reader(read_file)
-    headers = next(reader)
-    if headers[0] != 'timestamp':
-      logger.error('Missing timestamp in CSV in %s, skipping.', filename)
-      return
-    field2index = {}
-    for i, header in enumerate(headers):
-      for field in fields:
-        if field == header:
-          field2index[field] = i
-    for row in reader:
-      timestamp = row[0]
-      model_name = row[field2index['model_name']]
-      if skip_model(args, model_name):
-        continue
+    for line in read_file:
+      try:
+        r = json.loads(line.rstrip('\n|\r'))
+      except json.JSONDecodeError as e:
+        sys.exit(f'Invalid JSONL:\n{line}{e}')
+
+      # check that all fields exist.
+      for k in fields:
+        if k not in r:
+          sys.exit(f'JSONL record does not contain key {k}. JSONL: {r}')
+        for kk in fields[k]:
+          if kk not in r[k]:
+            sys.exit(f'JSONL record does not contain key {k}.{kk}. JSONL: {r}')
+
+      # Read in what we need.
       accelerator_model = clean_up_accelerator_model(
-          row[field2index['accelerator_model']])
+          r['experiment']['accelerator_model'])
       if accelerator_model != args.accelerator:
         continue
-      dynamo = row[field2index['dynamo']]
-      test = row[field2index['test']]
-      if test != _test_to_csv_field_name[args.test]:
+      dynamo = r['experiment']['dynamo']
+      test = r['experiment']['test']
+      if test != _test_to_field_name[args.test]:
         continue
-      batch_size = row[field2index['batch_size']]
-      median_total_time = row[field2index['median_total_time']]
+      total_times = r['metrics']['total_time'] if 'total_time' in r[
+          'metrics'] else []
+      if len(total_times) <= 1:
+        continue
+      # Skip total_times[0] because it includes compilation time.
+      median_total_time = np.median(total_times[1:])
+      dynamo = r['experiment']['dynamo']
+      batch_size = r['experiment']['batch_size']
+      timestamp = r['timestamp']
+      model_name = r['model']['model_name']
+
       if timestamp not in results_map:
         results_map[timestamp] = {}
       if dynamo not in results_map[timestamp]:
@@ -393,9 +391,6 @@ def main():
   args = parse_args()
   filenames = args.input_file
   results_map = {}
-
-  # Some CSV files have lots of errors from execution; expand CSV's size limit.
-  csv.field_size_limit(1024 * 1024)
 
   for filename in filenames:
     process_file(args, results_map, filename)

test/benchmarks/Makefile
@@ -12,7 +12,7 @@ endif
 
 $(TESTS):
 	$(QUIET_AGGREGATE)python3 ../../benchmarks/aggregate.py $(TEST_ARGS) \
-		--format=csv $(wildcard *.csv) > $@.tmp
+		--format=csv $(wildcard *.jsonl) > $@.tmp
 	$(QUIET_DIFF)git diff --no-index $@ $@.tmp
 	$(QUIET_RM)$(RM) $@.tmp
 

commit 9e1cecbd088be4eebd0d5f1a796ebc31e9e3d5fe

benchmarks/aggregate: fix --filter breakage
In "bd2ec6f8c benchmarks/aggregate: read JSONL files directly (#6191)"
we mistakenly stopped honoring --filter. Fix it and add a test to
prevent this from happening again.

While at it, remove the redundant assignment to `dynamo`.

benchmarks/aggregate.py
@@ -85,6 +85,9 @@ def process_file(args, results_map: Dict[str, Any], filename: str):
           r['experiment']['accelerator_model'])
       if accelerator_model != args.accelerator:
         continue
+      model_name = r['model']['model_name']
+      if skip_model(args, model_name):
+        continue
       dynamo = r['experiment']['dynamo']
       test = r['experiment']['test']
       if test != _test_to_field_name[args.test]:
@@ -100,10 +103,8 @@ def process_file(args, results_map: Dict[str, Any], filename: str):
       std = np.std(total_times[1:], ddof=1) if len(total_times) > 2 else 0.0
       dp = Datapoint(avg, std)
       median_total_time = np.median(total_times[1:])
-      dynamo = r['experiment']['dynamo']
       batch_size = r['experiment']['batch_size']
       timestamp = r['timestamp']
-      model_name = r['model']['model_name']
 
       if timestamp not in results_map:
         results_map[timestamp] = {}

test/benchmarks/Makefile
@@ -1,4 +1,4 @@
-TEST_ARGS = $(shell echo $@ | perl -pe 's/([^.]*)\.([^.]*)\.([^.]*)\.test/--accelerator=$$1 --test=$$2 --report=$$3/')
+TEST_ARGS = $(shell echo $@ | perl -pe 's/([^.]*)\.([^.]*)\.([^.]*)(?:\.tier([0-9]+))?\.test/--accelerator=$$1 --test=$$2 --report=$$3/; if (defined($$4)) { print "--filter-by-tier=$$4 " }')
 
 TESTS := $(wildcard *.test)
 all: $(TESTS)

test/benchmarks/v100.inference.latest.tier1.test
@@ -1,0 +1,2 @@
+# WorkloadNumber,Speedup(Inductor/Oldest Inductor),StdDev,ModelName(Inductor),Speedup(PytorchXLA/Oldest Inductor),StdDev,ModelName(PytorchXLA),Speedup(PytorchXLA_Eval/Oldest Inductor),StdDev,ModelName(PytorchXLA_Eval)
+0,1.51952596,6.914e-05,BERT_pytorch,1.56880282,7.138e-05,BERT_pytorch,1.36859903,6.227e-05,BERT_pytorch