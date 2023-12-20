7339c82e87c6f4e3c9f7a0e38f91b50eee1ed4d8
Wonjoo Lee <wonjoo@google.com>
2023-12-20 14:34:29 -0800

    Update aten_exp to do a manual type conversion (#6214)

--- test/test_core_aten_ops.py
+++ test/test_core_aten_ops.py
@@ -1569,7 +1569,6 @@ class AtenOpTest(unittest.TestCase):
     kwargs = dict()
     run_export_and_compare(self, torch.ops.aten.exp, args, kwargs)
 
-  @unittest.skip
   def test_aten_exp_2(self):
     args = (torch.randint(0, 10, (10, 10)).to(torch.int32),)
     kwargs = dict()
