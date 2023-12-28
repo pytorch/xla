61ba469bb35462b88af3fc31468c39c8d2f506a7
Wonjoo Lee <wonjoo@google.com>
2023-12-28 20:51:33 +0000

    Enable test_aten_add_Scalar_0

--- test/test_core_aten_ops.py
+++ test/test_core_aten_ops.py
@@ -295,7 +295,6 @@ class AtenOpTest(unittest.TestCase):
     run_export_and_compare(self, torch.ops.aten._adaptive_avg_pool3d, args,
                            kwargs)
 
-  @unittest.skip
   def test_aten_add_Scalar_0(self):
     args = (
         torch.randn((10, 10)).to(torch.float32),
