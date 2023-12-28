3a0cda14c60ee7a29a37e207a4d3ed5485376b00
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2023-12-28 15:48:55 -0800

    Update ops_xla_shape_fn.h

--- torch_xla/csrc/ops/ops_xla_shape_fn.h
+++ torch_xla/csrc/ops/ops_xla_shape_fn.h
@@ -275,6 +275,8 @@ xla::Shape TanOutputShape(const torch::lazy::Value& input);
 
 xla::Shape TanhOutputShape(const torch::lazy::Value& input);
 
+xla::Shape TopkOutputShape(const torch::lazy::Value& input);
+
 xla::Shape TrilOutputShape(const torch::lazy::Value& input);
 
 xla::Shape TriuOutputShape(const torch::lazy::Value& input);
