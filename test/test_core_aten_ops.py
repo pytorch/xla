35e3fba9ccc4095ca2ce854b99e7b9ee6349ea59
Wonjoo Lee <wonjoo@google.com>
2023-12-28 22:41:51 +0000

    Enable test_aten_floor_divide_1

--- test/test_core_aten_ops.py
+++ test/test_core_aten_ops.py
@@ -1760,7 +1760,6 @@ class AtenOpTest(unittest.TestCase):
     kwargs = dict()
     run_export_and_compare(self, torch.ops.aten.floor_divide, args, kwargs)
 
-  @unittest.skip
   def test_aten_floor_divide_1(self):
     args = (
         torch.randn((10, 10)).to(torch.float16),
