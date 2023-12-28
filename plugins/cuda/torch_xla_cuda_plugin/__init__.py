f592995a77aba873a0cc6bb5c87d1f1985efd168
Will Cromar <wcromar@google.com>
2023-12-28 21:26:07 +0000

    Fix DevicePlugin implementation

--- plugins/cuda/torch_xla_cuda_plugin/__init__.py
+++ plugins/cuda/torch_xla_cuda_plugin/__init__.py
@@ -6,14 +6,6 @@ class GpuPlugin(plugins.DevicePlugin):
   def library_path(self) -> str:
     return os.path.join(os.path.dirname(__file__), 'pjrt_c_api_gpu_plugin.so')
 
-  # def host_index(self):
-  #   return 0
-
-  # def configure_single_process(self):
-  #   return tpu.configure_one_chip_topology()
-
-  # def configure_multiprocess(self, local_rank, local_world_size):
-  #   return tpu.configure_topology(local_rank, local_world_size)
-
-  def local_process_count(self) -> int:
+  def physical_chip_count(self) -> int:
+    # TODO: default to actual device count
     return os.getenv('GPU_NUM_DEVICES', 1)
