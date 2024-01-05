11e33f1a670454afb32a1256b173dd5521f2d2c2
iefgnoix <isaacwxf23@gmail.com>
2024-01-05 22:08:53 +0000

    fix global_runtime_device_count for the spmd case

--- test/spmd/test_xla_spmd_python_api_interaction.py
+++ test/spmd/test_xla_spmd_python_api_interaction.py
@@ -99,6 +99,17 @@ class BasicRuntimeAPITest(test_xla_sharding_base.XlaShardingTest):
       self.assertGreaterEqual(xr.global_runtime_device_count(), 4)
     elif device_type == "CPU":
       self.assertEqual(xr.global_runtime_device_count(), 1)
+    elif device_type == 'CUDA':
+      command = 'nvidia-smi --list-gpus | wc -l'
+      result = subprocess.run(
+          command,
+          capture_output=True,
+          shell=True,
+          check=True,
+          text=True,
+      )
+      expected_gpu_cnt = int(result.stdout)
+      self.assertEqual(xr.global_runtime_device_count(), expected_gpu_cnt)
 
   def test_addressable_runtime_device_count(self):
     device_type = os.environ['PJRT_DEVICE']
