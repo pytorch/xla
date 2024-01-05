30ef2d30b5310afc7a7689fbd8938e1509ea7fca
Luke Boyer <lukeboyer4@gmail.com>
2024-01-05 18:48:46 -0500

    Remove skip from prod lowering test (#6265)

--- test/test_core_aten_ops.py
+++ test/test_core_aten_ops.py
@@ -3349,7 +3349,6 @@ class AtenOpTest(unittest.TestCase):
     kwargs = dict()
     run_export_and_compare(self, torch.ops.aten.prod, args, kwargs)
 
-  @unittest.skip
   def test_aten_prod_1(self):
     args = (torch.randint(0, 10, (10, 10)).to(torch.int32),)
     kwargs = dict()
