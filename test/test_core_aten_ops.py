a678dd699f1b9fa16103ccee82e8657789a03ef3
Wonjoo Lee <wonjoo@google.com>
2024-01-10 02:04:09 +0000

    Enable test_aten_div_Tensor

--- test/test_core_aten_ops.py
+++ test/test_core_aten_ops.py
@@ -1438,7 +1438,6 @@ class AtenOpTest(unittest.TestCase):
     kwargs = dict()
     run_export_and_compare(self, torch.ops.aten.div.Tensor, args, kwargs)
 
-  @unittest.skip
   def test_aten_div_Tensor_2(self):
     args = (
         torch.randint(0, 10, (10, 10)).to(torch.int32),
