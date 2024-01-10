4f003a20abddac483b50efad0f2e0935dbe4df3e
Wonjoo Lee <wonjoo@google.com>
2024-01-10 02:02:47 +0000

    Update rsqrt to do manual dtype conversion

--- test/test_core_aten_ops.py
+++ test/test_core_aten_ops.py
@@ -32,9 +32,9 @@ def diff_output(testcase, output1, output2, rtol, atol, equal_nan=True):
     if output2_cpu.dtype != output1.dtype:
       output2_cpu = output2_cpu.to(output1.dtype)
     # import pdb; pdb.set_trace()
-    # print(f'[WONJOO] output1={output1}')
-    # print(f'[WONJOO] output2_cpu={output2_cpu}')
-    # print(f'[WONJOO] output2_cpu-output1={output2_cpu-output1}')
+    print(f'[WONJOO] output1={output1}')
+    print(f'[WONJOO] output2_cpu={output2_cpu}')
+    print(f'[WONJOO] output2_cpu-output1={output2_cpu-output1}')
     testcase.assertTrue(
         torch.allclose(
             output1, output2_cpu, atol=atol, rtol=rtol, equal_nan=equal_nan))
@@ -3715,23 +3715,44 @@ class AtenOpTest(unittest.TestCase):
     kwargs = dict()
     run_export_and_compare(self, torch.ops.aten.round, args, kwargs)
 
-  @unittest.skip
   def test_aten_rsqrt_0(self):
     args = (torch.randn((10, 10)).to(torch.float32),)
     kwargs = dict()
-    run_export_and_compare(self, torch.ops.aten.rsqrt, args, kwargs)
+    # run_export_and_compare(self, torch.ops.aten.rsqrt, args, kwargs)
+    run_export_and_compare(
+        self,
+        torch.ops.aten.rsqrt,
+        args,
+        kwargs,
+        rtol=0.001,
+        atol=0.01,
+    )
 
-  @unittest.skip
   def test_aten_rsqrt_1(self):
     args = (torch.randn((10, 10)).to(torch.float16),)
     kwargs = dict()
-    run_export_and_compare(self, torch.ops.aten.rsqrt, args, kwargs)
+    # run_export_and_compare(self, torch.ops.aten.rsqrt, args, kwargs)
+    run_export_and_compare(
+        self,
+        torch.ops.aten.rsqrt,
+        args,
+        kwargs,
+        rtol=0.001,
+        atol=0.01,
+    )
 
-  @unittest.skip
   def test_aten_rsqrt_2(self):
     args = (torch.randint(0, 10, (10, 10)).to(torch.int32),)
     kwargs = dict()
-    run_export_and_compare(self, torch.ops.aten.rsqrt, args, kwargs)
+    # run_export_and_compare(self, torch.ops.aten.rsqrt, args, kwargs)
+    run_export_and_compare(
+        self,
+        torch.ops.aten.rsqrt,
+        args,
+        kwargs,
+        rtol=0.001,
+        atol=0.01,
+    )
 
   def test_aten_rsub_Scalar_0(self):
     args = (
--- torch_xla/csrc/ops/ops_xla_shape_fn.cpp
+++ torch_xla/csrc/ops/ops_xla_shape_fn.cpp
@@ -777,7 +777,11 @@ xla::Shape RoundOutputShape(const torch::lazy::Value& input) {
 }
 
 xla::Shape RsqrtOutputShape(const torch::lazy::Value& input) {
-  return GetXlaShape(input);
+  xla::Shape result_shape = GetXlaShape(input);
+  if (xla::primitive_util::IsIntegralType(result_shape.element_type())) {
+    result_shape.set_element_type(xla::PrimitiveType::F32);
+  }
+  return result_shape;
 }
 
 xla::Shape SeluOutputShape(const torch::lazy::Value& input) {
