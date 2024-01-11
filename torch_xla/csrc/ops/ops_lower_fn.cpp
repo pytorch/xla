11cc3830bcaec94302fc6117d22d4b20c226375c
Wonjoo Lee <wonjoo@google.com>
2024-01-10 17:27:09 -0800

    Fix some more core aten opset tests (#6284)

--- torch_xla/csrc/ops/ops_lower_fn.cpp
+++ torch_xla/csrc/ops/ops_lower_fn.cpp
@@ -700,6 +700,10 @@ torch_xla::XlaOpVector Round::Lower(LoweringContext* loctx) const {
 
 torch_xla::XlaOpVector Rsqrt::Lower(LoweringContext* loctx) const {
   xla::XlaOp xla_input = loctx->GetOutputOp(operand(0));
+  if (xla::primitive_util::IsIntegralType(XlaHelpers::TypeOfXlaOp(xla_input))) {
+    xla::PrimitiveType input_type = XlaHelpers::TypeOfXlaOp(xla_input);
+    xla_input = ConvertTo(xla_input, input_type, xla::PrimitiveType::F32);
+  }
   return ReturnOp(xla::Rsqrt(xla_input), loctx);
 }
 
