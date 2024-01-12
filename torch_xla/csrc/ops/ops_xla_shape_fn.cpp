29a38c79947d7b6e90a7312b9819e50addb0fbdd
Wonjoo Lee <wonjoo@google.com>
2024-01-12 21:40:37 +0000

    Add manual dtype conversion for acosh

--- torch_xla/csrc/ops/ops_xla_shape_fn.cpp
+++ torch_xla/csrc/ops/ops_xla_shape_fn.cpp
@@ -56,7 +56,11 @@ xla::Shape AcosOutputShape(const torch::lazy::Value& input) {
 }
 
 xla::Shape AcoshOutputShape(const torch::lazy::Value& input) {
-  return GetXlaShape(input);
+  xla::Shape result_shape = GetXlaShape(input);
+  if (xla::primitive_util::IsIntegralType(result_shape.element_type())) {
+    result_shape.set_element_type(xla::PrimitiveType::F32);
+  }
+  return result_shape;
 }
 
 xla::Shape AdaptiveAvgPool2dOutputShape(const torch::lazy::Value& input,
