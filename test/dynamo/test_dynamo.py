cfbf85c25e7bab89f23ad08d9cdbb4bd901807e4
Wonjoo Lee <wonjoo@google.com>
2024-01-16 21:33:26 +0000

    Update metric check for resnet18 dynamo unit test

--- test/dynamo/test_dynamo.py
+++ test/dynamo/test_dynamo.py
@@ -504,7 +504,9 @@ class DynamoTrainingOptimizerTest(unittest.TestCase):
     # Graph 3: optimizer
     # Graph 4: sync input for backward
     # Graph 5: sync input for backward (TODO(JackCaoG) understand why there are two graphs)
-    self.assertEqual(met.metric_data('CompileTime')[0], 5)
+    # Graph 6, 7: PyTorch has updated the number of captured by resnet with
+    # https://github.com/pytorch/pytorch/pull/117434. 
+    self.assertEqual(met.metric_data('CompileTime')[0], 7)
     # We execute 4 graphs per step when optimizer is enabled.
     self.assertEqual(met.metric_data('ExecuteTime')[0], sample_count * 4)
     # one for each forward, backward and optimizer
