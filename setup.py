acb505f1738c5dbdf5ca4d413d68b9e46c99667d
Will Cromar <wcromar@google.com>
2024-01-19 09:52:52 -0800

    Add option to register installed plugins automatically (#6328)

--- setup.py
+++ setup.py
@@ -346,6 +346,9 @@ setup(
         'absl-py>=1.0.0',
         'cloud-tpu-client>=0.10.0',
         'pyyaml',
+        # importlib.metadata backport required for PJRT plugin discovery prior
+        # to Python 3.10
+        'importlib_metadata>=4.6;python_version<"3.10"',
     ],
     package_data={
         'torch_xla': ['lib/*.so*',],
@@ -353,7 +356,8 @@ setup(
     entry_points={
         'console_scripts': [
             'stablehlo-to-saved-model = torch_xla.tf_saved_model_integration:main'
-        ]
+        ],
+        'torch_xla.plugins': ['tpu = torch_xla._internal.tpu:TpuPlugin',],
     },
     extras_require={
         # On Cloud TPU VM install with:
