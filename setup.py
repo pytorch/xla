1179453f3f82eb0c4ede2471fe876dd161286036
Will Cromar <wcromar@google.com>
2024-01-19 00:01:24 +0000

    formatting

--- setup.py
+++ setup.py
@@ -357,9 +357,7 @@ setup(
         'console_scripts': [
             'stablehlo-to-saved-model = torch_xla.tf_saved_model_integration:main'
         ],
-        'torch_xla.plugins': [
-            'tpu = torch_xla._internal.tpu:TpuPlugin',
-        ],
+        'torch_xla.plugins': ['tpu = torch_xla._internal.tpu:TpuPlugin',],
     },
     extras_require={
         # On Cloud TPU VM install with:
