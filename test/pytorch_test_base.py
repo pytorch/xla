e265fd7b403d4508a1bcdaad2af9519e6c2d0b4c
JackCaoG <59073027+JackCaoG@users.noreply.github.com>
2024-01-22 14:28:53 -0800

    disable test_EmbeddingBag_per_sample_weights_and_new_offsets (#6351)

--- test/pytorch_test_base.py
+++ test/pytorch_test_base.py
@@ -315,6 +315,7 @@ DISABLED_TORCH_TESTS_ANY = {
         'test_embedding_bag_device',  # FIXME! Unsupported device type for sparse layout: xla
         'test_embedding_scalar_weight_error_xla',  # tsl::CurrentStackTrace[abi:cxx11]
         'test_EmbeddingBag_per_sample_weights_and_no_offsets',  # FIXME! Unsupported device type for sparse layout: xla
+        'test_EmbeddingBag_per_sample_weights_and_new_offsets',  # precision
     },
 
     # test/nn/test_convolution.py
