0478d2f2cf3979d694be43eb2a036dbec1777ca0
Wonjoo Lee <wonjoo@google.com>
2024-01-22 20:23:33 +0000

    Add manual dtype conversion for aten_reciprocal

--- torch_xla/csrc/ops/ops_lower_fn.cpp
+++ torch_xla/csrc/ops/ops_lower_fn.cpp
@@ -680,6 +680,10 @@ torch_xla::XlaOpVector NeTensor::Lower(LoweringContext* loctx) const {
 
 torch_xla::XlaOpVector Reciprocal::Lower(LoweringContext* loctx) const {
   xla::XlaOp xla_input = loctx->GetOutputOp(operand(0));
+  if (xla::primitive_util::IsIntegralType(XlaHelpers::TypeOfXlaOp(xla_input))) {
+    xla::PrimitiveType input_type = XlaHelpers::TypeOfXlaOp(xla_input);
+    xla_input = ConvertTo(xla_input, input_type, xla::PrimitiveType::F32);
+  }
   return ReturnOp(BuildReciprocal(xla_input), loctx);
 }
 
--- torch_xla/csrc/ops/ops_xla_shape_fn.cpp
+++ torch_xla/csrc/ops/ops_xla_shape_fn.cpp
@@ -758,7 +758,11 @@ xla::Shape NeTensorOutputShape(const torch::lazy::Value& self,
 }
 
 xla::Shape ReciprocalOutputShape(const torch::lazy::Value& input) {
-  return GetXlaShape(input);
+  xla::Shape result_shape = GetXlaShape(input);
+  if (xla::primitive_util::IsIntegralType(result_shape.element_type())) {
+    result_shape.set_element_type(xla::PrimitiveType::F32);
+  }
+  return result_shape;
 }
 
 xla::Shape ReluOutputShape(const torch::lazy::Value& input) {
