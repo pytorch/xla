3865443554ddc5064b607e40b27340d619a7e6a7
Wonjoo Lee <wonjoo@google.com>
2024-01-22 07:19:09 +0000

    Run linter again

--- test/test_core_aten_ops.py
+++ test/test_core_aten_ops.py
@@ -29,7 +29,7 @@ def diff_output(testcase, output1, output2, rtol, atol, equal_nan=True):
     output2_cpu = output2.detach().cpu()
     if output2_cpu.dtype != output1.dtype:
       output2_cpu = output2_cpu.to(output1.dtype)
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     testcase.assertTrue(
         torch.allclose(
             output1, output2_cpu, atol=atol, rtol=rtol, equal_nan=equal_nan))
@@ -3107,7 +3107,11 @@ class AtenOpTest(unittest.TestCase):
         0.0,
     )
     kwargs = dict()
-    run_export_and_compare(self, torch.ops.aten.native_group_norm, args, kwargs, 
+    run_export_and_compare(
+        self,
+        torch.ops.aten.native_group_norm,
+        args,
+        kwargs,
         rtol=0.001,
         atol=0.01,
     )
