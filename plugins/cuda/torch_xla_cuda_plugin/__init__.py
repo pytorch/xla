61bd1d23392d46652986fd2b8a1dff3ac2728158
Will Cromar <wcromar@google.com>
2024-01-23 10:00:37 -0800

    Improve build process for PJRT plugins (#6314)

--- plugins/cuda/torch_xla_cuda_plugin/__init__.py
+++ plugins/cuda/torch_xla_cuda_plugin/__init__.py
@@ -1,10 +1,12 @@
 import os
 from torch_xla.experimental import plugins
-from torch_xla._internal import tpu
 
-class GpuPlugin(plugins.DevicePlugin):
+
+class CudaPlugin(plugins.DevicePlugin):
+
   def library_path(self) -> str:
-    return os.path.join(os.path.dirname(__file__), 'pjrt_c_api_gpu_plugin.so')
+    return os.path.join(
+        os.path.dirname(__file__), 'lib', 'pjrt_c_api_gpu_plugin.so')
 
   def physical_chip_count(self) -> int:
     # TODO: default to actual device count
