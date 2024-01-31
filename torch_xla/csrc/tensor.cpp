37cd4f3902f45f8920dd9429ae7adcf0175eff1e
Edward Z. Yang <ezyang@meta.com>
2024-01-31 13:54:48 -0800

    fixup more
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>

--- torch_xla/csrc/tensor.cpp
+++ torch_xla/csrc/tensor.cpp
@@ -764,15 +764,21 @@ c10::SymNode XLASymNodeImpl::sym_max(const c10::SymNode& other) {
 // Force guards when performing these logical operations
 
 c10::SymNode XLASymNodeImpl::sym_or(const c10::SymNode& other) {
-  return guard_bool(__FILE__, __LINE__) || other.guard_bool(__FILE__, __LINE__);
+  auto a = guard_bool(__FILE__, __LINE__) || other->guard_bool(__FILE__, __LINE__);
+  auto cnst = torch::lazy::MakeNode<SizeConstant>(a);
+  return c10::make_intrusive<XLASymNodeImpl>(cnst, PyType::BOOL);
 }
 
 c10::SymNode XLASymNodeImpl::sym_and(const c10::SymNode& other) {
-  return guard_bool(__FILE__, __LINE__) && other.guard_bool(__FILE__, __LINE__);
+  auto a = guard_bool(__FILE__, __LINE__) && other->guard_bool(__FILE__, __LINE__);
+  auto cnst = torch::lazy::MakeNode<SizeConstant>(a);
+  return c10::make_intrusive<XLASymNodeImpl>(cnst, PyType::BOOL);
 }
 
 c10::SymNode XLASymNodeImpl::sym_not() {
-  return !guard_bool(__FILE__, __LINE__);
+  auto a = !guard_bool(__FILE__, __LINE__);
+  auto cnst = torch::lazy::MakeNode<SizeConstant>(a);
+  return c10::make_intrusive<XLASymNodeImpl>(cnst, PyType::BOOL);
 }
 
 // NB: self is ignored here, only the arguments are used
