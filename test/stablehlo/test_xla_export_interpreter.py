a2b8e8c24f46f1f775927254ad00c0738fdefa3b
Siyuan Liu <lsiyuan@google.com>
2024-02-05 19:00:18 +0000

    check '_schema' attr for callsite

--- test/stablehlo/test_xla_export_interpreter.py
+++ test/stablehlo/test_xla_export_interpreter.py
@@ -27,6 +27,20 @@ class XLAExportInterpreterTest(unittest.TestCase):
         re.search(r'stablehlo.constant.* : tensor<2x3xf32>', shlo_text)
         is not None)
 
+  def test_constant_wrapping_scalar_variant(self):
+
+    class M(torch.nn.Module):
+
+      def forward(self, x):
+        return torch.ops.aten.rsub(x, 1.0)
+
+    ep = torch.export.export(M(), (torch.rand(2, 3),))
+    shlo_module = exported_program_to_stablehlo(ep)
+    shlo_text = shlo_module.get_stablehlo_text()
+    self.assertTrue(
+        re.search(r'stablehlo.constant.* : tensor<2x3xf32>', shlo_text)
+        is not None)
+
 
 if __name__ == '__main__':
   test = unittest.main()
