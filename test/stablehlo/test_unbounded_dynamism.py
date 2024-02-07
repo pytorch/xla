899ec5ce05c5de10ad9a263488c45710d77ab78c
Siyuan Liu <lsiyuan@google.coim>
2024-02-07 00:44:13 +0000

    disable addmm test

--- test/stablehlo/test_unbounded_dynamism.py
+++ test/stablehlo/test_unbounded_dynamism.py
@@ -60,6 +60,7 @@ class UnboundedDynamismExportTest(unittest.TestCase):
             r'tensor<f32>.*tensor<\?x197x768xf32>.*->.*tensor<\?x197x768xf32>',
             shlo_text) is not None)
 
+  @unittest.skip("Unbounded Dynamism not supported on addmm.")
   def test_addmm(self):
     args = (torch.rand((5)), torch.rand((10, 5)), torch.rand((5, 5)))
     constraints = [
