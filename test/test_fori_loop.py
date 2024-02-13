6a4ad4ff79421efe235631b277e561f7217f6390
manfeibaigithub <manfei@google.com>
2024-02-13 21:24:43 +0000

    use code from xb

--- test/test_fori_loop.py
+++ test/test_fori_loop.py
@@ -3,8 +3,9 @@ import unittest
 from typing import Callable, Dict, List
 
 import torch
-import torch_xla.core.xla_model as xm
 from torch._higher_order_ops.while_loop import while_loop
+import torch_xla.core.xla_model as xm
+import torch_xla.core.xla_builder as xb
 
 _TORCH_WHILE_LOOP_OPS = [
     torch._higher_order_ops.while_loop,
@@ -19,15 +20,18 @@ class WhileLoopTest(unittest.TestCase):
 
     def test_while_loop_tpu(self):
         def cond_fn(x):
-            z = torch.tensor(10, device=xm.xla_device())
-            return x < z
+            # z = torch.tensor(10, device=xm.xla_device())
+            # return x < z
+            return x < xb.Op.scalar(x.builder(), 10, dtype=xb.Type.S32)
 
         def body_fn(x):
-            y = torch.tensor(1, device=xm.xla_device())
-            return (x + y,)
+            # y = torch.tensor(1, device=xm.xla_device())
+            # return (x + y,)
+            x = x + xb.Op.scalar(x.builder(), 1, dtype=xb.Type.S32)
+            return xb.Op.tuple((x,))
 
         device = xm.xla_device()
-        x = torch.zeros(1, device=device)
+        x = torch.ones(1, device=device)
         res = while_loop(cond_fn, body_fn, (x, ))
         expected = _fake_while_loop(cond_fn, body_fn, (x, ))
         self.assertEqual(expected, res)
