12e6886fa5744963b9c8522c625d2f75d803fc60
manfeibaigithub <manfei@google.com>
2024-02-14 01:20:40 +0000

    add example code script again

---
+++ test/test_fori_loop_xla_second.py
@@ -0,0 +1,44 @@
+import os
+import unittest
+from typing import Callable, Dict, List
+
+import torch
+from torch._higher_order_ops.while_loop import while_loop, while_loop_dense
+import torch_xla.core.xla_model as xm
+import torch_xla.core.xla_builder as xb
+
+_TORCH_WHILE_LOOP_OPS = [
+    torch._higher_order_ops.while_loop,
+]
+
+def _fake_while_loop(cond_fn, body_fn, operands):
+    while cond_fn(*operands):
+        operands = body_fn(*operands)
+    return operands
+
+class WhileLoopTest(unittest.TestCase):
+
+    def test_while_loop_tpu(self):
+        def cond_fn(counter, x):
+            # z = torch.tensor(10, device=xm.xla_device())
+            # return x < z
+            return counter < xb.Op.scalar(x.builder(), 10, dtype=xb.Type.S32)
+
+        def body_fn(counter, x):
+            # y = torch.tensor(1, device=xm.xla_device())
+            # return (x + y,)
+            next_counter = counter + xb.Op.scalar(counter.builder(), 1, dtype=xb.Type.S32)
+            x = x + xb.Op.scalar(x.builder(), 1, dtype=xb.Type.S32)
+            return xb.Op.tuple((next_counter, x))
+
+        device = xm.xla_device()
+        x = torch.ones(1, dtype=torch.int, device=device)
+        res = while_loop(cond_fn, body_fn, (x, ))
+        expected = _fake_while_loop(cond_fn, body_fn, (x, ))
+        self.assertEqual(expected, res)
+
+
+
+if __name__ == '__main__':
+  test = unittest.main()
+  sys.exit(0 if test.result.wasSuccessful() else 1)
--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -26,14 +26,15 @@ def _xla_while_loop(cond_fn, body_fn, operands):
   # operands: list[Tensor]
   print("arrive the xla_while_loop!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
   print("operands type: ", type(operands))
-  internal_operands = operands[0]  # specific to test case operands: (x, )
+  # internal_operands = operands[0]  # specific to test case operands: (x, )
   print("before define the op_fn!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
   def op_fn(internal_x):
-    def cond(internal_x):
-      return cond_fn(internal_x)
-    def body(internal_x):
-      return body_fn(internal_x)
-    w = xb.Op.mkwhile((internal_x,), cond_fn, body_fn)
+    # def cond(internal_x):
+    #   return cond_fn(internal_x)
+    # def body(internal_x):
+    #   return body_fn(internal_x)
+    zero = xb.Op.scalar(a.builder(), 0, dtype=xb.Type.S32)
+    w = xb.Op.mkwhile((zero, internal_x), cond_fn, body_fn)
     return w.get_tuple_element(1)
   print("after define the op_fn!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
   op = xor.register('test_while', op_fn)
