d7319fb28d388d318e0e3eea1a5e2972e3e39dfc
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-02-14 16:32:08 -0800

    Update test_core_aten_ops.py

--- test/test_core_aten_ops.py
+++ test/test_core_aten_ops.py
@@ -3247,7 +3247,6 @@ class AtenOpTest(unittest.TestCase):
     kwargs = dict()
     run_export_and_compare(self, torch.ops.aten.replication_pad2d, args, kwargs)
 
-  @unittest.skip
   def test_aten_replication_pad3d_0(self):
     args = (
         torch.randn((1, 3, 2, 10)).to(torch.float32),
@@ -3263,7 +3262,6 @@ class AtenOpTest(unittest.TestCase):
     kwargs = dict()
     run_export_and_compare(self, torch.ops.aten.replication_pad3d, args, kwargs)
 
-  @unittest.skip
   def test_aten_replication_pad3d_1(self):
     args = (
         torch.randint(0, 10, (1, 3, 2, 10)).to(torch.int32),
