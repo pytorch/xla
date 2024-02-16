68e75febec3f13d5c54ccfbc02d41772be7cc84e
Wonjoo Lee <wonjoo@google.com>
2024-02-16 19:53:58 +0000

    Enable aten_relu tests

--- test/test_core_aten_ops.py
+++ test/test_core_aten_ops.py
@@ -3176,7 +3176,6 @@ class AtenOpTest(unittest.TestCase):
     kwargs = dict()
     run_export_and_compare(self, torch.ops.aten.relu, args, kwargs)
 
-  @unittest.skip
   def test_aten_relu_2(self):
     args = (torch.randint(0, 10, (10, 10)).to(torch.int32),)
     kwargs = dict()
