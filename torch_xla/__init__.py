c305af4bc7e4b2ce1d04135e8b5a06045298a97d
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-02-15 21:23:25 -0800

    format

--- torch_xla/__init__.py
+++ torch_xla/__init__.py
@@ -32,7 +32,8 @@ def _setup_xla_flags():
   flags = _set_missing_flags(flags,
                              (('xla_gpu_force_compilation_parallelism', '8'),))
   flags = _set_missing_flags(flags, (('xla_tpu_run_space_to_batch', 'false'),))
-  flags = _set_missing_flags(flags, (('xla_tpu_run_space_to_batch_on_new_platforms', 'false'),))
+  flags = _set_missing_flags(
+      flags, (('xla_tpu_run_space_to_batch_on_new_platforms', 'false'),))
   os.environ['XLA_FLAGS'] = ' '.join(flags)
 
 
