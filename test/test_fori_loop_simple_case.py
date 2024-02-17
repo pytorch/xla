024ce47d99014a4359fcc720741739e0e6dd4a4c
manfeibaigithub <manfei@google.com>
2024-02-17 00:54:00 +0000

    try xla test only xla

--- test/test_fori_loop_simple_case.py
+++ test/test_fori_loop_simple_case.py
@@ -22,28 +22,28 @@ def _fake_while_loop(cond_fn, body_fn, operands):
 
 class WhileLoopTest(unittest.TestCase):
 
-    def test_while_loop_tpu(self):
-        def cond_fn(x):
-            # z = torch.tensor(10, device=xm.xla_device())
-            return x.sum() <= 10
-            # return counter < xb.Op.scalar(x.builder(), 10, dtype=xb.Type.S32)
+    # def test_while_loop_tpu(self):
+    #     def cond_fn(x):
+    #         # z = torch.tensor(10, device=xm.xla_device())
+    #         return x.sum() <= 10
+    #         # return counter < xb.Op.scalar(x.builder(), 10, dtype=xb.Type.S32)
 
-        def body_fn(x):
-            # y = torch.tensor(1, device=xm.xla_device())
-            return (x+1, )
-            # next_counter = counter + xb.Op.scalar(counter.builder(), 1, dtype=xb.Type.S32)
-            # x = x + xb.Op.scalar(x.builder(), 1, dtype=xb.Type.S32)
-            # return xb.Op.tuple((next_counter, x))
+    #     def body_fn(x):
+    #         # y = torch.tensor(1, device=xm.xla_device())
+    #         return (x+1, )
+    #         # next_counter = counter + xb.Op.scalar(counter.builder(), 1, dtype=xb.Type.S32)
+    #         # x = x + xb.Op.scalar(x.builder(), 1, dtype=xb.Type.S32)
+    #         # return xb.Op.tuple((next_counter, x))
 
-        # device = xm.xla_device()
-        xx = torch.zeros(1) # , dtype=torch.int, device=device)
-        # res = while_loop(cond_fn, body_fn, (x, ))
-        res = while_loop(cond_fn, body_fn, (xx, ))
-        print("while_loop result: ", res)
-        # expected = _fake_while_loop(cond_fn, body_fn, (x, ))
-        expected = torch.tensor(11, dtype=torch.int) # , device=device)
-        print("expected result: ", expected)
-        self.assertEqual(expected, res[0])
+    #     # device = xm.xla_device()
+    #     xx = torch.zeros(1) # , dtype=torch.int, device=device)
+    #     # res = while_loop(cond_fn, body_fn, (x, ))
+    #     res = while_loop(cond_fn, body_fn, (xx, ))
+    #     print("while_loop result: ", res)
+    #     # expected = _fake_while_loop(cond_fn, body_fn, (x, ))
+    #     expected = torch.tensor(11, dtype=torch.int) # , device=device)
+    #     print("expected result: ", expected)
+    #     self.assertEqual(expected, res[0])
 
     def test_while_loop_tpu_xla(self):
         def cond_fn(x):
