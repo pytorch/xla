5050eb4f6cd3ea9f72dde72270c3e78497144d70
manfeibaigithub <manfei@google.com>
2024-02-17 01:48:42 +0000

    placeholder for xlacomputation

--- test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
+++ test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
@@ -21,17 +21,17 @@ def _fake_while_loop(cond_fn, body_fn, operands):
 class WhileLoopTest(unittest.TestCase):
 
     def test_while_loop_tpu(self):
-        def cond_fn(counter, x):
+        def cond_fn(x):
             # z = torch.tensor(10, device=xm.xla_device())
-            # return x < z
-            return counter < xb.Op.scalar(x.builder(), 10, dtype=xb.Type.S32)
+            return x.sum() <= 10
+            # return counter < xb.Op.scalar(x.builder(), 10, dtype=xb.Type.S32)
 
-        def body_fn(counter, x):
+        def body_fn(x):
             # y = torch.tensor(1, device=xm.xla_device())
-            # return (x + y,)
-            next_counter = counter + xb.Op.scalar(counter.builder(), 1, dtype=xb.Type.S32)
-            x = x + xb.Op.scalar(x.builder(), 1, dtype=xb.Type.S32)
-            return xb.Op.tuple((next_counter, x))
+            return (x + 1,)
+            # next_counter = counter + xb.Op.scalar(counter.builder(), 1, dtype=xb.Type.S32)
+            # x = x + xb.Op.scalar(x.builder(), 1, dtype=xb.Type.S32)
+            # return xb.Op.tuple((next_counter, x))
 
         device = xm.xla_device()
         x = torch.ones(1, dtype=torch.int, device=device)
--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -25,8 +25,18 @@ def while_loop(cond_fn, body_fn, operands):
 
 def _xla_while_loop(cond_fn, body_fn, operands):
   def op_fn(internal_x):
+    def cond_fn_placeholder(x):
+      return counter < xb.Op.scalar(x.builder(), 10, dtype=xb.Type.S32)
+
+    def body_fn_placeholder(x):
+      next_counter = counter + xb.Op.scalar(counter.builder(), 1, dtype=xb.Type.S32)
+      x = x + xb.Op.scalar(x.builder(), 1, dtype=xb.Type.S32)
+      return xb.Op.tuple((next_counter, x))
+
+    # device = xm.xla_device()
+    # x = torch.ones(1, dtype=torch.int, device=device)
     zero = xb.Op.scalar(internal_x.builder(), 0, dtype=xb.Type.S32)
-    w = xb.Op.mkwhile((zero, internal_x), cond_fn, body_fn)
+    w = xb.Op.mkwhile((zero, internal_x), cond_fn_placeholder, body_fn_placeholder)
     return w.get_tuple_element(1)
   op = xor.register('test_while', op_fn)
   return xu.as_list(op(operands[0]))
