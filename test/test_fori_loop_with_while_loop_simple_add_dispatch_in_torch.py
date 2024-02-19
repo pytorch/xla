d689acc3a157eabba88de0b15e0ffbb607ed4b78
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-02-18 22:11:18 -0800

    format

--- test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
+++ test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
@@ -4,25 +4,22 @@ from typing import Callable, Dict, List
 
 import torch
 import torch_xla
-# # from torch._higher_order_ops.while_loop import while_loop, while_loop_dense
-# from torch_xla.experimental.fori_loop import while_loop
 import torch_xla.experimental.fori_loop
 from torch._higher_order_ops.while_loop import while_loop
 import torch_xla.core.xla_model as xm
 import torch_xla.core.xla_builder as xb
 
-# _TORCH_WHILE_LOOP_OPS = [
-#     torch._higher_order_ops.while_loop,
-# ]
 
 def _fake_while_loop(cond_fn, body_fn, operands):
   while cond_fn(*operands):
     operands = body_fn(*operands)
   return operands
 
+
 class WhileLoopTest(unittest.TestCase):
 
   def test_while_loop_tpu(self):
+
     def cond_fn(x):
       return x.sum() <= 10
 
@@ -31,7 +28,7 @@ class WhileLoopTest(unittest.TestCase):
 
     device = xm.xla_device()
     x = torch.ones(1, dtype=torch.int, device=device)
-    res = while_loop(cond_fn, body_fn, (x, ))
+    res = while_loop(cond_fn, body_fn, (x,))
     print("while_loop result: ", res)
     expected = torch.tensor(11, dtype=torch.int, device=device)
     print("expected result: ", expected)
