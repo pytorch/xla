c9e7b5fd1010e8f6f56889b45d3b897f5c358d9c
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-02-18 21:19:26 -0800

    format

--- test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
+++ test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
@@ -16,35 +16,26 @@ import torch_xla.core.xla_builder as xb
 # ]
 
 def _fake_while_loop(cond_fn, body_fn, operands):
-    while cond_fn(*operands):
-        operands = body_fn(*operands)
-    return operands
+  while cond_fn(*operands):
+    operands = body_fn(*operands)
+  return operands
 
 class WhileLoopTest(unittest.TestCase):
 
-    def test_while_loop_tpu(self):
-        def cond_fn(x):
-            # z = torch.tensor(10, device=xm.xla_device())
-            return x.sum() <= 10
-            # return counter < xb.Op.scalar(x.builder(), 10, dtype=xb.Type.S32)
+  def test_while_loop_tpu(self):
+    def cond_fn(x):
+      return x.sum() <= 10
 
-        def body_fn(x):
-            # y = torch.tensor(1, device=xm.xla_device())
-            return (x + 1,)
-            # next_counter = counter + xb.Op.scalar(counter.builder(), 1, dtype=xb.Type.S32)
-            # x = x + xb.Op.scalar(x.builder(), 1, dtype=xb.Type.S32)
-            # return xb.Op.tuple((next_counter, x))
+    def body_fn(x):
+      return (x + 1,)
 
-        device = xm.xla_device()
-        x = torch.ones(1, dtype=torch.int, device=device)
-        # res = while_loop(cond_fn, body_fn, (x, ))
-        res = while_loop(cond_fn, body_fn, (x, ))
-        print("while_loop result: ", res)
-        # expected = _fake_while_loop(cond_fn, body_fn, (x, ))
-        expected = torch.tensor(11, dtype=torch.int, device=device)
-        print("expected result: ", expected)
-        self.assertEqual(expected, res[0])
-
+    device = xm.xla_device()
+    x = torch.ones(1, dtype=torch.int, device=device)
+    res = while_loop(cond_fn, body_fn, (x, ))
+    print("while_loop result: ", res)
+    expected = torch.tensor(11, dtype=torch.int, device=device)
+    print("expected result: ", expected)
+    self.assertEqual(expected, res[0])
 
 
 if __name__ == '__main__':
