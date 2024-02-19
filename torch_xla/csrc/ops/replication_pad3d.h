52b43666730150422d105aff937da412bceff1cf
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-02-18 22:33:59 -0800

    Create replication_pad3d.h

---
+++ torch_xla/csrc/ops/replication_pad3d.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+
+#include "torch_xla/csrc/ir.h"
+
+namespace torch_xla {
+
+class ReflectionPad3d : public XlaNode {
+ public:
+  ReflectionPad3d(const torch::lazy::Value& input,
+                  std::vector<int64_t> padding);
+
+  torch::lazy::NodePtr Clone(torch::lazy::OpList operands) const override;
+
+  XlaOpVector Lower(LoweringContext* loctx) const override;
+
+  std::string ToString() const override;
+
+  const std::vector<int64_t>& padding() const { return padding_; }
+
+ private:
+  std::vector<int64_t> padding_;
+};
+
+}  // namespace torch_xla
