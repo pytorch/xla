375834c3aa651d73c9b7b41769a16034a4432107
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-02-18 22:36:01 -0800

    Create reflection_pad3d_backward.h

---
+++ torch_xla/csrc/ops/reflection_pad3d_backward.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+
+#include "torch_xla/csrc/ir.h"
+
+namespace torch_xla {
+
+class ReflectionPad3dBackward : public XlaNode {
+ public:
+  ReflectionPad3dBackward(const torch::lazy::Value& gard_output,
+                          const torch::lazy::Value& input,
+                          std::vector<int64_t> padding);
+
+  torch::lazy::NodePtr Clone(torch::lazy::OpList operands) const override;
+
+  XlaOpVector Lower(LoweringContext* loctx) const override;
+
+  std::string ToString() const override;
+
+  const std::vector<int64_t>& padding() const { return padding_; }
+
+ private:
+  std::vector<int64_t> padding_;
+};
+
+}  // namespace torch_xla
