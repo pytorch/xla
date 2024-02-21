381a5302803205f3b31db645f3df2259a65f7003
manfeibaigithub <manfei@google.com>
2024-02-21 23:48:39 +0000

    return

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -83,8 +83,8 @@ def _xla_while_loop(cond_fn, body_fn, operands):
   xm.mark_step()
   cond_result = cond_fn(operands)
   cond_ctx = torch_xla._XLAC.lowering.LoweringContext()
-  cond_ctx_builder = cond_ctx.builder()
-  cond_ctx_builder.name_ = 'condctx'
+  # cond_ctx_builder = cond_ctx.builder()
+  # cond_ctx_builder.name_ = 'condctx'
   cond_ctx.build(list(cond_result))
   cond_hlo = cond_ctx.hlo()
   cond_computation = xb.computation_from_module_proto("condcomputation", cond_hlo)
@@ -95,8 +95,8 @@ def _xla_while_loop(cond_fn, body_fn, operands):
   xm.mark_step()
   body_result = body_fn(operands)
   body_ctx = torch_xla._XLAC.lowering.LoweringContext()
-  body_ctx_builder = body_ctx.builder()
-  body_ctx_builder.name_ = 'bodyctx'
+  # body_ctx_builder = body_ctx.builder()
+  # body_ctx_builder.name_ = 'bodyctx'
   body_ctx.build(list(body_result))
   body_hlo = body_ctx.hlo()
   body_computation = xb.computation_from_module_proto("bodycomputation", body_hlo)
