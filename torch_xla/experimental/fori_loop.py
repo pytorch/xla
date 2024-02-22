7ce860eeda7fe48741b8ec35d27bf214a16ea024
manfeibaigithub <manfei@google.com>
2024-02-22 18:53:21 +0000

    add

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -111,7 +111,7 @@ def _xla_while_loop(cond_fn, body_fn, operands):
 
   input_tuple = xb.Op.tuple(params)
 
-  w = xb.mkop('While', (input_tuple,), condition_computation=cond_computation, body_computation=body_computation)
+  w = xb.mkop('While', [input_tuple.op], condition_computation=cond_computation, body_computation=body_computation)
   # w # <torch_xla.core.xla_builder.Op object at 0x7f7d3e367f40>
   print("pass this line")
   name = 'fori_loop_ed_torch_func'
