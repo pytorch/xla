47dec8610117bd6ec21c34b3d7581a983bed80f8
manfeibaigithub <manfei@google.com>
2024-02-22 18:33:43 +0000

    add

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -120,7 +120,9 @@ def _xla_while_loop(cond_fn, body_fn, operands):
 
   result = torch_xla._XLAC._xla_user_computation('xla::_op_test_while', operands,
                                                    computation)
+  print("result: ", result)
   op = result[0] if len(result) == 1 else result
+  
 
   return xu.as_list(op(operands))
 
