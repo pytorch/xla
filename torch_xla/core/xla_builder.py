93d2684b398ad8de8fb57882c7a9872cf04eab09
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-02-23 13:59:09 -0800

    clean xla_builder.py

--- torch_xla/core/xla_builder.py
+++ torch_xla/core/xla_builder.py
@@ -762,9 +762,6 @@ def mkop(name, ops, **kwargs):
   builder = kwargs.get('builder', None)
   if builder is None:
     assert ops
-    # if not isinstance(ops, (list, tuple)):
-    #   builder = torch_xla._XLAC._xla_op_builder(ops)
-    # else:
     builder = torch_xla._XLAC._xla_op_builder(ops[0])
   return Op(torch_xla._XLAC._xla_op_create(builder, name, ops, kwargs))
 
