dec38c94a70d4cadfd112aa557cfa6b395a2fda8
manfeibaigithub <manfei@google.com>
2024-02-23 07:46:58 +0000

    add

--- test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
+++ test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
@@ -11,10 +11,13 @@ import torch_xla.core.xla_builder as xb
 
 
 def _fake_while_loop(cond_fn, body_fn, operands):
-  print("fake func operands: ", operands)
-  print("fake func *operands: ", *operands)
-  while cond_fn(*operands):
-    operands = body_fn(*operands)
+  # print("fake func operands: ", operands)
+  # print("fake func *operands: ", *operands)
+  # while cond_fn(*operands):
+  #   operands = body_fn(*operands)
+  # return operands
+  while cond_fn(operands):
+    operands = body_fn(operands)
   return operands
 
 
