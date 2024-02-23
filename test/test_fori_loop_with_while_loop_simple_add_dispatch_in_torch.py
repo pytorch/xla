4f01799f78c31d43a36feb069f2e1b907c3abc62
manfeibaigithub <manfei@google.com>
2024-02-23 07:23:03 +0000

    add

--- test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
+++ test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
@@ -23,7 +23,9 @@ class WhileLoopTest(unittest.TestCase):
     device = xm.xla_device()
 
     def cond_fn(x): # x = (xi,)
-      ten = torch.ones(1, dtype=torch.int32, device=device)
+      ten = torch.ones(1, dtype=torch.int32) # =device)
+      ten[0] = 10
+      ten.to(device)
       return x[0] <= ten[0] # ==x[0] # torch.equal(x[0], ten) # x[0] <= ten # 30
 
     def body_fn(x): # x = (xi,)
