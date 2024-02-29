a5fca6ab787a648670a3854288a084785aeffb4f
Siyuan Liu <lsiyuan@google.com>
2024-02-29 06:04:31 +0000

    add select fx pass

---
+++ test/stablehlo/test_export_fx_passes.py
@@ -0,0 +1,34 @@
+import re
+import sys
+import unittest
+
+import torch
+import torch_xla
+from torch.export import Dim, export
+import torch_xla.core.xla_model as xm
+from torch_xla.stablehlo import exported_program_to_stablehlo
+from utils import wrap_func_as_nn_module
+from torch_xla.experimental.unbounded_dynamism_export import *
+
+device = xm.xla_device()
+
+
+class ExportFxPassTest(unittest.TestCase):
+
+  def test_decompose_dynamic_shape_select(self):
+    args = (torch.rand((10, 197, 768)), 1, 0)
+    dynamic_shapes = ([{0: Dim("bs")}, None, None],)
+    m = wrap_func_as_nn_module(torch.ops.aten.select.int)
+    ep = export(m, args, dynamic_shapes=dynamic_shapes)
+    # ep.graph_module.graph.print_tabular()
+    out1 = ep(*args)
+    ep.graph_module.graph = decompose_dynamic_shape_select(
+        ep.graph_module.graph)
+    # ep.graph_module.graph.print_tabular()
+    out2 = ep(*args)
+    torch.allclose(out1, out2)
+
+
+if __name__ == "__main__":
+  test = unittest.main()
+  sys.exit(0 if test.result.wasSuccessful() else 1)
---
+++ torch_xla/experimental/unbounded_dynamism_export.py
@@ -0,0 +1,46 @@
+import torch
+from torch.fx import Graph
+
+aten = torch.ops.aten
+
+
+def get_symbolic_dims(shape):
+  symbolic_dims = []
+  for idx, size in enumerate(shape):
+    if not isinstance(size, int):
+      symbolic_dims.append(idx)
+  return symbolic_dims
+
+
+def decompose_dynamic_shape_select(graph: Graph):
+  for n in graph.nodes:
+    if n.op == "call_function" and n.target == aten.select.int:
+      import pdb
+      # pdb.set_trace()
+      select_src_shape = n.args[0].meta['val'].shape
+      symbolic_dims = get_symbolic_dims(select_src_shape)
+      if len(symbolic_dims) > 0:
+        with graph.inserting_before(n):
+          select_src_node = n.args[0]
+          select_dim = n.args[1]
+          select_idx = n.args[2]
+          slice_args = (select_src_node, select_dim, select_idx,
+                        (select_idx + 1), 1)
+          slice_node = graph.call_function(aten.slice, slice_args)
+
+          view_new_shape = []
+          for dim, size in enumerate(select_src_shape):
+            if dim == select_dim:
+              continue
+            if isinstance(size, int):
+              view_new_shape.append(size)
+            else:
+              get_dim_size_args = (select_src_node, dim)
+              get_dim_size_node = graph.call_function(aten.sym_size.int,
+                                                      get_dim_size_args)
+              view_new_shape.append(get_dim_size_node)
+          view_args = (slice_node, view_new_shape)
+          view_node = graph.call_function(aten.view, view_args)
+          n.replace_all_uses_with(view_node)
+        graph.erase_node(n)
+  return graph
