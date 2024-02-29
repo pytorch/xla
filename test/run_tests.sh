84286426a6ae71e8a3771b9dd4a5efa8e2431119
Mason Chang <masonchang@google.com>
2024-02-29 17:51:52 +0000

    Run test gpu device detection without setting PJRT_DEVICE

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -219,7 +219,7 @@ function run_xla_op_tests3 {
   run_torchrun "$CDIR/pjrt/test_torchrun.py"
   run_test "$CDIR/test_persistent_cache.py"
   run_test "$CDIR/test_devices.py"
-  run_test "$CDIR/test_gpu_device_detection.py"
+  run_coverage "$CDIR/test_gpu_device_detection.py"
   # NOTE: this line below is testing export and don't care about GPU
   PJRT_DEVICE=CPU CPU_NUM_DEVICES=1 run_coverage "$CDIR/test_core_aten_ops.py"
 }
