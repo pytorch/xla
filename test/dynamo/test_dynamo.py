14ade9cf7edb6c14111499a9086ce97b15946725
Yukio Siraichi <yukio.siraichi@gmail.com>
2024-03-02 12:56:49 -0300

    Add test.

--- test/dynamo/test_dynamo.py
+++ test/dynamo/test_dynamo.py
@@ -632,6 +632,24 @@ class DynamoErrorMessageTest(unittest.TestCase):
     self.assertIn('MarkStep', met.counter_names())
 
 
+class DynamoOperationsTests(test_utils.XlaTestCase):
+
+  def test_new_with_sizes(self):
+
+    def foo(x):
+      return x.new(*x.sizes()) + x
+
+    optfoo = torch.compile(backend="openxla")(foo)
+
+    t = torch.arange(10)
+    Xt = t.to(xm.xla_device())
+
+    expected = foo(t)
+    actual = optfoo(Xt)
+
+    self.assertEqual(expected, actual.cpu())
+
+
 if __name__ == '__main__':
   test = unittest.main()
   sys.exit(0 if test.result.wasSuccessful() else 1)
