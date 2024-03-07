39c6ad2454a0a92ec253394bd172bd7e4d518a38
manfeibaigithub <manfei@google.com>
2024-03-07 23:55:59 +0000

    fori_loop based on while_loop

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -23,7 +23,7 @@ def fori_loop(lower, upper, body_fun, init_val):
 
   def cond_fn(lower, upper, init_val):
     one_value = torch.tensor([1], dtype=torch.int32, device=device) # torch.ones(1, dtype=torch.int32, device=device)
-    lower = torch.add(lower, 1) # one_value)
+    lower = torch.add(lower, one_value)
     # init_val = init_val.clone()
     print("lower: ",lower)
     return lower[0] <= upper[0]
