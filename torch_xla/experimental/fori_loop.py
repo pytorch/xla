60102b3a476e1cba24a5ad0944dd04efe24dcb7c
manfeibaigithub <manfei@google.com>
2024-03-07 22:20:12 +0000

    fori_loop based on while_loop

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -27,7 +27,7 @@ def fori_loop(lower, upper, body_fun, init_val):
   # upper = torch.tensor([10], dtype=torch.int32, device=device)
   limit_range = upper - lower
 
-  iterator = lower_placeholder
+  # iterator = lower_placeholder
 
   def cond_fn(init, limit_range):
     return limit_range[0] >= init[0]
