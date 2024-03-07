3617f9bad249dede409836bfd2ab0910ea1fd4b0
manfeibaigithub <manfei@google.com>
2024-03-07 22:40:05 +0000

    fori_loop based on while_loop

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -56,6 +56,9 @@ def _xla_while_loop(cond_fn, body_fn, operands):
   cond_hlo = cond_ctx.hlo()
   cond_computation = xb.computation_from_module_proto("condcomputation",
                                                       cond_hlo)
+  cond_hlo_print = xb.get_computation_hlo(cond_computation)
+  print("cond_hlo: !!!!!!!!!")
+  print(cond_hlo_print)
 
   # generate body_fn xlacomputation
   body_result = body_fn(operands[0], operands[1])
@@ -65,6 +68,9 @@ def _xla_while_loop(cond_fn, body_fn, operands):
   body_hlo = body_ctx.hlo()
   body_computation = xb.computation_from_module_proto("bodycomputation",
                                                       body_hlo)
+  body_hlo_print = xb.get_computation_hlo(body_computation)
+  print("body_hlo: !!!!!!!!!")
+  print(body_hlo_print)
 
   # generate while xlacomputation
   input_tuple = xb.Op.tuple(tuple(params))
