5c04c01c3be777aab721bfc83a2b18c700dfbf3a
manfeibaigithub <manfei@google.com>
2024-03-07 23:34:18 +0000

    fori_loop based on while_loop

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -17,15 +17,18 @@ def fori_loop(lower, upper, body_fun, init_val):
   # init_val = torch.tensor([0], dtype=torch.int32, device=device)
   # lower = torch.tensor([0], dtype=torch.int32, device=device)
   # upper = torch.tensor([10], dtype=torch.int32, device=device)
-
+  # limit_range = upper - lower
   device = xm.xla_device()
+  # one_value = torch.tensor([0], dtype=torch.int32, device=device) # torch.ones(1, dtype=torch.int32, device=device)
 
   def cond_fn(lower, upper, init_val):
-    one_value = torch.tensor([0], dtype=torch.int32, device=device)
+    one_value = torch.tensor([0], dtype=torch.int32, device=device) # torch.ones(1, dtype=torch.int32, device=device)
     lower = torch.add(lower, one_value)
     return lower[0] <= upper[0]
   
   def body_fn(lower, upper, init_val):
+    # one_value = torch.tensor([0], dtype=torch.int32, device=device) # torch.ones(1, dtype=torch.int32, device=device)
+    # lower = torch.add(lower, one_value)
     return (lower.clone(), upper.clone(), body_fun(init_val))
 
   return while_loop(cond_fn, body_fn, (lower, upper, init_val))
@@ -75,6 +78,9 @@ def _xla_while_loop(cond_fn, body_fn, operands):
       body_computation=body_computation)
   name = 'fori_loop_ed_torch_func'
   computation = w.build(name)
+  hlo_print = xb.get_computation_hlo(computation)
+  print("while computation: !!!!!!!!!")
+  print(hlo_print)
 
   # gain final result with generated while xlacomputation
   result = torch_xla._XLAC._xla_user_computation('xla::_op_test_while',
