fd85fb555e5ca9861e3fca22a3be03e9fbdf3d77
manfeibaigithub <manfei@google.com>
2024-03-08 01:04:15 +0000

    fori_loop based on while_loop

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -26,16 +26,16 @@ def fori_loop(lower, upper, body_fun, *init_val):
 
   # device = xm.xla_device()
   def cond_fn(init, limit_value, *init_val):
-    one_value = torch.tensor([1], dtype=torch.int32, device=device) # torch.ones(1, dtype=torch.int32, device=device)
-    one_value_2 = torch.tensor([1], dtype=torch.int32, device=device)
-    torch.add(one_value, one_value_2)
-    a = torch.add(init_val[0], one_value)
-    b = torch.add(init_val[1], one_value_2)
+    one_value = torch.ones(1, dtype=torch.int32, device=device) # torch.ones(1, dtype=torch.int32, device=device)
+    one_value_2 = torch.ones(1, dtype=torch.int32, device=device)
+    # torch.add(one_value, one_value_2)
+    a = torch.add(init_val, one_value)
+    b = torch.add(init_val, one_value_2)
     c = (a[0] >= b[0]) and (limit_value[0] <= init[0])
     return c # (a[0] >= b[0]) and (limit_value[0] <= init[0])
 
   def body_fn(init, limit_value, *init_val):
-    one_value = torch.tensor([1], dtype=torch.int32, device=device)
+    one_value = torch.ones(1, dtype=torch.int32, device=device)
     return (torch.add(init, one_value), limit_value.clone(), body_fun(*init_val), init_val[1])
 
     # TODO(@manfei): init and limit_value has to be torch.tensor.
