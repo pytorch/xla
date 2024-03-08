d372e5c404d3e9851e446e36a0123c830528d9e6
manfeibaigithub <manfei@google.com>
2024-03-08 21:30:37 +0000

    test

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -23,10 +23,10 @@ def fori_loop(lower, upper, body_fun, *init_vals): # *init_val):
 
   def cond_fn(upper, lower, *init_vals):
     # init_val_compy = init_val.clone()
-    one_value1 = torch.tensor([1], dtype=torch.int32, device=device)
-    one_value2 = torch.tensor([1], dtype=torch.int32, device=device)
-    lower = torch.mul(lower, one_value1[0])
-    lower = torch.mul(lower, one_value2[0])
+    one_value1 = torch.tensor([0], dtype=torch.int32, device=device)
+    one_value2 = torch.tensor([0], dtype=torch.int32, device=device)
+    lower = torch.add(lower, 1) #one_value1[0])
+    lower = torch.sub(lower, one_value2[0])
     return lower[0] <= upper[0]
 
   def body_fn(upper, lower, *init_vals):
