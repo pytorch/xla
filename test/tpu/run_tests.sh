95075ed80016a084812950536af8be34603a7b22
Jiewen Tan <jwtan@google.com>
2024-03-09 05:02:26 +0000

    Add tpu ci

--- test/tpu/run_tests.sh
+++ test/tpu/run_tests.sh
@@ -18,3 +18,4 @@ python3 test/spmd/test_spmd_debugging.py
 python3 test/pjrt/test_dtypes.py
 python3 test/pjrt/test_dynamic_plugin_tpu.py
 python3 test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
+python3 test/test_pallas.py
