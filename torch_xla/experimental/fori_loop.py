ec7a5c51b548e59bd0746726b0fe59ab2fcbef51
manfeibaigithub <manfei@google.com>
2024-03-20 21:17:23 +0000

    test cond iter

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -23,19 +23,22 @@ def fori_loop(lower, upper, body_fun, *init_vals): # *init_val):
 
   def cond_fn(upper, lower, *init_vals):
     # init_val_compy = init_val.clone()
-    one_value1 = torch.tensor([0], dtype=torch.int32, device=device)
-    one_value2 = torch.tensor([0], dtype=torch.int32, device=device)
-    lower = torch.add(lower, one_value1[0])
-    lower = torch.sub(lower, one_value2[0])
+    # one_value1 = torch.tensor([0], dtype=torch.int32, device=device)
+    # one_value2 = torch.tensor([0], dtype=torch.int32, device=device)
+    # lower = torch.add(lower, one_value1[0])
+    # lower = torch.sub(lower, one_value2[0])
     return lower[0] <= upper[0]
 
   def body_fn(upper, lower, *init_vals):
     # one_value_original = torch.tensor(1, dtype=torch.int32, device=device)
     # (a, b) = init_vals
     # return (upper, torch.add(lower, 1), body_fun(a, b), b.clone())
-    return (upper, torch.add(lower, 1), (body_fun(*init_vals)).clone(), init_vals[1].clone()) # body_fun(one_value_original, init_val)) # body_fun(lower, init_val))
+    return (upper, torch.add(lower, 1), body_fun(*init_vals), init_vals[1]) # init_vals[1:])
+    # (body_fun(*init_vals)).clone(), init_vals[1].clone())
+    # body_fun(one_value_original, init_val)) # body_fun(lower, init_val))
 
   res = while_loop(cond_fn, body_fn, (upper, lower, *init_vals))
+  # res = _xla_while_loop(cond_fn, body_fn, (upper, lower, *init_vals))
   # print("upper: ", upper)
   # print("lower: ", lower)
   return res
