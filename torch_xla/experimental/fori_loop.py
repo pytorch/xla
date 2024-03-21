5c9ee66c10a533593d4245eb6e36e88e72fca039
manfeibaigithub <manfei@google.com>
2024-03-21 17:51:09 +0000

    test range

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -27,8 +27,8 @@ def fori_loop(lower, upper, body_fun, *init_vals): # *init_val):
     # one_value2 = torch.tensor([0], dtype=torch.int32, device=device)
     # lower = torch.add(lower, one_value1[0])
     # lower = torch.sub(lower, one_value2[0])
-    assert isinstance(init_vals[0], torch.int32)
-    assert isinstance(init_vals[1], torch.int32)
+    assert isinstance(init_vals[0], torch.Tensor)
+    assert isinstance(init_vals[1], torch.Tensor)
     return lower[0] <= upper[0]
 
   def body_fn(upper, lower, *init_vals):
