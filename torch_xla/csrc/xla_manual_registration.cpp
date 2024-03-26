be218314c07af83deb9dcbd6fbaba27fe69010d6
Yukio Siraichi <yukio.siraichi@gmail.com>
2024-03-26 16:51:19 -0300

    Skip functionalization when returning.

--- torch_xla/csrc/xla_manual_registration.cpp
+++ torch_xla/csrc/xla_manual_registration.cpp
@@ -25,7 +25,7 @@ at::Tensor nms_kernel(const at::Tensor& boxes, const at::Tensor& scores,
   XLATensorPtr xla_boxes = bridge::GetXlaTensor(boxes);
   XLATensorPtr xla_scores = bridge::GetXlaTensor(scores);
   return bridge::AtenFromXlaTensor(
-      tensor_methods::nms(xla_boxes, xla_scores, iou_threshold));
+      tensor_methods::nms(xla_boxes, xla_scores, iou_threshold), /*skip_functionalization=*/true);
 }
 
 }  // namespace
