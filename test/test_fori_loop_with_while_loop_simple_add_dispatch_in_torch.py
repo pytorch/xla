d0293199f3e106fa887f99375e0577cfc9e148fd
manfeibaigithub <manfei@google.com>
2024-03-27 23:10:49 +0000

    test

--- test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
+++ test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
@@ -82,7 +82,7 @@ class WhileLoopTest(unittest.TestCase):
     upper = torch.tensor([20], dtype=torch.int32, device=device)
     init_val = torch.tensor([1], dtype=torch.int32, device=device)
     # one_value = torch.tensor([1], dtype=torch.int32, device=device)
-    init_val_list = (init_val, one_value)
+    # init_val_list = (init_val, one_value)
     # lowers = torch.tensor(([1], [1], [1]), dtype=torch.int32, device=device) # lower, init_val, one_value
 
     def body_fun(a, b):
