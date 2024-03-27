eef62273da8bc41ff6296023a5bd5c76d81a84b7
manfeibaigithub <manfei@google.com>
2024-03-27 19:37:11 +0000

    test

--- test/cpp/test_xla_sharding.cpp
+++ test/cpp/test_xla_sharding.cpp
@@ -446,9 +446,9 @@ TEST_F(XLAShardingTest, TestForiLoop) {
   // Build simple addition with a sharded input.
   xla::XlaBuilder b("builder");
 //   b.SetSharding(tiled);
-  auto x = xla::Parameter(&b, 0, shape, "p0");
+  auto x = xla::Parameter(&b, /*parameter_number=*/0, shape, "p0");
   xla::Shape shape2 = xla::ShapeUtil::MakeShape(xla::PrimitiveType::F32, {});
-  auto zzz = xla::Parameter(&b, 0, shape2, "p1");
+  auto zzz = xla::Parameter(&b, /*parameter_number=*/1, shape2, "p1");
 //   b.ClearSharding();
   auto y = xla::Add(x, xla::ConstantR0<float>(&b, 3));
   xla::XlaComputation xla_computation =
