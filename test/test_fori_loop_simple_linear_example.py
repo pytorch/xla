7382f3ad6056e26c396bae951ed18f2867323479
manfeibaigithub <manfei@google.com>
2024-03-28 18:28:53 +0000

    test

--- test/test_fori_loop_simple_linear_example.py
+++ test/test_fori_loop_simple_linear_example.py
@@ -34,12 +34,13 @@ init_val = torch.tensor([1], dtype=torch.int32, device=device)
 #   linear = torch.nn.Linear(10, 20).to(xm.xla_device())
 #   # l_out = linear(l_in)
 #   return linear(l_in) # torch.add(a, b) # [0])
+linear_0 = torch.nn.Linear(10, 20).to(xm.xla_device())
 
 def body_fun(y, x, l_in_i):
   # l_in = torch.randn(10, device=xm.xla_device())
-  linear_0 = torch.nn.Linear(10, 20).to(xm.xla_device())
+  # linear_0 = torch.nn.Linear(10, 20).to(xm.xla_device())
   # l_out = linear(l_in)
-  return torch.add(y, x), linear_0(l_in_i) # linear(l_in) # torch.add(a, b) # [0])
+  return torch.add(y, x), linear_0(l_in_i), linear_0, l_in_i # additional return: body and input-placeholder   # linear(l_in) # torch.add(a, b) # [0])
 
 # TODO(@manfei), need to create new variable to seperate old/formal HLO/IR
 l_in_0 = torch.randn(10, device=xm.xla_device())
@@ -50,7 +51,7 @@ l_in_0 = torch.randn(10, device=xm.xla_device())
 #   # l_out = linear(l_in)
 #   return torch.add(x, y), linear(l_in) # linear(l_in) # torch.add(a, b) # [0])
 
-lower_, upper_, res_ = fori_loop(upper, lower, body_fun, one_value, init_val, l_in_0)
+lower_, upper_, res_ = fori_loop(upper, lower, body_fun, one_value, init_val, l_in_0, linear_0, l_in_0)
 
 print("lower_: ", lower_)
 print("upper_: ", upper_)
