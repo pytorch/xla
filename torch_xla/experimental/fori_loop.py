5f780f5490d54688790f798328b9624240b78865
manfeibaigithub <manfei@google.com>
2024-03-28 00:56:16 +0000

    test

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -24,7 +24,9 @@ def fori_loop(lower, upper, body_fun, one_value, init_val):
   device = xm.xla_device()
 
   # upper, lower, one_value, init_val
-  def cond_fn(upper, lower, one_value, init_val): #one_value, lower, upper, init_val): # loop_carry): # iter, upper, one_value): # lower, *init_vals):
+  # real(ov, lower, upper, x)
+  # def cond_fn(upper, lower, one_value, init_val): #one_value, lower, upper, init_val): # loop_carry): # iter, upper, one_value): # lower, *init_vals):
+  def cond_fn(one_value, lower, upper, init_val): 
     # lower, upper, one_value, init_val = loop_carry
     lower_compare = torch.add(lower, one_value)
     upper_compare = torch.add(upper, one_value)
@@ -32,7 +34,8 @@ def fori_loop(lower, upper, body_fun, one_value, init_val):
     return lower_compare[0] <= upper_compare[0]
 
   # def body_fn(upper, lowers): # , *init_vals):
-  def body_fn(upper, lower, one_value, init_val): # one_value, lower, upper, init_val): # loop_carry): # iter, upper, one_value):
+  # def body_fn(upper, lower, one_value, init_val): # one_value, lower, upper, init_val): # loop_carry): # iter, upper, one_value):
+  def body_fn(one_value, lower, upper, init_val):
     # lower, upper, one_value, init_val = loop_carry
     # return (torch.add(iter, one_value).clone(), upper.clone(), one_value.clone(), body_fun(x, one_value).clone())
     # one_value = torch.tensor([1], dtype=torch.int32, device=device)
@@ -40,8 +43,9 @@ def fori_loop(lower, upper, body_fun, one_value, init_val):
     # new_lower = torch.add(lower, one_value)
     new_init_val = body_fun(init_val, one_value)
     # return (new_lower, upper, one_value, new_init_val)
-    return (new_upper, lower, one_value, new_init_val) # one_value, lower, new_upper, new_init_val)
+    # return (new_upper, lower, one_value, new_init_val) # one_value, lower, new_upper, new_init_val)
     # return (upper, new_lower, one_value, new_init_val)
+    return (one_value, lower, new_upper, new_init_val)
 
   # loop_carruy_print = (lower, upper, one_value, init_val)
   # print("loop_carruy_print[0]: ", loop_carruy_print[0]) # tensor([1], device='xla:0', dtype=torch.int32)
