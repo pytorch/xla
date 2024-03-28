518aeae7e063b86519c2caa2cd4e9d9ed04ea460
manfeibaigithub <manfei@google.com>
2024-03-28 07:22:56 +0000

    test

--- test/test_fori_loop_simple_linear_example.py
+++ test/test_fori_loop_simple_linear_example.py
@@ -13,69 +13,74 @@ import torch_xla.core.xla_model as xm
 
 device = xm.xla_device()
 
-import numpy as np
-# create dummy data for training
-x_values = [i for i in range(11)]
-x_train = np.array(x_values, dtype=np.float32)
-x_train = x_train.reshape(-1, 1)
+l_in = torch.randn(10, device=xm.xla_device())
+linear = torch.nn.Linear(10, 20).to(xm.xla_device())
+l_out = linear(l_in)
+print(l_out)
 
-y_values = [2*i + 1 for i in x_values]
-y_train = np.array(y_values, dtype=np.float32)
-y_train = y_train.reshape(-1, 1)
+# import numpy as np
+# # create dummy data for training
+# x_values = [i for i in range(11)]
+# x_train = np.array(x_values, dtype=np.float32)
+# x_train = x_train.reshape(-1, 1)
 
-# import torch
-from torch.autograd import Variable
+# y_values = [2*i + 1 for i in x_values]
+# y_train = np.array(y_values, dtype=np.float32)
+# y_train = y_train.reshape(-1, 1)
 
-class linearRegression(torch.nn.Module):
-    def __init__(self, inputSize, outputSize):
-        super(linearRegression, self).__init__()
-        self.linear = torch.nn.Linear(inputSize, outputSize)
+# # import torch
+# from torch.autograd import Variable
 
-    def forward(self, x):
-        out = self.linear(x)
-        return out
+# class linearRegression(torch.nn.Module):
+#     def __init__(self, inputSize, outputSize):
+#         super(linearRegression, self).__init__()
+#         self.linear = torch.nn.Linear(inputSize, outputSize)
 
-# --- training ---
-inputDim = 1        # takes variable 'x' 
-outputDim = 1       # takes variable 'y'
-learningRate = 0.01 * xm.xrt_world_size()
-epochs = 10 # 100
+#     def forward(self, x):
+#         out = self.linear(x)
+#         return out
 
-model = linearRegression(inputDim, outputDim).to(device)
-# model = MNIST().to(device)
-##### For GPU #######
-if torch.cuda.is_available():
-    model.cuda()
+# # --- training ---
+# inputDim = 1        # takes variable 'x' 
+# outputDim = 1       # takes variable 'y'
+# learningRate = 0.01 * xm.xrt_world_size()
+# epochs = 10 # 100
 
-criterion = torch.nn.MSELoss() 
-optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
+# model = linearRegression(inputDim, outputDim).to(device)
+# # model = MNIST().to(device)
+# ##### For GPU #######
+# if torch.cuda.is_available():
+#     model.cuda()
 
-for epoch in range(epochs):
-    # Converting inputs and labels to Variable
-    if torch.cuda.is_available():
-        inputs = Variable(torch.from_numpy(x_train).cuda())
-        labels = Variable(torch.from_numpy(y_train).cuda())
-    else:
-        inputs = Variable(torch.from_numpy(x_train))
-        labels = Variable(torch.from_numpy(y_train))
+# criterion = torch.nn.MSELoss() 
+# optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
 
-    # Clear gradient buffers because we don't want any gradient from previous epoch to carry forward, dont want to cummulate gradients
-    optimizer.zero_grad()
+# for epoch in range(epochs):
+#     # Converting inputs and labels to Variable
+#     if torch.cuda.is_available():
+#         inputs = Variable(torch.from_numpy(x_train).cuda())
+#         labels = Variable(torch.from_numpy(y_train).cuda())
+#     else:
+#         inputs = Variable(torch.from_numpy(x_train))
+#         labels = Variable(torch.from_numpy(y_train))
 
-    # get output from the model, given the inputs
-    outputs = model(inputs)
+#     # Clear gradient buffers because we don't want any gradient from previous epoch to carry forward, dont want to cummulate gradients
+#     optimizer.zero_grad()
 
-    # get loss for the predicted output
-    loss = criterion(outputs, labels)
-    print(loss)
-    # get gradients w.r.t to parameters
-    loss.backward()
+#     # get output from the model, given the inputs
+#     outputs = model(inputs)
 
-    # update parameters
-    # optimizer.step()
-    xm.optimizer_step(optimizer)
+#     # get loss for the predicted output
+#     loss = criterion(outputs, labels)
+#     print(loss)
+#     # get gradients w.r.t to parameters
+#     loss.backward()
 
-    print('epoch {}, loss {}'.format(epoch, loss.item()))
+#     # update parameters
+#     # optimizer.step()
+#     xm.optimizer_step(optimizer)
+
+#     print('epoch {}, loss {}'.format(epoch, loss.item()))
 
 # --- while simple test case ---
 
@@ -96,22 +101,22 @@ print("upper_: ", upper_)
 print("res_: ", res_)
 
 # --- test ---
-for epoch in range(epochs):
-    with torch.no_grad(): # we don't need gradients in the testing phase
-        if torch.cuda.is_available():
-            predicted = model(Variable(torch.from_numpy(x_train).cuda())).cpu().data.numpy()
-        else:
-            predicted = model(Variable(torch.from_numpy(x_train))).data.numpy()
-        print(epoch, "-th prediction finised") # ed result: ", predicted)
+# for epoch in range(epochs):
+#     with torch.no_grad(): # we don't need gradients in the testing phase
+#         if torch.cuda.is_available():
+#             predicted = model(Variable(torch.from_numpy(x_train).cuda())).cpu().data.numpy()
+#         else:
+#             predicted = model(Variable(torch.from_numpy(x_train))).data.numpy()
+#         print(epoch, "-th prediction finised") # ed result: ", predicted)
 
-print("do one more prediction")
-with torch.no_grad(): # we don't need gradients in the testing phase
-    if torch.cuda.is_available():
-        predicted = model(Variable(torch.from_numpy(x_train).cuda())).cpu().data.numpy()
-    else:
-        predicted = model(Variable(torch.from_numpy(x_train))).data.numpy()
-    print(predicted)
-print("finished one more prediction")
+# print("do one more prediction")
+# with torch.no_grad(): # we don't need gradients in the testing phase
+#     if torch.cuda.is_available():
+#         predicted = model(Variable(torch.from_numpy(x_train).cuda())).cpu().data.numpy()
+#     else:
+#         predicted = model(Variable(torch.from_numpy(x_train))).data.numpy()
+#     print(predicted)
+# print("finished one more prediction")
 
 # --- draw ---
 # plt.clf()
