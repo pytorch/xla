0ddc043431c118372e356e6deabae69e99d7d97d
manfeibaigithub <manfei@google.com>
2024-03-28 16:16:50 +0000

    test

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -24,14 +24,14 @@ def fori_loop(lower, upper, body_fun, one_value, init_val):
   device = xm.xla_device()
 
   # def cond_fn(lower, upper, x):
-  def cond_fn(upper, lower, x):
+  def cond_fn(upper, lower, *x):
     return lower[0] < upper[0]
 
   # def body_fn(lower, upper, x):
-  def body_fn(upper, lower, x):
+  def body_fn(upper, lower, *x):
     one_value = torch.ones(1, dtype=torch.int32, device=device)
     # two_value = upper.clone()
-    return (torch.sub(upper, one_value), lower, body_fun(one_value, x)) # , one_value))
+    return (torch.sub(upper, one_value), lower, body_fun(one_value, *x)) # , one_value))
 
   # upper, lower, one_value, init_val
   # real(ov, lower, upper, x)
