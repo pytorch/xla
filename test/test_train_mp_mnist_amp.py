5f858e7b88088f865e7a98b646ee30792da7a5ff
iefgnoix <isaacwxf23@gmail.com>
2024-03-29 12:39:44 -0700

    Re-enable AMP test on GPU CI. (#6790)

--- test/test_train_mp_mnist_amp.py
+++ test/test_train_mp_mnist_amp.py
@@ -170,16 +170,23 @@ def train_mnist(flags, **kwargs):
       if step % flags.log_steps == 0:
         xm.add_step_closure(
             _train_update, args=(device, step, loss, tracker, writer))
+      if FLAGS.num_steps and FLAGS.num_steps == step:
+        break
 
   def test_loop_fn(loader):
     total_samples = 0
     correct = 0
     model.eval()
-    for data, target in loader:
+    for step, (data, target) in enumerate(loader):
       output = model(data)
       pred = output.max(1, keepdim=True)[1]
       correct += pred.eq(target.view_as(pred)).sum()
       total_samples += data.size()[0]
+      if step % FLAGS.log_steps == 0:
+        xm.add_step_closure(
+            test_utils.print_test_update, args=(device, None, epoch, step))
+      if FLAGS.num_steps and FLAGS.num_steps == step:
+        break
 
     accuracy = 100.0 * correct.item() / total_samples
     accuracy = xm.mesh_reduce('test_accuracy', accuracy, np.mean)
