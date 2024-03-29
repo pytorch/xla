317cbb52a2f46209df84546cc799a82cfe1d08cb
manfeibaigithub <manfei@google.com>
2024-03-29 05:10:34 +0000

    test

--- test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
+++ test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
@@ -75,50 +75,15 @@ class WhileLoopTest(unittest.TestCase):
     xm.mark_step()
     device = xm.xla_device()
 
-    # TODO(@manfei): lower, upper and init_val has to be torch.tensor.
-    # init_val = torch.tensor([1], dtype=torch.int32, device=device)
-    # one_value = torch.tensor([1], dtype=torch.int32, device=device)
     lower = torch.tensor([2], dtype=torch.int32, device=device)
     upper = torch.tensor([52], dtype=torch.int32, device=device)
     one_value = torch.tensor([1], dtype=torch.int32, device=device)
     init_val = torch.tensor([1], dtype=torch.int32, device=device)
-    # one_value = torch.tensor([1], dtype=torch.int32, device=device)
-    # init_val_list = (init_val, one_value)
-    # lowers = torch.tensor(([1], [1], [1]), dtype=torch.int32, device=device) # lower, init_val, one_value
 
     def body_fun(a, b):
-      return torch.add(a, b) # [0])
-    # _, _, res, _ = fori_loop(lower, upper, body_fun, init_val, one_value) # init_val_list) # init_val)
-    # A, B, res, D = fori_loop(lower, upper, body_fun, init_val, one_value) # init_val_list) # init_val)
-    # A, B, res, D = fori_loop(upper, body_fun, lowers) # lower, upper, body_fun, init_val, one_value)
-    # iter, upper, one_value, x
-    # iter_, upper_, res, one_value_ = fori_loop(lower, upper, body_fun, init_val, one_value)
-    # lower_, upper_, one_value_, res = fori_loop(lower, upper, body_fun, one_value, init_val)
-    # upper_, lower_, one_value_, res = fori_loop(upper, lower, body_fun, one_value, init_val)
-    # one_value, lower, upper, init_val
-    # one_value_, lower_, upper_, res_ = fori_loop(upper, lower, body_fun, one_value, init_val)
-    # upper, lower, one_value, init_val
-    # real(ov, lower, upper, x)
-    # one_value_, lower_, upper_, res_ = fori_loop(upper, lower, body_fun, one_value, init_val)
+      return torch.add(a, b)
+
     lower_, upper_, res_ = fori_loop(upper, lower, body_fun, one_value, init_val)
-    # upper, lower, init_val
-    # upper_, lower_, res_ = fori_loop(upper, lower, body_fun, init_val)
-    # print("one_value_: ", one_value_)
-    print("lower_: ", lower_)
-    print("upper_: ", upper_)
-    # print("one_value_: ", one_value_)
-    print("res_: ", res_)
-    # print("iter_: ", iter_)
-    # print("upper_: ", upper_)
-    # print("res: ", res)
-    # print("one_value_: ", one_value_)
-    # print("A: ", A) # lower_
-    # print("B: ", B) # upper_
-    # print("D: ", D) # one_value_
-    # print("lower[0] <= upper[0]: ", lower[0] <= upper[0])
-    # print("lower: ", lower)
-    # print("upper: ", upper)
-    # fori_loop(cond_fn, body_fn, (init, limit_value))
     expected = _fake_fori_loop(lower, upper, body_fun, init_val, one_value)
     print("expected: ", expected)
     self.assertEqual(expected, res_)
