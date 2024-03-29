5226c94c549d8496a7f3b17fcfa6fe7da3cfd908
manfeibaigithub <manfei@google.com>
2024-03-29 08:19:05 +0000

    test

--- test/test_fori_loop_simple_linear_example.py
+++ test/test_fori_loop_simple_linear_example.py
@@ -41,9 +41,10 @@ def body_fun(y, x, l_in_i):
   # l_in = torch.randn(10, device=xm.xla_device())
   # linear_0 = torch.nn.Linear(10, 20).to(xm.xla_device())
   l_out = linear_0(l_in_i)
-  placeholder_func = torch.rand(size = l_out.size(), device = device)
-  placeholder_input = torch.rand(size = l_in_i.size(), device = device)
-  return torch.add(y, x), l_out, placeholder_func, placeholder_input # linear_0(l_in_i), linear_0, l_in_i # additional return: body and input-placeholder   # linear(l_in) # torch.add(a, b) # [0])
+  # placeholder_func = torch.rand(size = l_out.size(), device = device)
+  # placeholder_input = torch.rand(size = l_in_i.size(), device = device)
+  # return torch.add(y, x), l_out, placeholder_func, placeholder_input # linear_0(l_in_i), linear_0, l_in_i # additional return: body and input-placeholder   # linear(l_in) # torch.add(a, b) # [0])
+  return torch.add(y, x), l_out
 
 # TODO(@manfei), need to create new variable to seperate old/formal HLO/IR
 l_in_0 = torch.randn(10, device=xm.xla_device())
