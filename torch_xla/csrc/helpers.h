13a2169f4ba9d63511a7162df2248b8d8a9d4a7f
Siyuan Liu <lsiyuan@google.com>
2024-03-29 12:49:18 -0700

    [r2.3] Use AddBufferDonor to replace SetUpAlias for input output aliasing (#6855)
    Co-authored-by: JackCaoG <59073027+JackCaoG@users.noreply.github.com>

--- torch_xla/csrc/helpers.h
+++ torch_xla/csrc/helpers.h
@@ -385,7 +385,6 @@ class XlaHelpers {
   static xla::StatusOr<xla::XlaComputation> WrapXlaComputation(
       const xla::XlaComputation& computation,
       const std::vector<xla::Shape>& parameter_shapes,
-      const std::vector<std::pair<int64_t, int64_t>>& input_output_alias_pair,
       const std::vector<size_t>& buffer_donor_indices);
 
   static torch::lazy::Shape ConvertXlaShapeToLazy(const xla::Shape& shape);
