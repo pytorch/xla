2828f0e00478b7ddc129f7695467f8ffc7845035
manfeibaigithub <manfei@google.com>
2024-03-29 17:48:16 +0000

    test formal change

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -12,45 +12,35 @@ import torch._higher_order_ops.while_loop
 from torch._higher_order_ops.while_loop import while_loop_op
 
 
-def fori_loop(lower, upper, body_fun, one_value, init_val):
+# TODO(@manfei): delete one_value?
+def fori_loop(upper, lower, body_fun, init_val, *input_value):
 
   device = xm.xla_device()
+  # weight_0 = body_fun.weight
+  # bias_0 = body_fun.bias
+  # one_value = torch.tensor([1], dtype=torch.int32, device=device)
 
-  def cond_fn(upper, lower, x):
+  def cond_fn(upper, lower, one_value, x, *input_value, weight_0, output_value, bias_0): # , bias_0): # , output_value):
     return lower[0] < upper[0]
 
-  def body_fn(upper, lower, x):
-    one_value = torch.ones(1, dtype=torch.int32, device=device)
-    return (torch.sub(upper, one_value), lower, body_fun(one_value, x))
+  def body_fn(upper, lower, one_value, x, *input_value, weight_0, output_value, bias_0): # , bias_0):
+    # weight = body_fun.weight
+    new_lower = torch.add(one_value, lower) ### !!! this matter, torch.add might would change the second argument's value, even we use a new variable to catch the result!!!
+    output_value = body_fun(*input_value) ### !!! due to the output_value is not actually used here, 
+    # --- !!! its original value would not be used, and it would be replaces by the result of body_fun
+    # --- !!! so, due to PTXLA is traced from result tensor, so the arguments `output_value` would not be included in the body_xlacomputation
+    # --- !!! so, we need to modify ini_python_binding.cpp to add a fake arguments in the xlacompputation
+    weight = body_fun.weight
+    bias = body_fun.bias
+    return upper, new_lower, one_value, torch.add(one_value, x), *input_value, weight, bias, output_value
 
-  def old_cond_fn(one_value, lower, upper, init_val):
-    lower_compare = torch.add(lower, one_value)
-    return lower_compare[0] <= upper[0]
-
-  def old_body_fn(one_value, lower, upper, init_val):
-    new_lower = torch.add(lower, one_value)
-    new_init_val = body_fun(init_val, one_value)
-    return (one_value, new_lower, upper, new_init_val)
-
-  res = _xla_while_loop(cond_fn, body_fn, lower, upper, init_val)
+  output_value = torch.zeros([20], dtype=torch.float32, device=device)
+  weight_0 = body_fun.weight
+  bias_0 = body_fun.bias
+  one_value = torch.tensor([1], dtype=torch.int32, device=device)
+  res = while_loop(cond_fn, body_fn, (upper, lower, one_value, init_val, *input_value, weight_0, bias_0, output_value))
   return res
 
-def fori_loop(lower, upper, user_body_func, *init_val):
-
-  device = xm.xla_device()
-
-  def cond_fn(upper, lower, *init_val):
-    return lower[0] < upper[0]
-
-  def body_fn(upper, lower, *init_val):
-    one_value_i = torch.ones(1, dtype=torch.int32, device=device)
-    res_list = list(user_body_func(*init_val))
-    res_list.insert(0, lower)
-    res_list.insert(0, torch.sub(upper, one_value_i))
-    return res_list
-
-  res = while_loop(cond_fn, body_fn, (lower, upper, *init_val))
-  return res
 
 @while_loop_op.py_impl(DispatchKey.XLA)
 def while_loop(cond_fn, body_fn, *carried_inputs, additional_inputs=None):
@@ -59,8 +49,7 @@ def while_loop(cond_fn, body_fn, *carried_inputs, additional_inputs=None):
   # carried_inputs: (Tuple of possibly nested dict/list/tuple of tensors)
   if additional_inputs is None:
     additional_inputs = tuple()
-  return _xla_while_loop(
-      cond_fn, body_fn, *carried_inputs, additional_inputs=additional_inputs)
+  return _xla_while_loop(cond_fn, body_fn, *carried_inputs, additional_inputs=additional_inputs) #  a=a, b=b, c=c,
 
 
 def _xla_while_loop(cond_fn, body_fn, *carried_inputs, additional_inputs):
@@ -70,40 +59,45 @@ def _xla_while_loop(cond_fn, body_fn, *carried_inputs, additional_inputs):
   fake_carried_inputs = []
   for carried_input in carried_inputs:
     device = carried_input.device
+    #TODO(@manfei) type = carried_input.type
     fake_carried_inputs.append(
         torch.randint(10, carried_input.size(),
                       dtype=carried_input.dtype).to(device))
   fake_carried_inputs = tuple(fake_carried_inputs)
 
-  # trans fake_carried_inputs from list(tensor) to list(xla::op)
-  kwargs = {}
-  if type(fake_carried_inputs) is tuple:
-    shapes = xb.tensor_shape(fake_carried_inputs)
-  else:
-    shapes = xb.tensor_shape((fake_carried_inputs))
-  builder = xb.create_builder('test_while')
-  params = []
-  for shape in shapes:
-    p = xb.mkparam(builder, len(params), shape)
-    params.append(p)
-
   # generate cond_fn xlacomputation
-  cond_result = cond_fn(*fake_carried_inputs)
+  # TODO(@manfei): specify which element is for which argument like a,b,c
+  cond_result = cond_fn(*fake_carried_inputs[:-3], weight_0=fake_carried_inputs[-3], output_value=fake_carried_inputs[-2], bias_0=fake_carried_inputs[-1])
   cond_ctx = torch_xla._XLAC.lowering.LoweringContext()
   cond_ctx.set_name_string("condctx")
-  cond_ctx.buildforiloop([cond_result], list(fake_carried_inputs[2:]))
+  additional_inputs_list = list(fake_carried_inputs[2:])
+  for i in range(len(additional_inputs)):
+    additional_inputs_list.append(additional_inputs[0])
+  cond_ctx.buildforiloop([cond_result], additional_inputs_list)
   cond_hlo = cond_ctx.hlo()
   cond_computation = xb.computation_from_module_proto("condcomputation",
                                                       cond_hlo)
 
   # generate body_fn xlacomputation
-  body_result = body_fn(*fake_carried_inputs)
+  body_result = body_fn(*fake_carried_inputs[:-3], weight_0=fake_carried_inputs[-3], output_value=fake_carried_inputs[-2], bias_0=fake_carried_inputs[-1])
   body_ctx = torch_xla._XLAC.lowering.LoweringContext()
   body_ctx.set_name_string("bodyctx")
   body_ctx.buildforiloop(list(body_result), [])
   body_hlo = body_ctx.hlo()
   body_computation = xb.computation_from_module_proto("bodycomputation",
                                                       body_hlo)
+
+  # trans fake_carried_inputs from list(tensor) to list(xla::op), which part could change init of xla::while
+  kwargs = {}
+  if type(carried_inputs) is tuple:
+    shapes = xb.tensor_shape(carried_inputs)
+  else:
+    shapes = xb.tensor_shape((carried_inputs))
+  builder = xb.create_builder('test_while')
+  params = []
+  for shape in shapes:
+    p = xb.mkparam(builder, len(params), shape)
+    params.append(p)
 
   # generate while xlacomputation
   input_tuple = xb.Op.tuple(tuple(params))
@@ -116,6 +110,7 @@ def _xla_while_loop(cond_fn, body_fn, *carried_inputs, additional_inputs):
 
   # gain final result with generated while xlacomputation
   result = torch_xla._XLAC._xla_user_computation('xla::_op_test_while',
-                                                 (carried_inputs), computation)
+                                                 (carried_inputs),
+                                                 computation)
 
   return result
