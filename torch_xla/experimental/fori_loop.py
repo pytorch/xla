b93dff34079e53c01f270814cbc950d2ab1d0fbb
manfeibaigithub <manfei@google.com>
2024-04-01 21:02:56 +0000

    test linear

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -143,6 +143,7 @@ def while_loop(cond_fn, body_fn, operands):
 def _xla_while_loop(cond_fn, body_fn, *original_operands):
   print("!!! arguments: original_operands: ", original_operands)
   # fake operands to split formal code
+  mid_original_operands = original_operands[0]
   operands = [] # fake_operands
   for original_operand in original_operands:
     device = original_operand.device
