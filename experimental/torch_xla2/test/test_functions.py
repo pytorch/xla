3c1fc33ebbe373585b9e442d5229055b33da8946
Will Cromar <wcromar@google.com>
2024-04-03 23:25:38 +0000

    restructure test

--- experimental/torch_xla2/test/test_functions.py
+++ experimental/torch_xla2/test/test_functions.py
@@ -1,3 +1,5 @@
+import functools
+from typing import Callable
 from absl.testing import absltest
 from absl.testing import parameterized
 import torch
@@ -6,20 +8,21 @@ import torch_xla2.functions
 import torch_xla2.tensor
 
 class TestTorchFunctions(parameterized.TestCase):
-  @parameterized.parameters(
-    [([[0.1, 1.2], [2.2, 3.1], [4.9, 5.2]],)],
-    [([0, 1],)],
-    [(3.14159,)],
-    [([],)],
-  )
-  def test_tensor(self, args):
-    expected = torch.tensor(*args)
+  @parameterized.named_parameters([
+    ('tensor', lambda: torch.tensor([[0.1, 1.2], [2.2, 3.1], [4.9, 5.2]])),
+    ('tensor_1d', lambda: torch.tensor([0, 1],)),
+    ('tensor_scalar', lambda: torch.tensor(3.14159,)),
+    ('tensor_empty', lambda: torch.tensor([],)),
+  ])
+  def test_tensor_constructor(self, func: Callable[[], torch.Tensor]):
+    expected = func()
 
     with torch_xla2.functions.XLAFunctionMode():
-      actual = torch.tensor(*args)
+      actual = func()
+      self.assertIsInstance(actual, torch_xla2.tensor.XLATensor2)
 
     # TODO: dtype is actually important
-    torch.testing.assert_close(torch_xla2.tensor.j2t(actual), expected, check_dtype=False)
+    torch.testing.assert_close(torch_xla2.tensor.j2t(actual._elem), expected, check_dtype=False)
 
 
 if __name__ == '__main__':
