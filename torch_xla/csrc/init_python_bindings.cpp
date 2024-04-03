fb11d8392ce5abee918568230a7c31346a538fb5
manfeibaigithub <manfei@google.com>
2024-04-03 06:51:21 +0000

    test

--- torch_xla/csrc/init_python_bindings.cpp
+++ torch_xla/csrc/init_python_bindings.cpp
@@ -897,13 +897,13 @@ class PyLoweringContext {
       xla::XlaBuilder* local_builder = lowering_ctx.builder();
       int64_t parameters_number_i = 2;
       // for (at::Tensor input_argument : input_arguments) {
-      for (int i = 0; i < 2; i++) {
-        xla::Shape shape =
-            xla::ShapeUtil::MakeShape(xla::PrimitiveType::S32, {1});
-        xla::XlaOp x = xla::Parameter(local_builder, parameters_number_i, shape,
-                                      "UnusedArgumentsPlaceholder");
-        parameters_number_i = parameters_number_i + 1;
-      }
+      // for (int i = 0; i < 2; i++) {
+      //   xla::Shape shape =
+      //       xla::ShapeUtil::MakeShape(xla::PrimitiveType::S32, {1});
+      //   xla::XlaOp x = xla::Parameter(local_builder, parameters_number_i, shape,
+      //                                 "UnusedArgumentsPlaceholder");
+      //   parameters_number_i = parameters_number_i + 1;
+      // }
       // hard-code to meet requirement
       // f32[20], /*index=5*/f32[20,10], s32[10]
       parameters_number_i = parameters_number_i + 1;
