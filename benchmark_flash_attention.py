a0bff127582a2bd955d340c13145818d274e5d9f
Jiewen Tan <jwtan@google.com>
2024-04-04 23:22:26 +0000

    Improve a bit

--- benchmark_flash_attention.py
+++ benchmark_flash_attention.py
@@ -7,10 +7,10 @@ import torch_xla
 import torch_xla.core.xla_model as xm
 import torch_xla.debug.profiler as xp
 import torch_xla.debug.metrics as met
+
 device = xm.xla_device()
-
 server = xp.start_server(9012)
-
+torch.set_default_dtype(torch.bfloat16)
 
 @xp.trace_me("Eager Attention")
 def attention(q, k, v):
@@ -20,28 +20,16 @@ def attention(q, k, v):
   return attn_output
 
 
-def time_execution(seq_len, func):
-  # Let's make sure the q, k, v are completely isolated from last run.
-  # Simulates 70B Llama 2
-  # bs, num_heads, seq_len, head_dim
-  shape = (2, 64, seq_len, 128)
-  q = torch.randn(shape, dtype=torch.bfloat16).to(device)
-  k = torch.randn(shape, dtype=torch.bfloat16).to(device)
-  v = torch.randn(shape, dtype=torch.bfloat16).to(device)
-  # We can't do the following in the cpu tensors. Otherwise they will end up in
-  # the autograd graph.
-  q.requires_grad = True
-  k.requires_grad = True
-  v.requires_grad = True
-  q.retain_grad()
-  k.retain_grad()
-  v.retain_grad()
+def time_execution(q, k, v, func):
+  start_time = time.time()
+  q.grad = None
+  k.grad = None
+  v.grad = None
 
   o = func(q, k, v)
   loss = nn.MSELoss()(o, torch.ones_like(o))
   loss.backward()
   xm.mark_step()
-  start_time = time.time()
   xm.wait_device_ops()
   end_time = time.time()
   return end_time - start_time
@@ -64,18 +52,34 @@ def shape_fn(q, k, v):
 
 @xp.trace_me("Flash Attention")
 def flash_attention_kernel(q, k, v):
-  return flash_attention(q, k, v)
+  return flash_attention(q, k, v, causal=False)
 
 
-xp.trace_detached('localhost:9012', '.', 600000)
+# xp.trace_detached('localhost:9012', '.', 120000)
 
 for seq_len in [1024, 2048, 4096, 8192, 16384]:
   print(f"seq_len: {seq_len}")
 
-  if seq_len < 8192:
-    repeat_n(lambda: time_execution(seq_len, attention))
+  # Let's make sure the q, k, v are completely isolated from last run.
+  # Simulates 70B Llama 2
+  # bs, num_heads, seq_len, head_dim
+  shape = (2, 64, seq_len, 128)
+  q = torch.randn(shape).to(device)
+  k = torch.randn(shape).to(device)
+  v = torch.randn(shape).to(device)
+  # We can't do the following in the cpu tensors. Otherwise they will end up in
+  # the autograd graph.
+  q.requires_grad = True
+  k.requires_grad = True
+  v.requires_grad = True
+  q.retain_grad()
+  k.retain_grad()
+  v.retain_grad()
 
-  repeat_n(lambda: time_execution(seq_len, flash_attention_kernel))
+  if seq_len < 8192:
+    repeat_n(lambda: time_execution(q, k, v, attention))
+
+  repeat_n(lambda: time_execution(q, k, v, flash_attention_kernel))
 
 # print(met.metrics_report())
 # o = flash_attention_kernel(q, k, v)
