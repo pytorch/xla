75b5c5583f7b160ef0519347ad91f3aaad6f59b2
manfeibaigithub <manfei@google.com>
2024-04-12 23:48:12 +0000

    update

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -68,7 +68,7 @@ def _xla_while_loop(cond_fn, body_fn, *original_carried_inputs, additional_input
         torch.randint(10, carried_input.size(),
                       dtype=carried_input.dtype).to(device))
   # fake_carried_inputs = tuple(fake_carried_inputs)
-  print("fake_carried_inputs first: ", fake_carried_inputs)
+  # print("fake_carried_inputs first: ", fake_carried_inputs)
   for additional_input in additional_inputs:
     device = additional_input.device
     #TODO(@manfei) type = carried_input.type
@@ -76,7 +76,7 @@ def _xla_while_loop(cond_fn, body_fn, *original_carried_inputs, additional_input
         torch.randint(10, additional_input.size(),
                       dtype=additional_input.dtype).to(device))
   fake_carried_inputs = tuple(fake_carried_inputs)
-  print("fake_carried_inputs second: ", fake_carried_inputs)
+  # print("fake_carried_inputs second: ", fake_carried_inputs)
 
   # generate cond_fn xlacomputation
   # TODO(@manfei): specify which element is for which argument like a,b,c
