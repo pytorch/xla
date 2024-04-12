349ed260f87d14b6199fd732fde90797701ce0b3
manfeibaigithub <manfei@google.com>
2024-04-12 06:07:34 +0000

    update

--- torch_xla/csrc/init_python_bindings.cpp
+++ torch_xla/csrc/init_python_bindings.cpp
@@ -936,28 +936,28 @@ class PyLoweringContext {
         // xtensor->shape().get().ToString()
         // xla_tensor->shaped_buffer().on_device_shape();
       }
-      for (int i = 0; i < 2; i++) {
-        xla::Shape shape =
-            xla::ShapeUtil::MakeShape(xla::PrimitiveType::S32, {1});
-        xla::XlaOp x = xla::Parameter(local_builder, parameter_idx, shape,
-                                      "UnusedArgumentsPlaceholder");
-        parameter_idx += 1;
-      }
-      xla::Shape shape1 = xla::ShapeUtil::MakeShape(xla::PrimitiveType::F32, {10});
-      xla::XlaOp x1 = xla::Parameter(local_builder, parameter_idx, shape1,
-                                      "LInITensor");
-      parameter_idx = parameter_idx + 1;
-      xla::Shape shape2 = xla::ShapeUtil::MakeShape(xla::PrimitiveType::F32, {20, 10});
-      xla::XlaOp x2 = xla::Parameter(local_builder, parameter_idx, shape2,
-                                      "WeightTensor");
-      parameter_idx = parameter_idx + 1;
-      xla::Shape shape4 = xla::ShapeUtil::MakeShape(xla::PrimitiveType::F32, {20});
-      xla::XlaOp x4 = xla::Parameter(local_builder, parameter_idx, shape4,
-                                      "BiasTensor");
-      parameter_idx = parameter_idx + 1;
-      xla::Shape shape3 = xla::ShapeUtil::MakeShape(xla::PrimitiveType::F32, {20});
-      xla::XlaOp x3 = xla::Parameter(local_builder, parameter_idx, shape3,
-                                      "LOutTensor");
+      // for (int i = 0; i < 2; i++) {
+      //   xla::Shape shape =
+      //       xla::ShapeUtil::MakeShape(xla::PrimitiveType::S32, {1});
+      //   xla::XlaOp x = xla::Parameter(local_builder, parameter_idx, shape,
+      //                                 "UnusedArgumentsPlaceholder");
+      //   parameter_idx += 1;
+      // }
+      // xla::Shape shape1 = xla::ShapeUtil::MakeShape(xla::PrimitiveType::F32, {10});
+      // xla::XlaOp x1 = xla::Parameter(local_builder, parameter_idx, shape1,
+      //                                 "LInITensor");
+      // parameter_idx = parameter_idx + 1;
+      // xla::Shape shape2 = xla::ShapeUtil::MakeShape(xla::PrimitiveType::F32, {20, 10});
+      // xla::XlaOp x2 = xla::Parameter(local_builder, parameter_idx, shape2,
+      //                                 "WeightTensor");
+      // parameter_idx = parameter_idx + 1;
+      // xla::Shape shape4 = xla::ShapeUtil::MakeShape(xla::PrimitiveType::F32, {20});
+      // xla::XlaOp x4 = xla::Parameter(local_builder, parameter_idx, shape4,
+      //                                 "BiasTensor");
+      // parameter_idx = parameter_idx + 1;
+      // xla::Shape shape3 = xla::ShapeUtil::MakeShape(xla::PrimitiveType::F32, {20});
+      // xla::XlaOp x3 = xla::Parameter(local_builder, parameter_idx, shape3,
+      //                                 "LOutTensor");
     }
 
     // hard-code modify body xlacomputation input arguments
