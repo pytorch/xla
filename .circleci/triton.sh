ade444dd39dd7f959f99569582a4f551ea45a5c4
Bhavya Bahl <bhavyabahl.1@gmail.com>
2024-04-15 22:26:51 +0000

    Rebuild torch xla for triton tests

--- .circleci/triton.sh
+++ .circleci/triton.sh
@@ -6,7 +6,13 @@ source ./xla_env
 source .circleci/common.sh
 
 PYTORCH_DIR=/tmp/pytorch
+pushd $PYTORCH_DIR
+export TORCH_CUDA_ARCH_LIST="8.6"
+python setup.py install
+
 XLA_DIR=$PYTORCH_DIR/xla
+export TF_CUDA_COMPUTE_CAPABILITIES="compute_86"
+build_torch_xla $XLA_DIR
 
 export GCLOUD_SERVICE_KEY_FILE="$XLA_DIR/default_credentials.json"
 export SILO_NAME='cache-silo-ci-dev-3.8_cuda_12.1'  # cache bucket for CI
