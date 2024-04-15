34ec3451a56bea339980158cd64ecffea3327829
manfeibaigithub <manfei@google.com>
2024-04-15 20:23:17 +0000

    update

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -53,12 +53,13 @@ def while_loop(cond_fn, body_fn, *carried_inputs, additional_inputs=None):
 def _xla_while_loop(cond_fn, body_fn, *original_carried_inputs, additional_inputs=()):
   print("!!! arrive here too too !!!")
   # print("carried_inputs: ", carried_inputs)
-  # print("additional_inputs: ", additional_inputs)
+  print("additional_inputs: ", additional_inputs)
   # import pdb; pdb.set_trace()
   # untuple carried_inputs from while_loop
   carried_inputs = original_carried_inputs[0]
   # TODO(@manfei): please clear pass additional_inputs in `while_loop`'s defination in this file
   if len(original_carried_inputs) == 2:
+    print("use original_carried_inputs for additional_inputs")
     additional_inputs = original_carried_inputs[1]
   # fake carried_inputs to split formal code
   fake_carried_inputs = []
