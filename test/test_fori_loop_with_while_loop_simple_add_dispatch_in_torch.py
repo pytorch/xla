db4d7b8c886b72945dc0ececf2d16a8b4958c061
manfeibaigithub <manfei@google.com>
2024-04-15 18:31:32 +0000

    update

--- test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
+++ test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
@@ -100,8 +100,8 @@ class WhileLoopTest(unittest.TestCase):
             self.linear = torch.nn.Linear(10, 20).to(xm.xla_device())
             # self.register_buffer("dec", torch.tensor(1))
 
-        # def forward(self, upper, lower, one_value, x, input_value, weight_0, bias_0, output_value):
-        def forward(self, upper, lower, one_value, x, input_value, output_value):
+        def forward(self, upper, lower, one_value, x, input_value, weight_0, bias_0, output_value):
+        # def forward(self, upper, lower, one_value, x, input_value, output_value):
             weight_1 = self.linear.weight
             bias_1 = self.linear.bias
             def cond_fn(upper, lower, one_value, x, input_value, weight_0, bias_0, output_value):
@@ -141,7 +141,8 @@ class WhileLoopTest(unittest.TestCase):
     # upper_, lower_, one_value_, add_res_x_, l_in_i_plus_1_, weight_, bias_, l_out_ = aaa
     # print("aaa: ", aaa)
     # bbb = simple_with_linear(upper, lower, one_value, init_val, l_in_0, output_value)
-    bbb = simple_with_linear(upper, lower, one_value, init_val, l_in_0, weight_0, bias_0, output_value) # , weight_0, bias_0)
+    # bbb = simple_with_linear(upper, lower, one_value, init_val, l_in_0, weight_0, bias_0, output_value) # , weight_0, bias_0)
+    bbb = simple_with_linear(upper, lower, one_value, init_val, l_in_0, output_value)
     print("bbb: ", bbb)
     # print("start test 6 !!!")
     return aaa
