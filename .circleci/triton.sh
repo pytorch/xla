015b1ad9a930fc0cee16c303ed3a9a56c5165429
Bhavya Bahl <bhavyabahl.1@gmail.com>
2024-04-16 20:05:38 +0000

    Separate build and test phase for triton

--- .circleci/triton.sh
+++ .circleci/triton.sh
@@ -31,8 +31,3 @@ XLA_DIR=$PYTORCH_DIR/xla
 export TF_CUDA_COMPUTE_CAPABILITIES="compute_86"
 export XLA_CUDA=1
 build_torch_xla $XLA_DIR
-
-export GCLOUD_SERVICE_KEY_FILE="$XLA_DIR/default_credentials.json"
-export SILO_NAME='cache-silo-ci-dev-3.8_cuda_12.1'  # cache bucket for CI
-export TRITON_PTXAS_PATH='/usr/local/cuda/bin/ptxas'
-python3 $XLA_DIR/test/test_triton.py
