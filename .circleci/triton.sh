cb0bb85d651bc3b8bd7ed8da228cf70158cb748f
Bhavya Bahl <bhavyabahl.1@gmail.com>
2024-04-16 18:34:26 +0000

    Create a separate CI tab for triton tests

--- .circleci/triton.sh
+++ .circleci/triton.sh
@@ -2,16 +2,34 @@
 
 set -ex
 
-source ./xla_env
 source .circleci/common.sh
-
 PYTORCH_DIR=/tmp/pytorch
+XLA_DIR=$PYTORCH_DIR/xla
+clone_pytorch $PYTORCH_DIR $XLA_DIR
+
+# Use bazel cache
+USE_CACHE=1
+
 pushd $PYTORCH_DIR
-export TORCH_CUDA_ARCH_LIST="8.6"
+checkout_torch_pin_if_available
+
+if ! install_deps_pytorch_xla $XLA_DIR $USE_CACHE; then
+  exit 1
+fi
+
+apply_patches
+
+python -c "import fcntl; fcntl.fcntl(1, fcntl.F_SETFL, 0)"
+
+export PATH=$PATH:/usr/local/cuda-12.1/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-12.1/lib64
+export USE_CUDA=1
+export TORCH_CUDA_ARCH_LIST='8.6'
 python setup.py install
 
 XLA_DIR=$PYTORCH_DIR/xla
 export TF_CUDA_COMPUTE_CAPABILITIES="compute_86"
+export XLA_CUDA=1
 build_torch_xla $XLA_DIR
 
 export GCLOUD_SERVICE_KEY_FILE="$XLA_DIR/default_credentials.json"
