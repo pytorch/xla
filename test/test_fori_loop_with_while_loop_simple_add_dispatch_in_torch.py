5fca07d41090cd6424fa8aa3c0b1220c79c6ac26
manfeibaigithub <manfei@google.com>
2024-04-16 18:28:38 +0000

    update

--- test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
+++ test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
@@ -115,7 +115,7 @@ class WhileLoopTest(unittest.TestCase):
     l_in_0 = torch.ones(10, device=xm.xla_device()) # input_value
     output_value = torch.zeros([20], dtype=torch.float32, device=device)
 
-    upper__, lower__, one_value__, torch_add_res__, input_value__, output_value_real__, weight__, bias__ = while_loop(cond_fn, body_fn, (upper, lower, one_value, init_val, l_in_0, output_value))
+    upper__, lower__, one_value__, torch_add_res__, input_value__, output_value_real__, weight__, bias__ = while_loop( cond_fn, body_fn, (upper, lower, one_value, init_val, l_in_0, weight_0, bias_0, output_value))
 
     expected = _fake_fori_loop(lower, upper, linear_0, l_in_0)
 
