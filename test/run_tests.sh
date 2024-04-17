19970bcd999028f1fa619c0ba32472d04ea7e3a3
Yukio Siraichi <yukio.siraichi@gmail.com>
2024-04-17 16:48:02 -0300

    Add `nms` to `XLA_EXPERIMENTAL` in run_tests.sh

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -104,7 +104,7 @@ function run_xla_hlo_debug {
 
 function run_dynamic {
   echo "Running in DynamicShape mode: $@"
-  XLA_EXPERIMENTAL="nonzero:masked_select:masked_scatter" run_test "$@"
+  XLA_EXPERIMENTAL="nonzero:masked_select:masked_scatter:nms" run_test "$@"
 }
 
 function run_eager_debug {
