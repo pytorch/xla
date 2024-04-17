7efad24377bcf17d11b599c8cc47edb0cabdd774
manfeibaigithub <manfei@google.com>
2024-04-17 19:40:59 +0000

    update

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -23,9 +23,12 @@ def fori_loop(upper, lower, body_fun, init_val, *input_value):
   def body_fn(upper, lower, one_value, x, *input_value, weight_0, output_value, bias_0):
     new_lower = torch.add(one_value, lower)
     output_value = body_fun(*input_value)
-    weight = body_fun.weight  ### not be used actually, initialized as placeholder xlacomputation requirement
-    bias = body_fun.bias  ### not be used actually, initialized as placeholder xlacomputation requirement
-    return upper, new_lower, one_value, torch.add(one_value, x), *input_value, weight, bias, output_value
+    if (hasattr(body_fun, 'weight') and hasattr(body_fun, 'bias')):
+      weight = body_fun.weight  ### not be used actually, initialized as placeholder xlacomputation requirement
+      bias = body_fun.bias  ### not be used actually, initialized as placeholder xlacomputation requirement
+      return upper, new_lower, one_value, torch.add(one_value, x), *input_value, weight, bias, output_value
+    else:
+      return upper, new_lower, one_value, torch.add(one_value, x), *input_value, output_value
 
   output_value = torch.zeros([20], dtype=torch.float32, device=device)
   weight_0 = body_fun.weight
