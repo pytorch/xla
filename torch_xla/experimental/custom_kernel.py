bee14edec85d640f78a16cb07dc271675de457cb
Jiewen Tan <jwtan@google.com>
2024-04-17 18:51:45 +0000

    initial commit

--- torch_xla/experimental/custom_kernel.py
+++ torch_xla/experimental/custom_kernel.py
@@ -12,23 +12,6 @@ from torch_xla.core.xla_model import XLA_LIB
 
 _XLA_USE_BF16 = os.environ.get("XLA_USE_BF16", "0") == "1"
 
-XLA_LIB.define(
-    "tpu_custom_call_(Tensor(a!) output, Tensor[] inputs, str payload) -> ()",)
-
-
-@impl(XLA_LIB, "tpu_custom_call_", "XLA")
-def tpu_custom_call_xla_(output: torch.Tensor, inputs: List[torch.Tensor],
-                         payload: str):
-  torch_xla._XLAC._xla_tpu_custom_call_(output, inputs, payload)
-
-
-@impl(XLA_LIB, "tpu_custom_call_", "CompositeExplicitAutograd")
-def tpu_custom_call_(output: torch.Tensor, inputs: List[torch.Tensor],
-                     payload: str):
-  # Do nothing for non-xla tensor.
-  return
-
-
 def _extract_backend_config(
     module: "jaxlib.mlir._mlir_libs._mlir.ir.Module") -> str | None:
   """
