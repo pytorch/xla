929ff17889b3ba93a105b67fae649627add3e0d3
manfeibaigithub <manfei@google.com>
2024-04-24 21:20:34 +0000

    down into cpp

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -114,7 +114,7 @@ def while_loop(cond_fn, body_fn, carried_inputs, additional_inputs=None):
       # res = mid.extend(list(additional_inputs))
       res = list(body_fn(*carried_inputs))
       res.extend(additional_inputs)
-      print("res: ", res)
+      # print("res: ", res)
       # return list(body_fn(*carried_inputs)).extend(additional_inputs)
       return res
     return _xla_while_loop(cond_fn, new_body_fn, carried_inputs, additional_inputs)
