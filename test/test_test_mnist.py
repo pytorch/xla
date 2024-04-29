9977250ddf35719689d8c5fab56874b21a398d1a
manfeibaigithub <manfei@google.com>
2024-04-29 23:48:16 +0000

    test

--- test/test_test_mnist.py
+++ test/test_test_mnist.py
@@ -281,6 +281,7 @@ def newnewnew_test():
   for name, param in simple_with_linear.named_parameters():
       additional_inputs.insert(-1, param)
 
+  print("in mnist additional_inputs: ", additional_inputs)
   upper__, lower__, one_value__, torch_add_res__, input_value__, bias__, weight__, output_value_real__, = _xla_while_loop(
       cond_fn, body_fn,
       (upper, lower, one_value, init_val, l_in_0, output_value), tuple(additional_inputs))
