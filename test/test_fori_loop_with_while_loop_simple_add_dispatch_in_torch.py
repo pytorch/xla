44f538ab9c107a80e30821b2c27e558b9cecba14
manfei <manfei@google.com>
2024-05-07 23:52:55 +0000

    get xlacomputation like compile

--- test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
+++ test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
@@ -100,6 +100,79 @@ class WhileLoopTest(unittest.TestCase):
     expected = _fake_fori_loop(lower, upper, body_fun, plus_value, init_val)
     self.assertEqual(expected, actual)
 
+  def test_while_loop_get_xlacomputation(self):
+
+    xm.mark_step()
+    device = xm.xla_device()
+    t1 = torch.randn(20, 5).to(device)
+    t2 = torch.randn(20, 5).to(device)
+    t3 = torch.add(t1, t2)
+
+    ### implement one new function for xlacomputation generation with post-order
+    print("before run _get_xla_computation: !!!!!!!!!")
+    res_xla_computation = torch_xla._XLAC._get_xla_computation([t3], [], True)
+    print("after run _get_xla_computation: !!!!!!!!!")
+    if res_xla_computation == null:
+      print("print computation from _get_xla_computation: null !!!!!!!!!!!!!")
+    else:
+      hlo_print = xb.get_computation_hlo(res_xla_computation)
+      print("print computation from _get_xla_computation: !!!!!!!!!")
+      print(hlo_print)
+
+    # ### test to see what _get_stablehlo would get
+    # # tensors, torch_xla._XLAC._xla_get_default_device(), [],
+    # res_hlo_xla_computation = torch_xla._XLAC._get_stablehlo([t3], torch_xla._XLAC._xla_get_default_device(), [], False)
+    # print("res hlo: ", res_hlo_xla_computation)
+
+    # ### test to get xlacomputation
+    # body_result0 = torch.add(t1, t2)
+    # body_ctx0 = torch_xla._XLAC.lowering.LoweringContext()
+    # body_ctx0.build(list(body_result0))
+    # body_hlo0 = body_ctx0.hlo()
+    # print("body_hlo0 finish !!!")
+    # print("type body_hlo0: ", type(body_hlo0))
+    # # print("body_hlo0: ", body_hlo0)
+
+    # ### test to use _get_stablehlo to get xlacomputation
+    # body_result = torch.add(t1, t2)
+    # # body_ctx = torch_xla._XLAC.lowering.LoweringContext()
+    # body_stable_hlo = torch_xla._XLAC._get_stablehlo([t3], torch_xla._XLAC._xla_get_default_device(), [], False)
+    # print("body_stable_hlo finish !!!")
+    # print("type body_stable_hlo: ", type(body_stable_hlo))
+    # print("body_stable_hlo: ", body_stable_hlo)
+
+    # if body_hlo0 == body_stable_hlo:
+    #   print("hlo and stablehlo are the same iteam")
+    # else:
+    #   print("hlo and stablehlo are not the same iteam")
+    # # body_ctx.set_name_string("bodyctx")
+    # # body_ctx.buildforiloop(list(body_result), [])
+    # # body_hlo = body_ctx.hlo()
+    # body_computation = xb.computation_from_module_proto("bodycomputation",
+    #                                                     body_stable_hlo)
+    # hlo_print = xb.get_computation_hlo(body_computation)
+    # print("print computation from _get_stablehlo: !!!!!!!!!")
+    # print(hlo_print)
+
+  def test_while_loop_get_xlacomputation(self):
+
+    xm.mark_step()
+    device = xm.xla_device()
+    t1 = torch.randn(20, 5).to(device)
+    t2 = torch.randn(20, 5).to(device)
+    t3 = torch.add(t1, t2)
+
+    ### implement one new function for xlacomputation generation with post-order
+    print("before run _get_xla_computation: !!!!!!!!!")
+    res_xla_computation = torch_xla._XLAC._get_xla_computation([t3], [], True)
+    print("after run _get_xla_computation: !!!!!!!!!")
+    if res_xla_computation:
+      hlo_print = xb.get_computation_hlo(res_xla_computation)
+      print("print computation from _get_xla_computation: !!!!!!!!!")
+      print(hlo_print)
+    else:
+      print("print computation from _get_xla_computation: null !!!!!!!!!!!!!")
+
 
 if __name__ == '__main__':
   test = unittest.main()
