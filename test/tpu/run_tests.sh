6f0b61e5d782913a0fc7743812f2a8e522189111
Jiewen Tan <jwtan@google.com>
2024-05-10 17:35:06 -0700

    [FSDPv2] Support MultiSlice (#7044)
    Summary:
    This pull request adds the multi-slice support for FSDPv2. Basically, the default setup is to use the dcn axis as the data axis, and it means we only do data parallel over multi-slices. In the future, we could also support FSDP over mutli-slices.
    
    Test Plan:
    PJRT_DEVICE=TPU python test/spmd/test_fsdp_v2.py

--- test/tpu/run_tests.sh
+++ test/tpu/run_tests.sh
@@ -11,6 +11,7 @@ python3 test/spmd/test_xla_distributed_checkpoint.py
 python3 test/spmd/test_train_spmd_linear_model.py
 python3 test/spmd/test_xla_spmd_python_api_interaction.py
 python3 test/spmd/test_xla_auto_sharding.py
+python3 test/spmd/test_fsdp_v2.py
 XLA_EXPERIMENTAL=nonzero:masked_select:nms python3 test/ds/test_dynamic_shape_models.py -v
 XLA_EXPERIMENTAL=nonzero:masked_select:nms python3 test/ds/test_dynamic_shapes.py -v
 python3 test/test_autocast.py
