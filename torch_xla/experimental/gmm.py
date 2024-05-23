0c885164c4ebc460bebff1a1e7f1a311e63652b2
Jiewen Tan <jwtan@google.com>
2024-05-23 00:06:03 +0000

    Remove even more code

--- torch_xla/experimental/gmm.py
+++ torch_xla/experimental/gmm.py
@@ -7,14 +7,28 @@ import torch_xla
 import numpy as np
 
 
-def _gmm(
-    lhs: torch.Tensor,
-    rhs: torch.Tensor,
-    group_sizes: torch.Tensor,
-    payload: str,
-) -> torch.Tensor:
+def gmm(lhs: torch.Tensor, rhs: torch.Tensor,
+        group_sizes: torch.Tensor) -> torch.Tensor:
+  """Compute lhs[sizes[i-1]:sizes[i], :] @ rhs for each group 'i'.
+
+  Args:
+    lhs: A 2d, jnp.ndarray with shape [m, k].
+    rhs: A 3d, jnp.ndarray with shape [num_groups, k, n].
+    group_sizes: A 1d, jnp.ndarray with shape [num_groups] and jnp.int32 dtype.
+    preferred_element_type: jnp.dtype, the element type for the output matrix.
+
+  Returns:
+    A 2d, jnp.ndarray with shape [m, n].
+  """
+  # Import JAX within the function such that we don't need to call the jax_import_guard()
+  # in the global scope which could cause problems for xmp.spawn.
+  jax_import_guard()
+  import jax
   import jax.numpy as jnp
-  from jax.experimental.pallas.ops.tpu.megablox.gmm import make_group_metadata
+  from jax.experimental.pallas.ops.tpu.megablox.gmm import gmm, make_group_metadata
+  from torch_xla.experimental.custom_kernel import trace_pallas
+
+  payload, _ = trace_pallas(gmm, lhs, rhs, group_sizes)
 
   m, n = lhs.shape[0], rhs.shape[2]
   # Create the metadata we need for computation.
@@ -38,29 +52,4 @@ def _gmm(
       num_active_tiles, group_metadata0, group_metadata1, group_metadata2,
       group_offset_torch, lhs, rhs
   ], payload, [output_shape], [lhs.dtype])
-  return out
-
-
-def gmm(lhs: torch.Tensor, rhs: torch.Tensor,
-        group_sizes: torch.Tensor) -> torch.Tensor:
-  """Compute lhs[sizes[i-1]:sizes[i], :] @ rhs for each group 'i'.
-
-  Args:
-    lhs: A 2d, jnp.ndarray with shape [m, k].
-    rhs: A 3d, jnp.ndarray with shape [num_groups, k, n].
-    group_sizes: A 1d, jnp.ndarray with shape [num_groups] and jnp.int32 dtype.
-    preferred_element_type: jnp.dtype, the element type for the output matrix.
-
-  Returns:
-    A 2d, jnp.ndarray with shape [m, n].
-  """
-  # Import JAX within the function such that we don't need to call the jax_import_guard()
-  # in the global scope which could cause problems for xmp.spawn.
-  jax_import_guard()
-  import jax
-  from jax.experimental.pallas.ops.tpu.megablox import gmm
-  from torch_xla.experimental.custom_kernel import trace_pallas
-
-  payload, _ = trace_pallas(gmm, lhs, rhs, group_sizes)
-  out = _gmm(lhs, rhs, group_sizes, payload)
   return out
