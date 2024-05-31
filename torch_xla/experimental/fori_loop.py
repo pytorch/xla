932d603ae3c95f9fadc1a59731d1788da5768ccd
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-05-30 21:11:15 -0700

    format

--- torch_xla/experimental/fori_loop.py
+++ torch_xla/experimental/fori_loop.py
@@ -90,7 +90,8 @@ def _xla_while_loop(cond_fn, body_fn, carried_inputs, additional_inputs=None):
 
   if additional_inputs:
     body_fn_inputs = [
-        carried_inputs[0], ] + fake_carried_inputs[1:] + list(additional_inputs)
+        carried_inputs[0],
+    ] + fake_carried_inputs[1:] + list(additional_inputs)
     cond_inputs = carried_inputs + additional_inputs
   else:
     body_fn_inputs = carried_inputs + additional_inputs
