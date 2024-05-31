bd221db50f7c11a46dc58219bf2cad51ce472c5f
iefgnoix <isaacwxf23@gmail.com>
2024-05-31 19:49:53 +0000

    Reuse DLDeviceType.

--- torch_xla/utils/dlpack.py
+++ torch_xla/utils/dlpack.py
@@ -1,24 +1,11 @@
 from typing import Any
 import enum
+from torch.utils.dlpack import DLDeviceType
 import torch_xla
 
 
 def to_dlpack(xla_tensor: Any):
   return torch_xla._XLAC._to_dlpack(xla_tensor)
-
-
-class DLDeviceType(enum.IntEnum):
-  # Enums as in DLPack specification (aten/src/ATen/dlpack.h)
-  kDLCPU = 1,
-  kDLGPU = 2,
-  kDLCPUPinned = 3,
-  kDLOpenCL = 4,
-  kDLVulkan = 7,
-  kDLMetal = 8,
-  kDLVPI = 9,
-  kDLROCM = 10,
-  kDLExtDev = 12,
-  kDLOneAPI = 14,
 
 
 def from_dlpack(ext_tensor: Any):
