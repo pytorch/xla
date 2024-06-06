468b321994b6257bb62915d68c5a7e17f5dec4c8
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-06-06 12:04:33 -0700

    Update test_while_loop.py to clean use case

--- test/test_while_loop.py
+++ test/test_while_loop.py
@@ -93,62 +93,6 @@ class WhileLoopTest(unittest.TestCase):
 
     self.assertTrue(torch.all(torch.eq(res, expected)))
 
-  def test_while_loop_tpu_MNIST_inside_loop(self):
-    device = xm.xla_device()
-    torch.set_grad_enabled(False)
-
-    class MNIST(torch.nn.Module):
-
-      def __init__(self):
-        super().__init__()
-        self.conv1 = torch.nn.Conv2d(1, 10, kernel_size=5, stride=1, padding=2)
-        self.bn1 = torch.nn.BatchNorm2d(10).eval()
-        self.conv2 = torch.nn.Conv2d(10, 20, kernel_size=5)
-        self.bn2 = torch.nn.BatchNorm2d(20).eval()
-        self.fc1 = torch.nn.Linear(500, 50)
-        self.fc2 = torch.nn.Linear(50, 10)
-        self.bnLayersWeights = []
-
-      def forward(self, iteri, x, y):
-
-        def cond_fn(iteri, x, y):
-          return iteri > 0
-
-        def body_fn(iteri, x, y):
-          y = F.relu(F.max_pool2d(self.conv1(x), 2))
-          y = self.bn1(y)
-          y = F.relu(F.max_pool2d(self.conv2(y), 2))
-          y = self.bn2(y)
-          y = torch.flatten(y, 1)
-          y = F.relu(self.fc1(y))
-          y = self.fc2(y)
-
-          return iteri - 1, x.clone(), F.log_softmax(y, dim=1)
-
-        return while_loop(cond_fn, body_fn, (iteri, x, y))
-
-      def forward_compare(self, iteri, x, y):
-        y = F.relu(F.max_pool2d(self.conv1(x), 2))
-        y = self.bn1(y)
-        y = F.relu(F.max_pool2d(self.conv2(y), 2))
-        y = self.bn2(y)
-        y = torch.flatten(y, 1)
-        y = F.relu(self.fc1(y))
-        y = self.fc2(y)
-        return iteri - 1, x.clone(), F.log_softmax(y, dim=1)
-
-    mnist = MNIST()
-    mnist.to(device)
-    l_in_0 = torch.randn(16, 1, 28, 28, dtype=torch.float32, device=device)
-    l_out = torch.randn(16, 10, dtype=torch.float32, device=device)
-    iteri = torch.tensor(3, dtype=torch.int64, device=device)
-    _, _, res = mnist(iteri, l_in_0, l_out)
-
-    # === expected result for one iteration to be compared since body_fn defined use the same input in each iteration ===
-    _, _, expected = mnist.forward_compare(iteri, l_in_0, l_out)
-
-    self.assertTrue(torch.all(torch.eq(res, expected)))
-
   # ====== fori_loop ======
   def test_fori_loop_addition_tpu(self):
     device = xm.xla_device()
