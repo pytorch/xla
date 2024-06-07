dc54f4025148ea376b2a98c52a81304bf39247b6
Can Karakus <cakarak@amazon.com>
2024-06-07 16:48:13 -0700

    modularize zero optimizer step function

--- torch_xla/distributed/zero_redundancy_optimizer.py
+++ torch_xla/distributed/zero_redundancy_optimizer.py
@@ -329,6 +329,31 @@ class ZeroRedundancyOptimizer(Optimizer):
       with torch.enable_grad():
         loss = closure()
 
+    self._reduce_gradients(**kwargs)
+
+    if self.grad_clipping:
+      # Update unscale/clip with sub partitions
+      self._clip_grad_norm(max_norm=self.max_norm)
+
+    self._update_parameters(**kwargs)
+
+    return loss
+
+  def _get_sharding_scheme(self, kwargs):
+    if "sharding_scheme" in kwargs:
+      return kwargs["sharding_scheme"]
+    else:
+      return [
+               {
+                 "scale_factor": 1.0,
+                 "sharding_group": self.sharding_groups,
+                 "group_size": self.local_world_size,
+               },
+             ]
+
+  def _reduce_gradients(self, **kwargs):
+    sharding_scheme = self._get_sharding_scheme(kwargs)
+
     # sync to base optimizer
     self._sync_param_groups(self.param_groups, self.base_optimizer.param_groups)
 
@@ -349,29 +374,33 @@ class ZeroRedundancyOptimizer(Optimizer):
           if self.coalesce_cc:
             padded_grads.append(padded_grad)
           else:
-            grad_shard = xm.reduce_scatter(
-                xm.REDUCE_SUM,
-                padded_grad,
-                scale=1.0 / self.local_world_size,
-                scatter_dim=0,
-                shard_count=self.local_world_size,
-                pin_layout=self.pin_layout,
-                groups=self.sharding_groups,
-            )
+            grad_shard = padded_grad
+            for step in sharding_scheme:
+              grad_shard = xm.reduce_scatter(
+                  xm.REDUCE_SUM,
+                  grad_shard,
+                  scale=step["scale_factor"] / step["group_size"],
+                  scatter_dim=0,
+                  shard_count=step["group_size"],
+                  pin_layout=self.pin_layout,
+                  groups=step["sharding_group"],
+              )
             if grad_shard.dtype != self.optimizer_dtype:
               grad_shard = grad_shard.to(dtype=self.optimizer_dtype)
             shard.grad = grad_shard
 
     if self.coalesce_cc:
-      grad_shards = xm.reduce_scatter(
-          xm.REDUCE_SUM,
-          padded_grads,
-          scale=1.0 / self.local_world_size,
-          scatter_dim=0,
-          shard_count=self.local_world_size,
-          pin_layout=self.pin_layout,
-          groups=self.sharding_groups,
-      )
+      grad_shards = padded_grads
+      for step in sharding_scheme:
+        grad_shards = xm.reduce_scatter(
+            xm.REDUCE_SUM,
+            grad_shards,
+            scale=step["scale_factor"] / step["group_size"],
+            scatter_dim=0,
+            shard_count=step["group_size"],
+            pin_layout=self.pin_layout,
+            groups=step["sharding_group"],
+        )
       index = 0
       for param_group, sharded_param_group in zip(
           self.param_groups, self.base_optimizer.param_groups):
@@ -385,23 +414,23 @@ class ZeroRedundancyOptimizer(Optimizer):
             shard.grad = grad_shard
             index += 1
 
-    if self.grad_clipping:
-      # Update unscale/clip with sub partitions
-      self._clip_grad_norm(max_norm=self.max_norm)
+  @torch.no_grad()
+  def _update_parameters(self, **kwargs):
+    sharding_scheme = self._get_sharding_scheme(kwargs)
+    kwargs.pop("sharding_scheme", None)
 
     # Step the wrapped optimizer
     # Closure already executed, pass none here
     self.base_optimizer.step(closure=None, **kwargs)
     # Remove shards' grads
     self.base_optimizer.zero_grad(set_to_none=True)
-    self.allgather_weights_and_update_full_parameter()
+    self.allgather_weights_and_update_full_parameter(sharding_scheme)
 
     # sync back
     self._sync_param_groups(self.base_optimizer.param_groups, self.param_groups)
 
-    return loss
+  def allgather_weights_and_update_full_parameter(self, sharding_scheme):
 
-  def allgather_weights_and_update_full_parameter(self):
     # All gather the new weights across the ranks and assign them to the full parameters
     sharded_data = []
     for param_group, sharded_param_group in zip(
@@ -411,25 +440,31 @@ class ZeroRedundancyOptimizer(Optimizer):
         if param.grad is not None or (self.use_grad_acc_hook and
                                       hasattr(shard, 'main_grad')):
           shard_data = shard.data
-          shard_data = shard_data.to(dtype=param.dtype)
+          if not self.higher_cc_precision:
+            shard_data = shard_data.to(dtype=param.dtype)
           if self.coalesce_cc:
             sharded_data.append(shard_data)
           else:
-            padded_param = xm.all_gather(
-                shard_data,
-                dim=0,
-                pin_layout=self.pin_layout,
-                groups=self.sharding_groups,
-            )
+            padded_param = shard_data
+            for step in reversed(sharding_scheme):
+              padded_param = xm.all_gather(
+                  padded_param,
+                  dim=0,
+                  pin_layout=self.pin_layout,
+                  groups=step["sharding_group"],
+              )
+            padded_param = padded_param.to(dtype=param.dtype)
             param.data.copy_(padded_param.data[:param.size(0)])
 
     if self.coalesce_cc:
-      padded_params = xm.all_gather(
-          sharded_data,
-          dim=0,
-          pin_layout=self.pin_layout,
-          groups=self.sharding_groups,
-      )
+      padded_params = sharded_data
+      for step in reversed(sharding_scheme):
+        padded_params = xm.all_gather(
+            padded_params,
+            dim=0,
+            pin_layout=self.pin_layout,
+            groups=step["sharding_group"],
+        )
       index = 0
       for param_group, sharded_param_group in zip(
           self.param_groups, self.base_optimizer.param_groups):
