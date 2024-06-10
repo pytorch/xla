3ba5521e3158b900d29a2b08eca270836398a066
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-06-10 10:06:22 -0700

    Update run_tests.sh

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -136,7 +136,7 @@ function run_torchrun {
   if [ -x "$(command -v nvidia-smi)" ] && [ "$XLA_CUDA" != "0" ]; then
     echo "Running torchrun test for GPU $@"
     num_devices=$(nvidia-smi --list-gpus | wc -l)
-    PJRT_DEVICE=CUDA torchrun --nnodes 1 --nproc-per-node $num_devices $@
+    PJRT_DEVICE=CUDA torchrun --nnodes 1 --nproc-per-node 1 $@
   fi
 }
 
