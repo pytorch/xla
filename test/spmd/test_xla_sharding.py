bb4f179640f9ff0764a9e3150218215cf061b30b
Jiewen Tan <jwtan@google.com>
2024-06-12 00:47:34 +0000

    skip tpu v2

--- test/spmd/test_xla_sharding.py
+++ test/spmd/test_xla_sharding.py
@@ -1213,7 +1213,7 @@ class BasicXlaShardingTest(test_xla_sharding_base.XlaShardingTest):
     self.assertEqual(xxx.shape, (8, 8))
     self.assertTrue(torch.allclose(x.cpu() + 1, xxx.cpu()))
 
-  @unittest.skipIf(xr.device_type() != 'TPU', "Skip non-TPU device")
+  @unittest.skipIf(xr.device_type() != 'TPU' and tpu.version() < 4, "Only runs on TPUv4")
   def test_spmd_reduce_scatter(self):
     xs.set_global_mesh(self._get_mesh((1, self.n_devices)))
     x = torch.ones(8, 8).to(xm.xla_device())
@@ -1233,7 +1233,7 @@ class BasicXlaShardingTest(test_xla_sharding_base.XlaShardingTest):
     expected_x = torch.ones(8 // self.n_devices, 8) * 4
     self.assertTrue(torch.allclose(x.cpu(), expected_x))
 
-  @unittest.skipIf(xr.device_type() != 'TPU', "Skip non-TPU device")
+  @unittest.skipIf(xr.device_type() != 'TPU' and tpu.version() < 4, "Only runs on TPUv4")
   def test_spmd_reduce_scatter_canonical_index(self):
     xs.set_global_mesh(self._get_mesh((1, self.n_devices)))
     x = torch.ones(8, 8).to(xm.xla_device())
