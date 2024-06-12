6abb432ffbc48593af35d17654aec80b1b2f6f46
Karakus <cakarak@amazon.com>
2024-06-12 16:52:29 -0700

    small fix to pass sharding_scheme instead of **kwargs
    cr: https://code.amazon.com/reviews/CR-132179892

--- torch_xla/distributed/zero_redundancy_optimizer.py
+++ torch_xla/distributed/zero_redundancy_optimizer.py
@@ -401,13 +401,16 @@ class ZeroRedundancyOptimizer(Optimizer):
             index += 1
 
   def _update_parameters(self, **kwargs):
+    sharding_scheme = self._get_sharding_scheme(**kwargs)
+    kwargs.pop("sharding_scheme", None)
+
     # Step the wrapped optimizer
     # Closure already executed, pass none here
     self.base_optimizer.step(closure=None, **kwargs)
     # Remove shards' grads
     self.base_optimizer.zero_grad(set_to_none=True)
 
-    self.allgather_weights_and_update_full_parameter()
+    self.allgather_weights_and_update_full_parameter(sharding_scheme)
 
     # sync back
     self._sync_param_groups(self.base_optimizer.param_groups, self.param_groups)
@@ -434,8 +437,8 @@ class ZeroRedundancyOptimizer(Optimizer):
 
     return loss
 
-  def allgather_weights_and_update_full_parameter(self):
-    sharding_scheme = self._get_sharding_scheme(**kwargs)
+
+  def allgather_weights_and_update_full_parameter(self, sharding_scheme):
 
     # All gather the new weights across the ranks and assign them to the full parameters
     sharded_data = []
