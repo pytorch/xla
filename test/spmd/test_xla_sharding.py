f92a4dd8308909886fc8f81fc6ff16b4e58747ef
Jiewen Tan <jwtan@google.com>
2024-06-12 20:47:50 +0000

    Fix

--- test/spmd/test_xla_sharding.py
+++ test/spmd/test_xla_sharding.py
@@ -1213,7 +1213,7 @@ class BasicXlaShardingTest(test_xla_sharding_base.XlaShardingTest):
     self.assertEqual(xxx.shape, (8, 8))
     self.assertTrue(torch.allclose(x.cpu() + 1, xxx.cpu()))
 
-  @unittest.skipIf(xr.device_type() != 'TPU' and tpu.version() < 4,
+  @unittest.skipIf(xr.device_type() != 'TPU' or tpu.version() < 4,
                    "Only runs on TPUv4")
   def test_spmd_reduce_scatter(self):
     xs.set_global_mesh(self._get_mesh((1, self.n_devices)))
@@ -1234,7 +1234,7 @@ class BasicXlaShardingTest(test_xla_sharding_base.XlaShardingTest):
     expected_x = torch.ones(8 // self.n_devices, 8) * 4
     self.assertTrue(torch.allclose(x.cpu(), expected_x))
 
-  @unittest.skipIf(xr.device_type() != 'TPU' and tpu.version() < 4,
+  @unittest.skipIf(xr.device_type() != 'TPU' or tpu.version() < 4,
                    "Only runs on TPUv4")
   def test_spmd_reduce_scatter_canonical_index(self):
     xs.set_global_mesh(self._get_mesh((1, self.n_devices)))
