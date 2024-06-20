6313c8ab59c9feb3eb9b02be31ef6f01378f2708
JackCaoG <jackcao@google.com>
2024-06-20 21:43:40 +0000

    add basic eager tests

---
+++ test/eager/test_eager.py
@@ -0,0 +1,51 @@
+import unittest
+import sys
+
+import torch
+import torch_xla
+import torch_xla.debug.metrics as met
+import torch_xla.core.xla_model as xm
+
+class Eager(unittest.TestCase):
+
+  @classmethod
+  def setUpClass(cls):
+    torch_xla.experimental.eager_mode(True)
+
+  def test_eager_basic(self):
+    met.clear_all()
+    self.assertTrue(torch_xla.experimental.is_eager_mode())
+    device = torch_xla.device()
+
+    t1 = torch.randn(5, 5, device=device)
+    xm.wait_device_ops()
+    self.assertEqual(met.metric_data("EagerOpCompileTime")[0], 1) 
+    self.assertEqual(met.metric_data("EagerOpExecuteTime")[0], 1) 
+
+    t1 *= 5
+    xm.wait_device_ops()
+    self.assertEqual(met.metric_data("EagerOpCompileTime")[0], 2) 
+    self.assertEqual(met.metric_data("EagerOpExecuteTime")[0], 2)
+
+  def test_eager_recompile(self):
+    self.assertTrue(torch_xla.experimental.is_eager_mode())
+    device = torch_xla.device()
+
+    t1 = torch.randn(5, 5, device=device)
+    met.clear_all()
+
+    t2 = torch.logsumexp(t1, 0)
+    xm.wait_device_ops()
+    self.assertEqual(met.metric_data("EagerOpCompileTime")[0], 1) 
+    self.assertEqual(met.metric_data("EagerOpExecuteTime")[0], 1)     
+
+    t3 = torch.logsumexp(t1, 0)
+    xm.wait_device_ops()
+    # make sure no recompilation
+    self.assertEqual(met.metric_data("EagerOpCompileTime")[0], 1) 
+    self.assertEqual(met.metric_data("EagerOpExecuteTime")[0], 2) 
+
+
+if __name__ == '__main__':
+  test = unittest.main()
+  sys.exit(0 if test.result.wasSuccessful() else 1)
