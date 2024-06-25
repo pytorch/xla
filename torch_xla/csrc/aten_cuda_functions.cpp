4129e86bc209512aaf120b702d7f38de725ca9df
Yukio Siraichi <yukio.siraichi@gmail.com>
2024-06-25 12:41:12 -0300

    Add Python module initialization.

--- torch_xla/csrc/aten_cuda_functions.cpp
+++ torch_xla/csrc/aten_cuda_functions.cpp
@@ -1,6 +1,7 @@
 #include "torch_xla/csrc/aten_cuda_functions.h"
 
 #include <c10/util/Exception.h>
+#include <pybind11/pybind11.h>
 
 static void fail(const char* name) {
   TORCH_CHECK(false, "Could not call the CUDA function: ", name,
@@ -16,3 +17,5 @@ void set_device(c10::DeviceIndex) { fail("c10::cuda::set_device()"); }
 void device_synchronize() { fail("c10::cuda::device_synchronize()"); }
 
 }  // namespace c10::cuda
+
+PYBIND11_MODULE(_XLAC_cuda_functions, m) {}
