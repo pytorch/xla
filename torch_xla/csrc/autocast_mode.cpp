53c77e2e6f07c70fcb7ad49f244a3db2241d6d44
lingzhi98 <103185827+lingzhi98@users.noreply.github.com>
2024-06-26 01:05:09 +0800

    Register sdp lower precision autocast (#7299)

--- torch_xla/csrc/autocast_mode.cpp
+++ torch_xla/csrc/autocast_mode.cpp
@@ -48,6 +48,7 @@ TORCH_LIBRARY_IMPL(aten, AutocastXLA, m) {
   KERNEL_XLA(prelu, lower_precision_fp)
   KERNEL_XLA(relu, lower_precision_fp)
   KERNEL_XLA(max_pool2d, lower_precision_fp)
+  KERNEL_XLA(scaled_dot_product_attention, lower_precision_fp)
 
   // fp32 cast policy
   // Commented out ops are included in the AutoCastCPU Policy,
