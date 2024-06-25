59d689e993506a71a2837a07325399f681fa3f8d
Jiewen Tan <jwtan@google.com>
2024-06-25 21:00:12 +0000

    fix linters

--- torch_xla/distributed/spmd/__init__.py
+++ torch_xla/distributed/spmd/__init__.py
@@ -4,7 +4,8 @@ from .xla_sharding import (Mesh, HybridMesh, ShardingType, ShardingSpec,
                            wrap_if_sharded, xla_patched_nn_linear_forward,
                            set_global_mesh, get_global_mesh,
                            _mark_manual_sharding, enable_manual_sharding,
-                           disable_manual_sharding, apply_backward_optimization_barrier)
+                           disable_manual_sharding,
+                           apply_backward_optimization_barrier)
 from .api import xla_distribute_tensor, xla_distribute_module, auto_policy
 
 __all__ = [
