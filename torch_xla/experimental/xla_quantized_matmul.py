289471cb3e1d12cd6b6346b57110e1bbab78a40e
Siyuan Liu <lsiyuan@google.com>
2024-07-09 14:37:40 -0700

    Asymmetric quantized matmul support (#7626)
    Co-authored-by: Siyuan Liu <lsiyuan@google.coim>

--- torch_xla/experimental/xla_quantized_matmul.py
+++ torch_xla/experimental/xla_quantized_matmul.py
@@ -5,12 +5,12 @@ from torch.library import impl
 from torch_xla.core.xla_model import XLA_LIB
 
 XLA_LIB.define(
-    "quantized_matmul(Tensor x, Tensor w, Tensor scale, int? block_size=-1, bool? int4_weight=False, bool? quantize_activation=False) -> Tensor"
+    "quantized_matmul(Tensor x, Tensor w, Tensor scale, Tensor? zero_point=None, int? block_size=-1, bool? int4_weight=False, bool? quantize_activation=False) -> Tensor"
 )
 
 
 def _check_per_channel_quant_weight_dtype_shapes(input_dim, output_dim, w,
-                                                 w_scaler):
+                                                 w_scaler, zero_point):
   assert w.dtype == torch.int8, f"Weight dtype is expected to be torch.int8, got {w.dtype}."
   assert w.dim(
   ) == 2, f"Weight tensor is expected to be 2D, got {w.dim()}D Tensor."
@@ -19,10 +19,14 @@ def _check_per_channel_quant_weight_dtype_shapes(input_dim, output_dim, w,
       1], f"Weight shape is expected to be [output_dim, input_dim], output_dim: {output_dim}, input_dim: {input_dim}, but got {w_shape}."
   assert w_scaler.dim() == 1 and w_scaler.shape[0] == w_shape[
       0], f"weight scaler shape is expect to be [out_channel,], got {w_scaler.shape}, weight shape {w_shape}."
+  if zero_point is not None:
+    assert zero_point.dim() == 1 and w_scaler.shape[0] == w_shape[
+        0], f"zero point shape is expect to be [out_channel,], got {zero_point.shape}, weight shape {w_shape}."
 
 
 def _check_blockwise_quant_weight_dtype_shapes(input_dim, output_dim,
-                                               block_size, w, w_scaler):
+                                               block_size, w, w_scaler,
+                                               zero_point):
   assert w.dtype == torch.int8, (
       f"Weight dtype is expected to be torch.int8, got {w.dtype}.")
   assert w.dim() == 3, (
@@ -40,12 +44,20 @@ def _check_blockwise_quant_weight_dtype_shapes(input_dim, output_dim,
   assert w_scaler.shape[0] == w_shape[0] and w_scaler.shape[1] == w_shape[-1], (
       f"weight scaler shape is expect to be [in_channel / block_size, out_channel], "
       f"got {w_scaler.shape}, weight shape {w_shape}.")
+  if zero_point is not None:
+    assert zero_point.dim() == 2, (
+        f"zero_point is expected to be 2D, got {zero_point.dim()}D Tensor.")
+    assert zero_point.shape[0] == w_shape[0] and zero_point.shape[1] == w_shape[
+        -1], (
+            f"zero_point shape is expect to be [in_channel / block_size, out_channel], "
+            f"got {zero_point.shape}, weight shape {w_shape}.")
 
 
 @impl(XLA_LIB, "quantized_matmul", "XLA")
 def quantized_matmul_xla(x: torch.Tensor,
                          w: torch.Tensor,
                          scaler: torch.Tensor,
+                         zero_point: torch.Tensor = None,
                          block_size: int = -1,
                          int4_weight: bool = False):
   """Quantized Matrix Multiply op on XLA devices.
@@ -58,6 +70,9 @@ def quantized_matmul_xla(x: torch.Tensor,
       scaler: torch.Tensor - Weight scaler.
          per-channel quant: [out_channel,].
          blockwise quant: [in_channel / block_size, out_channel].
+      zero_point: Optional[torch.Tensor] - Zero point tensor.
+        per-channel quant: [out_channel,].
+        blockwise quant: [in_channel / block_size, out_channel].
       block_size: The blocksize for blockwise quantization, -1 for per-channel quantization.
       int4_weight: if the weights are int4, the int4 weights need to be stored in a int8
                    container (unpacked).
@@ -68,39 +83,61 @@ def quantized_matmul_xla(x: torch.Tensor,
   if block_size == -1:
     # Per-channel quant.
     _check_per_channel_quant_weight_dtype_shapes(x.shape[-1], scaler.shape[0],
-                                                 w, scaler)
-    return F.linear(x, w) * scaler
+                                                 w, scaler, zero_point)
+    out = F.linear(x, w) * scaler
   else:
     # Blockwise quant.
     _check_blockwise_quant_weight_dtype_shapes(x.shape[-1], w.shape[-1],
-                                               block_size, w, scaler)
+                                               block_size, w, scaler,
+                                               zero_point)
     x = x.reshape(*x.shape[:-1], x.shape[-1] // block_size, block_size)
     out = torch.einsum('scn,...sc->...sn', w, x)
     out = torch.einsum('sn,...sn->...n', scaler, out)
-    return out
+  if zero_point is not None:
+    if block_size == -1:
+      # Per-channel quant.
+      zp_out = torch.einsum("...c,z->...z", x, zero_point)
+    else:
+      # Blockwise quant.
+      zp_out = x.sum(dim=-1)
+      zp_out = torch.matmul(zp_out, zero_point)
+    out -= zp_out
+  return out
 
 
 @impl(XLA_LIB, "quantized_matmul", "CompositeExplicitAutograd")
 def quantized_matmul(x: torch.Tensor,
                      w: torch.Tensor,
                      scaler: torch.Tensor,
+                     zero_point: torch.Tensor = None,
                      block_size: int = -1,
                      int4_weight: bool = False):
   if block_size == -1:
     # Per-channel quant.
     _check_per_channel_quant_weight_dtype_shapes(x.shape[-1], scaler.shape[0],
-                                                 w, scaler)
+                                                 w, scaler, zero_point)
     w = w.to(x.dtype)
-    return torch.mul(F.linear(x, w), scaler)
+    out = torch.mul(F.linear(x, w), scaler)
   else:
     # Blockwise quant.
     _check_blockwise_quant_weight_dtype_shapes(x.shape[-1], w.shape[-1],
-                                               block_size, w, scaler)
+                                               block_size, w, scaler,
+                                               zero_point)
     x = x.reshape(*x.shape[:-1], x.shape[-1] // block_size, block_size)
     w = w.to(x.dtype)
     out = torch.einsum('scn,...sc->...sn', w, x)
     out = torch.einsum('sn,...sn->...n', scaler, out)
-    return out
+
+  if zero_point is not None:
+    if block_size == -1:
+      # Per-channel quant.
+      zp_out = torch.einsum("...c,z->...z", x, zero_point)
+    else:
+      # Blockwise quant.
+      zp_out = x.sum(dim=-1)
+      zp_out = torch.matmul(zp_out, zero_point)
+    out -= zp_out
+  return out
 
 
 class XlaQuantizedLinear(torch.nn.Module):
@@ -108,18 +145,24 @@ class XlaQuantizedLinear(torch.nn.Module):
   def __init__(self,
                input_dim,
                output_dim,
-               block_size=-1,
+               is_symmetric: bool = False,
+               block_size: int = -1,
                int4_weight: bool = False):
     super().__init__()
     self.input_dim = input_dim
     self.output_dim = output_dim
+    self.is_symmetric = is_symmetric
     self.block_size = block_size
     self.int4_weight = int4_weight
     self.register_buffer('weight',
                          torch.zeros(output_dim, input_dim).to(torch.int8))
     self.register_buffer('weight_scaler', torch.zeros(output_dim))
+    if not self.is_symmetric:
+      self.register_buffer('zero_point', torch.zeros(output_dim))
+    else:
+      self.zero_point = None
 
-  def load_quantized_weight(self, weight, weight_scaler):
+  def load_quantized_weight(self, weight, weight_scaler, zero_point=None):
     '''
     weight (Tensor):
       per-channel quant: [out_channel, in_channel].
@@ -133,20 +176,22 @@ class XlaQuantizedLinear(torch.nn.Module):
       # Per-channel quant.
       _check_per_channel_quant_weight_dtype_shapes(self.input_dim,
                                                    self.output_dim, weight,
-                                                   weight_scaler)
+                                                   weight_scaler, zero_point)
     else:
       # Blockwise quant.
       _check_blockwise_quant_weight_dtype_shapes(self.input_dim,
                                                  self.output_dim,
                                                  self.block_size, weight,
-                                                 weight_scaler)
+                                                 weight_scaler, zero_point)
     self.weight = weight
     self.weight_scaler = weight_scaler
+    self.zero_point = zero_point
 
   def forward(self, x):
     return torch.ops.xla.quantized_matmul(
         x,
         self.weight,
         self.weight_scaler,
+        self.zero_point,
         block_size=self.block_size,
         int4_weight=self.int4_weight)
