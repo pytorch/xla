b9fc05cce471e2a8270e2dab043641f014d581cd
Will Cromar <wcromar@google.com>
2024-07-12 21:16:28 +0000

    typo

--- infra/terraform_modules/arc_v4_container_cluster/main.tf
+++ infra/terraform_modules/arc_v4_container_cluster/main.tf
@@ -73,7 +73,7 @@ resource "google_container_node_pool" "arc_v4_tpu_nodes" {
 
 resource "helm_release" "arc" {
   name             = "actions-runner-controller"
-  chart            = "oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controlle:0.9.3"
+  chart            = "oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller:0.9.3"
   namespace        = var.arc_namespace
   create_namespace = true
 }
