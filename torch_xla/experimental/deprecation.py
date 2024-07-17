67254b69bf20af021db3b90a746b73a33a4534b1
zpcore <zpcore@gmail.com>
2024-07-17 06:14:18 +0000

    add depreacated decorator

--- torch_xla/experimental/deprecation.py
+++ torch_xla/experimental/deprecation.py
@@ -21,5 +21,34 @@ def deprecated(module, new: FN) -> FN:
   return wrapped
 
 
+def mark_deprecated(module, new: FN) -> FN:
+  """Decorator to mark a function as deprecated and map to new function.
+
+  Args:
+    module: current module of the deprecated function is in. Assume current module name is X, you can use `from . import X` and pass X here.
+    new: new function that we map to. Need to include the path the new functioin is in.
+
+  Returns:
+    Wrapper of the new function.
+  """
+  already_warned = [False]
+
+  def decorator(func):
+
+    @functools.wraps(new)
+    def wrapped(*args, **kwargs):
+      if not already_warned[0]:
+        logging.warning(
+            f'{module.__name__}.{func.__name__} is deprecated. Use {new.__module__}.{new.__name__} instead.'
+        )
+        already_warned[0] = True
+
+      return new(*args, **kwargs)
+
+    return wrapped
+
+  return decorator
+
+
 def register_deprecated(module, new: FN):
   setattr(module, new.__name__, deprecated(module, new))
