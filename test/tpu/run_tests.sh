b9c19b45ae3a5314af252ff32b3e697b4e4ba93c
JackCaoG <59073027+JackCaoG@users.noreply.github.com>
2024-07-26 10:14:25 -0700

    add test for dynamo + traceable collectives (#7745)

--- test/tpu/run_tests.sh
+++ test/tpu/run_tests.sh
@@ -18,6 +18,7 @@ python3 test/test_autocast.py
 python3 test/test_grad_checkpoint.py
 python3 test/dynamo/test_dynamo.py
 python3 test/dynamo/test_dynamo_dynamic_shape.py
+python3 test/dynamo/test_traceable_collectives.py
 python3 test/spmd/test_spmd_debugging.py
 XLA_PARAMETER_WRAPPING_THREADSHOLD=1 python test/spmd/test_spmd_parameter_wrapping.py
 python3 test/pjrt/test_dtypes.py
