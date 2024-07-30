5da90a0133470bd43327ca22692850a48b54261d
Will Cromar <wcromar@google.com>
2024-07-30 18:06:26 +0000

    no explicit collectives, only sharding

--- experimental/torch_xla2/examples/ddp.py
+++ experimental/torch_xla2/examples/ddp.py
@@ -11,10 +11,8 @@ import torch.optim as optim
 import torch_xla2
 from jax.sharding import NamedSharding
 
-
 from jax.sharding import Mesh, PartitionSpec as P
 from jax.experimental import mesh_utils
-from jax.experimental.shard_map import shard_map
 
 # Initialize distributed communication
 dist.init_process_group(backend='jax', init_method='jax://')
@@ -25,15 +23,14 @@ class DistributedDataParallel(torch.nn.Module):
     if kwargs:
       logging.warning(f'Unsupported kwargs {kwargs}')
 
-    # Does this make sense? https://pytorch.org/docs/stable/generated/torch.nn.Module.html#torch.nn.Module.register_full_backward_pre_hook
     # TODO broadcast and/or check params
-    for p in module.parameters():
-      if p.requires_grad:
-        p.register_hook(lambda grad: print('backward hook') or torch.distributed._functional_collectives.all_reduce(grad, 'sum', ''))
+    # for p in module.parameters():
+    #   if p.requires_grad:
+    #     p.register_hook(lambda grad: print('backward hook') or breakpoint() or torch.distributed._functional_collectives.all_reduce(grad, 'sum', ''))
 
     super().__init__()
     self._module = module
-    self._mesh = Mesh(mesh_utils.create_device_mesh((4,)), axis_names=('torch_dist',))
+    self._mesh = Mesh(mesh_utils.create_device_mesh((4,)), axis_names=('batch',))
 
   def forward(self, *args):
     return self._module(*args)
@@ -69,27 +66,17 @@ loss_fn = nn.MSELoss()
 optimizer = optim.SGD(model.parameters(), lr=1)
 
 
-@functools.partial(
-      shard_map,
-      mesh=model._mesh,
-      in_specs=P('torch_dist'),
-      out_specs=P())
-def step_fn(data, target):
-    data, target = env.j2t_iso((data, target))
-    optimizer.zero_grad()
-    output = model(data)
-    loss = loss_fn(output, target)
-    loss.backward()
-    optimizer.step()
-
-    return jax.lax.psum(loss._elem, 'torch_dist')
-
 # Training loop
 for epoch in range(3):
     print(epoch)
     for data, target in dataloader:
-        data, target = jax.device_put([data.numpy(), target.numpy()], NamedSharding(model._mesh, P('torch_dist')))
-        jax_loss = step_fn(data, target)
-        print(jax_loss)
+        data, target = env.j2t_iso(jax.device_put([data.numpy(), target.numpy()], NamedSharding(model._mesh, P('batch'))))
+        # print(data._elem.addressable_shards, f(data).addressable_shards)
+        # exit(0)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = loss_fn(output, target)
+        loss.backward()
+        optimizer.step()
         print([p.grad for p in model.parameters()])
-        print([p.mean().numpy() for p in model.parameters()])
+        print([p._elem.addressable_shards for p in model.parameters()])
