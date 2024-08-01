9c22396322222e23cb9b9904d26004c0a3317da2
Siyuan Liu <lsiyuan@google.coim>
2024-08-01 23:02:17 +0000

    fix jax minor version

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 _date = '20240801'
 _libtpu_version = f'0.1.dev{_date}'
 _libtpu_storage_path = f'https://storage.googleapis.com/libtpu-nightly-releases/wheels/libtpu-nightly/libtpu_nightly-{_libtpu_version}+nightly-py3-none-any.whl'
-_jax_version = f'0.4.31.dev{_date}'
+_jax_version = f'0.4.32.dev{_date}'
 
 
 def _get_build_mode():
