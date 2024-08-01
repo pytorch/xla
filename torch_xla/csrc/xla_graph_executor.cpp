9932c63bcf6f5e3f8d44b3d72272de168f138b56
JackCaoG <jackcao@google.com>
2024-08-01 02:31:33 +0000

    make StepMarker's wait block for async device execution

--- torch_xla/csrc/xla_graph_executor.cpp
+++ torch_xla/csrc/xla_graph_executor.cpp
@@ -410,6 +410,15 @@ void XLAGraphExecutor::SyncTensorsGraph(std::vector<XLATensorPtr>* tensors,
       SyncTensorsGraphInternal(tensors, devices, config, warm_up_cache_only);
   if (wait && async != nullptr && !warm_up_cache_only) {
     async->mwait.Wait();
+    // async->mwait.Wait() will block until the async computation thread to
+    // return but the real device execution might not finish.
+    if (UseVirtualDevice()) {
+      // For spmd case we need to wait for all devices.
+      std::vector<std::string> spmd_device = {"SPMD:0"};
+      runtime::GetComputationClient()->WaitDeviceOps(spmd_device);
+    } else {
+      runtime::GetComputationClient()->WaitDeviceOps(devices);
+    }
   }
 }
 
