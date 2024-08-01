031c1f81e25bc5174947655e00945778f9183656
Will Cromar <wcromar@google.com>
2024-08-01 17:33:46 +0000

    docstring

--- torch_xla/experimental/callback.py
+++ torch_xla/experimental/callback.py
@@ -4,6 +4,12 @@ import torch_xla
 
 
 def on_ready_callback(tensor, callback: Callable[[torch.Tensor], None]):
+  """Installs callback on `tensor` to be called when underlying buffer is ready.
+
+  Note: Since `callback` will need to re-acquire the GIL since it is a Python
+  callable. If the main thread is blocking on `callback` and holding the GIL,
+  this will result in a deadlock.
+  """
 
   def _callback_wrapper():
     callback(tensor)
