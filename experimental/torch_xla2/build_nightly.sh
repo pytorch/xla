a3aa0c28cc0206bd18ec826eb8f50024bce51442
Chunnien Chan <121328115+chunnienc@users.noreply.github.com>
2024-08-14 14:39:48 -0700

    Update build_nightly.sh

--- experimental/torch_xla2/build_nightly.sh
+++ experimental/torch_xla2/build_nightly.sh
@@ -2,7 +2,9 @@
 set -ex
 
 NIGHTLY_VERSION=$(date '+%Y%m%d%H%M')
-VERSION_UPDATE_PATTERN="s/^__version__\s*=\s*\"([^\"]+)\"/__version__ = \"\1.dev$NIGHTLY_VERSION\"/g;"
 
+# Update the version to <version>.devYYYYMMDDHHMM in __init__.py
+VERSION_UPDATE_PATTERN="s/^__version__\s*=\s*\"([^\"]+)\"/__version__ = \"\1.dev$NIGHTLY_VERSION\"/g;"
 sed -r "$VERSION_UPDATE_PATTERN" torch_xla2/__init__.py --in-place
+
 hatch build -t wheel
