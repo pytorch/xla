b9b1b2f08354333fffdcd896bba0f2a35d1af3b7
Rémi Doreau <doreau@google.com>
2024-08-17 20:01:56 +0300

    [Op info test] Implemented the following ops:
    - special_i1
    - special_i1e
    - special_laguerre_polynomial_l
    - special_modified_bessel_i0
    - erfc

--- experimental/torch_xla2/torch_xla2/ops/jaten.py
+++ experimental/torch_xla2/torch_xla2/ops/jaten.py
@@ -2377,6 +2377,152 @@ def _aten_i0e(self):
   return jax.scipy.special.i0e(self)
 
 
+@op(torch.ops.aten.special_i1)
+@op_base.promote_int_input
+def _aten_special_i1(self):
+  return jax.scipy.special.i1(self)
+
+
+@op(torch.ops.aten.special_i1e)
+@op_base.promote_int_input
+def _aten_special_i1e(self):
+  return jax.scipy.special.i1e(self)
+
+
+@op(torch.ops.aten.special_laguerre_polynomial_l)
+@op_base.promote_int_input
+def _aten_special_laguerre_polynomial_l(self, n):
+  # Adapted from https://github.com/pytorch/pytorch/blob/f8f41dcb24cb4f4e87a51bb04847942dd835e496/aten/src/ATen/native/Math.h#L3106-L3134
+
+  @jnp.vectorize
+  def vectorized(x, n_i):
+    def negative_n(x):
+      return jnp.zeros_like(x)
+
+    def zero_n(x):
+      return jnp.ones_like(x)
+
+    def one_n(x):
+      return jnp.ones_like(x) - x
+
+    def zero_abs(x):
+      return jnp.ones_like(x)
+
+    def default(x):
+      def f(k, carry):
+        p, q = carry
+        return (q, ((k * 2 + (jnp.ones_like(x) - x)) * q - k * p) / (k + 1))
+
+      _, q = jax.lax.fori_loop(1, n_i, f, init_val=(1.0, jnp.ones_like(x) - x))
+      return q
+
+    return jnp.piecewise(
+        x, [n_i == 1, n_i == 0, jnp.abs(n_i) == jnp.zeros_like(x), n_i < 0], [
+            one_n, zero_n, zero_abs, negative_n, default]
+    )
+
+  return vectorized(self, n.astype(jnp.int64))
+
+
+@op(torch.ops.aten.special_modified_bessel_i0)
+@op_base.promote_int_input
+def _aten_special_modified_bessel_i0(self):
+  # Adapted from https://github.com/pytorch/pytorch/blob/f8f41dcb24cb4f4e87a51bb04847942dd835e496/aten/src/ATen/native/Math.h#L3182-L3268
+
+  def small(x):
+    A = jnp.array(
+        [
+            -4.41534164647933937950e-18,
+            3.33079451882223809783e-17,
+            -2.43127984654795469359e-16,
+            1.71539128555513303061e-15,
+            -1.16853328779934516808e-14,
+            7.67618549860493561688e-14,
+            -4.85644678311192946090e-13,
+            2.95505266312963983461e-12,
+            -1.72682629144155570723e-11,
+            9.67580903537323691224e-11,
+            -5.18979560163526290666e-10,
+            2.65982372468238665035e-09,
+            -1.30002500998624804212e-08,
+            6.04699502254191894932e-08,
+            -2.67079385394061173391e-07,
+            1.11738753912010371815e-06,
+            -4.41673835845875056359e-06,
+            1.64484480707288970893e-05,
+            -5.75419501008210370398e-05,
+            1.88502885095841655729e-04,
+            -5.76375574538582365885e-04,
+            1.63947561694133579842e-03,
+            -4.32430999505057594430e-03,
+            1.05464603945949983183e-02,
+            -2.37374148058994688156e-02,
+            4.93052842396707084878e-02,
+            -9.49010970480476444210e-02,
+            1.71620901522208775349e-01,
+            -3.04682672343198398683e-01,
+            6.76795274409476084995e-01,
+        ],
+        dtype=self.dtype,
+    )
+
+    def f(carry, val):
+      p, q, a = carry
+      p, q = q, a
+      return (p, q, ((x / 2.0) - 2.0) * q - p + val), None
+
+    (p, _, a), _ = jax.lax.scan(
+        f, init=(jnp.zeros_like(x), jnp.zeros_like(x), 0), xs=A)
+
+    return jnp.exp(x) * (0.5 * (a - p))
+
+  def default(x):
+    B = jnp.array(
+        [
+            -7.23318048787475395456e-18,
+            -4.83050448594418207126e-18,
+            4.46562142029675999901e-17,
+            3.46122286769746109310e-17,
+            -2.82762398051658348494e-16,
+            -3.42548561967721913462e-16,
+            1.77256013305652638360e-15,
+            3.81168066935262242075e-15,
+            -9.55484669882830764870e-15,
+            -4.15056934728722208663e-14,
+            1.54008621752140982691e-14,
+            3.85277838274214270114e-13,
+            7.18012445138366623367e-13,
+            -1.79417853150680611778e-12,
+            -1.32158118404477131188e-11,
+            -3.14991652796324136454e-11,
+            1.18891471078464383424e-11,
+            4.94060238822496958910e-10,
+            3.39623202570838634515e-09,
+            2.26666899049817806459e-08,
+            2.04891858946906374183e-07,
+            2.89137052083475648297e-06,
+            6.88975834691682398426e-05,
+            3.36911647825569408990e-03,
+            8.04490411014108831608e-01,
+        ],
+        dtype=self.dtype,
+    )
+
+    def f(carry, val):
+      p, q, b = carry
+      p, q = q, b
+      return (p, q, (32.0 / x - 2.0) * q - p + val), None
+
+    (p, _, b), _ = jax.lax.scan(
+        f, init=(jnp.zeros_like(x), jnp.zeros_like(x), 0), xs=B)
+
+    return jnp.exp(x) * (0.5 * (b - p)) / jnp.sqrt(x)
+
+  self = jnp.abs(self)
+  return jnp.piecewise(
+      self, [self <= 8], [small, default]
+  )
+
 @op(torch.ops.aten.special_bessel_j0)
 @op_base.promote_int_input
 def _aten_special_bessel_j0(self):
@@ -2956,6 +3102,11 @@ def _aten_special_chebyshev_polynomial_u(self, n):
 @op_base.promote_int_input
 def _aten_special_erfcx(x):
   return jnp.exp(x * x) * jax.lax.erfc(x)
+
+@op(torch.ops.aten.erfc)
+@op_base.promote_int_input
+def _aten_erfcx(x):
+  return jax.lax.erfc(x)
 
 
 @op(torch.ops.aten.special_hermite_polynomial_h)
