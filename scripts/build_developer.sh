e0d9a023cd4ccc587537c3a06e1569b8e95b1dd3
Yifei Teng <yifeit@google.com>
2024-08-18 16:12:09 -0700

    [developer] Add a script to automate building torch_xla
    This script replicates our CONTRIBUTING.md instructions.
    It builds PyTorch, TorchVision (optional), and PyTorch/XLA. Then it runs
    a test to verify the installation.

---
+++ scripts/build_developer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e  # Fail on any error.
+set -x  # Display commands being run.
+
+# Change to pytorch directory.
+cd "$(dirname "$(readlink -f "$0")")"
+cd ../../
+
+python3 setup.py bdist_wheel
+python3 setup.py install
+cd ..
+
+# Optionally install torchvision.
+if [ -d "vision" ]; then
+  cd vision
+  python3 setup.py develop
+fi
+
+cd ..
+cd pytorch/xla
+python3 setup.py develop
+
+# libtpu is needed to talk to the TPUs. If TPUs are not present,
+# installing this wouldn't hurt either.
+pip install torch_xla[tpu] -f https://storage.googleapis.com/libtpu-releases/index.html
+
+# Test that the library is installed correctly.
+python3 -c 'import torch_xla as xla; print(xla.device())'
+
