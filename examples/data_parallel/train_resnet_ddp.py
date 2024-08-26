87a50b152acaa16b064a8e8845c2de6d06995856
JackCaoG <jackcao@google.com>
2024-08-26 18:47:03 +0000

    Update comments in data parallel example to use sampler

--- examples/data_parallel/train_resnet_ddp.py
+++ examples/data_parallel/train_resnet_ddp.py
@@ -20,6 +20,28 @@ class TrainResNetDDP(TrainResNetBase):
     self.model = DDP(
         self.model, gradient_as_bucket_view=True, broadcast_buffers=False)
     self.optimizer = optim.SGD(self.model.parameters(), weight_decay=1e-4)
+    # below code is commented out because in this example we used a fake data
+    # loader that does not take sampler. However this logic is needed if you
+    # want each process to handle different parts of the data.
+    '''
+    train_sampler = None
+    if xr.world_size() > 1:
+      train_sampler = torch.utils.data.distributed.DistributedSampler(
+          train_dataset,
+          num_replicas=xr.world_size(),
+          rank=xr.global_ordinal(),
+          shuffle=True)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=FLAGS.batch_size,
+        sampler=train_sampler,
+        drop_last=FLAGS.drop_last,
+        shuffle=False if train_sampler else True,
+        num_workers=FLAGS.num_workers,
+        persistent_workers=FLAGS.persistent_workers,
+        prefetch_factor=FLAGS.prefetch_factor)
+    self.train_device_loader = pl.MpDeviceLoader(train_loader, self.device)
+    '''
 
 
 def _mp_fn(index):
--- examples/data_parallel/train_resnet_xla_ddp.py
+++ examples/data_parallel/train_resnet_xla_ddp.py
@@ -4,12 +4,38 @@ example_folder = os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0])))
 sys.path.append(example_folder)
 from train_resnet_base import TrainResNetBase
 
+import torch
 import torch_xla
 import torch_xla.core.xla_model as xm
 import torch_xla.runtime as xr
 
 
 class TrainResNetXLADDP(TrainResNetBase):
+
+  def __init__(self):
+    super().__init__()
+    # below code is commented out because in this example we used a fake data
+    # loader that does not take sampler. However this logic is needed if you
+    # want each process to handle different parts of the data.
+    '''
+    train_sampler = None
+    if xr.world_size() > 1:
+      train_sampler = torch.utils.data.distributed.DistributedSampler(
+          train_dataset,
+          num_replicas=xr.world_size(),
+          rank=xr.global_ordinal(),
+          shuffle=True)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=FLAGS.batch_size,
+        sampler=train_sampler,
+        drop_last=FLAGS.drop_last,
+        shuffle=False if train_sampler else True,
+        num_workers=FLAGS.num_workers,
+        persistent_workers=FLAGS.persistent_workers,
+        prefetch_factor=FLAGS.prefetch_factor)
+    self.train_device_loader = pl.MpDeviceLoader(train_loader, self.device)
+    '''
 
   def run_optimizer(self):
     # optimizer_step will call `optimizer.step()` and all_reduce the gradident
@@ -24,5 +50,7 @@ def _mp_fn(index):
 
 
 if __name__ == '__main__':
-  print('consider using train_resnet_spmd_data_parallel.py instead to get better performance')
+  print(
+      'consider using train_resnet_spmd_data_parallel.py instead to get better performance'
+  )
   torch_xla.launch(_mp_fn, args=())
