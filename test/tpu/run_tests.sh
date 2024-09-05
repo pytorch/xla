97d275107e6daff9eb919806a2d9168174832bd1
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-09-05 10:49:11 -0700

    Update run_tests.sh

--- test/tpu/run_tests.sh
+++ test/tpu/run_tests.sh
@@ -2,10 +2,10 @@
 set -xue
 
 # TODO: merge with other run_tests
-# python3 test/test_operations.py -v
+python3 test/test_operations.py -v
 python3 test/pjrt/test_runtime_tpu.py
 python3 test/pjrt/test_collective_ops_tpu.py
-# python3 test/spmd/test_xla_sharding.py
+python3 test/spmd/test_xla_sharding.py
 python3 test/spmd/test_xla_virtual_device.py
 python3 test/spmd/test_xla_distributed_checkpoint.py
 python3 test/spmd/test_train_spmd_linear_model.py
@@ -25,8 +25,6 @@ python3 test/pjrt/test_dtypes.py
 python3 test/pjrt/test_dynamic_plugin_tpu.py
 python3 test/test_while_loop.py
 python3 test/test_scan.py
-python3 test/test_pallas.py
-python3 test/test_pallas_spmd.py
 python3 test/test_input_output_aliases.py
 python3 test/test_gmm.py
 python3 test/eager/test_eager_spmd.py
@@ -53,6 +51,8 @@ if [[ -n "$TPU_VERSION" && "$TPU_VERSION" == "4" ]]; then
     python3 examples/eager/train_decoder_only_eager_spmd_data_parallel.py
     python3 examples/eager/train_decoder_only_eager_with_compile.py
     python3 examples/eager/train_decoder_only_eager_multi_process.py
+    python3 test/test_pallas.py
+    python3 test/test_pallas_spmd.py
 fi
 
 # Test `tpu-info` CLI compatibility
