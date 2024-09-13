bf132b30b1c44e545ed817b977079cc411826cdc
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-09-13 15:47:10 -0700

    Update ifrt_computation_client.cc

--- torch_xla/csrc/runtime/ifrt_computation_client.cc
+++ torch_xla/csrc/runtime/ifrt_computation_client.cc
@@ -320,7 +320,7 @@ ComputationClient::DataPtr IfrtComputationClient::TransferShardsToDevice(
     shard_shapes.push_back(ifrt_shard->buffer->shape());
   }
   xla::ifrt::Shape ifrt_shape(shape.dimensions());
-  xla::ifrt::DeviceList devices_list({client_->addressable_devices().begin(),
+  tsl::RCReference<xla::ifrt::DeviceList> devices_list = xla::ifrt::BasicDeviceList::Create({client_->addressable_devices().begin(),
                                       client_->addressable_devices().end()});
   std::unique_ptr<xla::ifrt::Sharding> ifrt_sharding =
       xla::ifrt::ConcreteSharding::Create(devices_list, xla::ifrt::MemoryKind(),
