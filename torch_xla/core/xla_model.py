e3cf356aaf05c02db1cca0ba19594cca8b85bf7f
JackCaoG <59073027+JackCaoG@users.noreply.github.com>
2024-10-07 10:13:23 -0700

    Raise error when minibatch is used in SPMD dataloading and per host b… (#8205)

--- torch_xla/core/xla_model.py
+++ torch_xla/core/xla_model.py
@@ -1299,6 +1299,22 @@ def send_cpu_data_to_device(
     shardings = None
     if input_sharding:
       shardings = [input_sharding.xla_spec(t) for t in tensors]
+    if input_sharding and input_sharding.minibatch:
+      # when minibatch is configured we must make sure batch dimension of
+      # the tensor is divisible by the local runtime device count.
+      for tensor, sharding in zip(tensors, shardings):
+        # assume batch dimension is 0
+        local_runtime_device_count = torch_xla.runtime.addressable_runtime_device_count(
+        )
+        if sharding and tensor.dim() > 0 and (tensor.size()[0] %
+                                              local_runtime_device_count) != 0:
+          raise RuntimeError(
+              "When minibatch is configured, batch dimension of the tensor " +
+              "must be divisible by local runtime device count.input data shape "
+              +
+              f"={tensor.size()}, local_runtime_device_count = {local_runtime_device_count}"
+          )
+
     xtensors = torch_xla._XLAC._xla_tensors_from_aten(tensors, devices,
                                                       shardings)
     return xtensors
