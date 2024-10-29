52c0ab00455d735f73810364ca87cef916f35a9a
Xiongfei Wei <isaacwxf23@gmail.com>
2024-10-29 03:15:17 +0000

    fix unknown flag -v

--- test/tpu/run_tests.sh
+++ test/tpu/run_tests.sh
@@ -27,7 +27,7 @@ python3 test/test_while_loop.py
 python3 test/test_scan.py
 python3 test/test_pallas.py -v
 python3 test/test_pallas_spmd.py
-python3 test/test_tpu_paged_attention_kernel.py -v
+python3 test/test_tpu_paged_attention_kernel.py
 python3 test/test_input_output_aliases.py
 python3 test/test_gmm.py
 python3 test/eager/test_eager_spmd.py
