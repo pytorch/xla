5b6917e0db348719e435ce914bfdf182f49e18cd
Chengji Yao <chengji.yao@bytedance.com>
2024-12-12 23:10:20 +0000

    test inplace update together with xm.save

--- test/test_inplace_update.py
+++ test/test_inplace_update.py
@@ -1,10 +1,11 @@
-import os
+import io
 import sys
 import unittest
 
 import torch
 import torch_xla
 import torch_xla.core.xla_model as xm
+from test_utils import temporary_env
 
 
 class InplaceUpdateTest(unittest.TestCase):
@@ -48,6 +49,26 @@ class InplaceUpdateTest(unittest.TestCase):
     expected = torch.tensor([[0, 0], [1, 1]], device=device)
     xm.mark_step()
     self.assertTrue(torch.all(torch.eq(y, expected)))
+
+  def test_xm_save(self):
+    with temporary_env(
+        XLA_DISABLE_FUNCTIONALIZATION="0", XLA_ENABLE_PARAM_ALIASING="0"):
+      xla_device = xm.xla_device()
+      t1 = torch.tensor([1], device=xla_device)
+      t2 = t1.detach()
+      xm.mark_step()
+
+      t2.add_(t2)
+      xm.mark_step()
+
+      # mark_step() causes t1 and t2 to be out of sync on the XLA side.
+
+      fobj = io.BytesIO()
+      xm.save({'t1': t1}, fobj)
+      fobj.seek(0)
+      saved = torch.load(fobj)
+
+      self.assertEqual(t1.item(), saved['t1'].item())
 
 
 if __name__ == '__main__':
