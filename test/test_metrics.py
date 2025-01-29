2c2ee05a392ff903a1d3402fbe354dea9ec1baa4
Chengji Yao <chengjiyao@google.com>
2025-01-29 01:21:48 +0000

    check file

--- test/test_metrics.py
+++ test/test_metrics.py
@@ -14,6 +14,13 @@ def XLAExperimentalContains(feat):
   return feat in experimental
 
 
+def check_metrics_file():
+  metrics_file = os.environ.get("XLA_METRICS_FILE", None)
+  if metrics_file is None:
+    return True
+  return os.path.exists(metrics_file)
+
+
 class MetricsTest(unittest.TestCase):
 
   def test_clear_counters(self):
@@ -30,6 +37,7 @@ class MetricsTest(unittest.TestCase):
     t1 += 2
     self.assertIn("xla::add", met.metrics_report())
     assert (len(met.counter_names()) > 0)
+    assert check_metrics_file()
 
   def test_clear_metrics(self):
     xla_device = xm.xla_device()
