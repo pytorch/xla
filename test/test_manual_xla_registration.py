8fde73e1779c7aa643e77f29b7e2e6b09b8d12ac
Pedro Goncalves Mokarzel <pgmoka@google.com>
2025-03-06 02:38:21 +0000

    Improve on existing tests. fix xla_sharding calls

--- test/test_manual_xla_registration.py
+++ test/test_manual_xla_registration.py
@@ -6,27 +6,32 @@ from torch.library import custom_op
 import torch_xla
 import time
 
-@custom_op("xla::custom_linear_with_einsum", schema="(Tensor input, Tensor weight) -> Tensor", mutates_args=())
-def custom_linear_with_einsum(input: Tensor, weight: Tensor):
-    return torch.einsum('...n,mn->...m', input, weight)
+@custom_op(
+    "xla::custom_einsum",
+    schema="(str function, Tensor input, Tensor weight) -> Tensor",
+    mutates_args=())
+def custom_einsum(function: str, input: Tensor, weight: Tensor):
+    return torch.einsum(function, input, weight)
 
-def test_lowering(func):
-  X = torch.zeros(3, 3, requires_grad=False, device='xla')
-  Y = torch.zeros(3, 3, requires_grad=False, device='xla')
+def is_einsum_lowered(func):
+  X = torch.zeros(3, 5, requires_grad=False, device='xla')
+  Y = torch.zeros(5, 7, requires_grad=False, device='xla')
 
   out = func(X, Y)
-  time.sleep(2)
   ir = torch_xla._XLAC._get_xla_tensors_text([out])
   return 'einsum' in ir
 
 class OperationLowered(unittest.TestCase):
-  def test_einsum_registration(self):
-    self.assertTrue(test_lowering(lambda a, b: torch.einsum('...n,mn->...m', a, b)),
-                    "Operation not lowered as expected")
+  def test_einsum_lowered(self):
+    for f in [torch.einsum, custom_einsum]:
+      self.assertTrue(
+        is_einsum_lowered(lambda a, b: f('...n,mn->...m', a, b)),
+        "Operation not lowered as expected")
 
-  def test_einsum_custom_registration(self):
-    self.assertTrue(test_lowering(lambda a, b: custom_linear_with_einsum(a, b)),
-                    "Operation not lowered as expected")
+  def test_einsum_not_lowered(self):
+    self.assertFalse(
+      is_einsum_lowered(lambda a, b: torch.einsum('ab,bc->ab', a, b)),
+      "Operation lowered as expected")
 
 if __name__ == '__main__':
   test = unittest.main()
