6a7ce475f444a06d4097fc0242e5d9df74b86ace
Yifei Teng <tengyifei@users.noreply.github.com>
2025-03-26 12:02:14 -0700

    Increase CPU node count in tpu-ci (#8885)

--- infra/tpu-pytorch/tpu_ci.tf
+++ infra/tpu-pytorch/tpu_ci.tf
@@ -3,7 +3,7 @@ module "v4_arc_cluster" {
   project_id        = "tpu-pytorch"
   cluster_name      = "tpu-ci"
   cpu_nodepool_name = "cpu-nodepool"
-  cpu_node_count    = 1
+  cpu_node_count    = 32
   tpu_nodepool_name = "tpu-nodepool"
   min_tpu_nodes     = 32
   max_tpu_nodes     = 32
