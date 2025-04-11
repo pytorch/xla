25e81e86e8706c33397df265c0493337f75627b2
Han Qi <hanq@google.com>
2025-04-11 22:51:18 +0000

    comments

--- torchax/test/test_interop.py
+++ torchax/test/test_interop.py
@@ -5,27 +5,6 @@ import torchax
 from torchax import interop
 import torchax
 
-class M1(torch.nn.Module):
-
-    def __init__(self):
-        super().__init__()
-        self.x = torch.ones(10, 10)
-
-class M(torch.nn.Module):
-
-    def __init__(self):
-        super().__init__()
-        self.a = torch.nn.Linear(100, 100)
-        self.b = torch.nn.Parameter(
-            torch.ones(10, 10)
-        )
-        c = torch.ones(10, 10)
-        self.register_buffer('c', c)
-        self.register_buffer('c2', c, persistent=False)
-        self.d = torch.ones(10, 10)
-        self.m1 = M1()
-
-
 
 class InteropTest(unittest.TestCase):
 
@@ -34,7 +13,28 @@ class InteropTest(unittest.TestCase):
 
 
     def test_mod_attr(self):
-        m = M()
+
+        class Child(torch.nn.Module):
+
+            def __init__(self):
+                super().__init__()
+                self.x = torch.ones(10, 10)
+
+        class ModuleWithUnregisteredTensor(torch.nn.Module):
+
+            def __init__(self):
+                super().__init__()
+                self.a = torch.nn.Linear(100, 100)
+                self.b = torch.nn.Parameter(
+                    torch.ones(10, 10)
+                )
+                c = torch.ones(10, 10)
+                self.register_buffer('c', c)
+                self.register_buffer('c2', c, persistent=False)
+                self.d = torch.ones(10, 10)
+                self.m1 = Child()
+
+        m = ModuleWithUnregisteredTensor()
         params, buffers = interop.extract_all_buffers(m)
         self.assertEqual(
             set(params.keys()), {'a.weight', 'a.bias', 'b'}
@@ -83,7 +83,8 @@ class InteropTest(unittest.TestCase):
 
     def test_module_with_shared_weights(self):
 
-        class M2(torch.nn.Module):
+        # arrange
+        class ModuleWithSharedWeights(torch.nn.Module):
 
             def __init__(self):
                 super().__init__()
@@ -93,23 +94,25 @@ class InteropTest(unittest.TestCase):
             def forward(self, x):
                 return self.a(self.b(x))
 
-        m = M2().to('jax')
+        m = ModuleWithSharedWeights().to('jax')
 
         m_jitted = interop.JittableModule(m, dedup_parameters=True)
-
 
         # a's weights and bias and b's weights and bias
         self.assertEqual(len(m.state_dict()), 4)
         
         # b's weights and bias are deduped
         self.assertEqual(len(m_jitted.params), 2)
-
         x = torch.randn(10, 10).to('jax')
-
         expected = m(x)
-        
-        torch.testing.assert_allclose(m_jitted(x), expected)
 
+        # act 
+        actual = m_jitted(x)
+        
+        # assert
+        torch.testing.assert_allclose(actual, expected)
+
+        # arrange
         # make sure buffer donation works
         functional_forward = interop.jax_jit(
             functools.partial(m_jitted.functional_call, 'forward'),
@@ -118,8 +121,10 @@ class InteropTest(unittest.TestCase):
             }
         )
 
-        torch.testing.assert_allclose(
-            functional_forward(m_jitted.params, m_jitted.buffers, x) , expected)
+        # act
+        actual = functional_forward(m_jitted.params, m_jitted.buffers, x)
+        # assert
+        torch.testing.assert_allclose(actual, expected)
 
 
         
