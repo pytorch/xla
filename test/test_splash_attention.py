13155c9c3c6da691d2a7b9f55db0327f8c29a6e6
Pei Zhang <piz@google.com>
2025-04-16 12:28:56 -0700

    Fix splash attention test (#8978)

--- test/test_splash_attention.py
+++ test/test_splash_attention.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import logging
 import sys
 import unittest
@@ -58,30 +59,13 @@ class SplashAttentionTest(unittest.TestCase):
         qkv_partition_spec=self.partition_spec,
         segment_ids_partition_spec=segment_ids_partition_spec,
     )
-    self.q, self.k, self.v, self.q_sa, self.k_sa, self.v_sa = self.ab_comparsion_input_generation(
-    )
-    segment_ids = torch.zeros(self.BATCH_SIZE, self.SEQ_LEN).to("xla")
-    for i in range(self.BATCH_SIZE):
-      segment_ids[i, :] = i
-    self.segment_ids_sa = segment_ids.clone().detach()
-    self.o = flash_attention(
-        self.q,
-        self.k,
-        self.v,
-        True,
-        segment_ids,
-        segment_ids,
-        partition_spec=self.partition_spec,
-        mesh=xs.get_global_mesh(),
-    )
-    torch_xla.sync()
-    loss = torch.sum(self.o)
-    loss.backward()
-    torch_xla.sync()
-    self.q_grad, k_grad, v_grad = self.q.grad, self.k.grad, self.v.grad
-    with torch.no_grad():
-      self.k_grad = self.maybe_reduce_kv_grad(k_grad)
-      self.v_grad = self.maybe_reduce_kv_grad(v_grad)
+
+  def _make_attention_mask_from_segment_ids(self, q_segment_ids,
+                                            kv_segment_ids):
+    return q_segment_ids.view(q_segment_ids.shape[0], 1, q_segment_ids.shape[1],
+                              1) != kv_segment_ids.view(kv_segment_ids.shape[0],
+                                                        1, 1,
+                                                        kv_segment_ids.shape[1])
 
   def maybe_repeat_kv(self, hidden_state):
     if hidden_state.size(1) == self.NUM_Q_HEADS:
@@ -89,61 +73,35 @@ class SplashAttentionTest(unittest.TestCase):
     num_kv_group = self.NUM_Q_HEADS // self.NUM_KV_HEADS
     return hidden_state.repeat_interleave(num_kv_group, dim=1)
 
-  def maybe_reduce_kv_grad(self, hidden_state_grad):
-    # For GQA, the kv grad shape is [BATCH_SIZE, NUM_Q_HEADS, SEQ_LEN,
-    # HEAD_DIM]. We need to convert it back to [BATCH_SIZE, NUM_KV_HEADS,
-    # SEQ_LEN, HEAD_DIM]. The returned grad should be sum over the kv heads over
-    # each group to preserve the magnitude of gradients.
-    if hidden_state_grad.size(1) == self.NUM_KV_HEADS:
-      return hidden_state_grad
-    num_kv_group = self.NUM_Q_HEADS // self.NUM_KV_HEADS
-    return hidden_state_grad.view(
-        self.BATCH_SIZE,
-        self.NUM_KV_HEADS,
-        num_kv_group,
-        self.SEQ_LEN,
-        self.HEAD_DIM,
-    ).sum(dim=2)
-
   def ab_comparsion_input_generation(self):
     q = torch.randn(
         self.BATCH_SIZE,
         self.NUM_Q_HEADS,
         self.SEQ_LEN,
         self.HEAD_DIM,
-        requires_grad=True).to("xla")
+    ).to("xla").requires_grad_()
     k = torch.randn(
         self.BATCH_SIZE,
         self.NUM_KV_HEADS,
         self.SEQ_LEN,
         self.HEAD_DIM,
-        requires_grad=True,
-    ).to("xla")
+    ).to("xla").requires_grad_()
     v = torch.randn(
         self.BATCH_SIZE,
         self.NUM_KV_HEADS,
         self.SEQ_LEN,
         self.HEAD_DIM,
-        requires_grad=True,
-    ).to("xla")
-    q.retain_grad()
-    k.retain_grad()
-    v.retain_grad()
-    q_sa = q.clone().detach().requires_grad_(True)
-    k_sa = k.clone().detach().requires_grad_(True)
-    v_sa = v.clone().detach().requires_grad_(True)
-    # Repeat the kv tensors to match the q tensor heads. This is required for flash
-    k = self.maybe_repeat_kv(k)
-    k.retain_grad()
-    v = self.maybe_repeat_kv(v)
-    v.retain_grad()
+    ).to("xla").requires_grad_()
+    q_sa = q.clone().detach().requires_grad_()
+    k_sa = k.clone().detach().requires_grad_()
+    v_sa = v.clone().detach().requires_grad_()
     torch_xla.sync()
     return q, k, v, q_sa, k_sa, v_sa
 
   def _attention(self, q, k, v, *, attn_mask=None, ab=None):
-    k = self.maybe_repeat_kv(k)
-    v = self.maybe_repeat_kv(v)
-    attn_weight = q @ k.transpose(-2, -1)
+    kk = self.maybe_repeat_kv(k)
+    vv = self.maybe_repeat_kv(v)
+    attn_weight = q @ kk.transpose(-2, -1)
     if attn_mask is not None:
       # Masked out the unrelevant parts.
       attn_weight = attn_weight.masked_fill(attn_mask,
@@ -151,7 +109,7 @@ class SplashAttentionTest(unittest.TestCase):
     if ab is not None:
       attn_weight = attn_weight + ab
     attn_weight = torch.nn.functional.softmax(attn_weight, dim=-1)
-    attn_output = attn_weight @ v
+    attn_output = attn_weight @ vv
     return attn_output
 
   @unittest.skipIf(xr.device_type() != "TPU" or tpu.version() < 3,
@@ -161,24 +119,20 @@ class SplashAttentionTest(unittest.TestCase):
     q, k, v, q_sa, k_sa, v_sa = self.ab_comparsion_input_generation()
     attention_mask = torch.triu(
         torch.ones(self.SEQ_LEN, self.SEQ_LEN), diagonal=1).to("xla")
-
     o = self._attention(q, k, v, attn_mask=attention_mask)
     torch_xla.sync()
     loss = torch.sum(o)
     loss.backward()
-    q_grad, k_grad, v_grad = q.grad, k.grad, v.grad
+    q_grad, k_grad, v_grad = q.grad.detach(), k.grad.detach(), v.grad.detach()
     torch_xla.sync()
 
     o_sa = splash_attention(q_sa, k_sa, v_sa, self.config.to_json())
     torch_xla.sync()
     loss_sa = torch.sum(o_sa)
     loss_sa.backward()
-    q_grad_sa, k_grad_sa, v_grad_sa = q_sa.grad, k_sa.grad, v_sa.grad
+    q_grad_sa, k_grad_sa, v_grad_sa = q_sa.grad.detach(), k_sa.grad.detach(
+    ), v_sa.grad.detach()
     torch_xla.sync()
-
-    with torch.no_grad():
-      k_grad = self.maybe_reduce_kv_grad(k_grad)
-      v_grad = self.maybe_reduce_kv_grad(v_grad)
 
     torch.testing.assert_close(o.cpu(), o_sa.cpu(), rtol=1e-3, atol=1e-5)
 
@@ -195,13 +149,13 @@ class SplashAttentionTest(unittest.TestCase):
     n_devices = xr.global_runtime_device_count()
     q = (
         torch.randn(self.BATCH_SIZE, self.NUM_HEADS, self.SEQ_LEN,
-                    self.HEAD_DIM).requires_grad_(True).to("xla"))
+                    self.HEAD_DIM).requires_grad_().to("xla"))
     k = (
         torch.randn(self.BATCH_SIZE, self.NUM_HEADS, self.SEQ_LEN,
-                    self.HEAD_DIM).requires_grad_(True).to("xla"))
+                    self.HEAD_DIM).requires_grad_().to("xla"))
     v = (
         torch.randn(self.BATCH_SIZE, self.NUM_HEADS, self.SEQ_LEN,
-                    self.HEAD_DIM).requires_grad_(True).to("xla"))
+                    self.HEAD_DIM).requires_grad_().to("xla"))
     o = splash_attention(q, k, v, self.config.to_json())
     torch_xla.sync()
     self.assertEqual(
@@ -213,25 +167,41 @@ class SplashAttentionTest(unittest.TestCase):
                    "This test only works on TPUv3+.")
   @with_jax_high_precision
   def test_splash_attention_segment_id(self):
-    # test the segment id in splash attention against the flash attention kernel
-    q_sa = self.q_sa.clone().detach().requires_grad_(True)
-    k_sa = self.k_sa.clone().detach().requires_grad_(True)
-    v_sa = self.v_sa.clone().detach().requires_grad_(True)
-    for i in [q_sa, k_sa, v_sa]:
-      i.retain_grad()
-    segment_ids_sa = self.segment_ids_sa.clone().detach()
+    q, k, v, q_sa, k_sa, v_sa = self.ab_comparsion_input_generation()
+    zeros = torch.zeros(self.BATCH_SIZE, self.SEQ_LEN // 4).to("xla")
+    segment_ids = torch.cat([zeros, zeros + 1, zeros + 2, zeros + 3], dim=1)
+    segment_ids_sa = segment_ids.clone().detach()
+
+    # For unknow reason, flash_attention will lose the tensor during backward
+    # when test with splash attention together. This is unable to reproduce
+    # locally and only fail in github CI. So I am testing against vanilla
+    # attention.
+    o = self._attention(
+        q,
+        k,
+        v,
+        attn_mask=self._make_attention_mask_from_segment_ids(
+            segment_ids, segment_ids))
+    loss = torch.sum(o)
+    torch_xla.sync()
+    loss.backward()
+    torch_xla.sync()
+    q_grad, k_grad, v_grad = q.grad.detach(), k.grad.detach(), v.grad.detach()
+
     o_sa = splash_attention(
         q_sa,
         k_sa,
         v_sa,
         self.config.to_json(),
-        decoder_segment_ids=segment_ids_sa.to("xla"))
+        decoder_segment_ids=segment_ids_sa,
+        causal=False)
     loss_sa = torch.sum(o_sa)
     loss_sa.backward()
-    q_grad_sa, k_grad_sa, v_grad_sa = q_sa.grad, k_sa.grad, v_sa.grad
+    q_grad_sa, k_grad_sa, v_grad_sa = q_sa.grad.detach(), k_sa.grad.detach(
+    ), v_sa.grad.detach()
     torch_xla.sync()
-    torch.testing.assert_close(self.o.cpu(), o_sa.cpu(), rtol=1e-3, atol=1e-5)
-    for org_grad, sa_grad in zip([self.q_grad, self.k_grad, self.v_grad],
+    torch.testing.assert_close(o.cpu(), o_sa.cpu(), rtol=1e-3, atol=1e-5)
+    for org_grad, sa_grad in zip([q_grad, k_grad, v_grad],
                                  [q_grad_sa, k_grad_sa, v_grad_sa],
                                  strict=False):
       torch.testing.assert_close(
@@ -241,6 +211,11 @@ class SplashAttentionTest(unittest.TestCase):
                    "This test only works on TPUv3+.")
   @with_jax_high_precision
   def test_splash_attention_aot_traceable(self):
+    q, k, v, q_sa, k_sa, v_sa = self.ab_comparsion_input_generation()
+    # Repeat the kv tensors to match the q tensor heads. This is required for
+    # flash attention kernel.
+    kk = self.maybe_repeat_kv(k)
+    vv = self.maybe_repeat_kv(v)
     from functorch.compile import aot_function, make_boxed_func
 
     def compiler(gm, _):
@@ -249,26 +224,24 @@ class SplashAttentionTest(unittest.TestCase):
     compiled_splash_attention = aot_function(
         splash_attention, fw_compiler=compiler)
 
-    q_sa = self.q_sa.clone().detach().requires_grad_(True)
-    k_sa = self.k_sa.clone().detach().requires_grad_(True)
-    v_sa = self.v_sa.clone().detach().requires_grad_(True)
-    for i in [q_sa, k_sa, v_sa]:
-      i.retain_grad()
-    segment_ids_sa = self.segment_ids_sa.clone().detach()
+    attention_mask = torch.triu(
+        torch.ones(self.SEQ_LEN, self.SEQ_LEN), diagonal=1).to("xla")
+    o = self._attention(q, kk, vv, attn_mask=attention_mask)
+    torch_xla.sync()
+    loss = torch.sum(o)
+    loss.backward()
+    q_grad, k_grad, v_grad = q.grad.detach(), k.grad.detach(), v.grad.detach()
+    torch_xla.sync()
     o_sa = compiled_splash_attention(
-        q_sa,
-        k_sa,
-        v_sa,
-        self.config.to_json(),
-        decoder_segment_ids=segment_ids_sa)
+        q_sa, k_sa, v_sa, self.config.to_json(), decoder_segment_ids=None)
     torch_xla.sync()
     loss_sa = torch.sum(o_sa)
     loss_sa.backward()
     torch_xla.sync()
-    q_grad_sa, k_grad_sa, v_grad_sa = q_sa.grad, k_sa.grad, v_sa.grad
-
-    torch.testing.assert_close(self.o.cpu(), o_sa.cpu(), rtol=1e-3, atol=1e-5)
-    for org_grad, sa_grad in zip([self.q_grad, self.k_grad, self.v_grad],
+    q_grad_sa, k_grad_sa, v_grad_sa = q_sa.grad.detach(), k_sa.grad.detach(
+    ), v_sa.grad.detach()
+    torch.testing.assert_close(o.cpu(), o_sa.cpu(), rtol=1e-3, atol=1e-5)
+    for org_grad, sa_grad in zip([q_grad, k_grad, v_grad],
                                  [q_grad_sa, k_grad_sa, v_grad_sa],
                                  strict=False):
       torch.testing.assert_close(
@@ -280,32 +253,41 @@ class SplashAttentionTest(unittest.TestCase):
   def test_splash_attention_cache_hit(self):
     xb._JAX_TO_XLA_COMPUTATION_CACHE.clear()
     starting_cache_misses = xb._jax_to_xla_computation_cache_num_misses()
-    q = self.q_sa.clone().detach().requires_grad_(True)
-    k = self.k_sa.clone().detach().requires_grad_(True)
-    v = self.v_sa.clone().detach().requires_grad_(True)
-    segment_ids = self.segment_ids_sa.clone().detach()
-    o = splash_attention(
-        q,
-        k,
-        v,
-        self.config.to_json(),
-        decoder_segment_ids=segment_ids.to("xla"))
-    loss = torch.sum(o)
+    q = (
+        torch.randn(self.BATCH_SIZE, self.NUM_HEADS, self.SEQ_LEN,
+                    self.HEAD_DIM).requires_grad_().to("xla"))
+    k = (
+        torch.randn(self.BATCH_SIZE, self.NUM_HEADS, self.SEQ_LEN,
+                    self.HEAD_DIM).requires_grad_().to("xla"))
+    v = (
+        torch.randn(self.BATCH_SIZE, self.NUM_HEADS, self.SEQ_LEN,
+                    self.HEAD_DIM).requires_grad_().to("xla"))
+    segment_ids = torch.zeros(self.BATCH_SIZE, self.SEQ_LEN).to("xla")
+    for i in range(self.BATCH_SIZE):
+      segment_ids[i, :] = i
+
+    q1 = q.clone().detach().requires_grad_()
+    k1 = k.clone().detach().requires_grad_()
+    v1 = v.clone().detach().requires_grad_()
+    segment_ids1 = segment_ids.clone().detach()
+    o1 = splash_attention(
+        q1, k1, v1, self.config.to_json(), decoder_segment_ids=segment_ids1)
+    loss = torch.sum(o1)
     loss.backward()
     torch_xla.sync()
 
-    q = self.q_sa.clone().detach().requires_grad_(True)
-    k = self.k_sa.clone().detach().requires_grad_(True)
-    v = self.v_sa.clone().detach().requires_grad_(True)
-    q = q * 2
-    segment_ids = self.segment_ids_sa.clone().detach()
-    o = splash_attention(
-        q,
-        k,
-        v,
+    q2 = q.clone().detach().requires_grad_()
+    k2 = k.clone().detach().requires_grad_()
+    v2 = v.clone().detach().requires_grad_()
+    q2_double = q2 * 2
+    segment_ids2 = segment_ids.clone().detach()
+    o2 = splash_attention(
+        q2_double,
+        k2,
+        v2,
         self.config.to_json(),
-        decoder_segment_ids=segment_ids.to("xla"))
-    loss = torch.sum(o)
+        decoder_segment_ids=segment_ids2)
+    loss = torch.sum(o2)
     loss.backward()
     torch_xla.sync()
     ending_cache_misses = xb._jax_to_xla_computation_cache_num_misses()
