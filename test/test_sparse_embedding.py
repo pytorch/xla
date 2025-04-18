4fce7f10e95d0907ed02ab76c92a3b6229362837
Andrew M. James <andrew.m.james2@gmail.com>
2025-04-18 15:48:06 +0000

    Simple embeding fwd/bwd working with correct dispatch

--- test/test_sparse_embedding.py
+++ test/test_sparse_embedding.py
@@ -1,40 +1,45 @@
 import itertools
 import random
 import unittest
+import sys
 from itertools import product
 
 import torch
 import torch_xla
 import torch.nn as nn
-import torch.nn.functional as FA
+import torch.nn.functional as F
+from torch.testing._internal.common_utils import TestCase
+
 from absl.testing import parameterized
-from torch_xla.experimental import sparse as sparse_xla
+from torch_xla.experimental.sparse import Embedding, EmbeddingBag, embedding as xla_embedding, embedding_bag as xla_embedding_bag, SparseCOOTensor
 
 index_dtypes = {torch.int32, torch.int64}
 float_dtypes = {torch.float32, torch.float16, torch.bfloat16}
 
-device = torch_xla.device()
+xla_device = torch_xla.device()
+
+# def _ref_embedding(input, init_args*, **init_kwargs):
+# upstream_embed = nn.Embedding(*init_args, **init_kwargs)
+# return upstream_embed(input)
 
 
-def functional_embedding(impl):
+class TestEmbeddingNN(TestCase):
 
-  def f(input):
-    impl()
+  def test_embedding_sparse_basic(self):
+    embedding = Embedding(10, 20, sparse=True, device=xla_device)
+    input = torch.tensor([[0, 2, 4, 5], [4, 3, 0, 9]],
+                         dtype=torch.long,
+                         device=xla_device)
+    output = embedding(input)
+    output.max().backward()
+    self.assertTrue(embedding.weight.grad.is_sparse)
+    self.assertEqual(embedding.weight.grad.shape, embedding.weight.shape)
 
-  @parameterized.parameters(*index_dtypes)
-  @parameterized.parameters(*float_dtypes)
-  def test_embedding_sparse_basic(self, index_dtype, float_dtype):
-    torch_embedding = nn.Embedding(10, 20, sparse=True, dtype=float_dtype)
-    xla_embedding = sparse_xla.Embedding.from_pretrained(
-        torch_embedding.weight.clone().detach().to(device), sparse=True)
-    input = torch.tensor([[0, 2, 4, 5], [4, 3, 0, 9]], dtype=index_dtype)
-    input_xla = torch.tensor([[0, 2, 4, 5], [4, 3, 0, 9]],
-                             dtype=index_dtype,
-                             device=device)
-    ref_out = torch_embedding(input)
-    xla_out = xla_embedding(input_xla)
-    self.assertTrue(torch.allclose(ref_out, xla_out))
-    ref_out.sum().backward()
-    xla_out.sum().backward()
-    torch_weight_grad = torch_embedding.weight.grad
-    xla_weight_grad = xla_embedding.weight.grad
+
+# test functional variant
+# test w/ optimizer
+# repeat for Bag
+
+if __name__ == "__main__":
+  test = unittest.main()
+  sys.exit(0 if test.result.wasSuccessful() else 1)
