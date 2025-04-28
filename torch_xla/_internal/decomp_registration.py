74bcf40e4e6eeea3ce4ca468a8a798dc516cabb8
Yukio Siraichi <yukio.siraichi@gmail.com>
2025-04-28 14:58:37 +0000

    Fix CustomCall.

--- torch_xla/_internal/decomp_registration.py
+++ torch_xla/_internal/decomp_registration.py
@@ -1,4 +1,4 @@
 import torch
 
-torch.library.register_kernel("aten::upsample_trilinear3d", "xla",
+torch.library.register_kernel("aten::upsample_trilinear3d", "XLA",
                               torch._decomp.decompositions.upsample_trilinear3d)
