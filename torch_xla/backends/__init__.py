4c0b52ee561b62c5ae0f4c6a755f6d7db5e4fe66
Yaoshiang Ho <yaoshiang@gmail.com>
2025-05-02 19:15:03 +0000

    formatter

--- torch_xla/backends/__init__.py
+++ torch_xla/backends/__init__.py
@@ -20,59 +20,62 @@ _HIGH: Final = 'high'
 _HIGHEST: Final = 'highest'
 
 # Use of variables with Final typehint instead of literals is valid.
-_PrecisionType: TypeAlias = Literal[_DEFAULT, _HIGH, _HIGHEST] # pyright: ignore[reportInvalidTypeForm]
+_PrecisionType: TypeAlias = Literal[
+    _DEFAULT, _HIGH, _HIGHEST]  # pyright: ignore[reportInvalidTypeForm]
+
 
 # Some of this description adapted from Jax documentation.
 def set_mat_mul_precision(precision: _PrecisionType) -> None:
-    r"""Control the default matmul and conv precision for 32bit inputs.
+  """Control the default matmul and conv precision for 32bit inputs.
 
-    Some platforms, like TPU, offer configurable precision levels for
-    matrix multiplication and convolution computations,
-    trading off accuracy for speed.
+  Some platforms, like TPU, offer configurable precision levels for
+  matrix multiplication and convolution computations,
+  trading off accuracy for speed.
 
-    This option can be used to control the default precision level for
-    computations involved in matrix multiplication and convolution on
-    32bit inputs. The levels describe the precision at
-    which scalar products are computed.
+  This option can be used to control the default precision level for
+  computations involved in matrix multiplication and convolution on
+  32bit inputs. The levels describe the precision at
+  which scalar products are computed.
 
-    On a TPU:
-    * `default` is the fastest and least precise, essentially
-    downcasting an FP32 to BF16 before multiplying.
+  On a TPU:
+  * `default` is the fastest and least precise, essentially
+  downcasting an FP32 to BF16 before multiplying.
 
-    * `high` breaks the FP32 inputs into upper and lower bits and
-    computes the product of the pair of upper bits, and the two pairs of
-    upper and lower bits. The pair of lower bits is ignored. Since the BF16
-    format has 9 bits of precision (7 bits of mantissa plus one implicit
-    leading one bit plus one sign bit), this delivers approximately
-    18 bits of precision, more than the TensorFloat32 (10 bits)
-    but less than a full FP32 (23 bits).
-    It requires three passes over the data.
+  * `high` breaks the FP32 inputs into upper and lower bits and
+  computes the product of the pair of upper bits, and the two pairs of
+  upper and lower bits. The pair of lower bits is ignored. Since the BF16
+  format has 9 bits of precision (7 bits of mantissa plus one implicit
+  leading one bit plus one sign bit), this delivers approximately
+  18 bits of precision, more than the TensorFloat32 (10 bits)
+  but less than a full FP32 (23 bits).
+  It requires three passes over the data.
 
-    * `highest` is the most precise, and the slowest.
-    It further breaks up each input into a triple of upper, middle,
-    and lower bits and effectively calculates
-    23 bits of precision. This is the most precise option, but also
-    requires six passes.
+  * `highest` is the most precise, and the slowest.
+  It further breaks up each input into a triple of upper, middle,
+  and lower bits and effectively calculates
+  23 bits of precision. This is the most precise option, but also
+  requires six passes.
 
-    Args:
-        precision (str): The precision to set for matrix multiplication. Must be
-            one of 'default', 'high', or 'highest'.
-    """
-    if precision not in [_DEFAULT, _HIGH, _HIGHEST]:
-        raise ValueError(f"Invalid precision: {precision}. "
-                         "Must be one of 'float32', 'bfloat16', or 'float16'.")
+  Args:
+      precision (str): The precision to set for matrix multiplication. Must be
+          one of 'default', 'high', or 'highest'.
+  """
+  if precision not in [_DEFAULT, _HIGH, _HIGHEST]:
+    raise ValueError(f"Invalid precision: {precision}. "
+                     "Must be one of 'float32', 'bfloat16', or 'float16'.")
 
-    torch_xla._XLAC._xla_set_mat_mul_precision(precision)
+  torch_xla._XLAC._xla_set_mat_mul_precision(precision)
+
 
 def get_mat_mul_precision() -> _PrecisionType:
-    r"""Get the current mat mul precision for 32bit inputs.
+  """Get the current mat mul precision for 32bit inputs.
 
-    Returns:
-        str: The current precision setting for matrix multiplication,
-            one of 'default', 'high', or 'highest'.
-    """
-    precision = torch_xla._XLAC._xla_get_mat_mul_precision()
-    assert precision in [_DEFAULT, _HIGH, _HIGHEST], (
-        f"Invalid precision: {precision}. "
-        "Must be one of 'float32', 'bfloat16', or 'float16'.")
-    return precision
+  Returns:
+      str: The current precision setting for matrix multiplication,
+          one of 'default', 'high', or 'highest'.
+  """
+  precision = torch_xla._XLAC._xla_get_mat_mul_precision()
+  assert precision in [_DEFAULT, _HIGH, _HIGHEST
+                      ], (f"Invalid precision: {precision}. "
+                          "Must be one of 'float32', 'bfloat16', or 'float16'.")
+  return precision
