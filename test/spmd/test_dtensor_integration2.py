02a1ba6f1a4751d23719855ecfe05507bd889791
Pei Zhang <piz@google.com>
2025-05-02 16:13:25 -0700

    update DTensor usage with upstream (#9079)
    Co-authored-by: Pei Zhang <piz@t1v-n-15c505fb-w-0.us-central2-b.c.tpu-pytorch.internal>

--- test/spmd/test_dtensor_integration2.py
+++ test/spmd/test_dtensor_integration2.py
@@ -4,8 +4,8 @@ import sys
 import torch
 from torch import nn
 import torch.optim as optim
-from torch.distributed._tensor import (DeviceMesh, Shard, distribute_tensor,
-                                       distribute_module)
+from torch.distributed.tensor import (DeviceMesh, Shard, distribute_tensor,
+                                      distribute_module)
 import torch_xla
 import torch_xla.debug.metrics as met
 import torch_xla.runtime as xr
