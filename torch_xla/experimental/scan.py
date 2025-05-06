357bba29be695c6c0f5fec0dbbac53f39b7018fb
Haifeng Jin <haifeng-jin@users.noreply.github.com>
2025-05-06 21:19:27 +0000

    only requires grad for floating point

--- torch_xla/experimental/scan.py
+++ torch_xla/experimental/scan.py
@@ -229,7 +229,8 @@ def value_and_grad_partitioned(
     return torch.empty_like(
         v, dtype=v.dtype, device=v.device, requires_grad=requires_grad)
 
-  fake_carry_pytree = tree_map(lambda v: make_fake_tensor(v, requires_grad=v.requires_grad), init)
+  fake_carry_pytree = tree_map(
+      lambda v: make_fake_tensor(v, requires_grad=v.is_floating_point()), init)
   fake_x_pytree = tree_map(
       lambda v: make_fake_tensor(v[0], requires_grad=v.requires_grad), xs)
 
