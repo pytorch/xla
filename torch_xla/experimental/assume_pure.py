9cf44673ca4d80080f828601554e57733c34834a
Bhavya Bahl <bbahl@google.com>
2025-05-09 17:34:10 +0000

    Add initial implementation of assume_pure in pytorch/xla

--- torch_xla/experimental/assume_pure.py
+++ torch_xla/experimental/assume_pure.py
@@ -1,6 +1,11 @@
 from torch_xla._internal.jax_workarounds import requires_jax
 import torch_xla.core.xla_builder as xb
+from functools import wraps
+import torch
+from torch.utils._pytree import tree_map, tree_flatten, tree_unflatten, tree_iter, PyTree
+import torch_xla
 
+_XLA_COMPUTATION_CACHE = {}
 
 @requires_jax
 def assume_pure(fn):
@@ -34,3 +39,61 @@ def j2t_autograd(fn):
   import torchax.interop
   return torchax.interop.j2t_autograd(
       fn, call_jax=lambda fn, *args: xb.call_jax(fn, args))
+
+
+def assume_pure_torch(use_cache=False):
+  """Decorator to mark a function as pure for PyTorch/XLA.
+
+  This decorator builds an XLA computation from the function and caches it.
+  The decorated function must be pure (i.e. no side-effects, behavior
+  only depends on inputs). 
+  Args:
+    fn: The function to be decorated.
+    use_cache: If True, caches the XLA computation for the function with
+      the same name as the function. It is the user's responsibility to ensure
+      that the function is called with the same input shapes and types each time
+      when using this.
+
+  NOTE: This decorator only works for forward pass. Using it with torch autograd
+  will lead to undefined behavior.
+  """
+  def wrapper(fn):
+    @wraps(fn)
+    def inner(*args, **kwargs):
+      global _XLA_COMPUTATION_CACHE
+      # if fn exists in the cache, return the cached result
+
+
+      # else get the hlo from the function, build XLA computation, cache the
+      # computation and return the result of calling the computation
+      def make_fake_tensor(v: torch.Tensor) -> torch.Tensor:
+        t = xb.create_placeholder_tensor(v.shape, v.dtype)
+        return t.requires_grad_(v.requires_grad)
+
+      fake_args = tree_map(make_fake_tensor, args)
+      fake_kwargs = tree_map(make_fake_tensor, kwargs)
+      
+      # TODO: Decide what to include in the cache key.
+      if use_cache and _XLA_COMPUTATION_CACHE.get(fn.__name__, None) is not None:
+        print(f"Using cached computation for {fn.__name__}")
+        fn_computation, output_tree_spec = _XLA_COMPUTATION_CACHE[fn.__name__]
+      else:
+        fake_outputs = fn(*fake_args, **fake_kwargs)
+        fake_outputs, output_tree_spec = tree_flatten(fake_outputs)
+
+        fn_ctx = torch_xla._XLAC.lowering.LoweringContext("FnComputation")
+        fn_ctx.set_name_string("fn_ctx")
+        fn_ctx.build(fake_outputs)
+        fn_hlo = fn_ctx.hlo()
+
+        fn_computation = xb.computation_from_module_proto(f"xla::xb_computation_{fn.__name__}", fn_hlo)
+        if use_cache:
+          _XLA_COMPUTATION_CACHE[fn.__name__] = (fn_computation, output_tree_spec)
+
+      result = torch_xla._XLAC._xla_user_computation(f"xla::xb_computation_{fn.__name__}",
+                                                    args,
+                                                    fn_computation)
+      result_tree = tree_unflatten(result, output_tree_spec)
+      return result_tree
+    return inner
+  return wrapper
