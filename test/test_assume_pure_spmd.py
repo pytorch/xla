40873e6833effe9006ef8abc0b027558c28835de
Yifei Teng <tengyifei@users.noreply.github.com>
2025-05-08 19:10:15 -0700

    [torchax] Support mark_sharding_with_gradients (#9122)

--- test/test_assume_pure_spmd.py
+++ test/test_assume_pure_spmd.py
@@ -7,7 +7,7 @@ import torch
 import torch_xla
 import torch_xla.runtime as xr
 from torch_xla.experimental.assume_pure import assume_pure
-from torch_xla.distributed.spmd import mark_sharding, set_global_mesh, get_1d_mesh, Mesh
+from torch_xla.distributed.spmd import mark_sharding, mark_sharding_with_gradients, set_global_mesh, get_1d_mesh, Mesh
 
 
 class AssumePureSpmdTest(unittest.TestCase):
@@ -36,6 +36,25 @@ class AssumePureSpmdTest(unittest.TestCase):
     N = xr.global_runtime_device_count()
     self.assertIn(f'devices=[{N}',
                   torch_xla._XLAC._get_xla_sharding_spec(result))
+
+  @unittest.skipUnless(xr.global_runtime_device_count() > 1,
+                       "Multiple devices required")
+  @unittest.skipIf(
+      torch.cuda.is_available() or os.environ.get('PJRT_DEVICE') == 'CUDA',
+      "TODO(https://github.com/pytorch/xla/issues/9017): Get these tests working on GPU"
+  )
+  def test_assume_pure_works_with_mark_sharding_with_gradients(self):
+    x = torch.randn((8, 4, 5, 128)).to('xla').requires_grad_(True)
+    result = assume_pure(mark_sharding_with_gradients)(
+        x, self.spmd_mesh, ("model", None, None, None))
+    result.sum().backward()
+    torch_xla.sync(wait=True)
+    N = xr.global_runtime_device_count()
+    self.assertIn(f'devices=[{N}',
+                  torch_xla._XLAC._get_xla_sharding_spec(result))
+    assert x.grad is not None
+    self.assertIn(f'devices=[{N}',
+                  torch_xla._XLAC._get_xla_sharding_spec(x.grad))
 
   @unittest.skipUnless(xr.global_runtime_device_count() > 1,
                        "Multiple devices required")
