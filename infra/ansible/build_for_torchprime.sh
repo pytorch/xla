9be6f3554122e452ec30c0cb449e6fa92d7ee8e7
Yifei Teng <yifeit@google.com>
2025-05-13 10:44:22 -0700

    build docker image in test

---
+++ infra/ansible/build_for_torchprime.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+export IMAGE_NAME="gcr.io/tpu-pytorch/for-torchprime-ci:test"
+export DOCKERFILE_PATH="infra/ansible/build_for_torchprime.Dockerfile"
+export CONTEXT_PATH="/tmp/wheels"
+
+docker build -t "${IMAGE_NAME}" "${CONTEXT_PATH}" -f "${DOCKERFILE_PATH}" --build-arg ansible_vars='{"arch":"amd64","accelerator":"tpu","pytorch_git_rev":"main","xla_git_rev":"foobar","bundle_libtpu":"0","package_version":"2.8","nightly_release":true}'
+docker push "${IMAGE_NAME}"
