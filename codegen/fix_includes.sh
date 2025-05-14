25c3bd93847aee815d7dccdcb4448722d195bd3d
Sunita Nadampalli <nadampal@amazon.com>
2025-05-14 16:30:52 +0000

    add aarch64 platform build support

--- codegen/fix_includes.sh
+++ codegen/fix_includes.sh
@@ -6,7 +6,11 @@ sed -i 's#<torch_xla/csrc/tensor.h>#"torch_xla/csrc/tensor.h"#' $@
 # remove the runfiles-prefix used in codegen for pytorch
 # `torchgen` generates relative includes path and does not support customizing the root,
 # so we have to fix them up.
-sed -i 's#bazel-out/k8-[^/]*/bin/codegen/lazy_tensor_generator.runfiles/torch/##' $@
+if [[ $(uname -m) == "x86_64" ]]; then
+    sed -i 's#bazel-out/k8-[^/]*/bin/codegen/lazy_tensor_generator.runfiles/torch/##' $@
+elif [[ $(uname -m) == "aarch64" ]]; then
+    sed -i 's#bazel-out/aarch64-[^/]*/bin/codegen/lazy_tensor_generator.runfiles/torch/##' $@
+fi
 
 # use the generated files that are in the compilation unit
 sed -i 's#<bazel-out/\(.*\)>#"bazel-out/\1"#' $@
