da5885da2d66c28dfae72ac0f6bc5f38285e0295
Rui Silva <rpsilva@amazon.com>
2025-05-19 21:04:50 +0000

    Part 1: Add annotate_custom_sharding API

--- torch_xla/distributed/spmd/__init__.py
+++ torch_xla/distributed/spmd/__init__.py
@@ -4,7 +4,8 @@ from .xla_sharding import (
     mark_sharding, mark_sharding_with_gradients, clear_sharding, get_1d_mesh,
     wrap_if_sharded, xla_patched_nn_linear_forward, set_global_mesh,
     get_global_mesh, _mark_manual_sharding, enable_manual_sharding,
-    disable_manual_sharding, apply_backward_optimization_barrier, shard_as)
+    disable_manual_sharding, apply_backward_optimization_barrier, shard_as,
+    annotate_custom_sharding)
 from .api import xla_distribute_tensor, xla_distribute_module, auto_policy
 
 __all__ = [
@@ -20,6 +21,7 @@ __all__ = [
     "mark_sharding",
     "mark_sharding_with_gradients",
     "shard_as",
+    "annotate_custom_sharding",
     "clear_sharding",
     "get_1d_mesh",
     "wrap_if_sharded",
