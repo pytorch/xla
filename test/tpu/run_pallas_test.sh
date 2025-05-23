601eeaa77d60973e8ed813959947ac8311494532
Gunhyun Park <gunhyun@google.com>
2025-05-23 07:31:03 +0000

    Address comments

--- test/tpu/run_pallas_test.sh
+++ test/tpu/run_pallas_test.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 set -xue
-CDIR="$(cd "$(dirname "$0")" ; pwd -P)"
-TEST_CDIR="$(dirname "$CDIR")"
+
+# Absolute path to the directory of this script.
+_TPU_DIR="$(
+    cd "$(dirname "$0")"
+    pwd -P
+)"
+
+# Absolute path to the test/ directory.
+_TEST_DIR="$(dirname "$_TPU_DIR")"
 
 # This test takes ~370 seconds to run
-python3 "$TEST_CDIR/test_pallas.py" -v
+python3 "$_TEST_DIR/test_pallas.py" -v
 
 # This test takes ~15 seconds to run
-python3 "$TEST_CDIR/test_pallas_spmd.py"
+python3 "$_TEST_DIR/test_pallas_spmd.py"
 
 # This test takes ~15 seconds to run
-XLA_DISABLE_FUNCTIONALIZATION=1 python3 "$TEST_CDIR/test_pallas_spmd.py"
+XLA_DISABLE_FUNCTIONALIZATION=1 python3 "$_TEST_DIR/test_pallas_spmd.py"
