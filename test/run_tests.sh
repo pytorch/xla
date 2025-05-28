e6d62b0f078df61b258a67beba1c72733803709e
Andrew M. James <andrew.m.james2@gmail.com>
2025-05-28 18:18:56 +0000

    remove missing test file

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -314,7 +314,6 @@ function run_xla_op_tests3 {
   run_test "$_TEST_DIR/../examples/scan/scan_examples.py"
 
   # Experimental sparse
-  run_test "$CDIR/test_sparse_coo.py"
   run_test "$CDIR/test_sparse_embedding.py"
 
   # CUDA tests
