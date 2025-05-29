6b153b4f3e806410809debb78bb1e6243ae8ca00
Yukio Siraichi <yukio.siraichi@gmail.com>
2025-05-29 10:53:38 -0300

    Reverted a unneeded changes.

--- torch_xla/_internal/decomp_registration.py
+++ torch_xla/_internal/decomp_registration.py
@@ -1,4 +1,4 @@
 import torch
 
-torch.library.register_kernel("aten::upsample_trilinear3d", "XLA",
+torch.library.register_kernel("aten::upsample_trilinear3d", "xla",
                               torch._decomp.decompositions.upsample_trilinear3d)
