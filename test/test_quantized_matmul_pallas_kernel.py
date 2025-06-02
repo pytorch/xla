ad184f42778fccc8a08f5e191eb517b7570602a7
Xiongfei Wei <isaacwxf23@gmail.com>
2025-06-02 19:08:26 +0000

    fix linter

--- test/test_quantized_matmul_pallas_kernel.py
+++ test/test_quantized_matmul_pallas_kernel.py
@@ -7,15 +7,16 @@ from absl.testing import parameterized
 import jax
 from jax._src import test_util as jtu
 from torch_xla.experimental.pallas_kernels.quantized_matmul_kernel import (
-  quantized_matmul,
-  quantize_array,
-  get_tuned_block_sizes,
-  TUNED_BLOCK_SIZES,
+    quantized_matmul,
+    quantize_array,
+    get_tuned_block_sizes,
+    TUNED_BLOCK_SIZES,
 )
 import jax.numpy as jnp
 import numpy as np
 
 jax.config.parse_flags_with_absl()
+
 
 @jtu.with_config(jax_numpy_dtype_promotion="standard")
 class QuantizedMatmulKernelTest(jtu.JaxTestCase):
@@ -23,22 +24,42 @@ class QuantizedMatmulKernelTest(jtu.JaxTestCase):
   def setUp(self):
     super().setUp()
     if not jtu.is_device_tpu_at_least(5):
-      self.skipTest('This kernel requires a Mosaic feature not available for TPU v4 or earlier.')
+      self.skipTest(
+          'This kernel requires a Mosaic feature not available for TPU v4 or earlier.'
+      )
 
-  def _test_quantized_matmul(self, dtype, bs, n_input_features, n_output_features, quantize_activation, batch_block_size=None, out_block_size=None, in_block_size=None, atol=1.5):
-    
+  def _test_quantized_matmul(self,
+                             dtype,
+                             bs,
+                             n_input_features,
+                             n_output_features,
+                             quantize_activation,
+                             batch_block_size=None,
+                             out_block_size=None,
+                             in_block_size=None,
+                             atol=1.5):
+
     prng_key = jax.random.key(1234)
     k0, k1 = jax.random.split(prng_key, 2)
     x = jax.random.normal(k0, (bs, n_input_features), dtype=dtype)
-    w = jax.random.normal(k1, (n_output_features, n_input_features), dtype=dtype)
+    w = jax.random.normal(
+        k1, (n_output_features, n_input_features), dtype=dtype)
     x_copy = x.copy()
     w_copy = w.copy()
     q_w, scalar_w = quantize_array(w)
     scalar_w = jnp.squeeze(scalar_w)
     assert scalar_w.shape == (n_output_features,)
 
-    output = quantized_matmul(x, q_w, scalar_w, quantize_activation=quantize_activation, batch_block_size=batch_block_size, out_block_size=out_block_size, in_block_size=in_block_size).block_until_ready()
-    expected = jax.lax.dot_general(x_copy, w_copy, dimension_numbers=(((1,), (1,)),((), ())))
+    output = quantized_matmul(
+        x,
+        q_w,
+        scalar_w,
+        quantize_activation=quantize_activation,
+        batch_block_size=batch_block_size,
+        out_block_size=out_block_size,
+        in_block_size=in_block_size).block_until_ready()
+    expected = jax.lax.dot_general(
+        x_copy, w_copy, dimension_numbers=(((1,), (1,)), ((), ())))
     # print(
     #     f'Output max diff: {jnp.max(jnp.abs(expected - output))}')
     # print(
@@ -56,9 +77,19 @@ class QuantizedMatmulKernelTest(jtu.JaxTestCase):
       n_output_features=[128, 256, 512],
       quantize_activation=[True],
   )
-  def test_quantized_matmul_various_input_shapes(self, dtype, bs, n_input_features, n_output_features, quantize_activation):
+  def test_quantized_matmul_various_input_shapes(self, dtype, bs,
+                                                 n_input_features,
+                                                 n_output_features,
+                                                 quantize_activation):
     self._test_quantized_matmul(
-        dtype, bs, n_input_features, n_output_features, quantize_activation=quantize_activation, batch_block_size=128, out_block_size=128, in_block_size=128)
+        dtype,
+        bs,
+        n_input_features,
+        n_output_features,
+        quantize_activation=quantize_activation,
+        batch_block_size=128,
+        out_block_size=128,
+        in_block_size=128)
 
   @parameterized.product(
       dtype=[jnp.bfloat16, jnp.float32],
@@ -67,11 +98,23 @@ class QuantizedMatmulKernelTest(jtu.JaxTestCase):
       n_output_features=[64, 192],
       quantize_activation=[True],
   )
-  def test_quantized_matmul_unaligned_input_shapes(self, dtype, bs, n_input_features, n_output_features, quantize_activation):
+  def test_quantized_matmul_unaligned_input_shapes(self, dtype, bs,
+                                                   n_input_features,
+                                                   n_output_features,
+                                                   quantize_activation):
     self._test_quantized_matmul(
-        dtype, bs, n_input_features, n_output_features, quantize_activation=quantize_activation, batch_block_size=128, out_block_size=128, in_block_size=128)
+        dtype,
+        bs,
+        n_input_features,
+        n_output_features,
+        quantize_activation=quantize_activation,
+        batch_block_size=128,
+        out_block_size=128,
+        in_block_size=128)
 
-  @patch('torch_xla.experimental.pallas_kernels.quantized_matmul_kernel.get_tpu_version')
+  @patch(
+      'torch_xla.experimental.pallas_kernels.quantized_matmul_kernel.get_tpu_version'
+  )
   def test_quantized_matmul_retrieve_block_sizes(self, get_tpu_version):
     tpu_version_to_use = 6
     get_tpu_version.return_value = tpu_version_to_use
@@ -82,7 +125,10 @@ class QuantizedMatmulKernelTest(jtu.JaxTestCase):
         break
     expected_block_sizes = TUNED_BLOCK_SIZES[key0]
     _, bs, n_output_features, n_input_features, activation_dtype, quantize_activation = key0
-    actual_block_sizes = get_tuned_block_sizes(bs, n_output_features, n_input_features, activation_dtype, quantize_activation)
+    actual_block_sizes = get_tuned_block_sizes(bs, n_output_features,
+                                               n_input_features,
+                                               activation_dtype,
+                                               quantize_activation)
     assert actual_block_sizes == expected_block_sizes, f"Expected block sizes {expected_block_sizes}, but got {actual_block_sizes} for key {key0}"
 
   @parameterized.product(
@@ -92,9 +138,16 @@ class QuantizedMatmulKernelTest(jtu.JaxTestCase):
       n_output_features=[128, 256],
       quantize_activation=[True],
   )
-  def test_quantized_matmul_use_tuned_block_sizes(self, dtype, bs, n_input_features, n_output_features, quantize_activation):
+  def test_quantized_matmul_use_tuned_block_sizes(self, dtype, bs,
+                                                  n_input_features,
+                                                  n_output_features,
+                                                  quantize_activation):
     self._test_quantized_matmul(
-        dtype, bs, n_input_features, n_output_features, quantize_activation=quantize_activation)
+        dtype,
+        bs,
+        n_input_features,
+        n_output_features,
+        quantize_activation=quantize_activation)
 
 
 if __name__ == "__main__":
--- torch_xla/experimental/pallas_kernels/quantized_matmul_kernel.py
+++ torch_xla/experimental/pallas_kernels/quantized_matmul_kernel.py
@@ -6,6 +6,7 @@ from jax.experimental import pallas as pl
 from jax.experimental.pallas import tpu as pltpu
 import jax.numpy as jnp
 
+
 def _quantize_array(
     x: jax.Array,  # [bs_block_size, in_block_size]
     x_max_val: jax.Array,  # [1, bs_block_size]
@@ -17,8 +18,11 @@ def _quantize_array(
   # Need to explicitly cast to f32 because Mosaic can't directly jnp.round a
   # bf16 array.
   # It seems x/0 in Pallas generates inf/-inf instead of an exception.
-  x_int = jnp.clip(jnp.round((x / scale).astype(jnp.float32)), int_min, int_max).astype(jnp.int8)
+  x_int = jnp.clip(
+      jnp.round((x / scale).astype(jnp.float32)), int_min,
+      int_max).astype(jnp.int8)
   return x_int, scale.astype(x.dtype)
+
 
 def matmul_kernel(
     x_ref,  # (batch_block_size, in_block_size)
@@ -32,16 +36,22 @@ def matmul_kernel(
     batch_block_size,
     out_block_size,
     in_block_size,
-  ):
-  bs_idx, out_idx, in_idx = pl.program_id(0), pl.program_id(1) , pl.program_id(2)
+):
+  bs_idx, out_idx, in_idx = pl.program_id(0), pl.program_id(1), pl.program_id(2)
   nsteps = pl.num_programs(2)
   x_ref_dtype = x_ref.dtype
-  assert x_ref.shape == (batch_block_size, in_block_size), "x_ref shape is not correct"
-  assert w_ref.shape == (out_block_size, in_block_size), "w_ref shape is not correct"
-  assert scalar_ref.shape == (1, out_block_size), "scalar_ref shape is not correct"
-  assert x_max_val.shape == (1, batch_block_size), "x_max_val shape is not correct"
-  assert out_ref.shape == (batch_block_size, out_block_size), "out_ref shape is not correct"
-  assert acc_ref.shape == (batch_block_size, out_block_size), "acc_ref shape is not correct"
+  assert x_ref.shape == (batch_block_size,
+                         in_block_size), "x_ref shape is not correct"
+  assert w_ref.shape == (out_block_size,
+                         in_block_size), "w_ref shape is not correct"
+  assert scalar_ref.shape == (1,
+                              out_block_size), "scalar_ref shape is not correct"
+  assert x_max_val.shape == (1,
+                             batch_block_size), "x_max_val shape is not correct"
+  assert out_ref.shape == (batch_block_size,
+                           out_block_size), "out_ref shape is not correct"
+  assert acc_ref.shape == (batch_block_size,
+                           out_block_size), "acc_ref shape is not correct"
 
   @pl.when(in_idx == 0)
   def _():
@@ -52,14 +62,14 @@ def matmul_kernel(
     acc_ref[...] += jax.lax.dot_general(
         x,
         w_ref[...],
-        (((1,), (1,)),((), ())),
+        (((1,), (1,)), ((), ())),
         preferred_element_type=jnp.int32,
     )
   else:
     acc_ref[...] += jax.lax.dot_general(
         x_ref[...],
         w_ref[...],
-        (((1,), (1,)),((), ())),
+        (((1,), (1,)), ((), ())),
     )
 
   @pl.when(in_idx == nsteps - 1)
@@ -71,8 +81,10 @@ def matmul_kernel(
       acc *= x_scale
     out_ref[...] = acc.astype(x_ref_dtype)
 
+
 def _next_multiple(x, multiple):
   return ((x + multiple - 1) // multiple) * multiple
+
 
 @functools.partial(
     jax.jit,
@@ -82,8 +94,7 @@ def _next_multiple(x, multiple):
         'batch_block_size',
         'out_block_size',
         'in_block_size',
-    ]
-)
+    ])
 def quantized_matmul(
     x: jax.Array,  # [bs, n_input_features]
     w: jax.Array,  # [n_output_features, n_input_features]
@@ -113,46 +124,69 @@ def quantized_matmul(
   orig_bs, orig_in_features = x.shape
   orig_out_features, _ = w.shape
   if batch_block_size is None or out_block_size is None or in_block_size is None:
-    batch_block_size, out_block_size, in_block_size = get_tuned_block_sizes(orig_bs, orig_out_features, orig_in_features, jnp.dtype(x.dtype).name, quantize_activation)
+    batch_block_size, out_block_size, in_block_size = get_tuned_block_sizes(
+        orig_bs, orig_out_features, orig_in_features,
+        jnp.dtype(x.dtype).name, quantize_activation)
 
   padded_bs = _next_multiple(orig_bs, batch_block_size)
   if orig_bs < padded_bs:
-    x = jnp.pad(x, ((0, padded_bs-orig_bs), (0, 0)))
-    x_max_val = jnp.pad(x_max_val, ((0, 0), (0, padded_bs-orig_bs)))
+    x = jnp.pad(x, ((0, padded_bs - orig_bs), (0, 0)))
+    x_max_val = jnp.pad(x_max_val, ((0, 0), (0, padded_bs - orig_bs)))
   padded_out_features = _next_multiple(orig_out_features, out_block_size)
-  if orig_out_features <  padded_out_features:
-    w = jnp.pad(w, ((0, padded_out_features-orig_out_features), (0, 0)))
-    scalar = jnp.pad(scalar, (0, padded_out_features-orig_out_features))
+  if orig_out_features < padded_out_features:
+    w = jnp.pad(w, ((0, padded_out_features - orig_out_features), (0, 0)))
+    scalar = jnp.pad(scalar, (0, padded_out_features - orig_out_features))
   padded_in_features = _next_multiple(orig_in_features, in_block_size)
   if orig_in_features < padded_in_features:
-    x = jnp.pad(x, ((0, 0), (0, padded_in_features-orig_in_features)))
-    w = jnp.pad(w, ((0, 0), (0, padded_in_features-orig_in_features)))
+    x = jnp.pad(x, ((0, 0), (0, padded_in_features - orig_in_features)))
+    w = jnp.pad(w, ((0, 0), (0, padded_in_features - orig_in_features)))
 
   if scalar.dtype != jnp.float32:
     scalar = scalar.astype(jnp.float32)
   scalar = jnp.expand_dims(scalar, axis=0)  # [1, n_output_features]
 
-  assert x.shape[1] == w.shape[1], f"x.shape[1] ({x.shape[1]}) must be equal to w.shape[1] ({w.shape[1]})"
-  assert w.shape[0] == scalar.shape[1], f"w.shape[0] ({w.shape[0]}) must be equal to scalar.shape[1] ({scalar.shape[1]})"
-  assert x_max_val.shape == (1, x.shape[0]), f"x_max_val.shape ({x_max_val.shape}) must be equal to (1, x.shape[0]) ({1, {x.shape[0]}})"
-  assert x.shape[0] % batch_block_size == 0, f"x.shape[0] ({x.shape[0]}) must be a multiple of block size ({batch_block_size})"
-  assert w.shape[0] % out_block_size == 0, f"w.shape[0] ({w.shape[0]}) must be a multiple of block size ({out_block_size})"
-  assert x.shape[1] % in_block_size == 0, f"x.shape[1] ({x.shape[1]}) must be a multiple of block size ({in_block_size})"
+  assert x.shape[1] == w.shape[
+      1], f"x.shape[1] ({x.shape[1]}) must be equal to w.shape[1] ({w.shape[1]})"
+  assert w.shape[0] == scalar.shape[
+      1], f"w.shape[0] ({w.shape[0]}) must be equal to scalar.shape[1] ({scalar.shape[1]})"
+  assert x_max_val.shape == (
+      1, x.shape[0]
+  ), f"x_max_val.shape ({x_max_val.shape}) must be equal to (1, x.shape[0]) ({1, {x.shape[0]}})"
+  assert x.shape[
+      0] % batch_block_size == 0, f"x.shape[0] ({x.shape[0]}) must be a multiple of block size ({batch_block_size})"
+  assert w.shape[
+      0] % out_block_size == 0, f"w.shape[0] ({w.shape[0]}) must be a multiple of block size ({out_block_size})"
+  assert x.shape[
+      1] % in_block_size == 0, f"x.shape[1] ({x.shape[1]}) must be a multiple of block size ({in_block_size})"
 
   acc_dtype = jnp.int32 if quantize_activation else x.dtype
   kernel = pl.pallas_call(
-      functools.partial(matmul_kernel, quantize_activation=quantize_activation, batch_block_size=batch_block_size, out_block_size=out_block_size, in_block_size=in_block_size),
+      functools.partial(
+          matmul_kernel,
+          quantize_activation=quantize_activation,
+          batch_block_size=batch_block_size,
+          out_block_size=out_block_size,
+          in_block_size=in_block_size),
       grid_spec=pltpu.PrefetchScalarGridSpec(
           num_scalar_prefetch=0,
           in_specs=[
-              pl.BlockSpec((batch_block_size, in_block_size), lambda b, o, i: (b, i)),
-              pl.BlockSpec((out_block_size, in_block_size), lambda b, o, i: (o, i)),
-              pl.BlockSpec((1, out_block_size), lambda b, o, i: (0, o)),  # scalar
-              pl.BlockSpec((1, batch_block_size), lambda b, o, i: (0, b)),  # x_max_val
+              pl.BlockSpec((batch_block_size, in_block_size), lambda b, o, i:
+                           (b, i)),
+              pl.BlockSpec((out_block_size, in_block_size), lambda b, o, i:
+                           (o, i)),
+              pl.BlockSpec((1, out_block_size), lambda b, o, i:
+                           (0, o)),  # scalar
+              pl.BlockSpec((1, batch_block_size), lambda b, o, i:
+                           (0, b)),  # x_max_val
           ],
-          out_specs=pl.BlockSpec((batch_block_size, out_block_size), lambda b, o, i: (b, o)),
-          scratch_shapes=[pltpu.VMEM((batch_block_size, out_block_size), acc_dtype)],
-          grid=(padded_bs // batch_block_size, padded_out_features // out_block_size, padded_in_features // in_block_size),
+          out_specs=pl.BlockSpec((batch_block_size, out_block_size),
+                                 lambda b, o, i: (b, o)),
+          scratch_shapes=[
+              pltpu.VMEM((batch_block_size, out_block_size), acc_dtype)
+          ],
+          grid=(padded_bs // batch_block_size,
+                padded_out_features // out_block_size,
+                padded_in_features // in_block_size),
       ),
       out_shape=jax.ShapeDtypeStruct((padded_bs, padded_out_features), x.dtype),
       compiler_params=pltpu.TPUCompilerParams(
@@ -163,7 +197,8 @@ def quantized_matmul(
 
   out = kernel(x, w, scalar, x_max_val)
 
-  return out[:orig_bs,:orig_out_features]
+  return out[:orig_bs, :orig_out_features]
+
 
 def quantize_array(x, n_bits: int = 8, dim: int = -1):
   max_val = jnp.max(jnp.abs(x), axis=dim, keepdims=True)
@@ -172,6 +207,7 @@ def quantize_array(x, n_bits: int = 8, dim: int = -1):
   scale = max_val / int_max
   x_int = jnp.clip(jnp.round((x / scale)), int_min, int_max).astype(jnp.int8)
   return x_int, scale.astype(x.dtype)
+
 
 # Below are tuned block sizes.
 
@@ -253,18 +289,21 @@ TUNED_BLOCK_SIZES = {
     (6, 2048, 8192, 3584, 'bfloat16', True): (2048, 2048, 3584),
 }
 
+
 def get_tpu_version() -> int:
   """Returns the numeric version of the TPU, or -1 if not on TPU."""
   kind = jax.devices()[0].device_kind
   if 'TPU' not in kind:
     return -1
   if kind.endswith(' lite'):
-    kind = kind[: -len(' lite')]
+    kind = kind[:-len(' lite')]
   assert kind[:-1] == 'TPU v', kind
   return int(kind[-1])
 
-def get_tuned_block_sizes(batch_size, n_output_features, n_input_features, activation_dtype, quantize_activation):
-    """
+
+def get_tuned_block_sizes(batch_size, n_output_features, n_input_features,
+                          activation_dtype, quantize_activation):
+  """
     Retrieve the tuned block sizes for the given parameters.
     
     Args:
@@ -277,6 +316,6 @@ def get_tuned_block_sizes(batch_size, n_output_features, n_input_features, activ
     Returns:
         tuple: A tuple containing the batch_block_size, out_block_size, and in_block_size.
     """
-    key = (get_tpu_version(), batch_size, n_output_features, n_input_features, activation_dtype, quantize_activation)
-    print(f'{key=}')
-    return TUNED_BLOCK_SIZES.get(key, (128, 128, 128))
+  key = (get_tpu_version(), batch_size, n_output_features, n_input_features,
+         activation_dtype, quantize_activation)
+  return TUNED_BLOCK_SIZES.get(key, (128, 128, 128))
