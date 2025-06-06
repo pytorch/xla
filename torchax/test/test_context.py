d1c8fff617a23fd2bb5e4febc6bf0176f0b35564
Han Qi <hanq@google.com>
2025-06-05 20:53:48 -0700

    Add an option to not use dlpack.
    DLpack introduces Jax arrays with wierd Layout.

--- torchax/test/test_context.py
+++ torchax/test/test_context.py
@@ -47,8 +47,7 @@ class TestContext(unittest.TestCase):
       y = torch.randn((3, 3))
       self.assertIsInstance(y, tensor.Tensor)
 
-    self.assertTrue(
-        torch.equal(torchax.tensor.j2t(x._elem), torchax.tensor.j2t(y._elem)))
+      self.assertTrue(torch.allclose(x, y))
 
   def test_different_manual_seed(self):
     with xla_env:
@@ -60,8 +59,7 @@ class TestContext(unittest.TestCase):
       y = torch.randn((3, 3))
       self.assertIsInstance(y, tensor.Tensor)
 
-    self.assertFalse(
-        torch.equal(torchax.tensor.j2t(x._elem), torchax.tensor.j2t(y._elem)))
+      self.assertFalse(torch.allclose(x, y))
 
   def test_jit_with_rng(self):
 
@@ -76,20 +74,14 @@ class TestContext(unittest.TestCase):
 
     # Result always expected to be the same for a jitted function because seeds
     # are baked in
-    torch.testing.assert_close(
-        torchax.tensor.j2t(random_jit()._elem),
-        torchax.tensor.j2t(random_jit()._elem),
-        atol=0,
-        rtol=0)
+    torch.testing.assert_close(random_jit(), random_jit(), atol=0, rtol=0)
 
   def test_generator_seed(self):
     with xla_env:
       x = torch.randn(2, 3, generator=torch.Generator().manual_seed(0))
       y = torch.randn(2, 3, generator=torch.Generator().manual_seed(0))
 
-    # Values will be different, but still check device, layout, dtype, etc
-    torch.testing.assert_close(
-        torchax.tensor.j2t(x._elem), torchax.tensor.j2t(y._elem))
+      torch.testing.assert_close(x, y)
 
   def test_buffer(self):
 
