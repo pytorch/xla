8d5964c43a44cfffa43a040838a24247ec19cbee
Gunhyun Park <gunhyun@google.com>
2025-06-10 17:43:34 +0000

    Migrate uses of `import torch_xla as xla` to `import torch_xla`

--- test/test_devices.py
+++ test/test_devices.py
@@ -4,7 +4,7 @@ from absl.testing import absltest, parameterized
 import torch
 from torch import nn
 from torch.utils.data import TensorDataset, DataLoader
-import torch_xla as xla
+import torch_xla
 import torch_xla.core.xla_model as xm
 import torch_xla.runtime as xr
 import torch_xla.debug.metrics as met
@@ -24,35 +24,35 @@ class TestDevices(parameterized.TestCase):
                             (0, torch.device('xla:0')),
                             (3, torch.device('xla:3')))
   def test_device(self, index, expected):
-    device = xla.device(index)
+    device = torch_xla.device(index)
     self.assertEqual(device, expected)
 
   def test_devices(self):
-    self.assertEqual(xla.devices(),
+    self.assertEqual(torch_xla.devices(),
                      [torch.device(f'xla:{i}') for i in range(4)])
 
   def test_real_devices(self):
-    self.assertEqual(xla.real_devices(), [f'CPU:{i}' for i in range(4)])
+    self.assertEqual(torch_xla.real_devices(), [f'CPU:{i}' for i in range(4)])
 
   def test_device_count(self):
-    self.assertEqual(xla.device_count(), 4)
+    self.assertEqual(torch_xla.device_count(), 4)
 
   def test_sync(self):
-    torch.ones((3, 3), device=xla.device())
-    xla.sync()
+    torch.ones((3, 3), device=torch_xla.device())
+    torch_xla.sync()
 
     self.assertEqual(met.counter_value('MarkStep'), 1)
 
   def test_step(self):
-    with xla.step():
-      torch.ones((3, 3), device=xla.device())
+    with torch_xla.step():
+      torch.ones((3, 3), device=torch_xla.device())
 
     self.assertEqual(met.counter_value('MarkStep'), 2)
 
   def test_step_exception(self):
     with self.assertRaisesRegex(RuntimeError, 'Expected error'):
-      with xla.step():
-        torch.ones((3, 3), device=xla.device())
+      with torch_xla.step():
+        torch.ones((3, 3), device=torch_xla.device())
         raise RuntimeError('Expected error')
 
     self.assertEqual(met.counter_value('MarkStep'), 2)
@@ -69,7 +69,7 @@ class TestDevices(parameterized.TestCase):
       def forward(self, x):
         return self.linear(x)
 
-    model = TrivialModel().to(xla.device())
+    model = TrivialModel().to(torch_xla.device())
 
     batch_size = 16
     num_samples = 100
@@ -85,8 +85,9 @@ class TestDevices(parameterized.TestCase):
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
     for inputs, labels in loader:
-      with xla.step():
-        inputs, labels = inputs.to(xla.device()), labels.to(xla.device())
+      with torch_xla.step():
+        inputs, labels = inputs.to(torch_xla.device()), labels.to(
+            torch_xla.device())
         optimizer.zero_grad()
         outputs = model(inputs)
         loss = loss_fn(outputs, labels)
