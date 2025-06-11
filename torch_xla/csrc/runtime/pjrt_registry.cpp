adac0df994b7f111e7eefaf08062f20754d65e31
Yukio Siraichi <yukio.siraichi@gmail.com>
2025-06-11 16:05:00 -0300

    Fix lint issues.

--- torch_xla/csrc/runtime/pjrt_registry.cpp
+++ torch_xla/csrc/runtime/pjrt_registry.cpp
@@ -150,11 +150,9 @@ InitializePjRt(const std::string& device_type) {
   } else if (device_type == "TPU_LEGACY") {
     XLA_ERROR() << "TPU_LEGACY client is no longer available.";
   } else if (device_type == "CUDA") {
-    TORCH_WARN(
-        "The XLA:CUDA device is deprecated in release 2.8. ",
-        "Future releases might remove XLA:CUDA support entirely. ",
-        "Use the PyTorch native CUDA backend, instead."
-    )
+    TORCH_WARN("The XLA:CUDA device is deprecated in release 2.8. ",
+               "Future releases might remove XLA:CUDA support entirely. ",
+               "Use the PyTorch native CUDA backend, instead.")
     TF_VLOG(1) << "Initializing PjRt GPU client...";
     bool async = sys_util::GetEnvBool(env::kEnvPjrtAsyncGpuClient, true);
     int local_process_rank = sys_util::GetEnvInt(env::kEnvPjRtLocalRank, 0);
