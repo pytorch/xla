97114ef9c353aa10c807872074dfe43c9870519b
Yaoshiang Ho <yho@google.com>
2025-06-18 19:56:48 +0000

    initial commit

--- setup.py
+++ setup.py
@@ -108,12 +108,12 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 
 USE_NIGHTLY = True  # Whether to use nightly or stable libtpu and JAX.
 
-_libtpu_version = '0.0.17'
-_libtpu_date = '20250611'
+_libtpu_version = '0.0.18'
+_libtpu_date = '20250618'
 
-_jax_version = '0.6.2'
-_jaxlib_version = '0.6.2'
-_jax_date = '20250611'  # Date for jax and jaxlib.
+_jax_version = '0.6.3'
+_jaxlib_version = '0.6.3'
+_jax_date = '20250618'  # Date for jax and jaxlib.
 
 if USE_NIGHTLY:
   _libtpu_version += f".dev{_libtpu_date}"
