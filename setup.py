1963a3384d614606caf47449fdfd45319bbc8607
Yaoshiang Ho <yho@google.com>
2025-06-23 15:49:12 +0000

    Another update to libtpu, jax, and jaxlib pins to see if they are now working.

--- setup.py
+++ setup.py
@@ -109,11 +109,11 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 USE_NIGHTLY = True  # Whether to use nightly or stable libtpu and JAX.
 
 _libtpu_version = '0.0.18'
-_libtpu_date = '20250620'
+_libtpu_date = '20250623'
 
 _jax_version = '0.6.3'
 _jaxlib_version = '0.6.3'
-_jax_date = '20250620'  # Date for jax and jaxlib.
+_jax_date = '20250623'  # Date for jax and jaxlib.
 
 if USE_NIGHTLY:
   _libtpu_version += f".dev{_libtpu_date}"
