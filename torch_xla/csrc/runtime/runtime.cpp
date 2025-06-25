b5b650a52ec0b5e2374b15d58560ffe32abe735d
Zhanyong Wan <wan@google.com>
2025-06-25 23:19:49 +0000

    Style improvements.

--- torch_xla/csrc/runtime/runtime.cpp
+++ torch_xla/csrc/runtime/runtime.cpp
@@ -1,7 +1,8 @@
+#include "torch_xla/csrc/runtime/runtime.h"
+
 #include <torch/csrc/lazy/backend/backend_device.h>
 
 #include "absl/log/absl_check.h"
-#include "torch_xla/csrc/device.h"
 #include "torch_xla/csrc/runtime/computation_client.h"
 #include "torch_xla/csrc/runtime/env_vars.h"
 #include "torch_xla/csrc/runtime/ifrt_computation_client.h"
@@ -10,12 +11,17 @@
 
 namespace torch_xla::runtime {
 
-std::atomic<bool> g_computation_client_initialized(false);
+static std::atomic<bool> g_computation_client_initialized(false);
 
 // Creates a new instance of a `ComputationClient` (e.g.
-// `PjRtComputationClient`), and initializes the computation client
+// `PjRtComputationClient`), and initializes the computation client.
+// Can only be called when g_computation_client_initialized is false.
 static absl::StatusOr<ComputationClient * absl_nonnull>
 InitializeComputationClient() {
+  ABSL_CHECK(!g_computation_client_initialized)
+      << "InitializeComputationClient() can only be called once.";
+  g_computation_client_initialized = true;
+
   if (sys_util::GetEnvBool("XLA_DUMP_FATAL_STACK", false)) {
     tsl::testing::InstallStacktraceHandler();
   }
@@ -25,27 +31,24 @@ InitializeComputationClient() {
   //
   // static bool use_ifrt = sys_util::GetEnvBool("XLA_USE_IFRT", false);
   const bool use_ifrt = false;
-  if (sys_util::GetEnvString(env::kEnvPjRtDevice, "") != "") {
-    auto* client =
-        (use_ifrt)
-            ? static_cast<ComputationClient*>(new IfrtComputationClient())
-            : static_cast<ComputationClient*>(new PjRtComputationClient());
-    g_computation_client_initialized = true;
-    return client;
-  } else {
+  if (sys_util::GetEnvString(env::kEnvPjRtDevice, "") == "") {
     return absl::FailedPreconditionError("$PJRT_DEVICE is not set.");
   }
+
+  if (use_ifrt) {
+    return new IfrtComputationClient();
+  }
+  return new PjRtComputationClient();
 }
 
-absl::StatusOr<ComputationClient * absl_nonnull> GetComputationClient() {
+const absl::StatusOr<ComputationClient * absl_nonnull>& GetComputationClient() {
   // Reference to singleton Status-wrapped ComputationClient instance.
   //
   // Since we only allow a single initialization, as soon as this function is
   // called, we store the initialization result in this trivially destructible
   // reference.
-  static auto& maybe_client =
-      *new absl::StatusOr<ComputationClient * absl_nonnull>(
-          InitializeComputationClient());
+  static const auto& maybe_client =
+      *new absl::StatusOr<ComputationClient*>(InitializeComputationClient());
   return maybe_client;
 }
 
--- torch_xla/csrc/runtime/runtime.h
+++ torch_xla/csrc/runtime/runtime.h
@@ -1,12 +1,14 @@
 #ifndef XLA_CLIENT_RUNTIME_H_
 #define XLA_CLIENT_RUNTIME_H_
 
+#include "absl/base/attributes.h"
+#include "absl/status/statusor.h"
 #include "torch_xla/csrc/runtime/computation_client.h"
 
 namespace torch_xla::runtime {
 
 // Returns the ComputationClient singleton.
-absl::StatusOr<ComputationClient * absl_nonnull> GetComputationClient();
+const absl::StatusOr<ComputationClient * absl_nonnull>& GetComputationClient();
 
 ABSL_DEPRECATED(
     "Use status::GetComputationClient(), instead. "
@@ -15,12 +17,13 @@ ABSL_DEPRECATED(
     "safer.")
 ComputationClient* absl_nonnull GetComputationClientOrDie();
 
-// Returns the ComputationClient singleton, if successfully initialized.
-// Returns a nullptr, if the ComputationClient wasn't initialized yet, or
-// if there was an error on initialization.
+// Returns the ComputationClient singleton if it was successfully initialized.
+// Returns a nullptr if the ComputationClient wasn't initialized yet.
+// Throws an exception if the ComputationClient was initialized but the
+// initialization failed.
 ComputationClient* GetComputationClientIfInitialized();
 
-// Run the XRT local service, this will block the caller unitl the server
+// Runs the XRT local service, this will block the caller unitl the server
 // being stopped.
 void RunLocalService(uint64_t service_port);
 
