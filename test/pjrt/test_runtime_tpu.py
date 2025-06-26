e2452c18231a1fcd7624d80689045638f0703593
Zhanyong Wan <wan@google.com>
2025-06-26 21:13:56 +0000

    Add a test on OOM error

--- test/pjrt/test_runtime_tpu.py
+++ test/pjrt/test_runtime_tpu.py
@@ -86,6 +86,18 @@ class TestExperimentalPjrtTpu(parameterized.TestCase):
     os.environ.pop(xenv.TPU_VISIBLE_CHIPS, None)
     os.environ.pop(xenv.TPU_PROCESS_BOUNDS, None)
 
+  def test_tensor_oom(self):
+    a = torch.randn((1000000000, 1000000000), device="xla")
+    with self.assertRaises(RuntimeError) as cm:
+      # The above tensor doesn't fit in HBM. It should result in a python
+      # exception instead of crashing.
+      a.sum()
+
+    self.assertEqual(
+        str(cm.exception),
+        "",
+    )
+
   def test_xla_devices_multiprocess(self):
     expected = _ordinal_to_device()
 
