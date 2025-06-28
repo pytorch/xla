1da7309009ffa969a043a16af7220dcf484e4cc0
qihqi <hanq@google.com>
2025-06-27 17:20:25 -0700

    Pin update to 20250617. After that there is a pallas regression (#9415)

--- setup.py
+++ setup.py
@@ -108,12 +108,12 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 
 USE_NIGHTLY = True  # Whether to use nightly or stable libtpu and JAX.
 
-_libtpu_version = '0.0.17'
-_libtpu_date = '20250611'
+_libtpu_version = '0.0.18'
+_libtpu_date = '20250617'
 
 _jax_version = '0.6.2'
 _jaxlib_version = '0.6.2'
-_jax_date = '20250611'  # Date for jax and jaxlib.
+_jax_date = '20250617'  # Date for jax and jaxlib.
 
 if USE_NIGHTLY:
   _libtpu_version += f".dev{_libtpu_date}"
@@ -434,7 +434,7 @@ setup(
         "Programming Language :: C++",
         "Programming Language :: Python :: 3",
     ],
-    python_requires=">=3.8.0",
+    python_requires=">=3.10.0",
     packages=packages_to_include,
     package_dir=package_dir_mapping,
     ext_modules=[
