b3a990d64bb704e867475cf2362438ce6393a68f
Yukio Siraichi <yukio.siraichi@gmail.com>
2025-07-08 09:23:46 -0300

    Fix lint issues.

--- test/cpp/test_status_show_cpp_error_context.cpp
+++ test/cpp/test_status_show_cpp_error_context.cpp
@@ -1,10 +1,10 @@
 #include "test/cpp/test_status_common.h"
 
-// This file instantiates the parameterized tests defined in `test_status_common.h`.
-// It specifically configures the test environment by explicitly setting the
-// `XLA_SHOW_CPP_ERROR_CONTEXT` environment variable to 'true' in the test fixture's
-// `SetUp` method.
+// This file instantiates the parameterized tests defined in
+// `test_status_common.h`. It specifically configures the test environment by
+// explicitly setting the `XLA_SHOW_CPP_ERROR_CONTEXT` environment variable to
+// 'true' in the test fixture's `SetUp` method.
 //
-// Any new `TEST_P` test cases added to `test_status_common.h` will automatically
-// be run in this mode (with C++ error context).
+// Any new `TEST_P` test cases added to `test_status_common.h` will
+// automatically be run in this mode (with C++ error context).
 INSTANTIATE_TEST_SUITE_WITH_MODE(SHOW);
