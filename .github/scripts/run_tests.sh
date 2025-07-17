58592c48244a51d0ce40cf063f055218d7058b28
Yukio Siraichi <yukio.siraichi@gmail.com>
2025-07-17 19:19:47 -0300

    Rename `XLA_SHOW_CPP_ERROR_CONTEXT` to `TORCH_SHOW_CPP_STACKTRACES` (#9482)

--- .github/scripts/run_tests.sh
+++ .github/scripts/run_tests.sh
@@ -57,8 +57,8 @@ function run_torch_xla_cpp_tests() {
                #"test_xla_backend_intf"
                "test_xla_sharding"
                "test_runtime"
-               "test_status_dont_show_cpp_error_context"
-               "test_status_show_cpp_error_context"
+               "test_status_dont_show_cpp_stacktraces"
+               "test_status_show_cpp_stacktraces"
                "test_debug_macros")
   for name in "${test_names[@]}"; do
     echo "Running $name cpp test..."
