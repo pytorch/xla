3b99bc8fa4a53e8f392368559034e2c79673090a
Chengji Yao <chengjiyao@google.com>
2025-07-22 19:43:48 +0000

    [Bugfix] fix ragged attention kernel auto-tuning table key

--- torch_xla/experimental/pallas_kernels/ragged_paged_attention_v2.py
+++ torch_xla/experimental/pallas_kernels/ragged_paged_attention_v2.py
@@ -25,6 +25,7 @@ from jax._src import dtypes
 from jax.experimental import pallas as pl
 from jax.experimental.pallas import tpu as pltpu
 import jax.numpy as jnp
+import logging
 
 DEFAULT_MASK_VALUE = -0.7 * float(jnp.finfo(jnp.dtype("float32")).max)
 # The page size is too small. We only have 32 SREGs in TC. If the pages
@@ -1421,12 +1422,12 @@ def simplify_key(key):
   return (
       jnp.dtype(q_dtype).name,
       jnp.dtype(kv_dtype).name,
-      next_power_of_2(num_q_heads_per_blk),
-      next_power_of_2(num_kv_heads_per_blk),
+      num_q_heads_per_blk,
+      num_kv_heads_per_blk,
       (head_dim + 127) // 128 * 128,
       next_power_of_2(page_size),
       next_power_of_2(max_num_batched_tokens),
-      next_power_of_2(page_size * pages_per_seq),
+      page_size * pages_per_seq,
   )
 
 
@@ -1502,6 +1503,8 @@ def get_tuned_block_sizes(
   elif device_name in TUNED_BLOCK_SIZES:
     if key in TUNED_BLOCK_SIZES[device_name]:
       bkv, bq = TUNED_BLOCK_SIZES[device_name][key]
+    else:
+      logging.warning(f"key({key}) is not in ragged attention kernel's tuning table!")
   return (min(pages_per_seq, bkv), min(max_num_batched_tokens, bq))
 
 
