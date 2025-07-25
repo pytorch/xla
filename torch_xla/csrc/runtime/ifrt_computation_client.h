92cd616b2b0139827bb1d078a28af68541e77f1e
Rui Silva <rpsilva@amazon.com>
2025-07-25 22:14:05 +0000

    Introduce PlatformVersion bindings

--- torch_xla/csrc/runtime/ifrt_computation_client.h
+++ torch_xla/csrc/runtime/ifrt_computation_client.h
@@ -118,6 +118,8 @@ class IfrtComputationClient : public ComputationClient {
 
   std::vector<std::string> GetAllDevices() const override;
 
+  std::string GetPlatformVersion() const override;
+
   int GetProcessIndex() const override { return client_->process_index(); };
 
   int GetNumProcesses() const override;
