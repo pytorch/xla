e3bbcfc561e8760f726e872b7dc6db8d7293a8a1
Xiongfei Wei <isaacwxf23@gmail.com>
2025-07-31 05:59:55 +0000

    Support torch.nn.functional.one_hot

--- torchax/torchax/ops/jtorch.py
+++ torchax/torchax/ops/jtorch.py
@@ -179,6 +179,11 @@ def _tpu_flash_attention(query, key, value, env):
   return wrap_flash_attention(query, key, value)
 
 
+@register_function(torch.nn.functional.one_hot)
+def one_hot(tensor, num_classes=-1):
+  return jax.nn.one_hot(tensor, num_classes)
+
+
 @register_function(torch.nn.functional.pad)
 def pad(tensor, pad, mode="constant", value=None):
   # For padding modes that have different names between Torch and NumPy, this
