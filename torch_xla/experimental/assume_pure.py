83d4253ef21aeabf8c67c3640cb664633f219ca5
qihqi <hanq@google.com>
2025-08-01 01:13:22 -0700

    make jax as an optional dependency (#9521)

--- torch_xla/experimental/assume_pure.py
+++ torch_xla/experimental/assume_pure.py
@@ -7,6 +7,7 @@ from torch.utils._pytree import tree_flatten, tree_unflatten
 import torch_xla
 from torch_xla._internal.jax_workarounds import requires_jax
 import torch_xla.core.xla_builder as xb
+from torch_xla._internal.jax_workarounds import maybe_get_jax, maybe_get_torchax
 
 _XLA_COMPUTATION_CACHE = {}
 
@@ -57,6 +58,11 @@ def assume_pure(fn, *, add_rng_seed_argument=False):
   we can call add_randn_p(a, rng_seed=0) to get one result and add_randn_p(a, rng_seed=0)
   to get another result.
   """
+  tx = maybe_get_torchax()
+  jax = maybe_get_jax()
+  if tx is None or jax is None:
+    raise AssertionError('Jax is required for this feature')
+
   from torchax.interop import jax_view
   import torchax
   if add_rng_seed_argument:
