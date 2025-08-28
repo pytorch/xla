5321d6583be764165ce4a93bcaf813711e323b46
Het Shah <hshah@tenstorrent.com>
2025-08-28 18:45:32 +0000

    New implementation (WIP)

--- torch_xla/csrc/xla_sharding_util.cpp
+++ torch_xla/csrc/xla_sharding_util.cpp
@@ -218,22 +218,37 @@ bool ShardingUtil::EqualOpShardings(const xla::OpSharding& a,
   return xla::protobuf_util::HaveSameSerialization(a, b);
 }
 
+// xla::OpSharding ShardingUtil::CreateIotaOpSharding(
+//     const py::list& dims, const py::list& reshape_dims,
+//     const py::list& transpose_perm) {
+//   TORCH_LAZY_COUNTER("CreateIotaOpSharding", 1);
+//   auto dims_vec = dims.cast<std::vector<int64_t>>();
+//   auto reshape_dims_vec = reshape_dims.cast<std::vector<int64_t>>();
+//   auto transpose_perm_vec = transpose_perm.cast<std::vector<int>>();
+//   CHECK_EQ(reshape_dims_vec.size(), transpose_perm_vec.size());
+//   std::vector<xla::OpSharding::Type> subgroup_types;
+//   if (dims_vec.size() > transpose_perm.size()) {
+//     subgroup_types.push_back(xla::OpSharding::REPLICATED);
+//   }
+//   return xla::HloSharding::Subgroup(
+//              xla::TileAssignment(dims_vec, reshape_dims_vec,
+//                                  transpose_perm_vec),
+//              subgroup_types)
+//       .ToProto();
+// }
 xla::OpSharding ShardingUtil::CreateIotaOpSharding(
     const py::list& dims, const py::list& reshape_dims,
-    const py::list& transpose_perm) {
+    const py::list& transpose_perm, const py::list& types) {
   TORCH_LAZY_COUNTER("CreateIotaOpSharding", 1);
   auto dims_vec = dims.cast<std::vector<int64_t>>();
   auto reshape_dims_vec = reshape_dims.cast<std::vector<int64_t>>();
   auto transpose_perm_vec = transpose_perm.cast<std::vector<int>>();
+  auto subgroup_types_vec = types.cast<std::vector<xla::OpSharding::Type>>();
   CHECK_EQ(reshape_dims_vec.size(), transpose_perm_vec.size());
-  std::vector<xla::OpSharding::Type> subgroup_types;
-  if (dims_vec.size() > transpose_perm.size()) {
-    subgroup_types.push_back(xla::OpSharding::REPLICATED);
-  }
   return xla::HloSharding::Subgroup(
              xla::TileAssignment(dims_vec, reshape_dims_vec,
                                  transpose_perm_vec),
-             subgroup_types)
+             subgroup_types_vec)
       .ToProto();
 }
 
